bdc5872330093de7551dd9fdbb9bda91026dec03
Timothy Scully <tim.scully@3drepo.org>
2015-04-16 12:58:25 +0100

    ISSUE #51 - Updated so doesn't stall on running mocha

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var assert = require("assert")
 describe('Dummy', function(){
   describe('Test', function(){
-    it('Should always be true', function(){
+    it('Should always be true', function(done){
       assert.equal(1,1);
+	  done();
     })
   })
 })
