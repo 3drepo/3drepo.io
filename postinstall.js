725384eec1e121ba6d638b18e0cbeda610401e07
carmenfan <wind5320@hotmail.com>
2015-06-03 12:15:28 +0100

    ISSUE #57 - add in symlink operations for QR code JS
    copy over qrcode javascripts from bower_component to public

--- postinstall.js
+++ postinstall.js
@@ -5,6 +5,8 @@ var platform = os.platform();
 var path = require('path');
 var exec = require('child_process').exec;
 
+var fs = require('fs');
+
 
 var public_dir = path.normalize("public/js/external");
 	
@@ -21,29 +23,68 @@ var bower_files = [
 'bower_components/masonry/masonry.js'
 ];
 
+var bower_qr_files = [
+'bower_components/jsqrcode/src/grid.js',
+'bower_components/jsqrcode/src/version.js',
+'bower_components/jsqrcode/src/detector.js',
+'bower_components/jsqrcode/src/formatinf.js',
+'bower_components/jsqrcode/src/errorlevel.js',
+'bower_components/jsqrcode/src/bitmat.js',
+'bower_components/jsqrcode/src/datablock.js',
+'bower_components/jsqrcode/src/bmparser.js',
+'bower_components/jsqrcode/src/datamask.js',
+'bower_components/jsqrcode/src/rsdecoder.js',
+'bower_components/jsqrcode/src/gf256poly.js',
+'bower_components/jsqrcode/src/gf256.js',
+'bower_components/jsqrcode/src/decoder.js',
+'bower_components/jsqrcode/src/qrcode.js',
+'bower_components/jsqrcode/src/findpat.js',
+'bower_components/jsqrcode/src/alignpat.js',
+'bower_components/jsqrcode/src/databr.js'
+];
+
+
 var x3dom_files = [
 'submodules/x3dom/dist/ammo.js',
 'submodules/x3dom/dist/x3dom.debug.js',
 'submodules/x3dom/dist/x3dom-full.debug.js'
 ];
 
- 
 
-exec(path.normalize('node_modules/.bin/bower') + ' install', function (error, stdout, stderr) {
-  console.log(stdout);
+var public_qr_dir = path.join(public_dir, 'qrcode') 
+fs.stat(public_qr_dir, function(err, stat){
+	if(err !== null){
+		//create qr folder in $public_dir if it doesn't exist
+		exec('mkdir ' + public_qr_dir , function (error, stdout, stderr) {
+			if (error !== null) {
+				console.log(error);
+			} 
+		});
+		
+	}
+	
+	exec(path.normalize('node_modules/.bin/bower') + ' install', function (error, stdout, stderr) {
+		//  console.log(stdout);
 
-  if (error !== null) {
-    console.log(error);
-  } else {
-    console.log('Bower install was successful.');
-  }
-	make_symlinks(bower_files);
+		  if (error !== null) {
+		    console.log(error);
+		  } else {
+		    console.log('Bower install was successful.');
+		  }
+
+			//create symlinks on callback to ensure the files exist (or windows will be unhappy)
+			make_symlinks(bower_files, public_dir);
+			//qr files are in a separate list as they need to be put inside $public_dir/qrcode
+			make_symlinks(bower_qr_files, public_qr_dir);
+		});
 });
+
+
 
 
 exec('cd ' + path.normalize('submodules/x3dom') + ' && python manage.py --build && cd '+ path.normalize('../../'), 
 		function (error, stdout, stderr) {
-			console.log(stdout);
+//			console.log(stdout);
 			if (error !== null) {
 				console.log(error);
 			} 
@@ -52,16 +93,18 @@ exec('cd ' + path.normalize('submodules/x3dom') + ' && python manage.py --build 
 			}
 			
 			//create symlinks on callback to ensure the files exist (or windows will be unhappy)
-			make_symlinks(x3dom_files);
+			make_symlinks(x3dom_files, public_dir);
 		});
 
 
-function make_symlinks(flist){
+function make_symlinks(flist, target_dir){
 	if (platform === 'win32') {
 		var index;
 		for (index = 0; index < flist.length; index++){
 			var fname = path.normalize(flist[index]);
-			exec('mklink /H ' + path.join(public_dir, path.basename(fname)) + ' '+ fname , function (error, stdout, stderr) {
+			var target_path = path.join(target_dir, path.basename(fname)) ;
+			
+			exec('mklink /H ' + target_path + ' '+ fname , function (error, stdout, stderr) {
 				if (error !== null) {
 					console.log(error);
 				} 
@@ -73,7 +116,8 @@ function make_symlinks(flist){
 		var index;
 		for (index = 0; index < flist.length; index++){
 			var fname = path.normalize(flist[index]);
-			exec('ln -s ' + fname + ' ' + path.join(public_dir, path.basename(fname)), function (error, stdout, stderr) {
+			var target_path = path.join(target_dir, path.basename(fname)) ;
+			exec('ln -s ' + fname + ' ' + path.join(target_dir, path.basename(fname)), function (error, stdout, stderr) {
 				if (error !== null) {
 					console.log(error);
 				} 
