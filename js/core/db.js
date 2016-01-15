6d43f30c15605cd3486d1e061247129a1527bc9a
Henry Liu <thenrikie@hotmail.com>
2016-01-15 18:14:03 +0000

    func to return db connection

--- js/core/db.js
+++ js/core/db.js
@@ -782,6 +782,39 @@ MongoWrapper.prototype.getUserPrivileges = function (username, database, callbac
 
 };
 
+
+/*******************************************************************************
+ * Return signleton db connection
+ *
+ * @param {string} dbName - Database name
+ * @param {function} callback - get db connection
+ *								pass to callback as parameter
+ * @return {promise} promise with db connection as resolved value 
+ ******************************************************************************/
+MongoWrapper.prototype.getDB = function(dbName, callback) {
+	"use strict";
+    
+    if(this._db) {
+
+    	callback && callback(null, db);
+    	return Promise.resolve(this._db.db(dbName));
+
+    } else {
+
+    	return new Promise((resolve, reject) => {
+			mongo.open(dbName, (err, db) => {
+				callback && callback(err, db);
+				if(err){
+					return reject(err);
+				} else {
+					return resolve(db);
+				}
+			});
+    	});
+
+    }
+};
+
 module.exports = function(logger) {
 	"use strict";
 
