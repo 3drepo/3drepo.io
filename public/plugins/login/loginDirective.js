6df7f57ae0ac4b5e0864cf8a782ad8e1e7668135
Pinakin Desai <pinakin.desai@3drepo.org>
2016-03-02 18:02:24 +0000

    ISSUE #186 - Created a login directive, moved home to Base, removed all bootstrap references, remade login page

---
+++ public/plugins/login/loginDirective.js
@@ -0,0 +1,61 @@
+/**
+ *	Copyright (C) 2015 3D Repo Ltd
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as
+ *	published by the Free Software Foundation, either version 3 of the
+ *	License, or (at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function () {
+	"use strict";
+
+	angular.module("3drepo")
+		.directive("login", login);
+
+	function login() {
+		return {
+			restrict: 'EA',
+			templateUrl: 'login.html',
+			scope: {},
+			controller: LoginCtrl,
+			controllerAs: 'vm',
+			bindToController: true
+		};
+	}
+
+	LoginCtrl.$inject = ["StateManager", "Auth", "serverConfig"];
+
+	function LoginCtrl(StateManager, Auth, serverConfig) {
+		var vm = this;
+
+		vm.user = { username: "", password: ""};
+		vm.version = serverConfig.apiVersion;
+		vm.backgroundImage = serverConfig.backgroundImage;
+
+		vm.login = function() {
+			Auth.login(vm.user.username, vm.user.password).then(
+				function (username) {
+					vm.errorMessage = null;
+					vm.user.username = null;
+					vm.user.password = null;
+					StateManager.setStateVar("account", username);
+					StateManager.updateState();
+				}, function (reason) {
+					vm.errorMessage = reason;
+					vm.user.password = null;
+					StateManager.setStateVar("account", null);
+					StateManager.updateState();
+				}
+			);
+		};
+	}
+}());
