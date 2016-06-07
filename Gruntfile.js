e6b54dddaba5a4720e7d5c17742e2b1f2528c8b6
Henry Liu <thenrikie@hotmail.com>
2016-06-07 16:33:37 +0100

    #222 fix mocha timeout and create database trigger email error

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,6 +99,7 @@ module.exports = function(grunt) {
           integrated: {
             options: {
               reporter: 'spec',
+              timeout: 10000,
               quiet: false, // Optionally suppress output to standard out (defaults to false)
               clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
             },
