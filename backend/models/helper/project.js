f6e4e42661a6c87f201d1ad9d8e6e10f16ba4cc8
Henry Liu <thenrikie@hotmail.com>
2016-07-06 12:21:35 +0100

    #224 import toy project with issues after verification

--- backend/models/helper/project.js
+++ backend/models/helper/project.js
@@ -123,11 +123,11 @@ function createAndAssignRole(project, account, username, desc, type) {
 	});
 }
 
-function importToy2(db, project){
+function importToyJSON(db, project){
 	'use strict';
 
-	let path = './backend/statics/toy';
-	console.log(process.cwd());
+	let path = '../../statics/toy';
+
 	let importCollectionFiles = {};
 
 	importCollectionFiles[`${project}.history.chunks`] = 'history.chunks.json';
@@ -158,7 +158,7 @@ function importToy2(db, project){
 			require('child_process').exec(
 			`mongoimport -j 4 --host ${host} --username ${username} --password ${password} --authenticationDatabase admin --db ${db} --collection ${collection} --file ${path}/${filename}`
 			, { 
-				cwd: process.cwd() 
+				cwd: __dirname
 			}, function (err, stdout) {
 				if(err){
 					console.log(err);
@@ -201,28 +201,7 @@ function importToyProject(username){
 
 	}).then(() => {
 
-		//import to queue in background
-		importQueue.importFile(
-			__dirname + '/../../statics/3dmodels/toy.ifc', 
-			'toy.ifc', 
-			account,
-			project,
-			username,
-			copy
-		).then(corID => Promise.resolve(corID)
-		).catch(errCode => {
-			//catch here to provide custom error message
-			console.log(errCode);
-
-			if(projectSetting){
-				projectSetting.errorReason = convertToErrorCode(errCode);
-				projectSetting.markModified('errorReason');
-			}
-
-			return Promise.reject(convertToErrorCode(errCode));
-
-		}).then(() => {
-
+		importToyJSON(account, project).then(() => {
 			//mark project ready
 
 			projectSetting.status = 'ok';
@@ -230,10 +209,10 @@ function importToyProject(username){
 			projectSetting.markModified('errorReason');
 			
 			return projectSetting.save();
-
+			
 		}).catch(err => {
 			// import failed for some reason(s)...
-			console.log(err.stack);
+			console.log(err);
 			//mark project failed
 			if(projectSetting){
 				projectSetting.status = 'failed';
@@ -242,6 +221,48 @@ function importToyProject(username){
 
 
 		});
+
+		//import to queue in background
+		// importQueue.importFile(
+		// 	__dirname + '/../../statics/3dmodels/toy.ifc', 
+		// 	'toy.ifc', 
+		// 	account,
+		// 	project,
+		// 	username,
+		// 	copy
+		// ).then(corID => Promise.resolve(corID)
+		// ).catch(errCode => {
+		// 	//catch here to provide custom error message
+		// 	console.log(errCode);
+
+		// 	if(projectSetting){
+		// 		projectSetting.errorReason = convertToErrorCode(errCode);
+		// 		projectSetting.markModified('errorReason');
+		// 	}
+
+		// 	return Promise.reject(convertToErrorCode(errCode));
+
+		// }).then(() => {
+
+		// 	//mark project ready
+
+		// 	projectSetting.status = 'ok';
+		// 	projectSetting.errorReason = undefined;
+		// 	projectSetting.markModified('errorReason');
+			
+		// 	return projectSetting.save();
+
+		// }).catch(err => {
+		// 	// import failed for some reason(s)...
+		// 	console.log(err.stack);
+		// 	//mark project failed
+		// 	if(projectSetting){
+		// 		projectSetting.status = 'failed';
+		// 		projectSetting.save();
+		// 	}
+
+
+		// });
 
 		//respond once project setting is created
 		return Promise.resolve();
@@ -403,6 +424,5 @@ module.exports = {
 	importToyProject,
 	convertToErrorCode,
 	addCollaborator,
-	removeCollaborator,
-	importToy2
+	removeCollaborator
 };
