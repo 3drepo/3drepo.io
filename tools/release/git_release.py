c85b15def121f3afaea60fe377dc442d8f8a1693
Timothy Scully <timothyscully@hotmail.com>
2016-07-06 15:40:17 +0100

    ISSUE #210 - Update to release tools

--- tools/release/git_release.py
+++ tools/release/git_release.py
@@ -9,12 +9,21 @@ def fatalError(message):
 
 numArguments = len(sys.argv)
 
-if numArguments < 1:
-    fatalError("Please specify release tag name")
+if numArguments < 2:
+    fatalError("Usage: " + sys.argv[0] + " <prod/dev> <version>")
 
-tagName = sys.argv[1]
+release_type = sys.argv[1]
+version      = sys.argv[2]
+
+production   = (release_type == "prod")
+branch       = "staging" if production else "master"
 
 os.system("cd ../..")
+
+code = os.system("git checkout " + branch)
+
+if code:
+    fatalError("git checkout failed")
 
 code = os.system("grunt webfont")
 
@@ -26,18 +35,14 @@ code = os.system("grunt frontend")
 if code:
     fatalError("Frontend compilation failed")
 
-code = os.system("git checkout master")
-
-if code:
-    fatalError("git checkout failed")
-
 code = 0
 
-code |= os.system("git add -f ./public/css/external/three-d-repo.css")
-code |= os.system("git add -f ./public/css/fonts/three-d-repo.woff")
-code |= os.system("git add -f ./public/css/fonts/three-d-repo.eot")
-code |= os.system("git add -f ./public/css/fonts/three-d-repo.ttf")
-code |= os.system("git add -f ./public/dist")
+if production:
+    code |= os.system("git add -f ./public/css/external/three-d-repo.css")
+    code |= os.system("git add -f ./public/css/fonts/three-d-repo.woff")
+    code |= os.system("git add -f ./public/css/fonts/three-d-repo.eot")
+    code |= os.system("git add -f ./public/css/fonts/three-d-repo.ttf")
+    code |= os.system("git add -f ./public/dist")
 
 if code:
     fatalError("git force add failed")
@@ -52,9 +57,11 @@ os.system("git clean -f -d")
 os.system("git commit -m \"Version string update\"")
 
 os.system("git tag -a " + tagName + " -m \" Version " + tagName + " \"")
-os.system("git push origin :refs/tags/latest")
-os.system("git tag -fa latest -m \"Update latest\"")
-os.system("git push origin --tags")
 
+if production:
+    os.system("git push origin :refs/tags/latest")
+    os.system("git tag -fa latest -m \"Update latest\"")
+
+os.system("git push origin --tags")
 os.system("git push")
 
