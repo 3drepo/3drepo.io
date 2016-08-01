b21c482bbc775a61226fe71a057888da8e14f23e
Pinakin Desai <pinakin.desai@3drepo.org>
2016-08-01 16:40:10 +0100

    ISSUE #232 - Added editing of federations

--- frontend/account/js/accountFederationsDirective.js
+++ frontend/account/js/accountFederationsDirective.js
@@ -45,40 +45,85 @@
 		// Init
 		vm.showInfo = true;
 		vm.federations = [
-			{name: "Cheese"},
-			{name: "Bacon"}
+			{
+				name: "Cheese",
+				projects: []
+			},
+			{
+				name: "Bacon",
+				projects: []
+			}
 		];
+		vm.showInfo = (vm.federations.length === 0);
+		vm.newFederationData = {};
 
+		/*
+		 * Watch accounts input
+		 */
 		$scope.$watch("vm.accounts", function () {
 			if (angular.isDefined(vm.accounts)) {
 				vm.accountsCopy = angular.copy(vm.accounts);
 			}
 		});
 
+		/*
+		 * Watch the new federation name
+		 */
+		/*
 		$scope.$watch("vm.newFederationData.name", function () {
-			if (angular.isDefined(vm.accounts)) {
-				vm.accountsCopy = angular.copy(vm.accounts);
-			}
+			vm.newFederationButtonDisabled = (angular.isUndefined(vm.newFederationData.name)) || (vm.newFederationData.name === "");
 		});
+		*/
 
-		vm.setupFederation = function (event) {
-			vm.newFederationData = 
-			vm.federation = [];
+		/*
+		 * Watch for change in edited federation
+		 */
+		$scope.$watch("vm.newFederationData", function () {
+			if (vm.federationOriginalData === null) {
+				vm.newFederationButtonDisabled = (angular.isUndefined(vm.newFederationData.name)) || (vm.newFederationData.name === "");
+			}
+			else {
+				vm.newFederationButtonDisabled = angular.equals(vm.newFederationData, vm.federationOriginalData);
+			}
+		}, true);
+
+		/**
+		 * Open the federation dialog
+		 *
+		 * @param event
+		 */
+		vm.setupNewFederation = function (event) {
+			vm.federationOriginalData = null;
+			vm.newFederationData = {projects: []};
 			UtilsService.showDialog("federationDialog.html", $scope, event);
 		};
 
+		/**
+		 * Close the federation dialog
+		 *
+		 */
 		vm.closeDialog = function () {
 			UtilsService.closeDialog();
 		};
 
+		/**
+		 * Toggle showing of projects in an account
+		 *
+		 * @param index
+		 */
 		vm.toggleShowProjects = function (index) {
 			vm.accountsCopy[index].showProjects = !vm.accountsCopy[index].showProjects;
 			vm.accountsCopy[index].showProjectsIcon = vm.accountsCopy[index].showProjects ? "folder_open" : "folder";
 		};
 
+		/**
+		 * Add a project to a federation
+		 *
+		 * @param accountIndex
+		 * @param projectIndex
+		 */
 		vm.addToFederation = function (accountIndex, projectIndex) {
-			console.log(accountIndex, projectIndex);
-			vm.federation.push({
+			vm.newFederationData.projects.push({
 				accountIndex: accountIndex,
 				account: vm.accountsCopy[accountIndex],
 				projectIndex: projectIndex,
@@ -88,9 +133,32 @@
 			vm.accountsCopy[accountIndex].projects[projectIndex].federated = true;
 		};
 
+		/**
+		 * Remove a project from a federation
+		 *
+		 * @param index
+		 */
 		vm.removeFromFederation = function (index) {
-			var item = vm.federation.splice(index, 1);
+			var item = vm.newFederationData.projects.splice(index, 1);
 			vm.accountsCopy[item[0].accountIndex].projects[item[0].projectIndex].federated = false;
+		};
+
+		/**
+		 * Save a federation
+		 */
+		vm.saveFederation = function () {
+		};
+
+		/**
+		 * Edit a federation
+		 *
+		 * @param event
+		 * @param index
+		 */
+		vm.editFederation = function (event, index) {
+			vm.federationOriginalData = vm.federations[index];
+			vm.newFederationData = angular.copy(vm.federationOriginalData);
+			UtilsService.showDialog("federationDialog.html", $scope, event);
 		};
 	}
 }());
