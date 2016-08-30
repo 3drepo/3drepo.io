e4ed5409e3be402330333f2cea6780c73d949b52
Henry Liu <thenrikie@hotmail.com>
2016-08-30 12:06:31 +0100

    #233 add api test for download latest

---
+++ backend/test/integrated/download.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/**
+ *  Copyright (C) 2014 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+let request = require('supertest');
+let expect = require('chai').expect;
+let app = require("../../services/api.js").createApp(
+	{ session: require('express-session')({ secret: 'testing'}) }
+);
+let log_iface = require("../../logger.js");
+let systemLogger = log_iface.systemLogger;
+let responseCodes = require("../../response_codes.js");
+
+
+
+
+describe('Download', function () {
+
+	let server;
+	let agent;
+	let username = 'testing';
+	let password = 'testing';
+	let project = 'testproject';
+
+
+	before(function(done){
+		server = app.listen(8080, function () {
+			console.log('API test server is listening on port 8080!');
+
+			agent = request.agent(server);
+			agent.post('/login')
+			.send({ username, password })
+			.expect(200, function(err, res){
+				expect(res.body.username).to.equal(username);
+				done(err);
+			});
+
+		});
+	});
+
+
+	after(function(done){
+		server.close(function(){
+			console.log('API test server is closed');
+			done();
+		});
+	});
+
+	it('should succee', function(done){
+
+		agent.get(`/${username}/${project}/download/latest`)
+		.expect(200, function(err, res){
+			done(err);
+		});
+	
+	});
+});
