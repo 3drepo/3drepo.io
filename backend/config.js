9bc9755dcad9c6894859dcd0495216d5ec1a4375
Timothy Scully <tim.scully@3drepo.org>
2016-11-01 11:10:13 +0000

    Version string update

--- backend/config.js
+++ backend/config.js
@@ -18,7 +18,7 @@
 (function() {
 	"use strict";
 
-	const VERSION="1.6.0";
+	const VERSION="1.6.1";
 
 	var config = require("app-config").config;
 	var frontend_scripts = require("../common_public_files.js");
