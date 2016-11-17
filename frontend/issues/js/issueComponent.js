e18179268551cc294147575baca9e0aa074c8b46
Pinakin Desai <pinakin.desai@3drepo.org>
2016-11-17 13:54:44 +0000

    ISSUE #286 - Changed default height for new issue

--- frontend/issues/js/issueComponent.js
+++ frontend/issues/js/issueComponent.js
@@ -723,7 +723,7 @@
 		 */
 		function setContentHeight() {
 			var i, length,
-				newIssueHeight = 425,
+				newIssueHeight = 285,
 				descriptionTextHeight = 80,
 				commentTextHeight = 80,
 				commentImageHeight = 170,
