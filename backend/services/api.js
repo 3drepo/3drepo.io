d615f4421e53a310de6325ab774054cbfa7bfb3e
Timothy Scully <tim.scully@3drepo.org>
2016-11-24 14:38:19 +0000

    ISSUE #209 - Tidy up of frontend, api and queue

--- backend/services/api.js
+++ backend/services/api.js
@@ -15,113 +15,105 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * Create API Express app
- * 
- * @param {Object} serverConfig - Configuration for server
- * @returns
- */
-module.exports.createApp = function (serverConfig) {
+(() => {
 	"use strict";
 
-	const sharedSession = serverConfig.session;
-	const log_iface = require("../logger.js");
-	const express = require("express");
-	const routes = require("../routes/routes.js")();
-	const compress = require("compression");
-	const responseCodes = require("../response_codes");
-	const C = require("../constants");
-	const cors = require("cors");
-	const bodyParser = require("body-parser");
+	/**
+	 * Create API Express app
+	 * 
+	 * @param {Object} serverConfig - Configuration for server
+	 * @returns
+	 */
+	module.exports.createApp = function (serverConfig) {
 
-	// Express app
-	let app = express();
+		const sharedSession = serverConfig.session;
+		const log_iface = require("../logger.js");
+		const express = require("express");
+		const routes = require("../routes/routes.js")();
+		const compress = require("compression");
+		const responseCodes = require("../response_codes");
+		const C = require("../constants");
+		const cors = require("cors");
+		const bodyParser = require("body-parser");
 
-	// Attach the encoders to the router
-	require("../encoders/x3dom_encoder.js")
-		.route(routes);
-	require("../encoders/json_encoder.js")
-		.route(routes);
+		// Express app
+		let app = express();
 
-	// Configure various middleware
-	app.use(sharedSession);
-	app.use(cors({ origin: true, credentials: true }));
+		// Attach the encoders to the router
+		require("../encoders/x3dom_encoder.js")
+			.route(routes);
+		require("../encoders/json_encoder.js")
+			.route(routes);
 
-	// put logger in req object
-	app.use(log_iface.startRequest);
+		// Configure various middleware
+		app.use(sharedSession);
+		app.use(cors({ origin: true, credentials: true }));
 
-	// init the singleton db connection for modelFactory
-	app.use((req, res, next) => {
-		// init the singleton db connection
-		let DB = require("../db/db")(req[C.REQ_REPO].logger);
-		DB.getDB("admin")
-			.then(db => {
-				// set db to singleton modelFactory class
-				require("../models/factory/modelFactory")
-					.setDB(db);
+		// put logger in req object
+		app.use(log_iface.startRequest);
+
+		// init the singleton db connection for modelFactory
+		app.use((req, res, next) => {
+			// init the singleton db connection
+			let DB = require("../db/db")(req[C.REQ_REPO].logger);
+			DB.getDB("admin")
+				.then(db => {
+					// set db to singleton modelFactory class
+					require("../models/factory/modelFactory")
+						.setDB(db);
+					next();
+				})
+				.catch(err => {
+					responseCodes.respond("Express Middleware", req, res, next, responseCodes.DB_ERROR(err), err);
+				});
+		});
+
+		app.use(bodyParser.urlencoded({
+			extended: true
+		}));
+
+		app.set("views", "./jade");
+		app.set("view_engine", "jade");
+
+		app.use(bodyParser.json({ limit: "2mb" }));
+		app.use(function (req, res, next) {
+			sharedSession(req, res, next);
+		});
+		app.use(compress());
+
+		app.use(function (req, res, next) {
+			// intercept OPTIONS method
+			if ("OPTIONS" === req.method) {
+				res.sendStatus(200);
+			} else {
 				next();
-			})
-			.catch(err => {
-				responseCodes.respond("Express Middleware", req, res, next, responseCodes.DB_ERROR(err), err);
-			});
-	});
+			}
+		});
 
-	app.use(bodyParser.urlencoded({
-		extended: true
-	}));
+		app.use("/", require("../routes/plan"));
+		//auth handler
+		app.use("/", require("../routes/auth"));
+		// os api handler
+		app.use("/os", require("../routes/osBuilding"));
+		// payment api header
+		app.use("/payment", require("../routes/payment"));
 
-	app.set("views", "./jade");
-	app.set("view_engine", "jade");
+		//project handlers
+		app.use("/:account", require("../routes/project"));
+		//groups handler
+		app.use("/:account/:project/groups", require("../routes/group"));
+		//issues handler
+		app.use("/:account/:project", require("../routes/issue"));
+		//mesh handler
+		app.use("/:account/:project", require("../routes/mesh"));
+		//texture handler
+		app.use("/:account/:project", require("../routes/texture"));
 
-	app.use(bodyParser.json({ limit: "2mb" }));
-	app.use(function (req, res, next) {
-		sharedSession(req, res, next);
-	});
-	app.use(compress());
+		//history handler
+		app.use("/:account/:project", require("../routes/history"));
 
-	app.use(function (req, res, next) {
-		// intercept OPTIONS method
-		if ("OPTIONS" === req.method) {
-			res.sendStatus(200);
-		} else {
-			next();
-		}
-	});
+		app.use("/", routes.router);
 
-	//info handler
-	app.get("/info", (req, res) => {
-		require("child_process")
-			.exec("git rev-parse --short HEAD", {
-				cwd: __dirname
-			}, function (err, stdout) {
-				res.status(200)
-					.json({ status: "OK", version: stdout.split("\n")[0] });
-			});
-	});
-
-	app.use("/", require("../routes/plan"));
-	//auth handler
-	app.use("/", require("../routes/auth"));
-	// os api handler
-	app.use("/os", require("../routes/osBuilding"));
-	// payment api header
-	app.use("/payment", require("../routes/payment"));
-
-	//project handlers
-	app.use("/:account", require("../routes/project"));
-	//groups handler
-	app.use("/:account/:project/groups", require("../routes/group"));
-	//issues handler
-	app.use("/:account/:project", require("../routes/issue"));
-	//mesh handler
-	app.use("/:account/:project", require("../routes/mesh"));
-	//texture handler
-	app.use("/:account/:project", require("../routes/texture"));
-
-	//history handler
-	app.use("/:account/:project", require("../routes/history"));
-
-	app.use("/", routes.router);
-
-	return app;
-};
+		return app;
+	};
+})();
