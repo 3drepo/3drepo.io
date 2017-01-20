10d1cfe5d861c8896d970e23f87537fe6bd98d20
Henry Liu <thenrikie@hotmail.com>
2017-01-20 16:20:49 +0000

    ISSUE #311 - fix Chat server: issue list of a fed project is not listening changes for issue from a sub project

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -95,14 +95,23 @@ function updateSettings(req, res, next){
 function _getProject(req){
 	'use strict';
 
-	return ProjectSetting.findById(getDbColOptions(req), req.params.project).then(setting => {
-		if(!setting){
+	let setting;
+	return ProjectSetting.findById(getDbColOptions(req), req.params.project).then(_setting => {
+
+		if(!_setting){
 			return Promise.reject({ resCode: responseCodes.PROJECT_INFO_NOT_FOUND});
 		} else {
+
+			setting = _setting;
+			setting = setting.toObject();
 			//compute permissions by user role
 			return ProjectHelpers.getProjectPermission( req.session.user.username, req.params.account, req.params.project).then(permissions => {
 				setting.permissions = permissions;
-				return Promise.resolve(setting);
+				return ProjectHelpers.listSubProjects(req.params.account, req.params.project, C.MASTER_BRANCH_NAME);
+			}).then(subProjects => {
+				//console.log('subProjects', subProjects)
+				setting.subProjects = subProjects;
+				return setting;
 			});
 		}
 	});
@@ -115,9 +124,9 @@ function getProjectSetting(req, res, next){
 	let place = utils.APIInfo(req);
 	_getProject(req).then(setting => {
 
-		setting = setting.toObject();
+		//setting = setting.toObject();
 		
-		let whitelist = ['owner', 'desc', 'type', 'permissions', 'properties', 'status', 'errorReason', 'federate'];
+		let whitelist = ['owner', 'desc', 'type', 'permissions', 'properties', 'status', 'errorReason', 'federate', 'subProjects'];
 		let resObj = {};
 
 		whitelist.forEach(key => {
