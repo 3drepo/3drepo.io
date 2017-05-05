78202fc75905869491baa03d120f37743316d3d5
Henry Liu <thenrikie@hotmail.com>
2017-05-05 15:56:11 +0100

    ISSUE #345 - fix migration script

--- scripts/db_migrations/v2/permisson.js
+++ scripts/db_migrations/v2/permisson.js
@@ -1,7 +1,6 @@
-use admin
 // add basic licence if dun have one
 print('init: create basic licence for everyone if they do not have one');
-db.system.users.update({ 'customData.billing.subscriptions.plan': {'$ne':'BASIC'}}, { 
+db.getSiblingDB('admin').system.users.update({ 'customData.billing.subscriptions.plan': {'$ne':'BASIC'}}, { 
     '$push': {
         'customData.billing.subscriptions': {
                     "plan" : "BASIC",
@@ -37,7 +36,7 @@ var permissionTemplates = [
 
 print('init: create default permission templates(viwer, commenter, collaborator) for everyone');
 // generate default permission templates 
-db.system.users.update({}, { '$addToSet': {
+db.getSiblingDB('admin').system.users.update({}, { '$addToSet': {
         'customData.permissionTemplates': { '$each': permissionTemplates }
     } 
 }, { multi: true});
@@ -54,7 +53,7 @@ function groupByDB(items){
     return obj;
 }
 
-var users = db.system.users.find();
+var users = db.getSiblingDB('admin').system.users.find();
 while(users.hasNext()){
     
     var user = users.next();
@@ -74,21 +73,18 @@ while(users.hasNext()){
         var findRoleWithAdmin = roles.find(function(role){
             return role.role === 'admin' || role.role === 'readWrite';
         });
-        
+
         if(findRoleWithAdmin){
-
-            print('admin role found in this database therefore ignoring all other permissions (as they are redundant)');
-
-            var count = db.system.users.count({
+            var count = db.getSiblingDB('admin').system.users.count({
                 'user': findRoleWithAdmin.db,
                 'customData.permissions.user': user.user
             });
 
             if(count === 0){
- 
+
                 print('creating teamsapce admin permission');
 
-                db.system.users.update({
+                db.getSiblingDB('admin').system.users.update({
                     'user': findRoleWithAdmin.db,
                 }, { $addToSet: {
                     'customData.permissions': {
@@ -101,107 +97,138 @@ while(users.hasNext()){
 
                 print('adding teamsapce admin permission');
 
-                db.system.users.update({
+                db.getSiblingDB('admin').system.users.update({
                     'user': findRoleWithAdmin.db,
                     'customData.permissions.user': user.user
                 }, { $addToSet: {
                     'customData.permissions.$.permissions': 'teamspace_admin'
                 }});
             }
+        }
 
-        } else {
-        
-            roles.forEach(function(role){
+     
+        roles.forEach(function(role){
 
-                print('processing role ' + role.role + ' on db ' + role.db);
+            if(role.role === 'admin' || role.role === 'readWrite'){
+                return;
+            }
 
-                var roleNameParts = role.role.split('.');
-                var permTemplate = roleNameParts.length > 1 && roleNameParts[roleNameParts.length - 1];
-                var project = roleNameParts[0];
+            print('processing role ' + role.role + ' on db ' + role.db);
 
-                if(permTemplate !== 'collaborator' && permTemplate !== 'viewer' && permTemplate !== 'commenter'){
-                    var roleDetail = db.system.roles.findOne(role);
+            var roleNameParts = role.role.split('.');
+            var permTemplate = roleNameParts.length > 1 && roleNameParts[roleNameParts.length - 1];
+            var project = roleNameParts[0];
 
-                    if(roleDetail){
-                        var actions = roleDetail.privileges.find(function(p){ 
-                            return p.resource.collection.endsWith('.history')
-                        }).actions;
+            if(permTemplate !== 'collaborator' && permTemplate !== 'viewer' && permTemplate !== 'commenter'){
+                var roleDetail = db.getSiblingDB('admin').system.roles.findOne(role);
 
-                        if(actions.indexOf('insert') !== -1){
-                            permTemplate = 'collaborator';
-                        } else {
-                            permTemplate = 'viewer';
-                        }
+                if(roleDetail){
+                    var actions = roleDetail.privileges.find(function(p){ 
+                        return p.resource.collection.endsWith('.history')
+                    }).actions;
 
-                        // convert this custom role to job
-                        print('creating job for this role as it is a custom role');
-                        db.system.users.update({ _id: user._id},{
-                            '$addToSet': {
-                                'customData.jobs':{
-                                    _id: role.role
-                                }
-                            }
-                        });
-                    }
-                }
-
-                if(!permTemplate){
-                    print('WARNING: Cannot find role definition in system.roles for role [' + role.role + ']. Possibly a mongodb role which its permissions cannot be translate into our system\'s context. Skipping all migration procedures for this role.')
-                    return;
-                }
-
-                print('template: ' + permTemplate + ' will be used for this role');
-
-                print('add model to customData.models');
-                db.system.users.update({ _id: user._id }, { '$addToSet': {
-                    'customData.models': {
-                        account: role.db,
-                        model: project
-                    }
-                }});
-
-                var currPerm = db.getSiblingDB(role.db).settings.findOne({ 
-                    _id: project, 
-                    'permissions.user': user.user 
-                }, {
-                    'permissions.$' : 1
-                });
-
-                if(currPerm){
-
-                    var updatePerm = (
-                        (permTemplate === 'commenter' && currPerm.permissions[0].permission === 'viewer') ||
-                        (permTemplate === 'collaborator' && currPerm.permissions[0].permission !== 'collaborator')
-                    );
-
-                    if(updatePerm){
-                        print('updating permission for this user on this role');
-                        db.getSiblingDB(role.db).settings.update({ _id: project, 'permissions.user': user.user }, {
-                            '$set':{
-                                'permissions.$.permission': permTemplate
-                            }
-                        });
+                    if(actions.indexOf('insert') !== -1){
+                        permTemplate = 'collaborator';
                     } else {
-                        print('No permission will be added for this user on this role as there is already a more inclusive or the same template set in the database');
+                        permTemplate = 'viewer';
                     }
 
-                } else {
+                    if(roleDetail.privileges[0] && roleDetail.privileges[0].resource && roleDetail.privileges[0].resource.collection){
+                        project = roleDetail.privileges[0].resource.collection.split('.')[0];
+                    }
+                    // convert this custom role to job
+                    print('creating job for this role as it is a custom role');
+                    
+                    var color = db.getSiblingDB(role.db).settings.roles.findOne({ _id: role.role});
+                    if(color){
+                        color = color.color;
+                    }
 
-                    print('adding permission for this user on this role');
-                    db.getSiblingDB(role.db).settings.update({ _id: project}, {
-                        '$addToSet':{
-                            permissions: {
-                                user: user.user,
-                                permission: permTemplate
+                    db.getSiblingDB('admin').system.users.update({ user: role.db},{
+                        '$addToSet': {
+                            'customData.jobs':{
+                                _id: role.role,
+                                color: color
                             }
                         }
                     });
+
+                    //assign job to this users
+                    print('assign job to user');
+                    var res = db.getSiblingDB('admin').system.users.update({user: role.db, 'customData.billing.subscriptions.assignedUser': user.user},{
+                        '$set': {
+                            'customData.billing.subscriptions.$.job': role.role
+                        }
+                    })
+
+                    if(res.nMatched === 0){
+                        print('WARNING: Cannot assign job ' + role.role + ' to user ' + user.user + ' because there is no subscription assigned to this user.')
+                    }
                 }
+            }
 
+            if(findRoleWithAdmin){
+                print('admin role found in this database therefore ignoring all other permissions (as they are redundant)');
+                return;
+            }
 
+            if(!permTemplate){
+                print('WARNING: Cannot find role definition in system.roles for role [' + role.role + ']. Possibly a mongodb role which its permissions cannot be translate into our system\'s context. Skipping all migration procedures for this role.')
+                return;
+            }
+
+            print('template: ' + permTemplate + ' will be used for this role');
+
+            print('add model to customData.models');
+            db.getSiblingDB('admin').system.users.update({ _id: user._id }, { '$addToSet': {
+                'customData.models': {
+                    account: role.db,
+                    model: project
+                }
+            }});
+
+            var currPerm = db.getSiblingDB(role.db).settings.findOne({ 
+                _id: project, 
+                'permissions.user': user.user 
+            }, {
+                'permissions.$' : 1
             });
 
-        }
+            if(currPerm){
+
+                var updatePerm = (
+                    (permTemplate === 'commenter' && currPerm.permissions[0].permission === 'viewer') ||
+                    (permTemplate === 'collaborator' && currPerm.permissions[0].permission !== 'collaborator')
+                );
+
+                if(updatePerm){
+                    print('updating permission for this user on this role');
+                    db.getSiblingDB(role.db).settings.update({ _id: project, 'permissions.user': user.user }, {
+                        '$set':{
+                            'permissions.$.permission': permTemplate
+                        }
+                    });
+                } else {
+                    print('No permission will be added for this user on this role as there is already a more inclusive or the same template set in the database');
+                }
+
+            } else {
+
+                print('adding permission for this user on this role');
+                db.getSiblingDB(role.db).settings.update({ _id: project}, {
+                    '$addToSet':{
+                        permissions: {
+                            user: user.user,
+                            permission: permTemplate
+                        }
+                    }
+                });
+            }
+
+
+        });
+
+        
     }
 
     print ('Finish processing user: ' + user.user);
