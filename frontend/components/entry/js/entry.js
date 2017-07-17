b246f7a74bc5ea6bdadc7a24985b83924583bb0c
James Milner <jamesmilner22@hotmail.com>
2017-07-17 15:38:09 +0100

    ISSUE #422 - Fix memory /4 and change to SERVER_VARS

--- frontend/components/entry/js/entry.js
+++ frontend/components/entry/js/entry.js
@@ -1,11 +1,11 @@
 // Eventually we won't need this as we will get the config/plugin structure statically rather than dynamically through pug
-if (!_PUG_VARS) {
-    console.error("_PUG_VARS is genrated in the pug/frotend.pug file. It should be defined")
+if (!SERVER_VARS) {
+    console.error("SERVER_VARS is generated in the pug/frotend.pug file. It should be defined")
 }
 
 angular.module('3drepo', ['ui.router', 'ngMaterial', 'ngAnimate', 'ngSanitize', 'vcRecaptcha'])
-        .constant('structure', _PUG_VARS.structure)
-        .constant('parentStates',  _PUG_VARS.parentStateJSON)
-        .constant('uiState',  _PUG_VARS.uiState);
+        .constant('structure', SERVER_VARS.structure)
+        .constant('parentStates',  SERVER_VARS.parentStateJSON)
+        .constant('uiState',  SERVER_VARS.uiState);
 
         
