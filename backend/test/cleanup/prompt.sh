dc069992406f649587067dd7a7fe81384541f4a9
Henry Liu <thenrikie@hotmail.com>
2017-07-28 15:45:21 +0100

    ISSUE #435 - fix prompt script

--- backend/test/cleanup/prompt.sh
+++
@@ -1,9 +0,0 @@
-while true; do
-    echo "This will destroy everything and load a copy of test data in your database and purge everything in your queue."
-    read -p "Are you sure to proceed? [DELETE/n] " yn
-    case $yn in
-        DELETE ) make install; break;;
-        [Nn]* ) exit 1;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
