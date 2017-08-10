7b7b21ded40f381cf3333189d369735e3c3f353c
James Milner <jamesmilner22@hotmail.com>
2017-08-10 17:03:32 +0100

    ISSUE #394 - Fix service worker with static index.html

--- frontend/components/home/js/serviceworker.service.js
+++ frontend/components/home/js/serviceworker.service.js
@@ -33,20 +33,18 @@
 		function registerSW(sw)  {
 		
 			console.log("ServiceWorker in navigator");
-			window.addEventListener("load", function() {
 
-				var swPath = path + sw + ".js";
-				console.log("ServiceWorker path: ", swPath);
+			var swPath = path + sw + ".js";
+			console.log("ServiceWorker path: ", swPath);
 
-				navigator.serviceWorker.register(swPath).then(function(registration) {
-					// Registration was successful
-					console.log("ServiceWorker (" + sw + ") registration successful with scope: ", registration.scope);
-				}, function(err) {
-					// registration failed :(
-					console.log("ServiceWorker (" + sw + ") registration failed: ", err);
-				});
+			navigator.serviceWorker.register(swPath).then(function(registration) {
+				// Registration was successful
+				console.log("ServiceWorker (" + sw + ") registration successful with scope: ", registration.scope);
+			}, function(err) {
+				// registration failed :(
+				console.log("ServiceWorker (" + sw + ") registration failed: ", err);
 			});
-	
+
 		}
 		
 
