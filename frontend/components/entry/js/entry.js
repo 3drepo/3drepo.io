dbd79cb46e3c7ca8a21d39a5e33c09fe87316d65
James Milner <jamesmilner22@hotmail.com>
2017-08-13 23:15:38 +0100

    ISSUE #394 - Client config

--- frontend/components/entry/js/entry.js
+++ frontend/components/entry/js/entry.js
@@ -1,30 +1,33 @@
 
-if (!window.ClientConfig) {
-	console.error("ClientConfig has not been provided...");
-} 
+if (window) {
+	if (!window.ClientConfig) {
+		console.error("ClientConfig has not been provided...");
+	} 
 
-// Add some offline UX
-window.addEventListener("load", function() {
+	// Add some offline UX
+	window.addEventListener("load", function() {
 
-	var offlineDiv = document.createElement("div");
-	offlineDiv.className = "connection";
-	document.body.appendChild(offlineDiv);
+		var offlineDiv = document.createElement("div");
+		offlineDiv.className = "connection";
+		document.body.appendChild(offlineDiv);
 
-	function updateOnlineStatus() {
-		var condition = navigator.onLine ? "Online" : "Offline";
-		document.querySelector(".connection").innerHTML = condition;
-		if (condition === "Online") {
-			offlineDiv.className = "connection online";
-			setTimeout(function(){
-				document.querySelector(".connection").innerHTML = "";
-			}, 1000 * 20); // Show it for twenty seconds
-		} else {
-			offlineDiv.className = "connection offline";
+		function updateOnlineStatus() {
+			var condition = navigator.onLine ? "Online" : "Offline";
+			document.querySelector(".connection").innerHTML = condition;
+			if (condition === "Online") {
+				offlineDiv.className = "connection online";
+				setTimeout(function(){
+					document.querySelector(".connection").innerHTML = "";
+				}, 1000 * 20); // Show it for twenty seconds
+			} else {
+				offlineDiv.className = "connection offline";
+			}
 		}
-	}
 
-	window.addEventListener("online",  updateOnlineStatus);
-	window.addEventListener("offline", updateOnlineStatus);
-});
+		window.addEventListener("online",  updateOnlineStatus);
+		window.addEventListener("offline", updateOnlineStatus);
+	});
+}
+
 
 angular.module("3drepo", ["ui.router", "ngMaterial", "ngAnimate", "ngSanitize", "vcRecaptcha"]);
