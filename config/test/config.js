2100b1792458b315362fb3ad15d7abfbc353ca73
James Milner <jamesmilner22@hotmail.com>
2017-10-10 10:30:15 +0100

    ISSUE #524 - Turn on headless, remove logging from CI

--- config/test/config.js
+++ config/test/config.js
@@ -48,7 +48,7 @@ module.exports = {
 		}
 	],
 	js_debug_level: 'debug',
-	consoleLogging: true,
+	consoleLogging: false,
 	logfile: {
 		filename: './3drepo.log',
 		console_level: 'info',
--- frontend/tests/e2e/ie/ie-config.ts
+++ frontend/tests/e2e/ie/ie-config.ts
@@ -12,7 +12,7 @@ export let config: Config = {
     'browserName': 'chrome',
     chromeOptions: {
       args: [
-        //"--headless", 
+        "--headless", 
         "--disable-gpu", 
         "--window-size=1920x1080",
         '--user-agent="Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'
