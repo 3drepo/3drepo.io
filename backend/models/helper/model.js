ead94caebe7b5d2e601b5c85759245664a2949bf
Charence Wong <charence@Charences-MacBook-Pro.local>
2017-10-31 16:38:46 +0000

    ISSUE #565 - Remove catch from removeModel

--- backend/models/helper/model.js
+++ backend/models/helper/model.js
@@ -1423,9 +1423,6 @@ function removeModel(account, model, forceRemove){
 
 		//remove model from all project
 		return Project.removeModel(account, model);
-	}).catch(err => {
-		systemLogger.logError(`Failed to removeModel:`, err);
-		return Promise.reject({resCode: responseCodes.MODEL_IS_A_SUBMODEL});
 	});
 
 }
