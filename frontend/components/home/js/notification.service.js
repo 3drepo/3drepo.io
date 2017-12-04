f34aa6334bc41f902de0146150af5ce051d132f2
James Milner <jamesmilner22@hotmail.com>
2017-12-04 11:08:27 +0000

    ISSUE #619 - Allowing muting

--- frontend/components/home/js/notification.service.js
+++ frontend/components/home/js/notification.service.js
@@ -65,28 +65,7 @@
 		socket.on("disconnect", function(){
 
 			console.error("The websocket for the notification service was disconnected");
-
-			var content = "Your connection to the 3D Repo's notification service has dropped. " + 
-			"3D Repo may not behave as expected when commenting and changing issues. Try refreshing the page" +
-			" to reconnect.";
-
-			var thirtySeconds = 30 * 1000; /* ms */
-			var lessThanThirtySeconds = true;
-
-			if (lastDialogOpen !== undefined) {
-				lessThanThirtySeconds = ((new Date) - lastDialogOpen) > thirtySeconds;
-				console.debug("Not showing user connection error dialog as it's been less than 30 seconds");
-			}
-			
-			// Stops it from appearing upon referesh
-			if (dialogOpen === false && lessThanThirtySeconds) {
-				dialogOpen = true;
-				lastDialogOpen = Date.now();
-				DialogService.text("Notification Service Disconnected", content, true).then(function(){
-					dialogOpen = false;
-				});
-			}
-			
+			DialogService.disconnected();
 			
 		});
 
