5242aedd96b8d869af7c11301055a011381838ec
James Milner <jamesmilner22@hotmail.com>
2017-12-12 10:40:32 +0000

    ISSUE #640 - Get login working

--- demo/demo.js
+++ demo/demo.js
@@ -15,25 +15,73 @@ var Module = {
 
 (function(){
 
+    login();
+
+    function login() {
+
+        // Replace as appropriate 
+        var LOGIN_URL = "https://api1.www.3drepo.io/api/login";
+        
+        // Get user credentials
+        var username = prompt("Please enter your 3drepo.io username");
+        var password = prompt("Please enter your 3drepo.io password");
+        var credentials = {username: username, password: password};
+        
+        var post = { 
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials),
+            mode: 'cors'
+        };
 
 
-    console.log("Initialising 3D Repo Viewer...");
-    viewer = new Viewer("3DRepo", document.body, viewerCallback, viewerErrorCallback);
-    viewer.unityLoaderPath = "./../frontend/unity/Release/UnityLoader.js";
-    viewer.prepareViewer();
-    viewer.insertUnityLoader();
+        // Use the Fetch API
+        fetch(LOGIN_URL, post)
+            .then(function(response) {
 
+                var validResponse = response.status !== 200;
 
-    var options = {
-        showAll : true,
-        getAPI: {
-            hostNames:  [location.href] // Must be an array
-        }
-    };
+                response.json().then(function(json){
+                    if (validResponse) {
+                        confirm(json.message)
+                        location.reload()
+                    } else {
+                        initialiseViewer(json)
+                    }
+                })
+            
+            })
+            .catch(function(error) {
+                confirm("Error", error)
+            })
 
-    viewer.init(options);
+    }
+   
+    function initialiseViewer(json) {
 
-    function viewerCallback() {}
-    function viewerErrorCallback() {}
+        console.log("Initialising 3D Repo Viewer...");
+        viewer = new Viewer("3DRepo", document.body, viewerCallback, viewerErrorCallback);
+        viewer.unityLoaderPath = "./../frontend/unity/Release/UnityLoader.js";
+        viewer.prepareViewer();
+        viewer.insertUnityLoader();
+    
+    
+        var options = {
+            showAll : true,
+            getAPI: {
+                hostNames:  [location.href] // Must be an array
+            }
+        };
+    
+        viewer.init(options);
+    
+        function viewerCallback() {}
+        function viewerErrorCallback() {}
+
+    }
+   
 
 }())
