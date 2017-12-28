3beaac2420fda8220aa7b65d9ca4a5f18c92599d
James Milner <jamesmilner22@hotmail.com>
2017-12-28 11:09:30 +0000

    ISSUE #509 - List files

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ if [ $1 = "backend" ]; then
     cd ./backend
     yarn run test
 else
-    cd ./frontend
     NODE_ENV=test NODE_CONFIG_DIR='./config' node "./backend/3drepo.js" & sleep 5 
+    cd ./frontend
     yarn run wdm:update
     yarn run wdm:start > /dev/null 2>&1 &
     yarn run e2e
