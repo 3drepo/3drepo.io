34f93d3a145f7f6a8afbc15ad277effaf0465bb7
James Milner <jamesmilner22@hotmail.com>
2018-02-13 15:00:14 +0000

    ISSUE #651 - Move whole folder over

---
+++ frontend/components/home/js/state-manager.run.ts
@@ -0,0 +1,79 @@
+/**
+ *	Copyright (C) 2014 3D Repo Ltd
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as
+ *	published by the Free Software Foundation, either version 3 of the
+ *	License, or (at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function StateManagerRun($location, $rootScope, $state, StateManager, AuthService, $timeout, AnalyticService) {
+
+	$rootScope.$on("$stateChangeStart", (event, toState, toParams, fromState, fromParams) => {
+
+		StateManager.state.changing = true;
+
+		for (let i = 0; i < StateManager.functions.length; i++) {
+			StateManager.setStateVar(StateManager.functions[i], false);
+		}
+
+		StateManager.clearQuery();
+
+		const stateChangeObject = {
+			toState,
+			toParams,
+			fromState,
+			fromParams,
+		};
+
+		StateManager.startStateChange(stateChangeObject);
+	});
+
+	$rootScope.$on("$stateChangeSuccess", (event, toState, toParams, fromState, fromParams) => {
+
+		const stateChangeObject = {
+			toState,
+			toParams,
+			fromState,
+			fromParams,
+		};
+
+		StateManager.handleStateChange(stateChangeObject);
+	});
+
+	$rootScope.$on("$locationChangeSuccess", () => {
+
+		AnalyticService.sendPageView(location);
+
+		const queryParams = $location.search();
+
+		if (Object.keys(queryParams).length === 0) {
+			StateManager.clearQuery();
+		} else {
+			StateManager.setQuery(queryParams);
+		}
+
+	});
+
+}
+
+export const StateManagerRunModule = angular
+	.module("3drepo")
+	.run([
+		"$location",
+		"$rootScope",
+		"$state",
+		"StateManager",
+		"AuthService",
+		"$timeout",
+		"AnalyticService",
+		StateManagerRun,
+	]);
