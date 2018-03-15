9064d283eeb9a3f29b2aaf3280c5b6dced2fe9bd
carmenfan <wind5320@hotmail.com>
2018-03-15 15:40:42 +0000

    ISSUE #818 sanitise status and priority coming from BCF

--- backend/models/issue.js
+++ backend/models/issue.js
@@ -93,23 +93,6 @@ let schema = Schema({
         	type: String
 	},
 
-
-	// TO-DO: remove this after db migration => viewpoints[0]=viewpoint
-	viewpoint: {
-		up: [Number],
-		position: [Number],
-		look_at: [Number],
-		view_dir: [Number],
-		right: [Number],
-		unityHeight : Number,
-		fov : Number,
-		aspect_ratio: Number,
-		far : Number,
-		near : Number,
-		clippingPlanes : [Schema.Types.Mixed ],
-		guid: Object
-	},
-
 	viewpoints: [{
 		up: [Number],
 		position: [Number],
@@ -595,13 +578,13 @@ schema.statics.createIssue = function(dbColOptions, data){
 		
 	}).then(count => {
 
-		// if(_.map(statusEnum).indexOf(data.status) === -1){
-		// 	return Promise.reject(responseCodes.ISSUE_INVALID_STATUS);
-		// }
+		if(_.map(statusEnum).indexOf(data.status) === -1){
+			return Promise.reject(responseCodes.ISSUE_INVALID_STATUS);
+		}
 
-		// if(_.map(priorityEnum).indexOf(data.priority) === -1){
-		// 	return Promise.reject(responseCodes.ISSUE_INVALID_PRIORITY);
-		// }
+		if(_.map(priorityEnum).indexOf(data.priority) === -1){
+		 	return Promise.reject(responseCodes.ISSUE_INVALID_PRIORITY);
+		}
 
 		issue.number  = count + 1;
 		issue.object_id = objectId && stringToUUID(objectId);
@@ -1317,7 +1300,7 @@ schema.methods.getBCFMarkup = function(account, model, unit){
 	_.get(this, "extras.Labels") && (markup.Markup.Topic.Labels = _.get(this, "extras.Labels"));
 	_.get(this, "extras.ModifiedDate") && (markup.Markup.Topic.ModifiedDate = _.get(this, "extras.ModifiedDate"));
 	_.get(this, "extras.ModifiedAuthor") && (markup.Markup.Topic.ModifiedAuthor = _.get(this, "extras.ModifiedAuthor"));
-	_.get(this, "extras.AssignedTo") && (markup.Markup.Topic.AssignedTo = _.get(this, "extras.AssignedTo"));
+	_.get(this, "extras.AssignedTo") && (markup.Markup.Topic.AssignedTo = this.assigned_roles.toString());
 	_.get(this, "extras.BimSnippet") && (markup.Markup.Topic.BimSnippet = _.get(this, "extras.BimSnippet"));
 	_.get(this, "extras.DocumentReference") && (markup.Markup.Topic.DocumentReference = _.get(this, "extras.DocumentReference"));
 	_.get(this, "extras.RelatedTopic") && (markup.Markup.Topic.RelatedTopic = _.get(this, "extras.RelatedTopic"));
@@ -1801,6 +1784,19 @@ schema.statics.importBCF = function(requester, account, model, revId, zipPath){
 				return Promise.all(promises).then(() => viewpoints);
 			}
 
+			function sanitise(data, list) {
+				if (!data) {
+					return data;
+				}
+
+				const dataSanitised = data.toLowerCase();
+				if(_.map(list).indexOf(dataSanitised) === -1) {
+					return data;
+				}
+				return dataSanitised;
+				
+			}
+
 			function createIssue(guid){
 
 				let issueFiles = files[guid];
@@ -1825,13 +1821,13 @@ schema.statics.importBCF = function(requester, account, model, revId, zipPath){
 						
 						issue.extras.Header = _.get(xml, "Markup.Header");
 						issue.topic_type = _.get(xml, "Markup.Topic[0].@.TopicType");
-						issue.status =_.get(xml, "Markup.Topic[0].@.TopicStatus");
+						issue.status = sanitise(_.get(xml, "Markup.Topic[0].@.TopicStatus"), statusEnum);
 						if( !issue.status || issue.status === "") {
 							issue.status = "open";
 						}
 						issue.extras.ReferenceLink = _.get(xml, "Topic[0].ReferenceLink");
 						issue.name = _.get(xml, "Markup.Topic[0].Title[0]._");
-						issue.priority =  _.get(xml, "Markup.Topic[0].Priority[0]._");
+						issue.priority =  sanitise(_.get(xml, "Markup.Topic[0].Priority[0]._"), priorityEnum);
 						issue.extras.Index =  _.get(xml, "Markup.Topic[0].Index[0]._");
 						issue.extras.Labels =  _.get(xml, "Markup.Topic[0].Labels[0]._");
 						issue.created = moment(_.get(xml, "Markup.Topic[0].CreationDate[0]._")).format("x");
@@ -1841,7 +1837,9 @@ schema.statics.importBCF = function(requester, account, model, revId, zipPath){
 						if (_.get(xml, "Markup.Topic[0].DueDate[0]._")) {
 							issue.due_date = moment(_.get(xml, "Markup.Topic[0].DueDate[0]._")).format("x");
 						}
-						issue.extras.AssignedTo = _.get(xml, "Markup.Topic[0].AssignedTo[0]._");
+						if(_.get(xml, "Markup.Topic[0].AssignedTo[0]._")) {
+							issue.assigned_roles = _.get(xml, "Markup.Topic[0].AssignedTo[0]._").split(",");
+						}
 						issue.desc = _.get(xml, "Markup.Topic[0].Description[0]._");
 						issue.extras.BimSnippet = _.get(xml, "Markup.Topic[0].BimSnippet");
 						issue.extras.DocumentReference = _.get(xml, "Markup.Topic[0].DocumentReference");
