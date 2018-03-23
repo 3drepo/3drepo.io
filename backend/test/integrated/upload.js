6e44c5d972a32a1da9fa501509abf18842ade05a
carmenfan <wind5320@hotmail.com>
2018-03-23 09:35:23 +0000

    ISSUE #763 update data

--- backend/test/integrated/upload.js
+++ backend/test/integrated/upload.js
@@ -113,7 +113,6 @@ describe('Uploading a model', function () {
 				agent2.post(`/testing/${myModel}/upload`)
 				.attach('file', __dirname + '/../../statics/3dmodels/8000cubes.obj')
 				.expect(400, function(err, res){
-					console.log("!!!!", res.body);
 					expect(res.body.value).to.equal(responseCodes.SIZE_LIMIT_PAY.value);
 					done(err);
 				});
