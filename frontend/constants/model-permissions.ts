1fa999598fb678b220fb97df124b01040a7ea05c
Mariusz Wójcik <mwojcik@apptension.com>
2018-09-04 10:10:36 +0200

    ISSUE #1085 - Add tooltips for permissions

--- frontend/constants/model-permissions.ts
+++ frontend/constants/model-permissions.ts
@@ -7,30 +7,43 @@ export const MODEL_ROLES_TYPES = {
 	NONE: "none"
 };
 
+export const MODEL_ROLES_DESC = {
+	UNASSIGNED: "No access",
+	VIEWER: "Can only view",
+	COMMENTER: "View and create issues",
+	COLLABORATOR: "Full access and ability to upload/download revisions",
+	ADMINSTRATOR: "Collaborator access and edit permissions"
+};
+
 export const MODEL_ROLES_LIST = [
 	{
 		key: MODEL_ROLES_TYPES.UNASSIGNED,
 		label: "Unassigned",
-		width: "100px"
+		width: "100px",
+		tooltip: MODEL_ROLES_DESC.UNASSIGNED
 	},
 	{
 		key: MODEL_ROLES_TYPES.VIEWER,
 		label: "Viewer",
-		width: "70px"
+		width: "70px",
+		tooltip: MODEL_ROLES_DESC.VIEWER
 	},
 	{
 		key: MODEL_ROLES_TYPES.COMMENTER,
 		label: "Commenter",
-		width: "100px"
+		width: "100px",
+		tooltip: MODEL_ROLES_DESC.COMMENTER
 	},
 	{
 		key: MODEL_ROLES_TYPES.COLLABORATOR,
 		label: "Collaborator",
-		width: "100px"
+		width: "100px",
+		tooltip: MODEL_ROLES_DESC.COLLABORATOR
 	},
 	{
 		key: MODEL_ROLES_TYPES.ADMINSTRATOR,
 		label: "Admin",
-		width: "70px"
+		width: "70px",
+		tooltip: MODEL_ROLES_DESC.ADMINSTRATOR
 	}
 ];
--- frontend/constants/project-permissions.ts
+++ frontend/constants/project-permissions.ts
@@ -3,15 +3,22 @@ export const PROJECT_ROLES_TYPES = {
 	UNASSIGNED: null
 };
 
+export const PROJECT_ROLES_DESC = {
+	UNASSIGNED: "No access",
+	ADMINSTRATOR: "Collaborator access and edit permissions"
+};
+
 export const PROJECT_ROLES_LIST = [
 	{
 		key: PROJECT_ROLES_TYPES.UNASSIGNED,
 		label: "Unassigned",
-		width: "100px"
+		width: "100px",
+		tooltip: PROJECT_ROLES_DESC.UNASSIGNED
 	},
 	{
 		key: PROJECT_ROLES_TYPES.ADMINSTRATOR,
 		label: "Admin",
-		width: "70px"
+		width: "70px",
+		tooltip: PROJECT_ROLES_DESC.ADMINSTRATOR
 	}
 ];
