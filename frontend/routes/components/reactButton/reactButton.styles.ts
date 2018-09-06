56f25037e556208a50d9e1aa0c2e72a3bcd3910b
Mariusz Wójcik <mwojcik@apptension.com>
2018-09-06 22:48:20 +0200

    ISSUE #1132 - Use proper component name

--- frontend/routes/components/reactButton/reactButton.styles.ts
+++ frontend/routes/components/reactButton/reactButton.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Button from '@material-ui/core/button';
+import Button from '@material-ui/core/Button';
 
 export const ButtonContainer = styled(Button)`
   color: red !important;
