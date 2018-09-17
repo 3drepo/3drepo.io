5a19705e709127a7de3bf6cd27372ade4e5151f9
Mariusz Wójcik <mwojcik@apptension.com>
2018-09-17 15:02:17 +0200

    ISSUE #1145 - Add basci structure for jobs route

--- frontend/routes/angularBindings.ts
+++ frontend/routes/angularBindings.ts
@@ -8,9 +8,11 @@ import { react2angular as wrap } from 'react2angular';
 // Routes
 import Users from './users/users.container';
 import DialogContainer from './components/dialogContainer/dialogContainer.container';
+import Jobs from './jobs/jobs.container';
 
 angular
 	.module('3drepo')
 
 	.component('users', wrap(Users, ['users', 'jobs', 'onUsersChange', 'active', 'limit']))
+	.component('jobs', wrap(Jobs))
 	.component('dialogContainer', wrap(DialogContainer));
