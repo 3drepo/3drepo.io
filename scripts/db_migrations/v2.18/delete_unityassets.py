a3c777d350906a4e075dcb1752d6f2aa352fe1f6
Kieron Gibbons <kieron_gibbons@hotmail.co.uk>
2018-09-21 10:59:19 +0100

    ISSUE 1151 - Updating unityAssets.json deletion script to include BSON check

--- scripts/db_migrations/v2.18/delete_unityassets.py
+++ scripts/db_migrations/v2.18/delete_unityassets.py
@@ -1,4 +1,3 @@
-import json
 import gridfs
 import sys
 import re
@@ -9,6 +8,8 @@ mongoURL = sys.argv[1]
 mongoPort = sys.argv[2]
 userName = sys.argv[3]
 password = sys.argv[4]
+missingFilesPath = sys.argv[5]
+
 connString = "mongodb://"+ userName + ":" + password +"@"+mongoURL + ":" + mongoPort + "/"
 
 ##### Enable dry run to not commit to the database #####
@@ -26,17 +27,27 @@ for database in db.database_names():
 ##### Get a model ID #####
     for model in db.settings.find({"federate": {"$ne": True}}, {"_id": 1}):
       model_id = model.get('_id')
-      print("\t--model:" + model_id)
-      colName = model_id + ".stash.json_mpc"
+      print("\t--model: " + model_id)
+      col_name = model_id + ".stash.json_mpc"
 
-##### Get file and update the json #####
-      fs = gridfs.GridFS(db, colName)
+##### Check if bson files exist before deleting #####
+      fs = gridfs.GridFS(db, col_name)
       unity_asset_files = fs.find({"filename" : {"$regex": "unityAssets.json$" }})
-
+      for asset_file in unity_asset_files:
+        rev_id = re.search(r"(?<=revision/).*?(?=/unityAssets\.json)", asset_file.filename).group(0)
+        col_name_check = model_id + ".stash.unity3d"
+        fs_check = db[col_name_check].find({"_id" : uuid.UUID(rev_id)})
+        if fs_check.count() > 0:
+          print "BSON found"
 ##### Delete unityAssets.json files #####
-      for single_file in unity_asset_files:
-        if dry_run:
-          print "NOT deleting: " + single_file.filename
+          if dry_run:
+            print "NOT deleting: " + asset_file.filename
+          else:
+            print "deleting: " + asset_file.filename
+            fs.delete(asset_file._id)
         else:
-          print "deleting: " + single_file.filename
-          fs.delete(single_file._id)
+          print "No BSON found...storing in: " + missingFilesPath
+          with open(missingFilesPath, "a") as log_file:
+            log_file.write(asset_file.filename + " \r\n")
+
+print "Exported list of missing BSON files to: " + missingFilesPath
