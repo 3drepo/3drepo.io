31d240d2964a1321f72cdfdac2bd39c2b3720738
Mariusz Wójcik <mwojcik@apptension.com>
2018-09-28 15:21:57 +0200

    ISSUE #1170 - Remove extra muo theme providers

--- frontend/routes/projects/projects.container.ts
+++ frontend/routes/projects/projects.container.ts
@@ -14,7 +14,6 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 import { connect } from '../../helpers/migration';
 import { bindActionCreators } from 'redux';
 import { createStructuredSelector } from 'reselect';
