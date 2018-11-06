b287600ed543794be17f837f1fff752674eaef29
Daniel Leszczynski <dleszczynski@apptension.com>
2018-11-06 12:25:34 +0100

    ISSUE #1183 - Add Chips component

---
+++ frontend/routes/components/chips/chips.component.tsx
@@ -0,0 +1,104 @@
+/**
+ *  Copyright (C) 2017 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as React from 'react';
+import { snakeCase } from 'lodash';
+
+import Input from '@material-ui/core/Input';
+import Chip from '@material-ui/core/Chip';
+
+import { ENTER_KEY } from '../../../constants/keys';
+
+import { ChipsGrid, ChipsContainer } from './chips.styles';
+
+interface IProps {
+	name: string;
+	inputPlaceholder: string;
+	value: any[];
+	onChange: (event) => void;
+	onBlur: (event) => void;
+}
+
+interface IState {
+	value: any[];
+}
+
+export class Chips extends React.PureComponent<IProps, IState> {
+	public state = {
+		value: []
+	};
+
+	public componentDidMount() {
+		this.setState({ value: this.props.value });
+	}
+
+	public handleChange = () => {
+		if (this.props.onChange) {
+			this.props.onChange({
+				target: {
+					value: this.state.value,
+					name: this.props.name
+				}
+			});
+		}
+	}
+
+	public handleNewChipSubmit = (event) => {
+		if (event.key === ENTER_KEY) {
+			this.setState({
+				value: [...this.state.value, {
+					label: event.target.value, value: snakeCase(event.target.value)
+				}]
+			}, this.handleChange);
+
+			event.target.value = '';
+			event.preventDefault();
+		}
+	}
+
+	public deleteChip = (type) => {
+		this.setState({
+			value: this.state.value.filter((typeItem) => typeItem.value !== type.value)
+		}, this.handleChange);
+	}
+
+	public render() {
+		const { name, onBlur, value, inputPlaceholder } = this.props;
+
+		return (
+			<ChipsGrid container direction="column">
+				<ChipsContainer>
+					{value.map(
+						(chip, index) => (
+							<Chip
+								key={index}
+								label={chip.label}
+								onDelete={() => this.deleteChip(chip)}
+							/>
+						)
+					)}
+				</ChipsContainer>
+				<Input
+					onKeyPress={(event) => this.handleNewChipSubmit(event)}
+					placeholder={ inputPlaceholder }
+					onBlur={onBlur}
+					name={name}
+				/>
+			</ChipsGrid>
+		);
+	}
+}
