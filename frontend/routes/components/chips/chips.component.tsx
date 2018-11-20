b2b12d0f93544bf98184144fe7b9f2dcac22fef5
Daniel Leszczynski <dleszczynski@apptension.com>
2018-11-20 13:36:39 +0100

    ISSUE #1183 - Disable adding the same topic type in model settings

--- frontend/routes/components/chips/chips.component.tsx
+++ frontend/routes/components/chips/chips.component.tsx
@@ -57,15 +57,22 @@ export class Chips extends React.PureComponent<IProps, IState> {
 		}
 	}
 
-	public handleNewChipSubmit: any = (event) => {
+	public handleNewChipSubmit: any = (event) => {		
 		if (event.key === ENTER_KEY) {
 			event.preventDefault();
 
-			this.setState({
-				value: [...this.state.value, {
-					label: event.target.value, value: snakeCase(event.target.value)
-				}]
-			}, this.handleChange);
+			const newChip = {
+				value: snakeCase(event.target.value), 
+				label: event.target.value
+			}
+
+			const chipExists = !!this.state.value.find(chip => (chip.value === newChip.value));
+
+			if (!chipExists) {
+				this.setState({
+					value: [...this.state.value, newChip]
+				}, this.handleChange);
+			}
 
 			event.target.value = '';
 		}
