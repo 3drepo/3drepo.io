90a46dd6d857c101f6e618d7823df539d0ffe741
carmenfan <wind5320@hotmail.com>
2018-11-21 16:24:55 +0000

    ISSUE #1234 arguments are wrong way round

--- frontend/components/risks/js/risks-list.component.ts
+++ frontend/components/risks/js/risks-list.component.ts
@@ -145,7 +145,7 @@ class RisksListController implements ng.IController {
 
 			case "downloadJSON":
 				const jsonEndpoint = this.account + "/" + this.model + "/risks.json";
-				this.PanelService.downloadJSON(jsonEndpoint, "risks");
+				this.PanelService.downloadJSON("risks", jsonEndpoint);
 				break;
 		}
 	}
