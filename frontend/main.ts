f434fadfe5e37cf42450fb52b91e7473bc2fe445
Daniel Leszczynski <dleszczynski@apptension.com>
2018-11-22 16:30:22 +0100

    ISSUE #1226 - Remove old templates

--- frontend/main.ts
+++ frontend/main.ts
@@ -44,9 +44,7 @@ import './routes/angularBindings';
 // @ts-ignore
 requireAll(require.context('./components', true, /\.css$/));
 // @ts-ignore
-requireAll(require.context('./components', true, /\.pug$/));
-// @ts-ignore
-requireAll(require.context('../pug/legal', true, /\.pug$/));
+requireAll(require.context('./components', true, /\.pug/));
 
 // Kickstart the application
 angular.bootstrap(document.body, ['3drepo'], { strictDi: true });
