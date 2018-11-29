9f5fe19115adb462b21e24357e2597ea3c0553e2
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-11-29 12:03:09 +0000

    ISSUE #1255 - Add additional parameter to groups method call

--- frontend/components/groups/js/groups.component.ts
+++ frontend/components/groups/js/groups.component.ts
@@ -205,7 +205,7 @@ class GroupsController implements ng.IController {
 						case "downloadJSON":
 							const jsonEndpoint = this.account + "/" + this.model +
 							"/groups/revision/master/head/?noIssues=true&noRisks=true";
-							this.panelService.downloadJSON("groups", jsonEndpoint);
+							this.panelService.downloadJSON("groups", jsonEndpoint, this.model, this.account);
 							break;
 						default:
 							console.error("Groups option menu selection unhandled");
