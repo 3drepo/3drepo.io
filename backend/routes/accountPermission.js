7eef2680dd5663c98e594d1ad2a9a13105a31217
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-12-03 16:54:51 +0000

    ISSUE #1230 - Account permission routes docs

--- backend/routes/accountPermission.js
+++ backend/routes/accountPermission.js
@@ -17,94 +17,125 @@
 
 "use strict";
 (function() {
+              const express = require("express");
+              const router = express.Router({ mergeParams: true });
+              const responseCodes = require("../response_codes");
+              const middlewares = require("../middlewares/middlewares");
+              const User = require("../models/user");
+              const utils = require("../utils");
+              const _ = require("lodash");
 
-	const express = require("express");
-	const router = express.Router({mergeParams: true});
-	const responseCodes = require("../response_codes");
-	const middlewares = require("../middlewares/middlewares");
-	const User = require("../models/user");
-	const utils = require("../utils");
-	const _ = require("lodash");
+              /**
+               * @api {get} /permissions/ List all permissions
+               * @apiName listPermissions
+               * @apiGroup Account Permission
+               */
+              router.get("/permissions", middlewares.isAccountAdmin, listPermissions);
+              /**
+               * @api {post} /permissions/ Create a permission
+               * @apiName createPermission
+               * @apiGroup Account Permission
+               */
+              router.post("/permissions", middlewares.isAccountAdmin, createPermission);
 
-	router.get("/permissions", middlewares.isAccountAdmin, listPermissions);
-	router.post("/permissions", middlewares.isAccountAdmin, createPermission);
-	router.put("/permissions/:user", middlewares.isAccountAdmin, updatePermission);
-	router.delete("/permissions/:user", middlewares.isAccountAdmin, deletePermission);
+              /**
+               * @api {put} /permissions/:user Update a permission
+               * @apiName updatePermission
+               * @apiGroup Account Permission
+               *
+               * @apiParam {String} user User to update
+               */
 
-	function listPermissions(req, res, next) {
+              router.put("/permissions/:user", middlewares.isAccountAdmin, updatePermission);
 
-		User.findByUserName(req.params.account).then(user => {
-			const permissions = user.toObject().customData.permissions;
-			return User.getAllUsersInTeamspace(req.params.account).then(users => {
-				users.forEach(_user => {
-					if(!_.find(permissions, {"user" : _user})) {
-						permissions.push({user: _user, permissions: []});
-					}
-				});
-				responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.OK, permissions);
+              /**
+               * @api {delete} /permissions/:user Delete a permission
+               * @apiName deletePermission
+               * @apiGroup Account Permission
+               *
+               * @apiParam {String} user User to delete
+               */
 
-			});
+              router.delete("/permissions/:user", middlewares.isAccountAdmin, deletePermission);
 
-		}).catch(err => {
+              function listPermissions(req, res, next) {
+                User.findByUserName(req.params.account)
+                  .then(user => {
+                    const permissions = user.toObject().customData.permissions;
+                    return User.getAllUsersInTeamspace(req.params.account).then(
+                      users => {
+                        users.forEach(_user => {
+                          if (
+                            !_.find(permissions, { user: _user })
+                          ) {
+                            permissions.push({
+                              user: _user,
+                              permissions: []
+                            });
+                          }
+                        });
+                        responseCodes.respond(
+                          utils.APIInfo(req),
+                          req,
+                          res,
+                          next,
+                          responseCodes.OK,
+                          permissions
+                        );
+                      }
+                    );
+                  })
+                  .catch(err => {
+                    responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
+                  });
+              }
 
-			responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
-		});
+              function createPermission(req, res, next) {
+                if (Object.keys(req.body).length === 2 && Object.prototype.toString.call(req.body.user) === "[object String]" && Object.prototype.toString.call(req.body.permissions) === "[object Array]") {
+                  User.findByUserName(req.params.account)
+                    .then(user => {
+                      return user.customData.permissions.add(req.body);
+                    })
+                    .then(permission => {
+                      responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.OK, permission);
+                    })
+                    .catch(err => {
+                      responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
+                    });
+                } else {
+                  responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.INVALID_ARGUMENTS, responseCodes.INVALID_ARGUMENTS);
+                }
+              }
 
-	}
+              function updatePermission(req, res, next) {
+                if (Object.keys(req.body).length === 1 && Object.prototype.toString.call(req.body.permissions) === "[object Array]") {
+                  User.findByUserName(req.params.account)
+                    .then(user => {
+                      return user.customData.permissions.update(req.params.user, req.body);
+                    })
+                    .then(permission => {
+                      responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.OK, permission);
+                    })
+                    .catch(err => {
+                      responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
+                    });
+                } else {
+                  responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.INVALID_ARGUMENTS, responseCodes.INVALID_ARGUMENTS);
+                }
+              }
 
-	function createPermission(req, res, next) {
+              function deletePermission(req, res, next) {
+                User.findByUserName(req.params.account)
+                  .then(user => {
+                    return user.customData.permissions.remove(req.params.user);
+                  })
+                  .then(() => {
+                    responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.OK, {});
+                  })
+                  .catch(err => {
+                    responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
+                  });
+              }
 
-		if (Object.keys(req.body).length === 2 &&
-			Object.prototype.toString.call(req.body.user) === "[object String]" &&
-			Object.prototype.toString.call(req.body.permissions) === "[object Array]") {
-
-			User.findByUserName(req.params.account).then(user => {
-
-				return user.customData.permissions.add(req.body);
-
-			}).then(permission => {
-				responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.OK, permission);
-			}).catch(err => {
-				responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
-			});
-		} else {
-			responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.INVALID_ARGUMENTS, responseCodes.INVALID_ARGUMENTS);
-		}
-
-	}
-
-	function updatePermission(req, res, next) {
-
-		if (Object.keys(req.body).length === 1 &&
-			Object.prototype.toString.call(req.body.permissions) === "[object Array]") {
-			User.findByUserName(req.params.account).then(user => {
-				return user.customData.permissions.update(req.params.user, req.body);
-
-			}).then(permission => {
-				responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.OK, permission);
-			}).catch(err => {
-				responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
-			});
-		} else {
-			responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.INVALID_ARGUMENTS, responseCodes.INVALID_ARGUMENTS);
-		}
-
-	}
-
-	function deletePermission(req, res, next) {
-
-		User.findByUserName(req.params.account).then(user => {
-
-			return user.customData.permissions.remove(req.params.user);
-
-		}).then(() => {
-
-			responseCodes.respond(utils.APIInfo(req), req, res, next, responseCodes.OK, {});
-
-		}).catch(err => {
-			responseCodes.respond(utils.APIInfo(req), req, res, next, err, err);
-		});
-	}
-
-	module.exports = router;
-}());
+              module.exports = router;
+            }());
