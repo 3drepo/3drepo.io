d1b9ede42a7b0ab38bfd5162826109857f74c34d
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-12-03 16:47:31 +0000

    ISSUE #1230 - Remove old version api doc json

--- backend/routes/_apidoc.js
+++ backend/routes/_apidoc.js
@@ -1,13 +1,1 @@
-/**
- * @api {get} /user Request User Information
- * @apiName GetUser
- * @apiGroup User
- * @apiVersion 0.1.0
- *
- * @apiSuccess {String} name The users name
- *
- * @apiSuccessExample Example data on success:
- * {
- * name:'Paul'
- * }
- */
+/* TODO : PLACE ALL DEPRECATED CODE HERE */
