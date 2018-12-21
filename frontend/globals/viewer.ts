8ce80f0fa7d6608d0fb3725081e1d79226ddfaa3
Mariusz Wójcik <mwojcik@apptension.com>
2018-12-21 09:15:37 +0100

    ISSUE #1268 - Handle screenshot promise on unity side

--- frontend/globals/viewer.ts
+++ frontend/globals/viewer.ts
@@ -337,8 +337,8 @@ export class Viewer {
 		UnityUtil.viewer = this;
 	}
 
-	public getScreenshot(promise) {
-		UnityUtil.requestScreenShot(promise);
+	public getScreenshot() {
+		return UnityUtil.requestScreenShot();
 	}
 
 	public diffToolSetAsComparator(account: string, model: string) {
