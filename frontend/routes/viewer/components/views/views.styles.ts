cd3862a707c92988c7cf2561de3d4df1a82322d4
Daniel Leszczynski <dleszczynski@apptension.com>
2019-01-08 14:33:34 +0100

    ISSUE #1259 - Style message

--- frontend/routes/viewer/components/views/views.styles.ts
+++ frontend/routes/viewer/components/views/views.styles.ts
@@ -59,7 +59,13 @@ export const ViewpointItem = styled(MenuItem)`
 ` as any;
 
 export const EmptyStateInfo = styled.p`
-  padding: 24px;
+  padding: 14px;
+  font-size: 13px;
+  color: ${COLOR.BLACK_60};
+  background-color: ${COLOR.BLACK_6};
+  margin: 25px;
+  border-radius: 6px;
+  text-align: center;
 `;
 
 export const NewItemWrapper = styled.div`
