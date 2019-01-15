37cb7af70d42ee5989c46904b23315b96d53dd91
Daniel Leszczynski <dleszczynski@apptension.com>
2019-01-15 11:56:48 +0100

    ISSUE #1272 - Add issues status logic to separate constants file

--- frontend/constants/issues.ts
+++ frontend/constants/issues.ts
@@ -5,16 +5,31 @@ import AdjustIcon from '@material-ui/icons/Adjust';
 
 export const ISSUE_PANEL_NAME = 'issue';
 
-export const ISSUE_STATUS_COLORS = {
-	NONE: '#777',
-	LOW: '#4CAF50',
-	MEDIUM: '#FF9800',
-	HIGH: '#F44336'
+const ISSUE_STATUSES = {
+	OPEN: 'open',
+	IN_PROGRESS: 'in progress',
+	FOR_APPROVAL: 'for approval',
+  CLOSED: 'closed'
 };
 
-export const ISSUE_STATUS_ICONS = {
-	'open': PanoramaFishEyeIcon,
-	'in progress': LensIcon,
-	'for approval': AdjustIcon,
-	'closed': CheckCircleIcon
+const ISSUE_PRIORITIES = {
+	NONE: 'none',
+	LOW: 'low',
+	MEDIUM: 'medium',
+	HIGH: 'high'
+};
+
+export const ISSUE_STATUSES_COLORS = {
+	[ISSUE_PRIORITIES.NONE]: '#777777',
+	[ISSUE_PRIORITIES.LOW]: '#4CAF50',
+	[ISSUE_PRIORITIES.MEDIUM]: '#FF9800',
+	[ISSUE_PRIORITIES.HIGH]: '#F44336',
+	[ISSUE_STATUSES.CLOSED]: '#0C2F54'
+};
+
+export const ISSUE_STATUSES_ICONS = {
+  [ISSUE_STATUSES.OPEN]: PanoramaFishEyeIcon,
+  [ISSUE_STATUSES.IN_PROGRESS]: LensIcon,
+  [ISSUE_STATUSES.FOR_APPROVAL]: AdjustIcon,
+  [ISSUE_STATUSES.CLOSED]: CheckCircleIcon
 };
--- frontend/helpers/issues.ts
+++ frontend/helpers/issues.ts
@@ -1,10 +1,6 @@
 import { pick, get } from 'lodash';
 import { getAPIUrl } from '../services/api';
-import More from '@material-ui/icons/MoreVert';
-import AdjustIcon from '@material-ui/icons/Adjust';
-import LensIcon from '@material-ui/icons/Lens';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import PanoramaFishEyeIcon from '@material-ui/icons/PanoramaFishEye';
+import { ISSUE_STATUSES_COLORS, ISSUE_STATUSES_ICONS } from '../constants/issues';
 
 export const prepareIssue = (issue, jobs = []) => {
 	const thumbnail = getAPIUrl(issue.thumbnail);
@@ -23,38 +19,10 @@ export const prepareIssue = (issue, jobs = []) => {
 	};
 };
 
-const ISSUE_STATUSES = {
-	OPEN: 'open',
-	IN_PROGRESS: 'in progress',
-	FOR_APPROVAL: 'for approval',
-  CLOSED: 'closed'
-};
-
-const ISSUE_PRIORITIES = {
-	NONE: 'none',
-	LOW: 'low',
-	MEDIUM: 'medium',
-	HIGH: 'high'
-};
-
-const ISSUE_STATUSES_COLORS = {
-	[ISSUE_PRIORITIES.NONE]: '#777777',
-	[ISSUE_PRIORITIES.LOW]: '#4CAF50',
-	[ISSUE_PRIORITIES.MEDIUM]: '#FF9800',
-	[ISSUE_PRIORITIES.HIGH]: '#F44336'
-};
-
-const ISSUE_STATUSES_ICONS = {
-  [ISSUE_STATUSES.OPEN]: PanoramaFishEyeIcon,
-  [ISSUE_STATUSES.IN_PROGRESS]: LensIcon,
-  [ISSUE_STATUSES.FOR_APPROVAL]: AdjustIcon,
-  [ISSUE_STATUSES.CLOSED]: CheckCircleIcon
-};
-
 export const getStatusIcon = (priority, status) => {
   const statusIcon = {
     Icon: ISSUE_STATUSES_ICONS[status] || null,
-    color: ISSUE_STATUSES_COLORS[priority] || null
+    color: ISSUE_STATUSES_COLORS[status] || ISSUE_STATUSES_COLORS[priority] || null
   };
 
   return {...statusIcon};
