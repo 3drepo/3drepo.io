c5cc9146d6915ffdb1052380b4c043067d2d5224
carmenfan <wind5320@hotmail.com>
2019-01-15 17:40:41 +0000

    ISSUE #1320 debugging

--- backend/test/integrated/infrastructure.js
+++ backend/test/integrated/infrastructure.js
@@ -99,6 +99,7 @@ describe("Infrastructure", function () {
 				agent.post(`/testing/${model}/upload`)
 					.attach("file", __dirname + "/../../statics/3dmodels/8000cubes.obj")
 					.expect(500, function(err, res) {
+						console.log(err, res.body);
 						done(err);
 					});
 
