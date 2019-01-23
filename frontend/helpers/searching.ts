37b345b3f9fd72498432ea9818cf5488b69f9986
Daniel Leszczynski <dleszczynski@apptension.com>
2019-01-23 18:03:06 +0100

    ISSUE #1273 - Change filtering logic

--- frontend/helpers/searching.ts
+++ frontend/helpers/searching.ts
@@ -1,4 +1,4 @@
-import { isArray } from 'lodash';
+import { isArray, groupBy, values, get } from 'lodash';
 import { DATA_TYPES } from '../routes/components/filterPanel/filterPanel.component';
 
 export const compareStrings = (string1, string2) => {
@@ -16,30 +16,35 @@ export const stringSearch = (superString: string, subString: string) => {
 
 export const searchByFilters = (items = [], filters = [], returnDefaultHidden = false) => {
 	const prefilteredItems = !returnDefaultHidden ? items.filter(({defaultHidden}) => !defaultHidden) : items;
+	const groupedFilters = groupBy(filters, 'relatedField');
 
 	if (!filters.length) {
 		return prefilteredItems;
 	}
 
 	return prefilteredItems.filter((item) => {
-		return filters.some((filter) => {
-			if (filter.type === DATA_TYPES.UNDEFINED) {
-				const values = isArray(item[filter.relatedField]) ? item[filter.relatedField] : [item[filter.relatedField]];
-				return values.every((value) => {
-					if (typeof value === 'string') {
-						return compareStrings(value, filter.value.value);
+		return values(groupedFilters).every((selectedFilters: any) => {
+				return selectedFilters.some((filter) => {
+					if (filter.type === DATA_TYPES.UNDEFINED) {
+						const filterValue =
+							isArray(item[filter.relatedField]) && !item[filter.relatedField].length ? [''] : item[filter.relatedField];
+						const itemValue = isArray(filterValue) ? filterValue || [''] : [filterValue];
+
+						return itemValue.every((value) => {
+							if (typeof value === 'string') {
+								return !filter.value.value ? filter.value.value === value : compareStrings(value, filter.value.value);
+							}
+							if (typeof value === 'number') {
+								return value === filter.value.value;
+							}
+							return false;
+						});
 					}
-					if (typeof value === 'number') {
-						return value === filter.value.value;
+					if (filter.type === DATA_TYPES.QUERY) {
+						return compareStrings(item.name, filter.value.value) || compareStrings(item.disc, filter.value.value);
 					}
 				});
 			}
-
-			if (filter.type === DATA_TYPES.QUERY) {
-				return compareStrings(item.name, filter.value.value) || compareStrings(item.disc, filter.value.value);
-			}
-
-			return false;
-		});
+		);
 	});
 };
