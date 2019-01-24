29903435f76736a248825d84cba6e18be07abf86
Daniel Leszczynski <dleszczynski@apptension.com>
2019-01-24 12:43:30 +0100

    ISSUE #1272 - Add date field

---
+++ frontend/routes/components/dateField/dateField.component.tsx
@@ -0,0 +1,80 @@
+/**
+ *  Copyright (C) 2017 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as React from 'react';
+import { DatePicker } from 'material-ui-pickers';
+
+import { Container, SuggestionsList, StyledTextField } from './dateField.styles';
+
+interface IProps {
+	inputId: string;
+	value: string;
+	name: string;
+	onChange: (event) => void;
+	onBlur: (event) => void;
+}
+
+interface IState {
+	value: any;
+}
+
+export class DateField extends React.PureComponent<IProps, IState> {
+	public state = {
+		value: ''
+	};
+
+	public componentDidMount() {
+		this.setState({
+			value: this.props.value
+		});
+	}
+
+	public componentDidUpdate(prevProps) {
+		if (!prevProps.value && this.props.value) {
+			this.setState({
+				value: this.props.value
+			});
+		}
+	}
+
+	public handleChange = (newDate) => {
+		if (this.props.onChange) {
+			this.props.onChange({
+				target: { value: newDate, name: this.props.name }
+			});
+		}
+		this.setState({ value: newDate });
+	}
+
+	public render() {
+		const { value } = this.state;
+		const { inputId, onBlur, name } = this.props;
+
+		return (
+			<Container>
+				<DatePicker
+					format="DD/MM/YYYY"
+					value={value}
+					onBlur={onBlur}
+					name={name}
+					onChange={this.handleChange}
+					inputId={inputId}
+				/>
+			</Container>
+		);
+	}
+}
