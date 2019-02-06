856daf21971cf3ffb438dfd6fb2cc598f501da5f
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2019-02-06 18:50:52 +0000

    ISSUE #1370 - Add additional messages to authorisation helpers

--- frontend/modules/auth/auth.helpers.ts
+++ frontend/modules/auth/auth.helpers.ts
@@ -1,10 +1,12 @@
 export const verificationMessages = {
-	success: 'Congratulations. You have successfully signed up for 3D Repo. You may now login to you account.',
+	success: `Congratulations. You have successfully signed up for 3D Repo. You may now login to you account.</div>
+	If you can't see your email in your inbox please check your spam folder or ask IT for assistance`,
 	alreadyVerified: 'You have already verified your account successfully. You may now login to your account.'
 };
 
 export const forgotPasswordMessages = {
-	success: 'Thank you. You will receive an email shortly with a link to change your password.'
+	success: ` Thank you. You will receive an email shortly with a link to change your password.
+	If you can't see your email in your inbox please check your spam folder or ask IT for assistance `
 };
 
 export const changePasswordMessages = {
