59e114bd7843d6ce5167f7f8170841f5335f3059
Santiago Montero <santiago.montero@3drepo.org>
2019-02-06 17:20:16 +0000

    ISSUE #1369 - Fixed tests compatibility issues with new packages

--- backend/test/integrated/bouncerHelper.js
+++ backend/test/integrated/bouncerHelper.js
@@ -13,7 +13,7 @@ let channel = null;
 let errorCode = 0;
 
 
-const logger = new (winston.Logger)({
+const logger =  winston.createLogger({
 	transports: [new (winston.transports.Console)({'timestamp': true}),
 	  new (winston.transports.File)({'filename': conf.logLocation? conf.logLocation : "./bouncer_worker.log"})
 	]
