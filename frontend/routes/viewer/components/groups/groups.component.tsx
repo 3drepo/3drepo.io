4c05e67567499ca44c1bed893c7829e5520e91f6
Daniel Leszczynski <dleszczynski@apptension.com>
2019-02-14 15:49:49 +0100

    ISSUE #1376 - Add basic ui components

--- frontend/routes/viewer/components/groups/groups.component.tsx
+++ frontend/routes/viewer/components/groups/groups.component.tsx
@@ -16,20 +16,71 @@
  */
 
 import * as React from 'react';
-
+import { ViewerPanel } from '../viewerPanel/viewerPanel.component';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import GroupWork from '@material-ui/icons/GroupWork';
+import AddIcon from '@material-ui/icons/Add';
+import { renderWhenTrue } from '../../../../helpers/rendering';
+import { ViewerPanelContent, ViewerPanelFooter, ViewerPanelButton } from '../viewerPanel/viewerPanel.styles';
 import { Container } from './groups.styles';
+import { ListContainer, Summary } from './../risks/risks.styles';
 
 interface IProps {
-	noop: string; // TODO: Remove sample
+	isPending?: boolean;
+	showDetails?: boolean;
+	closeDetails: () => void;
 }
 
 export class Groups extends React.PureComponent<IProps, any> {
+	public renderTitleIcon = () => {
+		if (this.props.showDetails) {
+			return (
+				<IconButton onClick={this.props.closeDetails} >
+					<ArrowBack />
+				</IconButton>
+			);
+		}
+		return <GroupWork />;
+	}
+
+	public renderActions = () => {
+		return [];
+	}
+
+	public renderListView = renderWhenTrue(() => (
+		<>
+			<ViewerPanelContent className="height-catcher">
+				groupsContent
+			</ViewerPanelContent>
+			<ViewerPanelFooter alignItems="center" justify="space-between">
+				<Summary>
+					{`0 groups displayed`}
+				</Summary>
+				<ViewerPanelButton
+					aria-label="Add group"
+					color="secondary"
+					variant="fab"
+				>
+					<AddIcon />
+				</ViewerPanelButton>
+			</ViewerPanelFooter>
+		</>
+	)
+	);
 
 	public render() {
 		return (
-			<Container>
-				Groups component
-			</Container>
+			<ViewerPanel
+			title="Groups"
+			Icon={this.renderTitleIcon()}
+			actions={this.renderActions()}
+			pending={this.props.isPending}
+		>
+			{/* {this.renderFilterPanel(this.props.searchEnabled && !this.props.showDetails)}
+			{this.renderDetailsView(this.props.showDetails)} */}
+			{this.renderListView(!this.props.showDetails)}
+		</ViewerPanel>
 		);
 	}
 }
