6da2d2a63732cef3f34f55946d1ba6e971f580f4
Charence Wong <charencewong@hotmail.com>
2019-03-01 10:33:22 +0000

    ISSUE #1407 - Add canComment flag to new comement form

--- frontend/routes/viewer/components/newCommentForm/newCommentForm.component.tsx
+++ frontend/routes/viewer/components/newCommentForm/newCommentForm.component.tsx
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2017 3D Repo Ltd
+ *  Copyright (C) 2019 3D Repo Ltd
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -39,6 +39,7 @@ import { VIEWER_EVENTS } from '../../../../constants/viewer';
 
 interface IProps {
 	innerRef: any;
+	canComment: boolean;
 	comment?: string;
 	screenshot?: string;
 	viewpoint?: any;
