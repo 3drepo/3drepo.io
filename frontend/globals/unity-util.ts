de073494b88eff441b69e258395045d48760adc3
Carmen Fan <wind5320@hotmail.com>
2019-03-18 17:06:38 +0000

    ISSUE #1417 reinstate reset before loadModel (#1418)

--- frontend/globals/unity-util.ts
+++ frontend/globals/unity-util.ts
@@ -216,7 +216,6 @@ export class UnityUtil {
 	}
 
 	public static toUnity(methodName, requireStatus, params?) {
-
 		if (requireStatus === UnityUtil.LoadingState.MODEL_LOADED) {
 			// Requires model to be loaded
 			UnityUtil.onLoaded().then(() => {
@@ -716,6 +715,7 @@ export class UnityUtil {
 	 *  @param {string} revision - ID of revision
 	 */
 	public static loadModel(account, model, branch, revision) {
+		UnityUtil.reset();
 		const params: any = {
 			database : account,
 			model
