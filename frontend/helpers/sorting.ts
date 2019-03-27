06ae6b5d607150cd79def605a97e4e935c480941
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2019-03-27 22:01:57 +0000

    ISSUE #1441 - Add sortByProperty Helper function

--- frontend/helpers/sorting.ts
+++ frontend/helpers/sorting.ts
@@ -23,3 +23,20 @@ export const sortByField = (data = [], options: {order: any, config: any}) => {
 		options.order
 	);
 };
+
+export const sortByProperty = (property: any) => {
+	let sortOrder = 1;
+
+	if (property[0] === '') {
+		sortOrder = -1;
+		property = property.substr(1);
+
+	}
+	return  (a, b) => {
+		if (sortOrder === -1) {
+			return b[property].localeCompare(a[property]);
+		} else {
+			return a[property].localeCompare(b[property]);
+		}
+	}
+}
