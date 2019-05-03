30a9f498c361cf2b1ce140a2c81dde92742d7d7f
Santiago Montero <santiago.montero@3drepo.org>
2019-05-03 16:00:43 +0100

    ISSUE #1493 - Fixed in firefox height of issue when creating a new one

--- frontend/components/height-setter/js/height-setter.component.ts
+++ frontend/components/height-setter/js/height-setter.component.ts
@@ -69,7 +69,7 @@ class HeightSetterController implements ng.IController, IBindings {
 			this.content.css('max-height', `${this.contentData.height - omittedHeight}px`);
 
 			this.observer.observe(this.content[0], {
-				attributes: false,
+				attributes: true,
 				childList: true,
 				subtree: true,
 				characterData: true
@@ -114,7 +114,7 @@ class HeightSetterController implements ng.IController, IBindings {
 				contentItem: this.contentData,
 				height: requestedHeight
 			});
-		});
+		}, 250);
 	}, 100, { leading: true });
 
 	public handleElementChange = (mutationsList) => {
