111a5ff1e99d3ca5f9f15cefd284a90b0f42b520
carmenfan <wind5320@hotmail.com>
2019-05-03 14:45:39 +0100

    ISSUE #1493 search change

--- frontend/helpers/searching.ts
+++ frontend/helpers/searching.ts
@@ -26,62 +26,64 @@ export const searchByFilters = (
 	if (!filters.length) {
 		return prefilteredItems;
 	}
-	return prefilteredItems.filter((item) => {
+	const res = prefilteredItems.filter((item) => {
 		const filteringResults: any = map(groupedFilters, (selectedFilters: any) => {
 			const filterType = get(selectedFilters[0], 'type', DATA_TYPES.UNDEFINED);
 
-			if (filterType === DATA_TYPES.UNDEFINED) {
-				return selectedFilters.some((filter) => {
-					const filterValue =
-						isArray(item[filter.relatedField]) && !item[filter.relatedField].length ? [''] : item[filter.relatedField];
-					const itemValue = isArray(filterValue) ? filterValue || [''] : [filterValue];
+			switch (filterType) {
+				case DATA_TYPES.UNDEFINED:
+					return selectedFilters.some((filter) => {
+						const filterValue =
+							isArray(item[filter.relatedField]) && !item[filter.relatedField].length ? [''] : item[filter.relatedField];
+						const itemValue = isArray(filterValue) ? filterValue || [''] : [filterValue];
 
-					return itemValue.every((value) => {
-						if (typeof value === 'string') {
-							return !filter.value.value ? filter.value.value === value : compareStrings(value, filter.value.value);
-						}
-						if (typeof value === 'number') {
-							return value === filter.value.value;
-						}
-						return false;
+						return itemValue.every((value) => {
+							if (typeof value === 'string') {
+								return !filter.value.value ? filter.value.value === value : compareStrings(value, filter.value.value);
+							}
+							if (typeof value === 'number') {
+								return value === filter.value.value;
+							}
+							return false;
+						});
 					});
-				});
-			}
+					break;
+				case DATA_TYPES.QUERY:
+					return selectedFilters.some((filter) => {
+						const logFound = item.comments && item.comments.length ? item.comments.some(({ comment }) => {
+							if (comment) {
+								return compareStrings(comment, filter.value.value);
+							}
+							return false;
+						}) : false;
 
-			if (filterType === DATA_TYPES.QUERY) {
-				return selectedFilters.some((filter) => {
-					const logFound = item.comments.length ? item.comments.some(({ comment }) => {
-						if (comment) {
-							return compareStrings(comment, filter.value.value);
-						}
-						return false;
-					}) : false;
-
-					return logFound || queryFields.some((field) => {
-						return compareStrings(item[field], filter.value.value);
+						return logFound || queryFields.some((field) => {
+							return compareStrings(item[field], filter.value.value);
+						});
 					});
-				});
-			}
+					break;
+				case DATA_TYPES.DATE:
+					return selectedFilters.every((filter) => {
+						const itemValue = item[filter.relatedField];
+						if (!itemValue) {
+							return false;
+						}
 
-			if (filterType === DATA_TYPES.DATE) {
-				return selectedFilters.every((filter) => {
-					const itemValue = item[filter.relatedField];
-					if (!itemValue) {
-						return false;
-					}
+						const boundryType = filter.value.value;
+						const boundryValue = filter.value.date;
 
-					const boundryType = filter.value.value;
-					const boundryValue = filter.value.date;
+						if (boundryType === 'from') {
+							return itemValue >= boundryValue;
+						}
 
-					if (boundryType === 'from') {
-						return itemValue >= boundryValue;
-					}
-
-					return itemValue <= boundryValue;
-				});
+						return itemValue <= boundryValue;
+					});
+					break;
 			}
 		});
 
 		return filteringResults.every((result) => result);
 	});
+	console.log(res);
+	return res;
 };
