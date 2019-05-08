9b61704d8a7ee19b05f1a4f98e0e8c0d48f12d74
carmenfan <wind5320@hotmail.com>
2019-05-08 11:11:30 +0100

    ISSUE #1497 set maxlength to 120

--- frontend/routes/viewer/components/previewDetails/previewDetails.component.tsx
+++ frontend/routes/viewer/components/previewDetails/previewDetails.component.tsx
@@ -116,7 +116,7 @@ export class PreviewDetails extends React.PureComponent<IProps, any> {
 						error={Boolean(form.errors.name)}
 						helperText={form.errors.name}
 						inputProps={
-    						{maxLength: 50}
+    						{maxLength: 120}
 						}
 					/>
 				)} />
