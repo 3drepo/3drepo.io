5611a7de328ed7581a40f580e5ff0d48f3d76ffb
Daniel Leszczynski <dleszczynski@apptension.com>
2019-06-27 09:24:32 +0200

    ISSUE #1577 - Fix styles indentations

--- frontend/routes/components/revisionsSwitch/revisionsSwitch.styles.ts
+++ frontend/routes/components/revisionsSwitch/revisionsSwitch.styles.ts
@@ -19,36 +19,36 @@ import styled from 'styled-components';
 import { COLOR, FONT_WEIGHT } from '../../../styles';
 
 export const Container = styled.div`
-  z-index: 2;
-  position: absolute;
-  color: ${COLOR.WHITE};
-  top: 60px;
-  left: 50%;
-  transform: translateX(-50%);
-  text-align: center;
-  cursor: pointer;
-  outline: none;
-  margin-top: 10px;
-  background-color: ${COLOR.REGENT_GRAY};
-  border-radius: 25px;
-  box-shadow: 0 3px 3px ${COLOR.BLACK_16};
-  padding: 0 12px;
-  height: 36px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 200px;
+	z-index: 2;
+	position: absolute;
+	color: ${COLOR.WHITE};
+	top: 60px;
+	left: 50%;
+	transform: translateX(-50%);
+	text-align: center;
+	cursor: pointer;
+	outline: none;
+	margin-top: 10px;
+	background-color: ${COLOR.REGENT_GRAY};
+	border-radius: 25px;
+	box-shadow: 0 3px 3px ${COLOR.BLACK_16};
+	padding: 0 12px;
+	height: 36px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-width: 200px;
 `;
 
 export const DisplayedText = styled.div`
-  font-weight: ${FONT_WEIGHT.NORMAL};
-  font-size: 14px;
-  text-shadow: none;
-  white-space: nowrap;
-  display: flex;
-  align-items: center;
+	font-weight: ${FONT_WEIGHT.NORMAL};
+	font-size: 14px;
+	text-shadow: none;
+	white-space: nowrap;
+	display: flex;
+	align-items: center;
 `;
 
 export const ProgressWrapper = styled.div`
-  color: ${COLOR.WHITE};
+	color: ${COLOR.WHITE};
 `;
