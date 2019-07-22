999052e0aff137e8fe79fe42140516a87949d2ca
carmenfan <wind5320@hotmail.com>
2019-07-22 17:21:45 +0100

    ISSUE #1608 show void on branch list

--- backend/routes/history.js
+++ backend/routes/history.js
@@ -139,7 +139,9 @@ function listRevisions(req, res, next) {
 	const branch = req.params.branch;
 	const showVoid = req.query.showVoid !== undefined;
 
-	History.listByBranch({account, model}, branch || null, {_id : 1, tag: 1, timestamp: 1, desc: 1, author: 1}, showVoid).then(histories => {
+	const projection = {_id : 1, tag: 1, timestamp: 1, desc: 1, author: 1, void: 1};
+
+	History.listByBranch({account, model}, branch || null, projection, showVoid).then(histories => {
 
 		histories = History.clean(histories);
 
