429bc33d0d48b2dab90a45b8befc8237a7f022b9
Charence Wong <charencewong@hotmail.com>
2019-07-26 12:52:20 +0100

    ISSUE #1600 - Fix getIssuePinColor

--- frontend/helpers/issues.ts
+++ frontend/helpers/issues.ts
@@ -73,10 +73,8 @@ export const getStatusIcon = (priority, status) => {
 };
 
 export const getIssuePinColor = (status: string, priority: string, selected: boolean = false) => {
-        const colorToUse = ISSUE_COLORS[status] || ISSUE_COLORS[priority] || ISSUE_COLORS.NONE;
+	const colorToUse = ISSUE_COLORS[status] || ISSUE_COLORS[priority] || ISSUE_COLORS.NONE;
 	return (selected) ? colorToUse.selectedColor : colorToUse.pinColor;
-		? (ISSUE_COLORS[status] ? ISSUE_COLORS[status].selectedColor : ISSUE_COLORS[priority].selectedColor)
-		: (ISSUE_COLORS[status] ? ISSUE_COLORS[status].pinColor : ISSUE_COLORS[priority].pinColor);
 };
 
 const isOpenIssue = (status) => status !== 'closed';
