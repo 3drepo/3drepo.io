24195d036038aebce3f4a4479b800551810d9a02
carmenfan <wind5320@hotmail.com>
2019-07-26 12:21:18 +0100

    update docs

--- api_project.js
+++ api_project.js
@@ -1,13 +1,13 @@
 define({
   "name": "3drepo.io",
-  "version": "3.4.0",
+  "version": "3.10.0",
   "description": "The backend for 3drepo.io",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-04T11:29:54.914Z",
+    "time": "2019-07-26T11:03:04.647Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
