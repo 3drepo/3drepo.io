4549a602f4b22794abbe498bab9c4bb8da281853
carmenfan <wind5320@hotmail.com>
2019-08-12 11:30:08 +0100

    ISSUE #1643 fix linter

--- frontend/services/viewer/pin.ts
+++ frontend/services/viewer/pin.ts
@@ -18,7 +18,7 @@ import { UnityUtil } from '../../globals/unity-util';
 import { PIN_COLORS } from '../../styles';
 
 export class Pin {
-	public id;
+	public id: string;
 	public type: string;
 	public position;
 	public highlighted: boolean;
@@ -37,7 +37,7 @@ export class Pin {
 	public coneDepth;
 
 	constructor(
-		id: , type: string,  position , norm, colors, viewpoint,
+		id: string, type: string,  position , norm, colors, viewpoint,
 		account: string, model: string
 	) {
 
