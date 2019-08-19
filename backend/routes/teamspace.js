ff172c1073d423da28d4331308c75d3c29316b19
Santiago Montero <santiago.montero@3drepo.org>
2019-08-19 18:25:24 +0100

    ISSUE #1543 - Update apiDOCS for /routes/teamspace

--- backend/routes/teamspace.js
+++ backend/routes/teamspace.js
@@ -27,88 +27,270 @@
 	const utils = require("../utils");
 
 	/**
-	 * @api {get} /:teamspace/quota Get Quota Information
-	 * @apiName getQuotaInfo
 	 * @apiGroup Teamspace
+	 * @apiName getQuotaInfo
+	 *
+	 * @api {get} /:teamspace/quota Get Quota Information
+	 *
+	 * @apiDescription It returns the quota information. Each teamspace has a space limit and a limit of collaborators.
+	 * The values returned are  space used (both these values are in bytes) and the collaborator limit.
+	 * If spaceLimit or collaboratorLimit are nulled it means that there are no space limit/member limit.
+	 *
+	 * @apiPermission teamSpaceAdmin
+	 *
+	 * @apiExample {get} Example usage:
+	 * GET /teamSpace1/quota HTTP/1.1
 	 *
 	 * @apiParam {String} teamspace Name of teamspace
+	 *
+	 * @apiSuccessExample {json} Success
+	 * HTTP/1.1 200 OK
+	 * {
+	 *     spaceLimit: 1048576,
+	 *	   collaboratorLimit: 12,
+	 *     spaceUsed: 2048
+	 * }
+	 *
 	 */
 	router.get("/quota", middlewares.isAccountAdmin, getQuotaInfo);
 
 	/**
-	 * @api {get} /:teamspace/members Get Member List
-	 * @apiName getMemberList
 	 * @apiGroup Teamspace
+	 * @apiName getMemberList
+	 *
+	 * @api {get} /:teamspace/members Get members list
+	 *
+	 * @apiDescription It returns a list of members identifying which of them are teamspace administrators, and their jobs.
+	 *
+	 * @apiPermission teamSpaceMember
 	 *
 	 * @apiParam {String} teamspace Name of teamspace
+	 *
+	 * @apiExample {get} Example usage:
+	 * GET /teamSpace1/members HTTP/1.1
+	 *
+	 * @apiSuccessExample {json} Success
+	 * HTTP/1.1 200 OK
+	 * {
+	 *    members: [
+	 *       {
+	 *          user: "teamSpace1",
+	 *          firstName: "Teamspace",
+	 *          lastName: "One",
+	 *          company: "Teamspace one",
+	 *          permissions: [
+	 *             "teamspace_admin"
+ 	 *          ],
+ 	 *          job: "jobA",
+	 *          isCurrentUser: true
+	 *       },
+	 *       {
+	 *          user: "unassignedTeamspace1UserJobA",
+ 	 *          firstName: "John",
+ 	 *          lastName: "Williams",
+ 	 *          company: "Teamspace One",
+	 *          permissions: [],
+	 *          job: "jobA",
+	 *          isCurrentUser: false
+	 *       },
+	 *       {
+	 *          user: "viewerTeamspace1Model1JobB",
+	 *          firstName: "Alice",
+	 *          lastName: "Stratford",
+	 *          company: "Teamspace one",
+	 *          permissions: [],
+	 *          job: "jobB",
+	 *          isCurrentUser: false
+	 *       }
+	 *    ]
+	 * }
+	 *
 	 */
 	router.get("/members", middlewares.isTeamspaceMember, getMemberList);
 
 	/**
-	 * @api {get} /:teamspace/members Get Member List
-	 * @apiName getMemberList
 	 * @apiGroup Teamspace
+	 * @apiName getBillingInfo
+	 *
+	 * @api {get} /:teamspace/billingInfo Get billing info
+	 *
+	 * @apiDescription It returns the teamspace billing info.
+	 *
+	 * @apiPermission teamSpaceAdmin
 	 *
 	 * @apiParam {String} teamspace Name of teamspace
+	 *
+	 * @apiExample {get} Example usage:
+	 * GET /teamSpace1/billingInfo HTTP/1.1
+	 *
+	 * @apiSuccessExample {json} Success
+	 * HTTP/1.1 200 OK
+	 * {
+	 *    vat: "GB 365684514",
+	 *    line1: "10 Downing Street",
+	 *    postalCode: "SW1A 2AA",
+	 *    city: "London",
+	 *    company: "Teamspace one",
+	 *    countryCode: "GB",
+	 *    lastName: "Voorhees",
+	 *    firstName: "Jason"
+	 * }
+	 *
 	 */
 	router.get("/billingInfo", middlewares.isAccountAdmin, getBillingInfo);
 
 	/**
-	* @api {get} /members/:user Get Member details
-	* @apiName getMemberList
-	* @apiGroup Teamspace
-	*/
-	router.get("/members/:user", middlewares.isTeamspaceMember, getTeamMemberInfo);
-
-	/**
-	* @api {get} /members/:user Get Member details
-	* @apiName getMemberList
-	* @apiGroup Teamspace
-	*/
-	router.get("/members/:user", middlewares.isTeamspaceMember, getTeamMemberInfo);
-
-	/**
-	 * @api {delete} /:teamspace/members/:user Remove a team member
-	 * @apiName removeTeamMember
 	 * @apiGroup Teamspace
+	 * @apiName getMemberInfo
+	 *
+	 * @api {get} /:teamspace/members/:user Get member's info
+	 *
+	 * @apiDescription It returns the teamspace's member small info .
+	 *
+	 * @apiPermission teamSpaceMember
+	 * @apiParam {String} teamspace Name of teamspace
+	 * @apiParam {String} user The username of the user you wish to query
+	 *
+	 * @apiExample {get} Example usage:
+	 * GET /teamSpace1/members/viewerTeamspace1Model1JobB HTTP/1.1
+	 *
+	 * @apiSuccessExample {json} Success
+	 * HTTP/1.1 200 OK
+	 * {
+	 *    user: "viewerTeamspace1Model1JobB",
+	 *    firstName: "Alice",
+	 *    lastName: "Stratford",
+	 *    company: "Teamspace one"
+	 * }
+	 */
+	router.get("/members/:user", middlewares.isTeamspaceMember, getTeamMemberInfo);
+
+	/**
+	 * @apiGroup Teamspace
+	 * @apiName removeTeamMember
+	 *
+	 * @api {delete} /:teamspace/members/:user Removes a user from the teamspace
+	 * @apiPermission teamSpaceAdmin
 	 *
 	 * @apiParam {String} teamspace Name of teamspace
-	 * @apiParam {String} user User (Member) to remove
+	 * @apiParam {String} user Username of the member to remove
+	 *
+	 * @apiExample {delete} Example usage:
+	 * DELETE /teamSpace1/members/viewerTeamspace1Model1JobB HTTP/1.1
+	 *
+	 * @apiSuccessExample {json} Success
+	 * HTTP/1.1 200 OK
+	 * {
+	 *    user: "viewerTeamspace1Model1JobB",
+	 * }
 	 */
-
 	router.delete("/members/:user", middlewares.isAccountAdmin, removeTeamMember);
 
-	/**
-	 * @api {get} /:teamspace/members/search/:searchString Search for a member without a membership
-	 * @apiName findUsersWithoutMembership
-	 * @apiGroup Teamspace
-	 *
-	 * @apiParam {String} searchString Search string provided to find member
-	 */
+	// 	 * /teamSpace1/members/search/imProjectAdmin@3drepo.org
 
+	/**
+	 * @apiGroup Teamspace
+	 * @apiName findUsersWithoutMembership
+	 *
+	 * @api {get} /:teamspace/members/search/:searchString Search for non-members
+	 *
+	 * @apiDescription It returns a list of users that dont belong to the teamspace and that their usernames matches partially with the string and if entered an email
+	 * it only matches if the string is the entire email address.
+	 *
+	 * In the result it's included their username, first name, last name, company and roles in other teamspaces.
+	 *
+	 * @apiPermission teamSpaceAdmin
+	 *
+	 * @apiParam {String} search Search string provided to find member
+	 *
+	 * @apiExample {get} Example usage:
+	 * GET /teamSpace1/members/search/project HTTP/1.1
+	 *
+	 * @apiSuccessExample {json} Success
+	 * HTTP/1.1 200 OK
+	 *
+	 * [
+	 *    {
+	 *       user: "projectowner",
+	 *       roles: [
+	 *          {
+	 *             role: "team_member",
+	 *             db: "projectowner"
+	 *          }
+	 *       ],
+	 *       firstName: "Project",
+	 *       lastName: "Owner",
+	 *       company: null
+	 *    },
+	 *    {
+	 *       user: "projectshared",
+	 *       roles: [
+	 *          {
+	 *             role: "team_member",
+	 *             db: "projectshared"
+	 *          }
+	 *       ],
+	 *       firstName: "Drink",
+	 *       lastName: "Coffee",
+	 *       company: null
+	 *    },
+	 *    {
+	 *       user: "project_username",
+	 *       roles: [
+	 *          {
+	 *             role: "team_member",
+	 *             db: "project_username"
+	 *          }
+	 *       ],
+	 *       firstName: "George",
+	 *       lastName: "Crown",
+	 *        company: null
+	 *    },
+	 * ]
+	 *
+	 */
 	router.get("/members/search/:searchString", middlewares.isAccountAdmin, findUsersWithoutMembership);
 
 	/**
-	 * @api {post} /:teamspace/members Create a Team Member
-	 * @apiName addTeamMember
 	 * @apiGroup Teamspace
+	 * @apiName addTeamMember
+	 *
+	 * @api {post} /:teamspace/members Add a team member
+	 * @apiPermission teamSpaceAdmin
 	 *
 	 * @apiParam {String} teamspace Name of teamspace
 	 * @apiParam {String} searchString Search string required to find team member.
 	 *
-	 * @apiSuccess (200) {Object} Team member profile
+	 * @apiExample {post} Example usage:
+	 * POST /teamSpace1/members HTTP/1.1
+	 * {
+	 *    job: "jobA",
+	 *    user: "projectshared",
+	 *    permissions: [
+	 *    ]
+	 * }
+	 *
+	 * @apiSuccessExample {json} Success
+	 * {
+	 *    job: "jobA",
+	 *    permissions: [
+	 *    ],
+	 *    user: "projectshared",
+	 *    firstName: "Drink",
+	 *    lastName: "Coffee",
+	 *    company: null
+	 * }
 	 *
 	 * @apiError User not found The <code>searchString</code> of the User was not found.
-	 * @apiErrorExample
+	 * @apiErrorExample Error
 	 * {
-	 *		"message": "User not found",
-	 *		"status": 404,
-	 *		"code": "USER_NOT_FOUND",
-	 *		"value": 1,
-	 *		"place": "POST /members"
+	 *		message: "User not found",
+	 *		status: 404,
+	 *		code: "USER_NOT_FOUND",
+	 *		value: 1,
+	 *		place: "POST /members"
 	 * }
 	 */
-
 	router.post("/members", middlewares.isAccountAdmin, addTeamMember);
 
 	function getBillingInfo(req, res, next) {
