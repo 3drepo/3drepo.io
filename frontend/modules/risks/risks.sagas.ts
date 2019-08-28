87ebe6deeb3b3b37a5b3feb5a3f48e8ffd252c8e
sanmont3drepo <42032433+sanmont3drepo@users.noreply.github.com>
2019-08-28 12:16:34 +0100

    ISSUE #1648 - Now risk likehood and consequence are editable after the risk was created (#1658)

--- frontend/modules/risks/risks.sagas.ts
+++ frontend/modules/risks/risks.sagas.ts
@@ -149,7 +149,7 @@ export function* saveRisk({ teamspace, model, riskData, revision, finishSubmitti
 		yield put(RisksActions.saveRiskSuccess(preparedRisk));
 		yield put(SnackbarActions.show('Risk created'));
 	} catch (error) {
-		yield put(DialogActions.showErrorDialog('save', 'risk', error));
+		yield put(DialogActions.showEndpointErrorDialog('save', 'risk', error));
 	}
 }
 
@@ -171,7 +171,7 @@ export function* updateRisk({ teamspace, modelId, riskData }) {
 		yield put(RisksActions.saveRiskSuccess(preparedRisk));
 		yield put(SnackbarActions.show('Risk updated'));
 	} catch (error) {
-		yield put(DialogActions.showErrorDialog('update', 'risk', error));
+		yield put(DialogActions.showEndpointErrorDialog('update', 'risk', error));
 	}
 }
 
