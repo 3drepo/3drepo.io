ce03d86e635b011839b574d3afdc8d1faa618f81
carmenfan <wind5320@hotmail.com>
2019-11-01 17:00:00 +0000

    ISSUE #1767 use nRevisions instead of timestamp

--- frontend/routes/teamspaces/components/modelGridItem/modelGridItem.component.tsx
+++ frontend/routes/teamspaces/components/modelGridItem/modelGridItem.component.tsx
@@ -157,8 +157,8 @@ export const ModelGridItem = memo((props: IProps) => {
 	};
 
 	const handleClick = useCallback((e) => {
-		const { history, teamspace, timestamp, model } = props;
-		if (timestamp) {
+		const { history, teamspace, nRevisions, model } = props;
+		if (nRevisions > 0) {
 			history.push(`${ROUTES.VIEWER}/${teamspace}/${model}`);
 			analyticsService.sendEvent(EVENT_CATEGORIES.MODEL, EVENT_ACTIONS.VIEW);
 		} else {
