1eafcde51b1780664d88929b63c99620e34b51ef
carmenfan <wind5320@hotmail.com>
2019-11-05 11:18:16 +0000

    ISSUE #1777 use hasExpandable for UI related stuff, hasChildren should be length of children array > 0

--- frontend/modules/tree/treeProcessing/transforming.ts
+++ frontend/modules/tree/treeProcessing/transforming.ts
@@ -32,6 +32,7 @@ const getFlattenNested = (tree, maps, data = [], idx = 0, level = 1, parentId = 
 		parentId,
 		rootParentId,
 		hasChildren: Boolean(tree.children),
+		expandable: false,
 		deepChildrenNumber: 0,
 		childrenIds: [],
 		subTreeRoots: []
@@ -44,8 +45,9 @@ const getFlattenNested = (tree, maps, data = [], idx = 0, level = 1, parentId = 
 	let subTreeRoots = [];
 	if (tree.children) {
 
-		const hasChildren = tree.children.some((child) => Boolean(child.name));
+		const hasChildren = tree.children.length;
 		rowData.hasChildren = hasChildren;
+		rowData.expandable = tree.children.some((child) => Boolean(child.name));
 		rootParentId = rowData.isModel ? nodeID : rootParentId;
 
 		let nHiddenChildren = 0;
