9e20c42181b9e1fcd1fa3c3bf5a6bd1ed90b9c05
carmenfan <wind5320@hotmail.com>
2019-12-09 14:21:31 +0000

    ISSUE #1832 new Users reported

--- scripts/reporting/statsGen/dbStats.js
+++ scripts/reporting/statsGen/dbStats.js
@@ -1,15 +1,78 @@
 const dateFormat = require('dateformat');
 const fs = require('fs');
 
+"use strict";
+
 DBStats = {};
 
-const generateFileName = () => {
-	const date = dateFormat(new Date(), "dd-mm-yy");
-	return `dbStats_${date}.csv`;
+const writeNewUserEntry = (stream, currentY, currentM, count, total, nextM, nextY) => {
+	if(currentM !== -1) {
+		if(nextY && nextM) {
+			let month = currentM;
+			let year = currentY;
+			let counter = count;
+			do{
+				stream.write(`${month},${year},${counter},${total}\n`);
+				counter = 0;
+				year = month == 12? year + 1 : year;
+				month = month + 1 > 12? 1 : month+1;
+				if(year > 2018) break;
+			} while(!(month === nextM && year === nextY));
+		} else {
+			stream.write(`${currentM},${currentY},${count},${total}\n`);
+		}
+	}
+
+}
+
+const reportNewUsersPerMonth = async (dbConn, stream) => {
+	const col = await dbConn.db("admin").collection("system.users");
+	const users = await col.find(
+		{"customData.inactive" : {"$ne": true}, "customData.createdAt": {"$exists": true}},
+	).sort({"customData.createdAt" : 1}).toArray();
+
+	stream.write("NEW USERS PER MONTH\n");
+	stream.write("Month, Year, Count, Total\n");
+	let currentMonth = -1, currentYear = -1, currentCount = 0, total = 0;
+	users.forEach((user) => {
+		const createdAt = user.customData.createdAt;
+		const month = createdAt.getMonth() + 1;
+		const year = createdAt.getFullYear();
+
+		if(currentMonth === month && currentYear === year) {
+			++currentCount;
+		} else {
+			total += currentCount;
+			writeNewUserEntry(stream, currentYear, currentMonth, currentCount, total, month, year);
+
+			currentMonth = month;
+			currentYear = year;
+			currentCount = 1;
+		}
+	});
+
+	if(currentMonth !== -1) {
+		total += currentCount;
+		writeNewUserEntry(stream, currentYear, currentMonth, currentCount, total);
+	}
+}
+
+const createReport = async(dbConn, stream) => {
+	await reportNewUsersPerMonth(dbConn, stream);
 }
 
 DBStats.createDBReport = async (dbConn) => {
-	console.log("Creating DB statistics report...");
+	console.log("[DB] Creating DB statistics report...");
+	const fname = Utils.generateFileName("dbStats");
+	const writeStream = fs.createWriteStream(fname);
+	writeStream.once('open', () => {
+		createReport(dbConn, writeStream).then(() => {
+			console.log("[DB] Generated. DBStats written to", fname);
+			writeStream.end();
+		}).catch((err) => {
+			console.error("[DB] Failed to create report", err);
+		});
+	});
 
 }
 
--- scripts/reporting/statsGen/userList.js
+++ scripts/reporting/statsGen/userList.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const Utils = require(`./utils.js`);
 
+"use strict";
+
 UserList = {};
 
 UserList.createUsersReport = async  (dbConn) => {
-	console.log("Creating users list...");
+	console.log("[USERS] Creating users list...");
 
 	const db = dbConn.db("admin");
 
@@ -14,7 +16,7 @@ UserList.createUsersReport = async  (dbConn) => {
 	const fname = Utils.generateFileName("usersList");
 	const writeStream = fs.createWriteStream(fname);
 	writeStream.once('open', () => {
-		writeStream.write("Username,Email,FirstName,Last Name,Country,Company,Date Created,Mail Optout,Verified\n");
+		writeStream.write("Username,Email,First Name,Last Name,Country,Company,Date Created,Last Login,Mail Optout,Verified\n");
 		users.forEach((user) => {
 			if(!Utils.skipUser(user.user) && user.customData) {
 				writeStream.write(`${user.user},${user.customData.email},${user.customData.firstName},${user.customData.lastName},`);
@@ -26,11 +28,13 @@ UserList.createUsersReport = async  (dbConn) => {
 				else {
 					writeStream.write(",,");
 				}
-				if(user.customData.createdAt) {
-					writeStream.write(Utils.formatDate(user.customData.createdAt));
-				}
 
+				user.customData.createdAt && writeStream.write(Utils.formatDate(user.customData.createdAt));
 				writeStream.write(",");
+
+				user.customData.lastLoginAt && writeStream.write(Utils.formatDate(user.customData.lastLoginAt));
+				writeStream.write(",");
+
 				user.customData.mailListOptOut && writeStream.write("Yes"),
 				user.customData.inactive && writeStream.write(",No"),
 
@@ -39,7 +43,7 @@ UserList.createUsersReport = async  (dbConn) => {
 
 
 		});
-		console.log("Users stats written to", fname);
+		console.log("[USERS] users list generated, written to ", fname);
 		writeStream.end();
 	});
 
