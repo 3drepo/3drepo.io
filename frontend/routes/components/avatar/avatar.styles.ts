c843db109742aff790c6de1b488f3611820469fe
Michał Kubiak <mkubiak@apptension.com>
2020-01-17 09:56:18 +0100

    ISSUE #1859 - Remove additional outline from avatar image

--- frontend/routes/components/avatar/avatar.styles.ts
+++ frontend/routes/components/avatar/avatar.styles.ts
@@ -34,7 +34,6 @@ export const Container = styled.div`
 	height: ${getSize}px;
 	min-width: ${getSize}px;
 	font-size: ${(props: any) => props.fontSize || DEFAULT_FONT_SIZE}px;
-	margin: -1px;
 
 	${StyledIcon} {
 		font-size: ${(props: any) => getSize(props) * 0.6}px;
@@ -47,7 +46,6 @@ export const StyledAvatar = styled(Avatar)`
 		height: 100%;
 		background: #e8eaf6;
 		color: ${COLOR.PRIMARY_MAIN};
-		border: 1px solid #e8eaf6;
 		font-size: inherit;
 	}
 `;
