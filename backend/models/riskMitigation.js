e61f777f5ffbeba00c879e6a6ecbdace578811c2
Charence Wong <charencewong@hotmail.com>
2020-02-07 12:23:38 +0000

    ISSUE #1847 - Get criteria for risk from mitigations

---
+++ backend/models/riskMitigation.js
@@ -0,0 +1,73 @@
+/**
+ *  Copyright (C) 2020 3D Repo Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.ap
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+"use strict";
+
+const db = require("../handler/db");
+const responseCodes = require("../response_codes.js");
+const utils = require("../utils");
+const TeamspaceSettings = require("./teamspaceSetting");
+
+class RiskMitigation {
+	getRiskMitigationCollection(account) {
+		return db.getCollection(account, "mitigations");
+	}
+
+	async getCriteria(account) {
+		const criteriaFields = [
+			"associated_activity",
+			"category",
+			"element",
+			"location_desc",
+			"mitigation_stage",
+			"mitigation_type",
+			"risk_factor",
+			"scope"
+		];
+
+		const mitigationColl = await this.getRiskMitigationCollection(account);
+		const criteriaPromises = [];
+		const criteria = {};
+
+		// Get teamspace categories
+		let teamspaceCategories = await TeamspaceSettings.getRiskCategories(account);
+		teamspaceCategories = teamspaceCategories.map(x => x.label);
+
+		criteriaFields.forEach((field) => {
+			const promise = mitigationColl.distinct(field, {});
+			criteriaPromises.push(promise);
+			promise.then((values) => {
+				criteria[field] = values;
+			});
+		});
+
+		return Promise.all(criteriaPromises).then(() => {
+			// Append teamspace categories
+			criteria['category'] = [...new Set(Object.assign([], criteria.category).concat(teamspaceCategories))];
+
+			return criteria;
+		});
+	}
+
+	async findMitigationSuggestions(account, criteria) {
+		const mitigationColl = await this.getRiskMitigationCollection(account);
+		let suggestions = await mitigationColl.find({}).toArray();
+
+		return suggestions;
+	}
+}
+
+module.exports = new RiskMitigation();
--- backend/routes/riskMitigation.js
+++ backend/routes/riskMitigation.js
@@ -20,6 +20,7 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const responseCodes = require("../response_codes.js");
 const utils = require("../utils");
+const RiskMitigation = require("../models/riskMitigation");
 
 /**
  * @api {get} /:teamspace/mitigations/criteria Get mitigation criteria
@@ -138,192 +139,25 @@ router.post("/mitigations", findMitigationSuggestions);
 
 function findMitigationSuggestions(req, res, next) {
 	const place = utils.APIInfo(req);
+	const {account} = req.params;
+	const criteria = req.body;
 
-	const suggestions = [
-		{
-			"mitigation_desc":"Replace all openings required in floor slabs with precast  service openings.",
-			"mitigation_details":"Replace all openings required in floor slabs with precast  service openings.Replace all openings required in floor slabs with precast  service openings.Replace all openings required in floor slabs with precast  service openings.",
-			"mitigation_stage":"Preliminary Design",
-			"mitigation_type":"Eliminate"
-		},
-		{
-			"mitigation_desc":"Cast in mesh in openings to reduce risk of person falling through.",
-			"mitigation_details":"Cast in mesh in openings to reduce risk of person falling through.Cast in mesh in openings to reduce risk of person falling through.Cast in mesh in openings to reduce risk of person falling through.",
-			"mitigation_stage":"Preliminary Design",
-			"mitigation_type":"Reduce"
-		},
-		{
-			"mitigation_desc":"Group small floor openings together to create one large opening.",
-			"mitigation_details":"Group small floor openings together to create one large opening.Group small floor openings together to create one large opening.Group small floor openings together to create one large opening.",
-			"mitigation_stage":"Group small floor openings together to create one large opening.",
-			"mitigation_type":"Control"
-		},
-		{
-			"mitigation_desc":"Locate floor openings away from passageways, work areas, and the structure perimeter.",
-			"mitigation_details":"Locate floor openings away from passageways, work areas, and the structure perimeter.Locate floor openings away from passageways, work areas, and the structure perimeter.Locate floor openings away from passageways, work areas, and the structure perimeter.",
-			"mitigation_stage":"Preliminary Design",
-			"mitigation_type":"Control"
-		},
-		{
-			"mitigation_desc":"Structural engineer liaises with architects during design phase to assist in design of handrail and guard elements.",
-			"mitigation_details":"Structural engineer liaises with architects during design phase to assist in design of handrail and guard elements.Structural engineer liaises with architects during design phase to assist in design of handrail and guard elements.Structural engineer liaises with architects during design phase to assist in design of handrail and guard elements.",
-			"mitigation_stage":"Preliminary Design",
-			"mitigation_type":"Inform"
-		},
-		{
-			"mitigation_desc":"Avoid low walls in circulation areas. Avoid hidden alcoves and offsets.",
-			"mitigation_details":"Avoid low walls in circulation areas. Avoid hidden alcoves and offsets.Avoid low walls in circulation areas. Avoid hidden alcoves and offsets.Avoid low walls in circulation areas. Avoid hidden alcoves and offsets.",
-			"mitigation_stage":"Detail Design",
-			"mitigation_type":"Eliminate"
-		},
-		{
-			"mitigation_desc":"Provide safe walking surface joint covers. Any covering should be: strong enough to support any loads likely to be placed on it ; and fixed in position to prevent accidental dislodgement.",
-			"mitigation_details":"Provide safe walking surface joint covers. Any covering should be: strong enough to support any loads likely to be placed on it ; and fixed in position to prevent accidental dislodgement.Provide safe walking surface joint covers. Any covering should be: strong enough to support any loads likely to be placed on it ; and fixed in position to prevent accidental dislodgement.Provide safe walking surface joint covers. Any covering should be: strong enough to support any loads likely to be placed on it ; and fixed in position to prevent accidental dislodgement.",
-			"mitigation_stage":"Detail Design",
-			"mitigation_type":"Reduce"
-		},
-		{
-			"mitigation_desc":"Locate openings away from building edges.",
-			"mitigation_details":"Locate openings away from building edges.Locate openings away from building edges.Locate openings away from building edges.",
-			"mitigation_stage":"Detail Design",
-			"mitigation_type":"Reduce"
-		},
-		{
-			"mitigation_desc":"Design permanent grating in opening, to be installed when opening is created during construction.",
-			"mitigation_details":"Design permanent grating in opening, to be installed when opening is created during construction.Design permanent grating in opening, to be installed when opening is created during construction.Design permanent grating in opening, to be installed when opening is created during construction.",
-			"mitigation_stage":"Detail Design",
-			"mitigation_type":"Control"
-		},
-		{
-			"mitigation_desc":"Specify guardrail systems around floor openings except at the entrance to stairways.",
-			"mitigation_details":"Specify guardrail systems around floor openings except at the entrance to stairways.Specify guardrail systems around floor openings except at the entrance to stairways.Specify guardrail systems around floor openings except at the entrance to stairways.",
-			"mitigation_stage":"Detail Design",
-			"mitigation_type":"Control"
-		},
-		{
-			"mitigation_desc":"All steel columns should have a hole in which to install guardrails and lifelines at 50 and 100 cm above each floor.",
-			"mitigation_details":"All steel columns should have a hole in which to install guardrails and lifelines at 50 and 100 cm above each floor.All steel columns should have a hole in which to install guardrails and lifelines at 50 and 100 cm above each floor.All steel columns should have a hole in which to install guardrails and lifelines at 50 and 100 cm above each floor.",
-			"mitigation_stage":"Detail Design",
-			"mitigation_type":"Inform"
-		},
-		{
-			"mitigation_desc":"Avoid work activity running below unfinished slab/floor.",
-			"mitigation_details":"Avoid work activity running below unfinished slab/floor.Avoid work activity running below unfinished slab/floor.Avoid work activity running below unfinished slab/floor.",
-			"mitigation_stage":"Preconstruction",
-			"mitigation_type":"Eliminate"
-		},
-		{
-			"mitigation_desc":"Provide requisite guardrails and toe boards at all slab openings.",
-			"mitigation_details":"Provide requisite guardrails and toe boards at all slab openings.Provide requisite guardrails and toe boards at all slab openings.Provide requisite guardrails and toe boards at all slab openings.",
-			"mitigation_stage":"Preconstruction",
-			"mitigation_type":"Reduce"
-		},
-		{
-			"mitigation_desc":"Provide warning markings and/or colour change.",
-			"mitigation_details":"Provide warning markings and/or colour change.Provide warning markings and/or colour change.Provide warning markings and/or colour change.",
-			"mitigation_stage":"Preconstruction",
-			"mitigation_type":"Control"
-		},
-		{
-			"mitigation_desc":"Provide protective grate to support weight of person over opening.",
-			"mitigation_details":"Provide protective grate to support weight of person over opening.Provide protective grate to support weight of person over opening.Provide protective grate to support weight of person over opening.",
-			"mitigation_stage":"Preconstruction",
-			"mitigation_type":"Control"
-		},
-		{
-			"mitigation_desc":"Inform contractor to Design-in permanent cast-in sockets around floor openings to enable early installation of permanent railings.",
-			"mitigation_details":"Inform contractor to Design-in permanent cast-in sockets around floor openings to enable early installation of permanent railings.Inform contractor to Design-in permanent cast-in sockets around floor openings to enable early installation of permanent railings.Inform contractor to Design-in permanent cast-in sockets around floor openings to enable early installation of permanent railings.",
-			"mitigation_stage":"Preconstruction",
-			"mitigation_type":"Inform"
-		},
-		{
-			"mitigation_desc":"Securely fix  cover with adequate SWL over opening with fixings requiring a tool.  Inspect regularly",
-			"mitigation_details":"Securely fix  cover with adequate SWL over opening with fixings requiring a tool.  Inspect regularlySecurely fix  cover with adequate SWL over opening with fixings requiring a tool.  Inspect regularlySecurely fix  cover with adequate SWL over opening with fixings requiring a tool.  Inspect regularly",
-			"mitigation_stage":"Site work and Change Control",
-			"mitigation_type":"Eliminate"
-		},
-		{
-			"mitigation_desc":"Ensure work is carried out only when weather conditions do not jeopardise the   health and safety of the workers.",
-			"mitigation_details":"Ensure work is carried out only when weather conditions do not jeopardise the   health and safety of the workers.Ensure work is carried out only when weather conditions do not jeopardise the   health and safety of the workers.Ensure work is carried out only when weather conditions do not jeopardise the   health and safety of the workers.",
-			"mitigation_stage":"Site work and Change Control",
-			"mitigation_type":"Reduce"
-		},
-		{
-			"mitigation_desc":"Provide safe lighting levels, including access and depression.",
-			"mitigation_details":"Provide safe lighting levels, including access and depression.Provide safe lighting levels, including access and depression.Provide safe lighting levels, including access and depression.",
-			"mitigation_stage":"Site work and Change Control",
-			"mitigation_type":"Control"
-		},
-		{
-			"mitigation_desc":"Inform Site team of any activities when covers or guardrails need to be removed.",
-			"mitigation_details":"Inform Site team of any activities when covers or guardrails need to be removed.Inform Site team of any activities when covers or guardrails need to be removed.Inform Site team of any activities when covers or guardrails need to be removed.",
-			"mitigation_stage":"Site work and Change Control",
-			"mitigation_type":"Inform"
-		},
-		{
-			"mitigation_desc":"Where necessary to locate equipment within 3m from edge of roof, provide railings or means of securing fall protection devices with appropriate signage.",
-			"mitigation_details":"Where necessary to locate equipment within 3m from edge of roof, provide railings or means of securing fall protection devices with appropriate signage.Where necessary to locate equipment within 3m from edge of roof, provide railings or means of securing fall protection devices with appropriate signage.Where necessary to locate equipment within 3m from edge of roof, provide railings or means of securing fall protection devices with appropriate signage.",
-			"mitigation_stage":"Preliminary Design",
-			"mitigation_type":"Eliminate"
-		}
-	];
-
-	responseCodes.respond(place, req, res, next, responseCodes.OK, suggestions);
+	RiskMitigation.findMitigationSuggestions(account, criteria).then((suggestions) => {
+		responseCodes.respond(place, req, res, next, responseCodes.OK, suggestions);
+	}).catch(err => {
+		responseCodes.respond(place, req, res, next, err, err);
+	});
 }
 
 function findMitigationCriteria(req, res, next) {
 	const place = utils.APIInfo(req);
+	const {account} = req.params;
 
-	const criteria = {
-		"associated_activity":[
-			"All construction",
-			"Site tests",
-			"Logistics"
-		],
-		"category":[
-			"Safety Electricity"
-		],
-		"element":[
-			"Doors",
-			"Floors",
-			"Pipes",
-			"Vents",
-			"Walls"
-		],
-		"location_desc":[
-			"Tower 1 - Level 0",
-			"Tower 1 - Level 1",
-			"Tower 1 - Level 2",
-			"Tower 2 - Level 0",
-			"Tower 2 - Level 1",
-			"Tower 3 - Level 0",
-			"Tower 3 - Level 1",
-			"Tower 3 - Level 2"
-		],
-		"mitigation_stage":[
-			"Preliminary Design",
-			"Detail Design",
-			"Preconstruction",
-			"Site work and Change Control"
-		],
-		"mitigation_type":[
-			"Eliminate",
-			"Reduce",
-			"Control",
-			"Inform"
-		],
-		"risk_factor":[
-			"Factor 2",
-			"Factor 5",
-			"Factor 8"
-		],
-		"scope":[
-			"General concrete",
-			"In situ concrete"
-		]
-	};
-
-	responseCodes.respond(place, req, res, next, responseCodes.OK, criteria);
+	RiskMitigation.getCriteria(account).then((criteria) => {
+		responseCodes.respond(place, req, res, next, responseCodes.OK, criteria);
+	}).catch(err => {
+		responseCodes.respond(place, req, res, next, err, err);
+	});
 }
 
 module.exports = router;
