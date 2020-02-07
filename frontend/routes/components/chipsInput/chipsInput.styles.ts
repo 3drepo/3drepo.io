21eb012b169f0d8220a2babcef2cc966269a41c6
Michał Kubiak <mkubiak@apptension.com>
2020-02-07 15:23:19 +0100

    ISSUE #1847 - Add new component displaying chips

---
+++ frontend/routes/components/chipsInput/chipsInput.styles.ts
@@ -0,0 +1,49 @@
+/**
+ *  Copyright (C) 2017 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import styled from 'styled-components';
+
+import ChipInput from 'material-ui-chip-input';
+
+export const StyledChipInput = styled(ChipInput)`
+	&& {
+		margin-top: 10px;
+
+		div[class^="WAMuiChipInput-standard"] {
+			min-height: 32px;
+		}
+		div[class*="-focused-"]:after {
+			transform: scale(0);
+		}
+		div[class^="MuiChip-root"] {
+			height: 24px;
+		}
+		span[class^="MuiChip-label"] {
+			padding-left: 8px;
+			padding-right: 8px;
+		}
+		svg[class^="MuiSvgIcon-root"] {
+			width: 16px;
+			height: 16px;
+			margin-left: -4px;
+			margin-right: 4px;
+		}
+		input[class^="MuiInputBase-input"] {
+			transform: translateY(-3px);
+		}
+	}
+`;
