3eaafa7396804b1819bc4bf7fe9e6fe37d327e93
Michał Kubiak <mkubiak@apptension.com>
2020-03-10 16:26:59 +0100

    ISSUE #1916 - Remove risk comment pin position

--- frontend/routes/viewerGui/components/risks/components/riskDetails/riskDetails.component.tsx
+++ frontend/routes/viewerGui/components/risks/components/riskDetails/riskDetails.component.tsx
@@ -312,18 +312,10 @@ export class RiskDetails extends React.PureComponent<IProps, IState> {
 	public postComment = async (teamspace, model, { comment, screenshot }, finishSubmitting) => {
 		const viewpoint = await this.props.viewer.getCurrentViewpoint({ teamspace, model });
 
-		const pinData = await this.props.viewer.getPinData();
-		let position;
-
-		if (pinData) {
-			position = pinData.pickedPos;
-		}
-
 		const riskCommentData = {
 			_id: this.riskData._id,
 			rev_id: this.riskData.rev_id,
 			comment,
-			position,
 			viewpoint: {
 				...viewpoint,
 				screenshot
