ec5b20d88ade85113e5cb96a383867e517a93809
Carmen Fan <wind5320@hotmail.com>
2020-04-03 16:03:28 +0100

    ISSUE #1847 have resource to use this as well

--- backend/models/ticket.js
+++ backend/models/ticket.js
@@ -624,11 +624,9 @@ class Ticket {
 	}
 
 	async attachResourceFiles(account, model, id, username, sessionId, resourceNames, files) {
-		const quota = await User.getQuotaInfo(account);
-		const spaceLeft = ((quota.spaceLimit === null || quota.spaceLimit === undefined ? Infinity : quota.spaceLimit) - quota.spaceUsed) * 1024 * 1024;
 		const spaceToBeUsed = files.reduce((size, file) => size + file.size,0);
 
-		if (spaceLeft < spaceToBeUsed) {
+		if (!User.hasSufficientQuota(account, spaceToBeUsed)) {
 			throw responseCodes.SIZE_LIMIT_PAY;
 		}
 
