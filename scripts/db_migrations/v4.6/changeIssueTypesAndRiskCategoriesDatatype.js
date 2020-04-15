86a801beb8f55c7cc038bc720164648daf1675cb
Carmen Fan <wind5320@hotmail.com>
2020-04-15 16:58:05 +0100

    ISSUE #1971 migration script done

--- scripts/db_migrations/v4.6/changeIssueTypesAndRiskCategoriesDatatype.js
+++ scripts/db_migrations/v4.6/changeIssueTypesAndRiskCategoriesDatatype.js
@@ -6,7 +6,7 @@ function isObject(data) {
 
 function updateTeamspaceSetting(dbConn) {
 	var setting = dbConn.getCollection("teamspace").findOne();
-	if(setting.topicTypes && (setting.topicTypes.length === 0 || isObject(setting.topicTypes[0]))
+	if(setting && setting.topicTypes && (setting.topicTypes.length === 0 || isObject(setting.topicTypes[0]))
 		&& setting.riskCategories && (setting.riskCategories.length === 0  || isObject(setting.riskCategories[0]))) {
 		var issueMapping = {};
 		var riskMapping = {};
@@ -28,31 +28,49 @@ function updateTeamspaceSetting(dbConn) {
 		}
 		return {riskMapping, issueMapping};
 	}
-	else
+	else {
+		if(!setting) print("This teamspace has no teamspace setting. Skipping...");
 		return {riskMapping:{}, issueMapping:{}};
+	}
 
 }
 
 function updateIssue(dbConn, mapping, colName) {
-	var typeLabel = "topic_type";
+	updateTicket(dbConn, mapping, colName, "topic_type");
+}
+
+function updateRisk(dbConn, mapping, colName) {
+	updateTicket(dbConn, mapping, colName, "category");
+}
+
+function updateTicket(dbConn, mapping, colName, typeLabel) {
 	dbConn.getCollection(colName).find({"comments.action.property": typeLabel}, {"comments": 1})
-		.ToArray().forEach(function(issue) {
-			for(var i = 0; i < issue.comments.length; ++i) {
-				var property = issue.comments.action.property;
-				if( property === typeLabel) {
-					var from = issue.comments.action.from;
-					if(mapping[from]) {
-						issue.comments.action.from = mapping[from];
-					}
-					var to = issue.comments.action.to;
-					if(mapping[from]) {
-						issue.comments.action.to = mapping[to];
+		.toArray().forEach(function(ticket) {
+			var hasChange = false;
+			for(var i = 0; i < ticket.comments.length; ++i) {
+				if(ticket.comments[i].action) {
+					var property = ticket.comments[i].action.property;
+					if( property === typeLabel) {
+						var from = ticket.comments[i].action.from;
+						if(mapping[from]) {
+							ticket.comments[i].action.from = mapping[from];
+							hasChange = true;
+						}
+						var to = ticket.comments[i].action.to;
+						if(mapping[to]) {
+							ticket.comments[i].action.to = mapping[to];
+							hasChange = true;
+						}
 					}
 				}
+			}
+			if(!dryRun) {
+				dbConn.getCollection(colName).update({_id: ticket._id}, {$set:{comments: ticket.comments}});
 			}
 
 	});
 }
+
 
 function editComments(dbConn, mapping) {
 
@@ -62,6 +80,10 @@ function editComments(dbConn, mapping) {
 	dbConn.getCollectionNames().forEach(function(col) {
 		if(checkIssues && col.endsWith(".issues")) {
 			updateIssue(dbConn, mapping.issueMapping, col);
+		}
+
+		if(checkRisks && col.endsWith(".risks")) {
+			updateRisk(dbConn, mapping.riskMapping, col);
 		 }
 	 });
 
@@ -77,10 +99,8 @@ function updateTeamspace(ts) {
 
 var toIgnore = ["admin", "local", "notifications"];
 
-updateTeamspace("subscriptionTest");
 
-/*
 db.getSiblingDB("admin").adminCommand({listDatabases:1}).databases.forEach(function(database){
 	if(toIgnore.indexOf(database.name) === -1)
 		updateTeamspace(database.name);
-});*/
+});
