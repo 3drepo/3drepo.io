15c298104f5bd66983a79ab970a0e19c993e5fbb
Carmen Fan <wind5320@hotmail.com>
2020-04-16 15:13:17 +0100

    ISSUE #1966 fix rounding to match unity

--- frontend/routes/viewerGui/components/measurements/components/measureItem/measureItem.component.tsx
+++ frontend/routes/viewerGui/components/measurements/components/measureItem/measureItem.component.tsx
@@ -74,29 +74,32 @@ interface IProps extends IMeasure {
 	colors: string[];
 }
 
-export const getValue = (measureValue: number, units: string, type: number, modelUnit: string) => {
-	const isAreaMeasurement = type === MEASURE_TYPE.AREA;
-	const isRecalculationNeeded = modelUnit !== 'mm';
-	const value = cond([
-		[(is) => eq(is, true), () => isAreaMeasurement ? measureValue * 1000 * 1000 : measureValue * 1000],
-		[stubTrue, () => measureValue],
-	])(isRecalculationNeeded);
+const roundNumber = (num : number, numDP: number) => {
+	const factor = Math.pow(10, numDP);
+	return Math.round((num + Number.EPSILON)* factor)/factor;
+}
 
-	if (isAreaMeasurement) {
-		return cond([
-			[matches('mm'), () => Math.round(value).toString()],
-			[matches('cm'), () => Math.round(value / 100).toString()],
-			[matches('m'), () => (Math.round((Number(value / 1000000) + Number.EPSILON) * 100) / 100).toFixed(2)],
-			[stubTrue, () => Math.round(value).toString()]
-		])(units);
+export const getValue = (measureValue: number, units: string, type: number, modelUnits: string) => {
+	const isAreaMeasurement = type === MEASURE_TYPE.AREA;
+
+	const factor = isAreaMeasurement ? 2 : 1;
+
+	let roundedValueMM = cond([
+			[matches('mm'), () => Math.round(measureValue)],
+			[matches('cm'), () => roundNumber(measureValue, 1 * factor) * Math.pow(10, factor)],
+			[matches('dm'), () => roundNumber(measureValue, 2 * factor) * Math.pow(100, factor)],
+			[matches('m'),  () => roundNumber(measureValue, 3 * factor) * Math.pow(1000, factor)],
+			[stubTrue, () => Math.round(measureValue)]
+	])(modelUnits);
+
+	if (units === 'mm') {
+		roundedValueMM =  Math.round(roundedValueMM);
+	} else {
+		const valueInUnits = roundedValueMM / Math.pow(1000, factor);
+		roundedValueMM = roundNumber(valueInUnits, 2);
 	}
 
-	return cond([
-		[matches('mm'), () => Math.round(value).toString()],
-		[matches('cm'), () => Math.round(value / 10).toString()],
-		[matches('m'), () => (Math.round((Number(value / 1000) + Number.EPSILON) * 100) / 100).toFixed(2)],
-		[stubTrue, () => Math.round(value).toString()]
-	])(units);
+	return Number.parseFloat(roundedValueMM.toPrecision(7)).toString(); // Unity only gives 7sf
 };
 
 export const getUnits = (units: string, type: number) => {
