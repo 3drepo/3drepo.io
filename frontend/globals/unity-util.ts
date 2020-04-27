9e22cf4c250751f2ace68c5821b5f2d524588a81
Santiago Montero <santiago.montero@3drepo.org>
2020-04-27 17:30:00 +0100

    ISSUE #2007 - Fix mapinitialise not being called in unity

--- frontend/globals/unity-util.ts
+++ frontend/globals/unity-util.ts
@@ -1119,7 +1119,7 @@ export class UnityUtil {
 	 * @param surveyingInfo - array of survey points and it's respective latitude and longitude value
 	 */
 	public static mapInitialise(surveyingInfo: [object]) {
-		UnityUtil.toUnity('MapsInitiate', UnityUtil.LoadingState.MODEL_LOADING, JSON.stringify(surveyingInfo));
+		UnityUtil.toUnity('MapsInitiate', UnityUtil.LoadingState.VIEWER_READY, JSON.stringify(surveyingInfo));
 	}
 
 	/**
