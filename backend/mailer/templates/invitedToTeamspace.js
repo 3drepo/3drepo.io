6db5a552659b730f90b608588c42e48df1d62ada
Santiago Montero <santiago.montero@3drepo.org>
2020-05-26 17:28:05 +0100

    ISSUE #2044 - Simplification of html sourcode for templates

--- backend/mailer/templates/invitedToTeamspace.js
+++ backend/mailer/templates/invitedToTeamspace.js
@@ -16,20 +16,14 @@
  */
 
 "use strict";
-const { ctaButton,  basicEmailTemplateWithLogo } = require("./basicEmailTemplate");
+const { ctaButton,  basicEmailTemplateWithLogo, SIGN_OFF } = require("./basicEmailTemplate");
 
 const html = ({name, company, teamspace, url}) => basicEmailTemplateWithLogo(`
-	<table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
-		<tbody><tr>
-			<td valign="top" class="mcnTextContent" style="padding: 0px 0px 29px;color: #8A8A8A;font-family: Roboto, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;font-size: 16px;line-height: 150%;">
-				${name} <em>(${company})</em> invites you to join their teamspace <strong>${teamspace}</strong>. Please press on the button below to sign up and start collaborating.
-			</td>
-		</tr>
-		</tbody>
-	</table>
-	${(ctaButton(`Join ${teamspace}`, url))}
-	<div style="height:30px" />
-`);
+		${name} <em>(${company})</em> invites you to join their teamspace <strong>${teamspace}</strong>. Please press on the button below to sign up and start collaborating.
+		${(ctaButton(`Join ${teamspace}`, url))}
+		${SIGN_OFF}
+		<br />
+	`);
 
 const subject = data => `${data.name} has invited you to collaborate in 3D Repo`;
 
