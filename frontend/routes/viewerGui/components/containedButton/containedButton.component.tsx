e6ab9eed31a3d217eb836d7b79acf375400f4b21
Michał Kubiak <mkubiak@apptension.com>
2020-06-18 17:40:40 +0200

    ISSUE #2087 - Add containedButtons component

---
+++ frontend/routes/viewerGui/components/containedButton/containedButton.component.tsx
@@ -0,0 +1,38 @@
+/**
+ *  Copyright (C) 2020 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+
+import { ButtonProps } from '@material-ui/core/Button';
+
+import { StyledButton } from './containedButton.styles';
+
+interface IProps extends ButtonProps {
+	children: React.ReactNode;
+	icon?: any;
+	onClick?: () => void;
+}
+
+export const ContainedButton = ({ children, icon, onClick, ...props }: IProps) => {
+	const IconComponent = icon;
+	return (
+		<StyledButton color="primary" variant="contained" size="small" squeezed={Boolean(icon)} onClick={onClick} {...props}>
+			{icon && <IconComponent style={{ fontSize: 14 }} />}
+			{children}
+		</StyledButton>
+	);
+};
