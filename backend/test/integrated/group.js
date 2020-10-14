374cb6c0b6410d099c21e1d59c719840661dadbc
Charence Wong <charencewong@hotmail.com>
2020-10-14 11:42:31 +0100

    ISSUE #2243 - debug test

--- backend/test/integrated/group.js
+++ backend/test/integrated/group.js
@@ -742,6 +742,7 @@ describe("Groups", function () {
 				function(done) {
 					agent.get(`/${username}/${model}/revision/master/head/groups/${groupId}`)
 						.expect(200 , function(err, res) {
+							console.log(res.body);
 							expect(res.body.author).to.equal(username);
 							expect(res.body.objects[0].shared_ids.length).to.equal(74);
 							done(err);
