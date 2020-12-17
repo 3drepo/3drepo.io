b209914d52243f266940a0faa5bdc715d076e0b9
Charence Wong <charencewong@hotmail.com>
2020-12-17 17:11:45 +0000

    ISSUE #1972 - catch invalid tag error in ticket creation

--- backend/models/ticket.js
+++ backend/models/ticket.js
@@ -450,13 +450,15 @@ class Ticket extends View {
 		}
 
 		// Assign rev_id for issue
-		const history = await History.getHistory({ account, model }, branch, newTicket.revId, { _id: 1 });
-
-		if (!history && (newTicket.revId || (newTicket.viewpoint || {}).highlighted_group_id)) {
-			throw responseCodes.INVALID_TAG_NAME;
-		} else if (history) {
-			newTicket.rev_id = history._id;
-		}
+		History.getHistory({ account, model }, branch, newTicket.revId, { _id: 1 }).then((history) => {
+			if (history) {
+				newTicket.rev_id = history._id;
+			}
+		}).catch(() => {
+			if (newTicket.revId || (newTicket.viewpoint || {}).highlighted_group_id) {
+				throw responseCodes.INVALID_TAG_NAME;
+			}
+		});
 
 		newTicket = this.filterFields(newTicket, ["viewpoint", "revId"]);
 
