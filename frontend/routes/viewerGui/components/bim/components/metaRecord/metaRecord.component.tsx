6e7e0916a27cc8f6951843141eaf9fd9fcd05657
Michał Kubiak <michal.kubiak@my.com>
2020-12-24 13:43:51 +0100

    ISSUE #2314 - Add highlight all similar element & copy rule to bim panel (#2324)
    * ISSUE #2314 - Add highlight all similar element & copy rule to bim panel
    
    * ISSUE #2314 - Use Tree action to handle selecting nodes
    
    * ISSUE #2314 - Rename highlighting to selecting elements & add tooltips
    
    * ISSUE #2314 - Handle selecting nodes without selecting a new active node
    
    * ISSUE #2314 - Remove unnecessary API method
    
    * ISSUE #2314 fix type of undefined error
    
    * ISSUE #2314 linter fix
    
    Co-authored-by: Michał Kubiak <mkubiak@apptension.com>
    Co-authored-by: Carmen Fan <wind5320@hotmail.com>

--- frontend/routes/viewerGui/components/bim/components/metaRecord/metaRecord.component.tsx
+++ frontend/routes/viewerGui/components/bim/components/metaRecord/metaRecord.component.tsx
@@ -17,8 +17,13 @@
 
 import React from 'react';
 
+import Tooltip from '@material-ui/core/Tooltip';
+
 import { StarIcon } from '../../../../../components/starIcon/starIcon.component';
-import { Container, MetaKey, MetaKeyText, MetaValue, StarIconWrapper  } from './metaRecord.styles';
+import {
+	Actions, Container, MetaKey, MetaKeyText, MetaValue, StarIconWrapper, StyledCopyIcon, StyledIconButton,
+	StyledSelectSimilarIcon,
+} from './metaRecord.styles';
 
 interface IProps {
 	starred: boolean;
@@ -26,6 +31,8 @@ interface IProps {
 	className?: string;
 	value?: string;
 	onStarClick: () => void;
+	selectAllSimilar: (rules) => void;
+	copyRules: (rules) => void;
 }
 
 interface IState {
@@ -36,6 +43,16 @@ export class MetaRecord extends React.PureComponent<IProps, IState> {
 	public state = {
 		hasDelayedClick: false
 	};
+
+	private get rules() {
+		const { value, name } = this.props;
+
+		return [{
+			field: name,
+			operator: 'IS',
+			values: [value],
+		}];
+	}
 
 	private get isStarred() {
 		const { starred } = this.props;
@@ -56,7 +73,7 @@ export class MetaRecord extends React.PureComponent<IProps, IState> {
 	}
 
 	public render() {
-		const { value, name } = this.props;
+		const { value, name, copyRules } = this.props;
 
 		return (
 			<Container className={this.props.className}>
@@ -70,9 +87,23 @@ export class MetaRecord extends React.PureComponent<IProps, IState> {
 					<MetaKeyText>{name}</MetaKeyText>
 				</MetaKey>
 				<MetaValue>{value}</MetaValue>
+				<Actions>
+					<Tooltip title="Copy group filter to clipboard">
+						<StyledIconButton onClick={() => copyRules(this.rules)}>
+							<StyledCopyIcon />
+						</StyledIconButton>
+					</Tooltip>
+					<Tooltip title="Select elements with same parameter value">
+						<StyledIconButton onClick={this.handleSelectAllSimilar}>
+							<StyledSelectSimilarIcon />
+						</StyledIconButton>
+					</Tooltip>
+				</Actions>
 			</Container>
 		);
 	}
+
+	private handleSelectAllSimilar = () => this.props.selectAllSimilar(this.rules);
 
 	private handleStarClick = () => {
 		if (this.starClickTimeout) {
