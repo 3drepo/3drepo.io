fbe1748da07acabddc300c60b5551d4f9fae538b
Charence Wong <charencewong@hotmail.com>
2021-01-05 14:52:41 +0000

    ISSUE #2372 - remove mongoose from ref

--- backend/models/ref.js
+++ backend/models/ref.js
@@ -16,37 +16,14 @@
  */
 
 "use strict";
-const mongoose = require("mongoose");
 const db = require("../handler/db");
-const ModelFactory = require("./factory/modelFactory");
 const ModelSettings = require("./modelSetting");
-
-const Schema = mongoose.Schema;
-
-const refSchema = Schema({
-	_id: Object,
-	shared_id: Object,
-	type: { type: String, default: "ref"},
-	project: String,
-	owner: String,
-	_rid: Object,
-	parents: [],
-	name: String
-});
-
-refSchema.methods = {};
 
 function getRefCollectionName(model) {
 	return model + ".scene";
 }
 
-const Ref = ModelFactory.createClass(
-	"Ref",
-	refSchema,
-	arg => {
-		return `${arg.model}.scene`;
-	}
-);
+const Ref = {};
 
 Ref.findRef = async function(account, model, query, projection) {
 	return db.find(account, getRefCollectionName(model), query, projection);
