f94c8e19a88009d498338a7f796c8aca923c62dd
Charence Wong <charencewong@hotmail.com>
2021-02-19 09:14:55 +0000

    ISSUE #2449 - revert cookie

--- backend/services/session.js
+++ backend/services/session.js
@@ -27,8 +27,8 @@ const C = require("../constants");
 const store = getSessionStore(expressSession);
 const useragent = require("useragent");
 
-module.exports.session = function(config) {
-	// const isSSL = config.public_protocol === "https";
+module.exports.session = function(config, req) {
+	const isSSL = config.public_protocol === "https";
 	return expressSession({
 		secret: config.cookie.secret,
 		resave: true,
@@ -38,11 +38,10 @@ module.exports.session = function(config) {
 			maxAge: config.cookie.maxAge,
 			domain: config.cookie.domain,
 			path: "/",
-			// secure: isSSL,
+			secure: isSSL,
 			// None can only applied with secure set to true, which requires SSL.
 			// None is required for embeddable viewer to work.
-			// sameSite:  isSSL ? "None" : "Lax"
-			sameSite:  "Lax"
+			sameSite:  isSSL ? "None" : "Lax"
 		},
 		store: store
 	});
