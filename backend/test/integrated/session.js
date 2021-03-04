ba7555f36aac746e36626c8268d99aeecdd9910a
Charence <charencewong@hotmail.com>
2021-03-04 15:18:17 +0000

    ISSUE #2449 - debug tests

--- backend/test/integrated/session.js
+++ backend/test/integrated/session.js
@@ -121,6 +121,7 @@ describe("Cross-site requests", function () {
 					.send({ username, password })
 					.set({"Referer":`${config.public_protocol}://3rdparty.net`})
 					.expect(200, function(err, res) {
+						console.log(res.body);
 						expect(res.body.username).to.equal(username);
 						done(err);
 					});
@@ -151,6 +152,7 @@ describe("Cross-site requests", function () {
 					.send({ username, password })
 					.set({"Referer":`${config.public_protocol}://3rdparty.net/login`})
 					.expect(200, function(err, res) {
+						console.log(res.body);
 						expect(res.body.username).to.equal(username);
 						done(err);
 					});
