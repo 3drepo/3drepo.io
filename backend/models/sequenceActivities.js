0766cb456c6af3d099029d41ced25c813913e9df
Santiago Montero <santiago.montero@3drepo.org>
2021-03-10 17:59:14 +0000

    ISSUE #2441 - Fixed sequence_id property, fixed tests and added fetch tests

--- backend/models/sequenceActivities.js
+++ backend/models/sequenceActivities.js
@@ -33,7 +33,7 @@ const activityEditSchema = yup.object().shape({
 	name: yup.string(),
 	startDate: yup.number(),
 	endDate: yup.number(),
-	parent: yup.array().of(yup.string()),
+	parent: yup.string(),
 	resources: yup.object(),
 	data: yup.array().of(keyValueSchema)
 }).noUnknown();
@@ -42,7 +42,7 @@ const activitySchema = yup.object().shape({
 	name: yup.string().required(),
 	startDate: yup.number().required(),
 	endDate: yup.number().required(),
-	parent: yup.array().of(yup.string()),
+	parent: yup.string(),
 	resources: yup.object(),
 	data: yup.array().of(keyValueSchema).required()
 }).noUnknown();
@@ -101,14 +101,14 @@ SequenceActivities.create = async (account, model, sequenceId, activity) => {
 	}
 
 	const _id = nodeuuid();
-	activity = {...activity, sequence_id:  utils.stringToUUID(sequenceId), _id: utils.stringToUUID(_id)};
+	activity = {...activity, sequenceId:  utils.stringToUUID(sequenceId), _id: utils.stringToUUID(_id)};
 
 	await Promise.all([
 		db.insert(account, model + ".activities", activity),
 		FileRef.removeFile(account, model, "activities", sequenceId)
 	]);
 
-	return {...activity, _id, sequence_id: sequenceId};
+	return {...activity, _id, sequenceId: sequenceId};
 };
 
 SequenceActivities.edit = async (account, model, sequenceId, activityId, activity) => {
@@ -126,7 +126,7 @@ SequenceActivities.edit = async (account, model, sequenceId, activityId, activit
 		throw responseCodes.INVALID_ARGUMENTS;
 	}
 
-	const query =  {_id: utils.stringToUUID(activityId), sequence_id: utils.stringToUUID(sequenceId)};
+	const query =  {_id: utils.stringToUUID(activityId), sequenceId: utils.stringToUUID(sequenceId)};
 	const {result} = await db.update(account, model + ".activities", query, {$set: activity});
 
 	if (!result.n) {
@@ -134,8 +134,6 @@ SequenceActivities.edit = async (account, model, sequenceId, activityId, activit
 	}
 
 	await FileRef.removeFile(account, model, "activities", sequenceId);
-
-	return {...activity, _id: activityId, sequence_id: sequenceId};
 };
 
 SequenceActivities.remove = async (account, model, sequenceId, activityId) => {
@@ -145,7 +143,7 @@ SequenceActivities.remove = async (account, model, sequenceId, activityId) => {
 		throw responseCodes.SEQUENCE_NOT_FOUND;
 	}
 
-	const query =  {_id: utils.stringToUUID(activityId), sequence_id: utils.stringToUUID(sequenceId)};
+	const query =  {_id: utils.stringToUUID(activityId), sequenceId: utils.stringToUUID(sequenceId)};
 	const {result} = await db.remove(account, model + ".activities", query);
 
 	if (!result.n) {
@@ -169,7 +167,7 @@ SequenceActivities.get = async (account, model, sequenceId) => {
 		activities = await FileRef.getSequenceActivitiesFile(account, model, utils.uuidToString(sequenceId));
 	} catch(e) {
 		activities = await createActivitiesTree(account, model, sequenceId);
-		await FileRef.storeFile(account, model + ".activities.ref", account, sequenceId, JSON.stringify(activities),  {"_id":sequenceId});
+		await FileRef.storeFile(account, model + ".activities.ref", account, utils.uuidToString(nodeuuid()), JSON.stringify(activities),  {"_id": sequenceId});
 	}
 
 	return activities;
