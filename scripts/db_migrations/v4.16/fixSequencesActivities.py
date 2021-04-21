af9a86a8f67f7afbee25c6e6bced6452a67bab2e
Santiago Montero <santiago.montero@3drepo.org>
2021-04-21 13:24:23 +0100

    ISSUE #2440 - added migration from old tasks collection schema , moved removal of activities files to a separate file, added more information on trial run

--- scripts/db_migrations/v4.16/fixSequencesActivities.py
+++ scripts/db_migrations/v4.16/fixSequencesActivities.py
@@ -5,6 +5,12 @@ import StringIO
 import os
 import json
 import uuid
+import datetime
+import time
+
+def strToTimestamp(date_time_str):
+  date_time_obj = datetime.datetime.strptime(date_time_str, '%d %b %Y %H:%M:%S')
+  return int(time.mktime(date_time_obj.timetuple()))*1000
 
 def getFileGridFS(db, collection_name, link):
   fs = gridfs.GridFS(db, collection_name)
@@ -22,8 +28,13 @@ def getFileFs(fsfolder, link):
   f = open(filename, "r")
   return f.read()
 
+def getRef(db, collection_name, id):
+  return db[collection_name + ".ref"].find_one({"_id": id})
+
+
 def getFile(db, fsfolder, collection_name, id):
-  entry = db[collection_name + ".ref"].find_one({"_id": id})
+  entry = getRef(db, collection_name, id)
+
   if entry == None:
     return None
 
@@ -36,40 +47,6 @@ def getFile(db, fsfolder, collection_name, id):
       return getFileGridFS(db, collection_name, id)
 
   return getFileGridFS(db, collection_name,entry["link"])
-
-
-def removeFileFs(fsfolder, link):
-  filename = os.path.join(fsfolder, link)
-
-  if not os.path.isfile(filename):
-    return False
-
-  os.remove(filename)
-  return True
-
-def removeFileGridFs(db, collection_name, link):
-  fs = gridfs.GridFS(db, collection_name)
-  gridfsfile =  fs.find_one({ "filename": link })
-
-  if gridfsfile:
-    fs.delete(gridfsfile._id)
-
-def removeFile(db, fsfolder, collection_name, id):
-
-  entry = db[collection_name + ".ref"].find_one({"_id": id})
-
-  if entry:
-    if entry["type"] == "fs":
-      if removeFileFs(fsfolder, entry["link"]):
-        print "File succesfully removed from filesystem" + entry["link"]
-      else:
-        print "File unsuccesfully removed from filesystem" + entry["link"]
-
-    removeFileGridFs(db, collection_name, entry["link"])
-  else:
-    print "Fileref not found " + id
-
-  db[collection_name + ".ref"].delete_one({"_id": id})
 
 
 def migrateData(data):
@@ -122,6 +99,35 @@ else:
 
 connString = "mongodb://"+ userName + ":" + password +"@"+mongoURL + ":" + mongoPort + "/"
 
+def wasMigrated(db, modelId, sequenceId):
+  activitiesCollName = modelId + ".activities"
+  results = db[activitiesCollName].find_one({"sequenceId": sequenceId})
+  return results != None
+
+def hasActivitiesCollection(db, modelId):
+  activitiesCollName =  modelId + ".activities"
+  results = db[activitiesCollName].find()
+  return results != None and results.count() > 0
+
+def hasTasksCollection(db, modelId):
+  tasksCollName =  modelId + ".tasks"
+  results = db[tasksCollName].find()
+  return results != None and results.count() > 0
+
+def testActivitiesFile(db, modelId, sequenceId):
+  activitiesCollName = modelId + ".activities"
+  ref = getRef(db, activitiesCollName, id)
+  if not ref:
+    print("Warning: file reference for sequence " + str(sequenceId) + " was not found")
+    return
+  else:
+    print("File reference for sequence " + str(sequenceId) + " was found. Ref type:" + ref["type"] + ", link:" + entry["link"])
+
+  fileContents = getFile(db, fsDirectory, activitiesCollName, str(sequenceId))
+  if fileContents:
+    print ("File for sequence " + str(sequenceId) + " was found")
+  else:
+    print ("Warning: File for sequence " + str(sequenceId) + " was NOT found")
 
 def updateActivitiesSchema(db, modelId, sequenceId):
   activitiesCollName = modelId + ".activities"
@@ -133,9 +139,34 @@ def updateActivitiesSchema(db, modelId, sequenceId):
     print ("error fetching file fs for sequence: " + str(sequenceId))
 
 
+def insertActivityFromTask(db, modelId, sequenceId, task): # sequenceId is expected to uuid.UUID
+  activitiesCollName = modelId + ".activities"
+  activity = None
+
+  if db[activitiesCollName]:
+    activity = db[activitiesCollName].find_one({"_id": task["_id"]})
+
+  if activity == None:
+    if "data" in task and "Planned Start" in task["data"] and "Planned Finish" in task["data"] :
+      activity = {"_id": task["_id"], "name": task["name"], "sequenceId": sequenceId}
+      activity["startDate"] = strToTimestamp(task["data"]["Planned Start"])
+      activity["endDate"] = strToTimestamp(task["data"]["Planned Finish"])
+      activity["data"] = migrateData(task["data"])
+      if "parents" in task and len(task["parents"]) > 0:
+        activity["parent"] = task["parents"][0]
+
+      db[activitiesCollName].insert_one(activity)
+
+def migrateTasksCollection(db, modelId, sequenceId):
+  tasksCollName =  modelId + ".tasks"
+  tasks = db[tasksCollName].find({})
+  for task in tasks:
+    insertActivityFromTask(db, modelId, sequenceId, task)
 
 #### Connect to the Database #####
 db = MongoClient(connString)
+
+
 for database in db.database_names():
     if database != "admin" and database != "local":
         db = MongoClient(connString)[database]
@@ -143,11 +174,28 @@ for database in db.database_names():
 
 ##### Get a model ID and find entries #####
         for setting in db.settings.find(no_cursor_timeout=True):
-            modelId = setting["_id"]
+            modelId = str(setting["_id"])
             print("\t--model: " +  modelId)
             for entry in db[modelId + ".sequences"].find({}):
                 entryId = entry["_id"]
                 print("\t\t--sequence: " + str(entryId))
+
                 if not dryRun:
-                  updateActivitiesSchema(db, modelId, entryId)
-                  removeFile(db, fsDirectory , str(modelId) + ".activities",  str(entryId))
+                  if hasTasksCollection(db, modelId) and not hasActivitiesCollection(db, modelId):
+                    print("\t\t\t--Migrating sequence: " + str(entryId) + " from tasks")
+                    migrateTasksCollection(db, modelId, entryId)
+                    db[modelId + ".tasks"].drop()
+                  else:
+                      if not wasMigrated(db, modelId, entryId):
+                        updateActivitiesSchema(db, modelId, entryId)
+                      else:
+                        print("\t\t\t--Skipping sequence: " + str(entryId) + ", sequence already migrated")
+                else:
+                  if hasTasksCollection(db, modelId) and not hasActivitiesCollection(db, modelId):
+                    print("\t\t\t--Sequence: " + str(entryId) + " has old tasks schema (tasks collection)")
+                  else:
+                    testActivitiesFile(db, modelId, entryId)
+                  if wasMigrated(db, modelId, entryId):
+                    print("\t\t\t--Sequence: " + str(entryId) + " already migrated")
+
+
