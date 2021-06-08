33966bb780badcc531d95cba9b62a6b2c38f0224
Michał Kubiak <mkubiak@apptension.com>
2021-06-08 12:37:39 +0200

    ISSUE #2584 - Remove GA from Analytics service

--- frontend/services/analytics.ts
+++ frontend/services/analytics.ts
@@ -1,19 +1,6 @@
-import ReactGA from 'react-ga';
 import TagManager from 'react-gtm-module';
 
 import { clientConfigService } from './clientConfig';
-
-export const EVENT_CATEGORIES = {
-	ISSUE: 'Issue',
-	RISK: 'Risk',
-	MODEL: 'Model'
-};
-
-export const EVENT_ACTIONS = {
-	CREATE: 'create',
-	EDIT: 'edit',
-	VIEW: 'view'
-};
 
 class AnalyticsService {
 	public init() {
@@ -29,41 +16,6 @@ class AnalyticsService {
 				TagManager.initialize(gtm);
 			}
 		}
-	}
-
-	public sendPageView(location, tracker = '') {
-		if (!this.isGoogleAnalyticEnabled) {
-			return;
-		}
-
-		if (tracker) {
-			ReactGA.pageview(location.pathname, [`${tracker}.send`]);
-		} else {
-			ReactGA.pageview(location.pathname);
-		}
-	}
-
-	public sendPageViewReferer(location) {
-		this.sendPageView(location, 'referer');
-	}
-
-	public sendEvent(category, action) {
-		if (!this.isGoogleAnalyticEnabled) {
-			return;
-		}
-
-		ReactGA.event({ category, action });
-	}
-
-	public setUserId(userId) {
-		if (!this.isGoogleAnalyticEnabled) {
-			return;
-		}
-		ReactGA.set({ userId });
-	}
-
-	private get isGoogleAnalyticEnabled() {
-		return Boolean(ReactGA.ga());
 	}
 }
 
