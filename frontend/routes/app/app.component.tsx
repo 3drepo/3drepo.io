270d88aed4b72340fbf57e51fc64bfc278d2bcc1
Carmen Fan <wind5320@hotmail.com>
2021-06-10 11:25:06 +0100

    Revert "ISSUE #2618 - Restore liveChat component"
    This reverts commit 26b1161c15920b5621f917641225fd293e5d354e.

--- frontend/routes/app/app.component.tsx
+++ frontend/routes/app/app.component.tsx
@@ -27,7 +27,6 @@ import { clientConfigService } from '../../services/clientConfig';
 import { isStaticRoute, STATIC_ROUTES } from '../../services/staticPages';
 import { DialogContainer } from '../components/dialogContainer';
 import { Intercom } from '../components/intercom';
-import { LiveChat } from '../components/liveChat';
 import { PrivateRoute } from '../components/privateRoute';
 import { SnackbarContainer } from '../components/snackbarContainer';
 import StaticPageRoute from '../components/staticPageRoute/staticPageRoute.component';
@@ -150,7 +149,6 @@ export class App extends React.PureComponent<IProps, IState> {
 				</Switch>
 				<DialogContainer />
 				<SnackbarContainer />
-				{!Boolean(clientConfigService.intercomLicense) &&  <LiveChat />}
 				<Intercom />
 			</AppContainer>
 		);
