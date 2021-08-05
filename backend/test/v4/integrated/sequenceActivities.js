6bbb29ca065b2ef7feab3b3de759630c27be2d9b
Charence <charencewong@hotmail.com>
2021-08-05 11:05:28 +0100

    ISSUE #2017 - try debug seq act again

--- backend/test/v4/integrated/sequenceActivities.js
+++ backend/test/v4/integrated/sequenceActivities.js
@@ -393,6 +393,10 @@ describe("Sequences", function () {
 				const res = await agent.get(`/${username}/${model}/sequences/${sequenceId}/activities`)
 					.expect(200);
 
+				console.log("====== res.body.activities ========");
+				console.log(res.body.activities);
+				console.log("====== newActivitiesList.activities ========");
+				console.log(newActivitiesList.activities);
 				expect(sortById(res.body.activities)).to.deep.equal(sortById(newActivitiesList.activities))
 			});
 		});
