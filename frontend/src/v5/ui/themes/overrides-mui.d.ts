19fc5e377cb86e2231f9fe8ec42864619259f967
Michał Kubiak <mkubiak@apptension.com>
2021-08-17 13:28:26 +0200

    ISSUE #2694 - Update shadows interface

--- frontend/src/v5/ui/themes/overrides-mui.d.ts
+++ frontend/src/v5/ui/themes/overrides-mui.d.ts
@@ -23,10 +23,16 @@ interface IGradient {
 }
 
 interface IShadow {
-	small: string;
-	medium: string;
-	large: string;
-	xlarge: string;
+	level_1: string;
+	level_2: string;
+	level_3: string;
+	level_4: string;
+	level_5: string;
+	level_6: string;
+	level_7: string;
+	level_8: string;
+	level_9: string;
+	level_10: string;
 }
 
 declare module '@material-ui/core/styles/createPalette' {
