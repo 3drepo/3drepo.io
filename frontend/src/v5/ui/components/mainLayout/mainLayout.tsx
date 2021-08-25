894c8a20e1ed2084b3a70ead4ae613bd2efdff02
Santiago Montero <santiago.montero@3drepo.org>
2021-08-25 13:02:11 -0300

    ISSUE #2721 - Added examples for pluralization and for mixed translations with components

--- frontend/src/v5/ui/components/mainLayout/mainLayout.tsx
+++ frontend/src/v5/ui/components/mainLayout/mainLayout.tsx
@@ -14,14 +14,18 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { Button } from '@material-ui/core';
+import { Button, Link } from '@material-ui/core';
 import { CurrentUserHooksSelectors } from '@/v5/services/selectorsHooks/currentUserSelectors.hooks';
 import React, { SyntheticEvent } from 'react';
 import { CurrentUserActionsDispatchers } from '@/v5/services/actionsDispatchers/currentUsersActions.dispatchers';
 import { i18n } from '@lingui/core';
 import { I18nProvider, Trans } from '@lingui/react';
 
-i18n.load('es', { 'Hello world! {name}': 'Hola mundo! {name}' });
+i18n.load('es', {
+	'Hello world! {name}': 'Hola mundo! {name}',
+	'Read <0>Description</0> below.': 'Lea la <0>Descripcion</0> abajo.',
+	'{count, plural, =1 {There is one car} other {There are {count} cars}}': '{count, plural, =1 {Hay un auto} other {Hay {count} autos}}',
+});
 i18n.activate('es');
 
 export const MainLayout = (): JSX.Element => {
@@ -32,6 +36,8 @@ export const MainLayout = (): JSX.Element => {
 		CurrentUserActionsDispatchers.fetchUser('teamSpace1');
 	};
 
+	const cars = ['ford', 'mazda'];
+
 	return (
 		<I18nProvider i18n={i18n}>
 			<div>
@@ -40,6 +46,15 @@ export const MainLayout = (): JSX.Element => {
 					Main Layout:
 					{userName}
 				</h1>
+				<br />
+				With a embeded component
+				<br />
+				<Trans id="Read <0>Description</0> below." components={[<Link href="/docs" />]} />
+				<br />
+				With plurals
+				<br />
+				<Trans id="{count, plural, =1 {There is one car} other {There are {count} cars}}" values={{ count: cars.length }} />
+				<br />
 				<Button onClick={onHandleClick}>Click me</Button>
 			</div>
 		</I18nProvider>
