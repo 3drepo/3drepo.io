aec083fc8f99f88b1db17e9a1aa569c5267f65b3
Emil Litwiniec <elitwiniec@apptension.com>
2021-09-03 11:18:19 +0200

    ISSUE # 2697 - Implement dashboardList and dashboardListItem components

---
+++ frontend/src/v5/ui/components/dashboard/dashboardList/dashboardListItem/dashboardListItem.styles.tsx
@@ -0,0 +1,59 @@
+/**
+ *  Copyright (C) 2021 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import styled, { css } from 'styled-components';
+
+export const Container = styled.li`
+  box-sizing: border-box;
+  background-color: ${({ theme, color }) => color ?? theme.palette.primary.contrast};
+  height: 80px;
+  width: 100%;
+  list-style: none;
+  border: 1px solid ${({ theme }) => theme.palette.base.light};
+  border-bottom-style: none;
+	border-top-style: none;
+
+  &:last-child {
+    border-radius: 0 0 5px 5px;
+		border-bottom-style: solid;
+  }
+
+  &:first-child {
+    border-radius: 5px 5px 0 0;
+    border-top-style: solid;
+  }
+	
+	&:only-child {
+		border-radius: 5px;
+	}
+  
+
+  ${({ selected, theme }) => selected && css`
+    background-color: ${theme.palette.secondary.main};
+    border: none;
+  `}
+	
+  :hover {
+    border: none;
+    box-shadow: ${({ theme }) => theme.palette.shadows.large}; 
+    z-index: 100;
+
+    & + li {
+      border-top: 0;
+    }
+  }
+`;
