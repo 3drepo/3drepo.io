e6206207afaf817daecd1cb4502eee33a7b85d79
Charence <charencewong@hotmail.com>
2021-09-08 11:18:50 +0100

    ISSUE #2706 - make test run again

--- frontend/src/v5/ui/routes/index.tsx
+++ frontend/src/v5/ui/routes/index.tsx
@@ -14,7 +14,6 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 import { theme } from '@/v5/ui/themes/theme';
 import { MuiThemeProvider } from '@material-ui/core';
 import { ThemeProvider } from 'styled-components';
