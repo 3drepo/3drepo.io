65f3932bb27ff89db0a66e71c090f0f780f0b1c3
Christos Tsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2021-10-08 16:06:20 +0100

    Issue 2801 - Get federation stats endpoint (#2807)
    * ISSUE #2801 getFederationStats processor and model methods implemented
    getFederationStats tests added
    
    * ISSUE #2801 yarn lint:fix
    
    * ISSUE #2801 small syntax fixes
    in e2e tests, subModels are part of federation properties
    
    * ISSUE #2801 issue count and risk count added to federation stats
    
    * ISSUE #2801 issue and risk models created and
    getIssueCount and getRiskCount methods moved
    issues and risks model tests added
    modelId removed from issues and risks in federations e2e tests
    
    * ISSUE #2801 getFederationStats docs updated to include ticket numbers
    
    * ISSUE #2801 federation perms class removed
    issues and risks model methods renamed
    
    * ISSUE #2801 federation middleware test removed
    issues and risks in federation e2e tests are filtered based on status
    yarn lint fix
    
    * ISSUE #2801 tweak output values to match swagger
    
    * ISSUE #2801 fix test after change on output
    
    Co-authored-by: Carmen Fan <wind5320@hotmail.com>

--- backend/tests/v5/e2e/routes/teamspaces/projects/federations/federations.test.js
+++ backend/tests/v5/e2e/routes/teamspaces/projects/federations/federations.test.js
@@ -38,12 +38,24 @@ const project = {
 	name: ServiceHelper.generateRandomString(),
 };
 
+const modelWithRevId = ServiceHelper.generateUUIDString();
+const modelWithoutRevId = ServiceHelper.generateUUIDString();
+
 const modelSettings = [
 	{
 		_id: ServiceHelper.generateUUIDString(),
 		name: ServiceHelper.generateRandomString(),
 		isFavourite: true,
-		properties: { ...ServiceHelper.generateRandomModelProperties(), federate: true },
+		properties: { ...ServiceHelper.generateRandomModelProperties(),
+			federate: true,
+			subModels: [{ model: modelWithRevId }] },
+	},
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: ServiceHelper.generateRandomString(),
+		properties: { ...ServiceHelper.generateRandomModelProperties(),
+			federate: true,
+			subModels: [{ model: modelWithoutRevId }] },
 	},
 	{
 		_id: ServiceHelper.generateUUIDString(),
@@ -51,13 +63,81 @@ const modelSettings = [
 		properties: { ...ServiceHelper.generateRandomModelProperties(), federate: true },
 	},
 	{
-		_id: ServiceHelper.generateUUIDString(),
+		_id: modelWithRevId,
+		name: ServiceHelper.generateRandomString(),
+		properties: ServiceHelper.generateRandomModelProperties(),
+	},
+	{
+		_id: modelWithoutRevId,
 		name: ServiceHelper.generateRandomString(),
 		properties: ServiceHelper.generateRandomModelProperties(),
 	},
 ];
 
+const issues = [
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: 'issue1',
+		status: 'open',
+	},
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: 'issue2',
+		status: 'closed',
+	},
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: 'issue3',
+		status: 'in progress',
+	},
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: 'issue3',
+		status: 'void',
+	},
+];
+
+const risks = [
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: 'risk1',
+		mitigation_status: 'unmitigated',
+	},
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: 'risk2',
+		mitigation_status: 'proposed',
+	},
+	{
+		_id: ServiceHelper.generateUUIDString(),
+		name: 'risk3',
+		mitigation_status: 'void',
+	},
+];
+
+const getUnresolvedIssues = (issuesList) => issuesList.filter((i) => i.status !== 'void' && i.status !== 'closed');
+
+const getUnresolvedRisks = (risksList) => risksList.filter((i) => i.mitigation_status !== 'void' && i.mitigation_status !== 'agreed_fully'
+		&& i.mitigation_status !== 'rejected');
+
 const container = modelSettings.find(({ properties }) => !properties.federate);
+
+const revisions = [
+	ServiceHelper.generateRevisionEntry(),
+	ServiceHelper.generateRevisionEntry(),
+	ServiceHelper.generateRevisionEntry(true),
+];
+
+const federationWithRev = modelSettings[0];
+const federationWithoutRev = modelSettings[1];
+const federationWithoutSubModels = modelSettings[2];
+const federationWithRevIssues = [issues[0], issues[1]];
+const federationWithRevRisks = [risks[0], risks[1]];
+const federationWithoutRevIssues = [issues[2]];
+const federationWithoutRevRisks = [risks[2]];
+
+const latestRevision = revisions.filter((rev) => !rev.void)
+	.reduce((a, b) => (a.timestamp > b.timestamp ? a : b));
 
 const setupData = async () => {
 	await ServiceHelper.db.createTeamspace(teamspace, [users.tsAdmin.user]);
@@ -71,11 +151,37 @@ const setupData = async () => {
 		model.name,
 		model.properties,
 	));
+	const federationWithRevIssueProms = federationWithRevIssues.map((issue) => ServiceHelper.db.createIssue(
+		teamspace,
+		federationWithRev._id,
+		issue,
+	));
+	const federationWithRevRiskProms = federationWithRevRisks.map((risk) => ServiceHelper.db.createRisk(
+		teamspace,
+		federationWithRev._id,
+		risk,
+	));
+	const federationWithoutRevIssueProms = federationWithoutRevIssues.map((issue) => ServiceHelper.db.createIssue(
+		teamspace,
+		federationWithoutRev._id,
+		issue,
+	));
+	const federationWithoutRevRiskProms = federationWithoutRevRisks.map((risk) => ServiceHelper.db.createRisk(
+		teamspace,
+		federationWithoutRev._id,
+		risk,
+	));
+
 	return Promise.all([
 		...userProms,
 		...modelProms,
+		...federationWithRevIssueProms,
+		...federationWithRevRiskProms,
+		...federationWithoutRevIssueProms,
+		...federationWithoutRevRiskProms,
 		ServiceHelper.db.createUser(nobody),
 		ServiceHelper.db.createProject(teamspace, project.id, project.name, modelSettings.map(({ _id }) => _id)),
+		...revisions.map((revision) => ServiceHelper.db.createRevision(teamspace, modelWithRevId, revision)),
 	]);
 };
 
@@ -112,6 +218,71 @@ const testGetFederationList = () => {
 	});
 };
 
+const formatToStats = (federation, issueCount, riskCount, latestRev) => {
+	const formattedStats = {
+		code: federation.properties.properties.code,
+		status: federation.properties.status,
+		subModels: federation.properties.subModels
+			? federation.properties.subModels.map(({ model }) => model) : undefined,
+		lastUpdated: latestRev ? latestRev.getTime() : undefined,
+		tickets: {
+			issues: issueCount,
+			risks: riskCount,
+		},
+	};
+
+	return formattedStats;
+};
+
+const testGetFederationStats = () => {
+	const route = (federationId) => `/v5/teamspaces/${teamspace}/projects/${project.id}/federations/${federationId}/stats`;
+	describe('Get federation stats', () => {
+		test('should fail without a valid session', async () => {
+			const res = await agent.get(route(federationWithRev._id)).expect(templates.notLoggedIn.status);
+			expect(res.body.code).toEqual(templates.notLoggedIn.code);
+		});
+
+		test('should fail if the user is not a member of the teamspace', async () => {
+			const res = await agent.get(`${route(federationWithRev._id)}?key=${nobody.apiKey}`).expect(templates.teamspaceNotFound.status);
+			expect(res.body.code).toEqual(templates.teamspaceNotFound.code);
+		});
+
+		test('should fail if the project does not exist', async () => {
+			const res = await agent.get(`/v5/teamspaces/${teamspace}/projects/dflkdsjfs/federations/${federationWithRev._id}/stats?key=${users.tsAdmin.apiKey}`).expect(templates.projectNotFound.status);
+			expect(res.body.code).toEqual(templates.projectNotFound.code);
+		});
+
+		test('should fail if the user does not have access to the federation', async () => {
+			const res = await agent.get(`${route(federationWithRev._id)}?key=${users.noProjectAccess.apiKey}`).expect(templates.notAuthorized.status);
+			expect(res.body.code).toEqual(templates.notAuthorized.code);
+		});
+
+		test('should fail if the federation does not exist', async () => {
+			const res = await agent.get(`${route('jibberish')}?key=${users.tsAdmin.apiKey}`).expect(templates.federationNotFound.status);
+			expect(res.body.code).toEqual(templates.federationNotFound.code);
+		});
+
+		test('should return the federation stats correctly if the user has access (subModels with revisions)', async () => {
+			const res = await agent.get(`${route(federationWithRev._id)}?key=${users.tsAdmin.apiKey}`).expect(templates.ok.status);
+			expect(res.body).toEqual(formatToStats(federationWithRev,
+				getUnresolvedIssues(federationWithRevIssues).length,
+				getUnresolvedRisks(federationWithRevRisks).length, latestRevision.timestamp));
+		});
+
+		test('should return the federation stats correctly if the user has access (subModels without revisions)', async () => {
+			const res = await agent.get(`${route(federationWithoutRev._id)}?key=${users.tsAdmin.apiKey}`).expect(templates.ok.status);
+			expect(res.body).toEqual(formatToStats(federationWithoutRev,
+				getUnresolvedIssues(federationWithoutRevIssues).length,
+				getUnresolvedRisks(federationWithoutRevRisks).length));
+		});
+
+		test('should return the federation stats correctly if the user has access (no subModels)', async () => {
+			const res = await agent.get(`${route(federationWithoutSubModels._id)}?key=${users.tsAdmin.apiKey}`).expect(templates.ok.status);
+			expect(res.body).toEqual(formatToStats(federationWithoutSubModels, 0, 0));
+		});
+	});
+};
+
 const testAppendFavourites = () => {
 	const route = `/v5/teamspaces/${teamspace}/projects/${project.id}/federations/favourites`;
 	describe('Append Favourite Federations', () => {
@@ -139,7 +310,7 @@ const testAppendFavourites = () => {
 			expect(res.body.code).toEqual(templates.invalidArguments.code);
 		});
 
-		test('should fail if the list containers a container', async () => {
+		test('should fail if the favourites list provided has a container', async () => {
 			const res = await agent.patch(`${route}?key=${users.noProjectAccess.apiKey}`)
 				.expect(templates.invalidArguments.status).send({ federations: [container._id] });
 			expect(res.body.code).toEqual(templates.invalidArguments.code);
@@ -212,6 +383,7 @@ describe('E2E routes/teamspaces/projects/federations', () => {
 	});
 	afterAll(() => ServiceHelper.closeApp(server));
 	testGetFederationList();
+	testGetFederationStats();
 	testAppendFavourites();
 	testDeleteFavourites();
 });
