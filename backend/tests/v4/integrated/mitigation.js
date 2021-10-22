9f5df89d939869a69b90498be35d9f443debccbc
Keif Gwinn <keif.gwinn@3drepo.org>
2021-10-22 11:15:34 +0100

    #2827 debug structure

--- backend/tests/v4/integrated/mitigation.js
+++ backend/tests/v4/integrated/mitigation.js
@@ -241,7 +241,9 @@ describe("Mitigations", function () {
 		it("should succeed", function(done) {
 			agent.get(`/${username}/mitigations/criteria`)
 				.expect(200, function(err, res) {
-					expect(res.body.sort()).to.deep.equal(goldenCriteria.sort());
+					console.log(res.body)
+					const listOrder = (a, b) => a.name < b.name ? -1 : 1;
+					expect(res.body.sort(listOrder)).to.deep.equal(goldenCriteria.sort(listOrder));
 					done(err);
 				});
 		});
