148c3b5b5fe659ef85dd49573c311904a196720e
Keif Gwinn <keif.gwinn@3drepo.org>
2021-10-26 14:58:33 +0100

    #2827 fixed criteria

--- backend/tests/v4/integrated/mitigation.js
+++ backend/tests/v4/integrated/mitigation.js
@@ -376,23 +376,20 @@ describe("Mitigations", function () {
 		});
 
 		it("by multiple criteria should succeed", function(done) {
-
-			// const criteria = {
-			// 		mitigation_stage: ['Preliminary Design'],
-			// 		mitigation_type: ['Eliminate'],
-			// 		category: ['Fall-From open edge'],
-			// 		location_desc: ['High Level-Near Opening'],
-			// 		element: ['Slab'],
-			// 		risk_factor: ['Physical-Opening'],
-			// 		scope: ['In situ concrete'],
-			// 		associated_activity: ['Install construction']
-			// }
-
-			const criteria = {};
-			Object.keys(goldenCriteria).forEach((key) => {
-				criteria[key] = goldenCriteria[key][0];
-			});
-
+			// const criteria = {};
+			// Object.keys(goldenCriteria).forEach((key) => {
+			// 	criteria[key] = goldenCriteria[key][0];
+			// });
+			const criteria = {
+				mitigation_stage: ['Preliminary Design'],
+				mitigation_type: ['Eliminate'],
+				category: ['Fall-From open edge'],
+				location_desc: ['High Level-Near Opening'],
+				element: ['Slab'],
+				risk_factor: ['Physical-Opening'],
+				scope: ['In situ concrete'],
+				associated_activity: ['Install construction']
+		}
 			console.log(criteria)
 			agent.post(`/${username}/mitigations`)
 				.send(criteria)
