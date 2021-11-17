82d2760f7960d619ea1006aa1cca6531128ae759
Santiago Montero <santiago.montero@3drepo.org>
2021-11-17 13:21:07 +0000

    ISSUE #2857  - when compiling languages compacts the catalog

--- frontend/internals/intl/formatter.js
+++ frontend/internals/intl/formatter.js
@@ -1,1 +1,7 @@
-exports.compile = (msgs) => msgs;
+exports.compile = (msgs) => Object.keys(msgs).reduce((compacted, key) => {
+	if (msgs[key]) {
+		compacted[key] = msgs[key];
+	}
+
+	return compacted;
+}, {});
