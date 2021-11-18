44768b13c186afb6a0d32ffcd2be55b67f00cfd7
Charence <charencewong@hotmail.com>
2021-11-18 12:44:34 +0000

    ISSUE #2769 - satisfy lint

--- backend/tests/v5/e2e/routes/teamspaces/projects/containers/containers.test.js
+++ backend/tests/v5/e2e/routes/teamspaces/projects/containers/containers.test.js
@@ -109,7 +109,7 @@ const models = [
 ];
 
 // setup sub model
-models[2].properties.subModels = [ { model: models[1]._id, database: teamspace } ];
+models[2].properties.subModels = [{ model: models[1]._id, database: teamspace }];
 
 const revisions = [
 	ServiceHelper.generateRevisionEntry(),
