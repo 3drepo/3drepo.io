ffa301b944a80792d6a8147cbd9ad4855a19b8aa
ChristosTsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2021-11-26 15:52:44 +0000

    ISSUE #2865 validateLogoutData method removed and checkUserExists is called instead Elastic method for creating login record decoupled fom loginRecord model respond method now accepts a username so we can log it in the logout call as well unit tests for auth input validation and middleware added

---
+++ backend/tests/v5/unit/models/loginRecords.test.js
@@ -0,0 +1,68 @@
+ const { src } = require('../../helper/path');
+
+ const _ = require('lodash');
+ 
+ const db = require(`${src}/handler/db`);
+ const { templates } = require(`${src}/utils/responseCodes`);
+ 
+ const LoginRecord = require(`${src}/models/loginRecord`);
+ 
+ const testSaveLoginRecord = () => {    
+     const checkResults = (fn, teamspace, arr, dataOverride) => {
+         expect(fn.mock.calls.length).toBe(1);
+         expect(fn.mock.calls[0][2]).toEqual({ user });
+         expect(fn.mock.calls[0][3]).toEqual(determineAction(teamspace, arr, dataOverride));
+     };
+ 
+     describe('Add containers to favourites', () => {
+         test('Should add favourite containers if the user has no favourites entry', async () => {
+             jest.spyOn(db, 'findOne').mockResolvedValue({ customData: {} });
+             const fn = jest.spyOn(db, 'updateOne').mockImplementation(() => {});
+             const teamspace = 'teamspace3';
+             const arr = ['e', 'f'];
+             await expect(User.appendFavourites(user, teamspace, arr)).resolves.toBe(undefined);
+             checkResults(fn, teamspace, arr, {});
+         });
+ 
+         test('Should add favourite containers under a new teamspace', async () => {
+             jest.spyOn(db, 'findOne').mockResolvedValue({ customData: favouritesData });
+             const fn = jest.spyOn(db, 'updateOne').mockImplementation(() => {});
+             const teamspace = 'teamspace3';
+             const arr = ['e', 'f'];
+             await expect(User.appendFavourites(user, teamspace, arr)).resolves.toBe(undefined);
+             checkResults(fn, teamspace, arr);
+         });
+ 
+         test('Should add favourite containers on an existing teamspace', async () => {
+             jest.spyOn(db, 'findOne').mockResolvedValue({ customData: favouritesData });
+             const fn = jest.spyOn(db, 'updateOne').mockImplementation(() => {});
+             const teamspace = 'teamspace1';
+             const arr = ['d', 'e'];
+             await expect(User.appendFavourites(user, teamspace, arr)).resolves.toBe(undefined);
+             checkResults(fn, teamspace, arr);
+         });
+ 
+         test('Should add favourite containers on an existing teamspace and ignore duplicate entries', async () => {
+             jest.spyOn(db, 'findOne').mockResolvedValue({ customData: favouritesData });
+             const fn = jest.spyOn(db, 'updateOne').mockImplementation(() => {});
+             const teamspace = 'teamspace1';
+             const arr = ['a', 'b', 'c', ' d', 'e'];
+             await expect(User.appendFavourites(user, teamspace, arr)).resolves.toBe(undefined);
+             checkResults(fn, teamspace, arr);
+         });
+ 
+         test('Should return error when trying to add favourites to a user that doesnt exist', async () => {
+             jest.spyOn(db, 'findOne').mockResolvedValue(undefined);
+             const fn = jest.spyOn(db, 'updateOne').mockImplementation(() => {});
+             await expect(User.appendFavourites(user, 'teamspace3', ['e', 'f']))
+                 .rejects.toEqual(templates.userNotFound);
+             expect(fn.mock.calls.length).toBe(0);
+         });
+     });
+ };
+ 
+ 
+ describe('models/loginRecord', () => {
+    testSaveLoginRecord();
+ });
+ 
