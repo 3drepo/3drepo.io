a68512b782d3200262362901f6bff62e4e5dd65e
Carmen Fan <wind5320@hotmail.com>
2021-12-13 17:58:04 +0000

    ISSUE #2868 quota check was getting in the way of permissions

--- backend/src/v5/middleware/dataConverter/inputs/teamspaces/projects/models/commons/revisions.js
+++ backend/src/v5/middleware/dataConverter/inputs/teamspaces/projects/models/commons/revisions.js
@@ -29,13 +29,13 @@ const Revisions = {};
 
 const ACCEPTED_MODEL_EXT = [
 	'.x', '.obj', '.3ds', '.md3', '.md2', '.ply',
-	'mdl', '.ase', '.hmp', '.smd', '.mdc', '.md5',
-	'stl', '.lxo', '.nff', '.raw', '.off', '.ac',
-	'bvh', '.irrmesh', '.irr', '.q3d', '.q3s', '.b3d',
-	'dae', '.ter', '.csm', '.3d', '.lws', '.xml', '.ogex',
-	'ms3d', '.cob', '.scn', '.blend', '.pk3', '.ndo',
-	'ifc', '.xgl', '.zgl', '.fbx', '.assbin', '.bim', '.dgn',
-	'rvt', '.rfa', '.spm', '.dwg', '.dxf',
+	'.mdl', '.ase', '.hmp', '.smd', '.mdc', '.md5',
+	'.stl', '.lxo', '.nff', '.raw', '.off', '.ac',
+	'.bvh', '.irrmesh', '.irr', '.q3d', '.q3s', '.b3d',
+	'.dae', '.ter', '.csm', '.3d', '.lws', '.xml', '.ogex',
+	'.ms3d', '.cob', '.scn', '.blend', '.pk3', '.ndo',
+	'.ifc', '.xgl', '.zgl', '.fbx', '.assbin', '.bim', '.dgn',
+	'.rvt', '.rfa', '.spm', '.dwg', '.dxf',
 ];
 
 Revisions.validateUpdateRevisionData = async (req, res, next) => {
