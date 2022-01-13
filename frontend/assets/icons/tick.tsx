67e3ba55794e996238b1ff6ec6be950c70e0b859
Amantini <amantini1997>
2022-01-13 14:59:40 +0000

    ISSUE #2887 - add SVGs for copy-to-clipboard action

---
+++ frontend/assets/icons/tick.tsx
@@ -0,0 +1,24 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+
+export default () => (
+	<svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M1 5.64286L6.57143 10.2857L14 1" stroke="#00C1D4" strokeWidth="1.33333" />
+	</svg>
+);
