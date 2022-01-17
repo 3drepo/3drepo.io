73f6e6dcd3bca0a3b26964e9063715f3c384dc8e
Amantini <amantini1997>
2022-01-17 14:51:23 +0000

    ISSUE #2887 - update SVG icons

--- frontend/assets/icons/tick.tsx
+++ frontend/assets/icons/tick.tsx
@@ -18,7 +18,7 @@
 import React from 'react';
 
 export default () => (
-	<svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path d="M1 5.64286L6.57143 10.2857L14 1" stroke="#00C1D4" strokeWidth="1.33333" />
+	<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M1 6L7 11L15 1" stroke="#00C1D4" strokeWidth="1.33333" />
 	</svg>
 );
