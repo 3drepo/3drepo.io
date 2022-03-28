300e2376e039aefad12f5ea15cc5f97ebb738c67
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-03-28 12:44:34 +0100

    ISSUE #3082 - create signup sidebar

---
+++ frontend/src/v5/ui/routes/userSignup/userSignupSidebar/userSignupSidebar.component.tsx
@@ -0,0 +1,111 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { FormattedMessage } from 'react-intl';
+import {
+	Container,
+	Logo,
+	BulletPoint,
+	BulletPointMessage,
+	BulletPointTitle,
+	BulletPointBody,
+	BulletPointIcon,
+	Check,
+	BookADemoButton,
+	LaptopIcon,
+	MainTitle,
+} from './userSignupSidebar.styles';
+
+export const UserSignupSidebar = () => (
+	<Container>
+		<Logo />
+		<MainTitle>
+			<FormattedMessage
+				id="userSignup.sidebar"
+				defaultMessage={`
+					3D Repo is the most accessible BIM 
+					collaboration platform to enable open 
+					and transparent communication for AECO 
+					projects of all sizes.
+				`}
+			/>
+		</MainTitle>
+		<BulletPoint>
+			<BulletPointIcon>
+				<Check />
+			</BulletPointIcon>
+			<BulletPointMessage>
+				<BulletPointTitle>
+					<FormattedMessage
+						id="userSignup.sidebar.noSoftwareInstallation.title"
+						defaultMessage="Security & encryption"
+					/>
+				</BulletPointTitle>
+				<BulletPointBody>
+					<FormattedMessage
+						id="userSignup.sidebar.noSoftwareInstallation.message"
+						defaultMessage="Reduce required BIM coordination and data checking time by up to 35%."
+					/>
+				</BulletPointBody>
+			</BulletPointMessage>
+		</BulletPoint>
+		<BulletPoint>
+			<BulletPointIcon>
+				<Check />
+			</BulletPointIcon>
+			<BulletPointMessage>
+				<BulletPointTitle>
+					<FormattedMessage
+						id="userSignup.sidebar.securityAndExnryption.title"
+						defaultMessage="No software installation"
+					/>
+				</BulletPointTitle>
+				<BulletPointBody>
+					<FormattedMessage
+						id="userSignup.sidebar.securityAndExnryption.message"
+						defaultMessage="Automating manual tasks can boost related data accuracy by up to 95%."
+					/>
+				</BulletPointBody>
+			</BulletPointMessage>
+		</BulletPoint>
+		<BulletPoint>
+			<BulletPointIcon>
+				<Check />
+			</BulletPointIcon>
+			<BulletPointMessage>
+				<BulletPointTitle>
+					<FormattedMessage
+						id="userSignup.sidebar.integration.title"
+						defaultMessage="Integration - open API’s"
+					/>
+				</BulletPointTitle>
+				<BulletPointBody>
+					<FormattedMessage
+						id="userSignup.sidebar.integration.message"
+						defaultMessage="Get the whole team on the same page and talking to each other."
+					/>
+				</BulletPointBody>
+			</BulletPointMessage>
+		</BulletPoint>
+		<BookADemoButton
+			to={{ pathname: 'https://3drepo.com/demo/' }}
+			startIcon={<LaptopIcon />}
+		>
+			<FormattedMessage id="userSignup.sidebar.bookADemo" defaultMessage="Book A Demo" />
+		</BookADemoButton>
+	</Container>
+);
---
+++ frontend/src/v5/ui/routes/userSignup/userSignupSidebar/userSignupSidebar.styles.ts
@@ -0,0 +1,111 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import CheckBase from '@assets/icons/check.svg';
+import { Button } from '@controls/button';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import LatopIconBase from '@assets/icons/outlined/laptop-outlined.svg';
+import { Display } from '@/v5/ui/themes/media';
+
+export const Container = styled.div`
+	margin: auto;
+	height: 100%;
+	width: 400px;
+	padding: 0 70px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	background-color: ${({ theme }) => theme.palette.secondary.main};
+	color: ${({ theme }) => theme.palette.primary.contrast};
+	
+	@media (max-width: ${Display.Tablet}px) {
+		display: none;
+	}
+`;
+
+export const Logo = styled.img.attrs({
+	src: 'assets/images/3drepo-logo-white.png',
+	alt: '3D Repo',
+})`
+	width: 100px;
+`;
+
+export const MainTitle = styled.div`
+	${({ theme }) => theme.typography.h2};
+	font-weight: lighter;
+	letter-spacing: 0.03rem;
+	margin: 36px 0 22px 0;
+`;
+
+export const BulletPoint = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: flex-start;
+`;
+
+export const BulletPointIcon = styled.div`
+	display: flex;
+    align-items: center;
+    justify-content: center;
+	border: solid 1px ${({ theme }) => theme.palette.primary.main};
+	border-radius: 50%;
+	min-width: 21px;
+	min-height: 21px;
+	margin-right: 10px;
+`;
+
+export const Check = styled(CheckBase)`
+	width: 13px;
+	height: 13px;
+	color: ${({ theme }) => theme.palette.primary.main};
+`;
+
+export const BulletPointMessage = styled.div`
+	display: flex;
+	flex-direction: column;
+	${({ theme }) => theme.typography.body1};
+`;
+
+export const BulletPointTitle = styled.div`
+	${({ theme }) => theme.typography.h3};
+	font-size: 1rem;
+	font-weight: 600;
+`;
+
+export const BulletPointBody = styled.div`
+	color: ${({ theme }) => theme.palette.base.main};
+    margin-bottom: 10px;
+    line-height: 1.4rem;
+`;
+
+export const BookADemoButton = styled(Button).attrs({
+	component: Link,
+	target: '_blank',
+	variant: 'contained',
+	color: 'primary',
+})`
+	width: fit-content;
+	font-weight: 300;
+	margin: 28px 0 0 0;
+    padding: 10px 20px 10px 28px;
+`;
+
+export const LaptopIcon = styled(LatopIconBase)`
+	width: 16px;
+	height: 16px;
+`;
