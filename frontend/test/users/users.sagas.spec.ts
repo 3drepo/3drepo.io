74884e407655614fbb1b2751eb25f5fdf301e3c0
Santiago Montero <santiago.montero@3drepo.org>
2022-04-26 09:47:15 -0300

    ISSUE #3073 - Added test for testing a bad api request in users

--- frontend/test/users/users.sagas.spec.ts
+++ frontend/test/users/users.sagas.spec.ts
@@ -15,6 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { DialogsActions, DialogsTypes } from '@/v5/store/dialogs/dialogs.redux';
 import { UsersActions } from '@/v5/store/users/users.redux';
 import * as UsersSaga from '@/v5/store/users/users.sagas';
 import { expectSaga } from 'redux-saga-test-plan';
@@ -26,6 +27,12 @@ describe('Users: sagas', () => {
 	const members = [];
 	const teamspace = 'myTeamspace'
 
+	// This action creator is being hijacked becayse we actually dont care about the props for testing,
+	// only that a dialog of type alert is being dispatched.
+	const spy = jest.spyOn(DialogsActions, 'open').mockImplementation((modalType,props) => {
+		return ({ type: DialogsTypes.OPEN, modalType});
+	});
+
 	describe('fetch', () => {
 		it('should fetch users data and dispatch FETCH_SUCCESS', async () => {
 			mockServer
@@ -35,6 +42,17 @@ describe('Users: sagas', () => {
 			await expectSaga(UsersSaga.default)
 					.dispatch(UsersActions.fetchUsers(teamspace))
 					.put(UsersActions.fetchUsersSuccess(teamspace, members))
+					.silentRun();
+		});
+
+		it('should handle fetch users api error and dispatch a dialog opening for an alert', async () => {
+			mockServer
+				.get(`/teamspaces/nonExistentTeamspace/members`)
+				.reply(404)
+
+			await expectSaga(UsersSaga.default)
+					.dispatch(UsersActions.fetchUsers(teamspace))
+					.put(DialogsActions.open('alert'))
 					.silentRun();
 		});
 
