bbaa8b73fbb853970e915b725bd0e9fcecb45ef9
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-05-16 15:40:22 +0100

    ISSUE #3226 - fix header and delete button

--- frontend/src/v5/ui/v4Adapter/overrides/cards/issues/shapes.overrides.ts
+++ frontend/src/v5/ui/v4Adapter/overrides/cards/issues/shapes.overrides.ts
@@ -19,7 +19,14 @@ import { css } from 'styled-components';
 import { Content } from '@/v4/routes/viewerGui/components/risks/components/riskDetails/riskDetails.styles';
 import { MeasuringTypeContainer } from '@/v4/routes/viewerGui/components/risks/components/shapesFormTab/shapesFormTab.styles';
 import { Wrapper as Measurement } from '@/v4/routes/viewerGui/components/measurements/components/measuringType/measuringType.styles';
-import { SectionHeader, List, StyledIconButton as ExpandListButton, Container as MeasurementType } from '@/v4/routes/viewerGui/components/measurements/components/measurementsList/measurementsList.styles';
+import {
+	SectionHeader,
+	List,
+	StyledIconButton as ExpandListButton,
+	Container as MeasurementType,
+	Title,
+	Units,
+} from '@/v4/routes/viewerGui/components/measurements/components/measurementsList/measurementsList.styles';
 import { StyledTextField, MeasurementPoint, MeasurementValue } from '@/v4/routes/viewerGui/components/measurements/components/measureItem/measureItem.styles';
 import { ColorSelect, Dot, StyledIconButton as ColorPickerStyledButton } from '@/v4/routes/components/colorPicker/colorPicker.styles';
 import { StyledMarkdownField, StyledLinkableField, ActionsLine } from '@/v4/routes/components/textField/textField.styles';
@@ -50,6 +57,14 @@ export const measurementsStying = css`
 				font-size: 24px;
 			}
 		}
+			
+		${Title} {
+			font-size: 13px;
+		}
+
+		${Units} {
+			padding-right: 7px;
+		}
 	}
 
 	${MeasurementType}:not(:last-of-type) {
@@ -60,15 +75,16 @@ export const measurementsStying = css`
 	${List} {
 		// icons
 		${SmallIconButtonStyled} {
-			height: 7px;
-			width: 7px;
+			height: 13px;
+			width: 13px;
+			box-sizing: border-box;
 			padding: 4px;
 			&, &:hover {
 				background-color: ${({ theme }) => theme.palette.secondary.main};
 			}
 
 			svg {
-				font-size: 13px;
+				font-size: 11px;
 				color: ${({ theme }) => theme.palette.primary.contrast};
 			}
 		}
@@ -169,151 +185,7 @@ export default css`
 		${measurementsStying}
 
 		${MeasurementType} {
-			margin: 0 -14px;
-		}
-	}
-`;
-
-
-	// expandable list header
-	${SectionHeader} {
-		background-color: inherit;
-		border-bottom: none;
-
-		${ExpandListButton} {
-			margin: 0;
-			background-color: transparent;
-		}
-
-		* {
-			font-size: 12px;
-			color: ${({ theme }) => theme.palette.secondary.main};
-			font-weight: 500;
-
-			svg {
-				font-size: 24px;
-			}
-		}
-	}
-
-	${MeasurementType}:not(:last-of-type) {
-		border-bottom: 1px solid ${({ theme }) => theme.palette.base.lightest};
-	}
-
-	// expandable list
-	${List} {
-		// icons
-		${SmallIconButtonStyled} {
-			height: 7px;
-			width: 7px;
-			padding: 4px;
-			&, &:hover {
-				background-color: ${({ theme }) => theme.palette.secondary.main};
-			}
-
-			svg {
-				font-size: 13px;
-				color: ${({ theme }) => theme.palette.primary.contrast};
-			}
-		}
-
-		svg {
-			font-size: 17px;
-		}
-
-		&:last-of-type {
-			margin-bottom: -5px;
-		}
-
-		// measurement item
-		li {
-			background-color: inherit;
-			border: none;
-
-			button {
-				&:hover {
-					background-color: transparent;
-				}
-				
-				svg {
-					color: ${({ theme }) => theme.palette.secondary.main};
-				}
-			}
-
-			&:last-of-type {
-				padding-bottom: 10px;
-			}
-		}
-
-		${MeasurementPoint},
-		${MeasurementValue} {
-			font-size: 12px;
-			color: ${({ theme }) => theme.palette.secondary.main};
-		}
-
-		.MuiFormControl-root {
-			width: 113%;
-			margin-left: -15px;
-		}
-
-		${StyledTextField} {
-			overflow: visible;
-
-			${StyledMarkdownField},
-			${StyledLinkableField} {
-				font-size: 12px;
-				color: ${({ theme }) => theme.palette.secondary.main};
-			}
-
-			${ActionsLine} {
-				bottom: 4px;
-
-				button {
-					margin: 0;
-				}
-			}
-		}
-	}
-`;
-
-export const colorPickerStyling = css`
-	// color picker
-	${ColorSelect} {
-		width: unset;
-		padding-left: 8px;
-		border-radius: 5px;
-		background-color: ${({ theme }) => theme.palette.tertiary.lightest};
-
-		${Dot} {
-			width: 13px;
-			height: 13px;
-		}
-
-		${ColorPickerStyledButton} {
-			margin: 0;
-
-			&:hover {
-				background-color: transparent;
-			}
-		}
-	}
-`;
-
-export default css`
-	${colorPickerStyling}
-
-	${Content} {
-		align-items: center;
-		
-		${MeasuringTypeContainer} {
-			color: ${({ theme }) => theme.palette.base.main};
-			font-size: 10px;
-		}
-
-		${measurementsStying}
-
-		${MeasurementType} {
-			margin: 0 -14px;
+			margin: 0 -16px;
 		}
 	}
 `;
