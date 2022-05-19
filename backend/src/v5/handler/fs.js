e5a3476de8519928d15e260568eeec151e9f7066
Carmen Fan <wind5320@hotmail.com>
2022-05-19 12:32:46 +0100

    ISSUE #3276 remove erroneous/deprecated changes/files

--- backend/src/v5/handler/fs.js
+++ backend/src/v5/handler/fs.js
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2022 3D Repo Ltd
+ *  Copyright (C) 2021 3D Repo Ltd
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -15,8 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* istanbul ignore file */
 const { v4Path } = require('../../interop');
 // eslint-disable-next-line import/no-dynamic-require, security/detect-non-literal-require, require-sort/require-sort
-const Mailer = require(`${v4Path}/mailer/mailer`);
-
-module.exports = Mailer;
+module.exports = require(`${v4Path}/handler/fs`);
