6e6b16132313d7481284668b4345c2dadd20da3f
Carmen Fan <wind5320@hotmail.com>
2022-05-20 18:01:51 +0100

    ISSUE #3256 more tests

--- backend/tests/v5/unit/services/filesManager.test.js
+++ backend/tests/v5/unit/services/filesManager.test.js
@@ -23,14 +23,41 @@ const db = require(`${src}/handler/db`);
 jest.mock('../../../../src/v5/models/fileRefs');
 const fileRefs = require(`${src}/models/fileRefs`);
 
+jest.mock('../../../../src/v5/handler/fs');
+const FSHandler = require(`${src}/handler/fs`);
+
+jest.mock('../../../../src/v5/handler/gridfs');
+const GridFSHandler = require(`${src}/handler/gridfs`);
+
 const FilesManager = require(`${src}/services/filesManager`);
+
+const { templates } = require(`${src}/utils/responseCodes`);
 
 const testRemoveAllFilesFromModel = () => {
 	describe('Remove all files from model', () => {
+		test('Should not do any calls if no ref collections are found', async () => {
+			const model = generateRandomString();
+			db.listCollections.mockResolvedValueOnce([
+				`${model}.${generateRandomString()}`,
+				`${model}.${generateRandomString()}`,
+				generateRandomString(),
+			].map((name) => ({ name })));
+			const teamspace = generateRandomString();
+
+			await FilesManager.removeAllFilesFromModel(teamspace, model);
+
+			expect(db.listCollections).toHaveBeenCalledTimes(1);
+			expect(db.listCollections).toHaveBeenCalledWith(teamspace);
+
+			expect(fileRefs.getAllRemovableEntriesByType).not.toHaveBeenCalled();
+		});
+
 		test('Should do the relevant calls to remove files', async () => {
 			const model = generateRandomString();
+
 			const refCol1 = `${model}.${generateRandomString()}.ref`;
 			const refCol2 = `${model}.${generateRandomString()}.ref`;
+
 			db.listCollections.mockResolvedValueOnce([
 				`${model}.${generateRandomString()}`,
 				`${model}.${generateRandomString()}`,
@@ -39,10 +66,62 @@ const testRemoveAllFilesFromModel = () => {
 				`${model}.${generateRandomString()}.aaref`,
 				generateRandomString(),
 				refCol2,
-			]);
+			].map((name) => ({ name })));
+
+			const refCol1Data = [
+				{
+					_id: 'fs',
+					links: [generateRandomString(), generateRandomString(), generateRandomString()],
+				},
+				{
+					_id: 'gridfs',
+					links: [generateRandomString(), generateRandomString(), generateRandomString()],
+				},
+			];
+
+			fileRefs.getAllRemovableEntriesByType
+				.mockImplementation((ts, col) => Promise.resolve(col === refCol1 ? refCol1Data : [{ _id: 'fs', links: [] }]));
+
 			const teamspace = generateRandomString();
 
 			await FilesManager.removeAllFilesFromModel(teamspace, model);
+
+			expect(db.listCollections).toHaveBeenCalledTimes(1);
+			expect(db.listCollections).toHaveBeenCalledWith(teamspace);
+
+			expect(fileRefs.getAllRemovableEntriesByType).toHaveBeenCalledTimes(2);
+			expect(fileRefs.getAllRemovableEntriesByType).toHaveBeenNthCalledWith(1, teamspace, refCol1);
+			expect(fileRefs.getAllRemovableEntriesByType).toHaveBeenNthCalledWith(2, teamspace, refCol2);
+
+			expect(FSHandler.removeFiles).toHaveBeenCalledTimes(1);
+			expect(FSHandler.removeFiles).toHaveBeenCalledWith(refCol1Data[0].links);
+
+			expect(GridFSHandler.removeFiles).toHaveBeenCalledTimes(1);
+			expect(GridFSHandler.removeFiles).toHaveBeenCalledWith(teamspace, refCol1, refCol1Data[1].links);
+		});
+
+		test('Should error if the type of storage is unknown', async () => {
+			const model = generateRandomString();
+
+			const refCol1 = `${model}.${generateRandomString()}.ref`;
+
+			db.listCollections.mockResolvedValueOnce([
+				refCol1,
+			].map((name) => ({ name })));
+
+			fileRefs.getAllRemovableEntriesByType
+				.mockResolvedValueOnce([{ _id: 'aaafs', links: [generateRandomString()] }]);
+
+			const teamspace = generateRandomString();
+
+			await expect(FilesManager.removeAllFilesFromModel(teamspace, model))
+				.rejects.toEqual(templates.fileNotFound);
+
+			expect(db.listCollections).toHaveBeenCalledTimes(1);
+			expect(db.listCollections).toHaveBeenCalledWith(teamspace);
+
+			expect(fileRefs.getAllRemovableEntriesByType).toHaveBeenCalledTimes(1);
+			expect(fileRefs.getAllRemovableEntriesByType).toHaveBeenNthCalledWith(1, teamspace, refCol1);
 		});
 	});
 };
