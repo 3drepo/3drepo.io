83b001123fb4c048c5fc1d4f5121bbebfbdcba5f
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-05-24 10:58:17 +0100

    ISSUE #3262 - remove unused import

--- frontend/src/v5/ui/v4Adapter/dialogs/v4DialogsOverrides.styles.ts
+++ frontend/src/v5/ui/v4Adapter/dialogs/v4DialogsOverrides.styles.ts
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import generalDialogStyles from './generalDialogStyling.overrides';
 import attachResourcesDialog from './attachResourcesDialog.overrides';
 import screenshotDialog from './screenshotDialog.overrides';
