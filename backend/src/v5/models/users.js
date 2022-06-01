277fbedad6ce8a80a382fbe279cf5eb5a3832da0
ChristosTsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2022-06-01 10:39:59 +0100

    ISSUE #3306 getQuotaInfo basic implementation

--- backend/src/v5/models/users.js
+++ backend/src/v5/models/users.js
@@ -267,4 +267,8 @@ User.uploadAvatar = (username, avatarBuffer) => updateUser(username, { $set: { '
 User.updateResetPasswordToken = (username, resetPasswordToken) => updateUser(username,
 	{ $set: { 'customData.resetPasswordToken': resetPasswordToken } });
 
+User.getQuotaInfo = async () => {
+	
+}
+
 module.exports = User;
