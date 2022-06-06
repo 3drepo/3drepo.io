f5004de2958a8032fd1e69a5d15aa2803ec809f2
The-Daniel <bibshall@gmail.com>
2022-06-06 16:20:04 +0100

    ISSUE #3317 - general ui changes for the safetiBase window

---
+++ frontend/src/v5/ui/v4Adapter/overrides/cards/safetiBase/safetiBase.overrides.ts
@@ -0,0 +1,22 @@
+import { StyledTab, StyledTabs, TabContent } from '@/v4/routes/viewerGui/components/risks/components/riskDetails/riskDetails.styles';
+import { css } from 'styled-components';
+
+export default css`
+	${StyledTabs} {
+		width: auto;
+		left: 0;
+		${StyledTab} {
+			font-size: 13px;
+		}
+	}
+	
+	.MuiTabScrollButton-root {
+		display: none;
+	}
+	${TabContent} {
+		padding: 0 15px;
+		/* TODO - fix after new palette is released */
+		background-color: #F7F8FA;
+		
+	}
+`;
