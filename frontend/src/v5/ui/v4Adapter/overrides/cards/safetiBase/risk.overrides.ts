c306300d5991ee6225540f4952a9e3176fb147bb
The-Daniel <bibshall@gmail.com>
2022-06-06 17:34:38 +0100

    ISSUE #3317 - add missing license headers

--- frontend/src/v5/ui/v4Adapter/overrides/cards/safetiBase/risk.overrides.ts
+++ frontend/src/v5/ui/v4Adapter/overrides/cards/safetiBase/risk.overrides.ts
@@ -1,3 +1,20 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { css } from 'styled-components';
 import { Header } from '@/v4/routes/viewerGui/components/risks/components/levelOfRisk/levelOfRisk.styles';
 import { DateFieldContainer } from '@/v4/routes/viewerGui/components/risks/components/mainRiskFormTab/mainRiskFormTab.styles';
