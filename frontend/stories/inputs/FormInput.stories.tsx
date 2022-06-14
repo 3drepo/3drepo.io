fb01ef0ac383578547eb3f816c4248d707bd232a
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-06-14 12:40:09 +0100

    ISSUE #3008 - create form file input story component

--- frontend/stories/inputs/FormInput.stories.tsx
+++ frontend/stories/inputs/FormInput.stories.tsx
@@ -14,8 +14,9 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { FormFileInput } from '@controls/formFileInput/FormFileInput.component';
+import { FormFileInput } from '@controls/formFileInput/formFileInput.component';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { useForm } from 'react-hook-form';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -50,26 +51,31 @@ export default {
 } as ComponentMeta<typeof FormFileInput>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof FormFileInput> = (args) => <FormFileInput {...args} />;
+const Template: ComponentStory<typeof FormFileInput> = (args) => {
+	const {
+		control,
+	} = useForm<{ fileAvatar: File }>({
+		mode: 'onChange',
+	});
+	return <FormFileInput control={control} name="file" {...args} />;
+}
 
 export const ContainedPrimaryButton = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 ContainedPrimaryButton.args = {
 	label: 'Contained button',
-	name: 'file',
 	buttonProps: {
 		color: 'primary',
 		variant: 'contained',
 	},
 };
 
-export const FilledSecondaryButtonWithError = Template.bind({});
-FilledSecondaryButtonWithError.args = {
-	label: 'Filled button',
-	name: 'file',
+export const OutlinedSecondaryButtonWithError = Template.bind({});
+OutlinedSecondaryButtonWithError.args = {
+	label: 'Outlined button',
 	buttonProps: {
 		color: 'secondary',
-		variant: 'filled',
+		variant: 'outlined',
 	},
 	formError: {
 		message: 'Error message',
