67899f733597704dba53de431062ab0ee06fa8e8
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-06-23 17:31:12 +0100

    ISSUE #3307 - add top bar styling

---
+++ frontend/src/v5/ui/routes/dashboard/teamspaces/teampsace.styles.ts
@@ -0,0 +1,49 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import styled from 'styled-components';
+import { Typography } from '@mui/material';
+
+export const Content = styled.section`
+	background-color: ${({ theme }) => theme.palette.tertiary.lightest};
+	overflow-y: auto;
+	flex-grow: 1;
+`;
+
+export const TopBar = styled.div`
+	background: ${({ theme }) => theme.palette.gradient.secondary};
+	padding: 30px 75px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+	// TODO: fix after new palette is released
+	border-top: 1px solid ${({ theme }) => theme.palette.secondary.mid};
+`;
+
+export const TeamspaceInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	color: ${({ theme }) => theme.palette.primary.contrast};
+	margin-left: 29px;
+`;
+
+export const TeamspaceName = styled(Typography).attrs({
+	variant: 'h1',
+})``;
