c36a07ffdd48248fbc764f4dd6b0cd01e6de5733
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-06-27 17:55:39 +0100

    ISSUE #3307 - fix imports order

--- frontend/src/v5/ui/routes/dashboard/teamspaces/teamspaceContent/teamspaceContent.component.tsx
+++ frontend/src/v5/ui/routes/dashboard/teamspaces/teamspaceContent/teamspaceContent.component.tsx
@@ -18,13 +18,13 @@
 import { discardSlash } from '@/v5/services/routing/routing';
 import { useRouteMatch, Route, Switch, Redirect } from 'react-router-dom';
 import { ScrollArea } from '@controls/scrollArea';
-import { ProjectsList } from '../projects/projectsList.component';
 import { NOT_FOUND_ROUTE_PATH } from '@/v5/ui/routes/routes.constants';
+import { ProjectsList } from '../projects/projectsList.component';
 
 export const TeamspaceContent = () => {
 	let { path } = useRouteMatch();
 	path = discardSlash(path);
-	
+
 	return (
 		<ScrollArea variant="base" autoHide>
 			<Switch>
