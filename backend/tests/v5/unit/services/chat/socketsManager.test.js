a1158f3e6b4991d169a9670d72ad925df83f433f
Christos Tsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2022-07-05 12:32:06 +0100

    Issue 3312 - Additional real time events (#3366)
    * ISSUE #3312 new eventManager and chat events added
    New event methods added and attached to events
    deleteContainer and deleteFederation processor method modified to fire events
    updateRevisionStatus now fires event
    addModel now fires event with different params
    
    * ISSUE #3312 projectEvents file added
    createProjectMessage method added in chat service
    CONTAINER_NEW_REVISION event is fired from queueTasksCompleted method
    revisionAdded and modelAdded method added in modelEvents
    
    * ISSUE #3312 NEW_MODEL even trigger moved to container and federation processors
    
    * ISSUE #3312 NEW model event reigger moved back to model method
    Small adjustments to testUpdateRevisionStatus test
    
    * ISSUE #3312 unit tests added for all new events
    
    * ISSUE #3312 README.md file updated to include new events
    
    * ISSUE #3312 NEW_REVISION event is triggered from newRevisionProcessed method
    joinRoom method changed so that it supports projects channel
    unit tests adjusted
    
    * ISSUE #3312 NEW_REVISION event is triggered from newRevisionProcessed method
    joinRoom method changed so that it supports projects channel
    unit tests adjusted
    
    * ISSUE #3312 projectEventsListener deleted and its method is moved to modelEventsListener
    DELETE_MODEL event is thrown from modelSettings instead of containers
    all tests adjusted
    commented tests in socketsManager.test.js removed
    
    * ISSUE #3312 socket header is retrieved in all routes necessary and passed through to processor and model methods
    change in joinRoom method to cover scenario of teamspace admin in project model
    e2e tests for most chat events added
    
    * ISSUE #3312 test adjustments to include sender param
    
    * ISSUE #3312 sender is not sent to revisionAdded callback method
    revisionAddedTest and revisionUpdateTest e2e tests added
    
    * ISSUE #3312 yarn lint:fix
    
    * ISSUE #3312 name is passed in NEW_MODEL event
    README.md updated
    
    * ISSUE #3312 revision id is passed in new revision chat events
    test adjusted
    
    * ISSUE #3312 revisionAddedTest test removed and its checks are placed in queueFinishedTest
    
    * ISSUE #3312 try catch added in createDirectMessage, createModelMessage, createProjectMessage and removed from event callbacks
    eventsListsener method names renamed
    uneccessary tests removed from eventsListener and new ones added
    
    * ISSUE #3312 references of sender are removed from routes, processors and models
    all tests are adjusted
    
    * ISSUE #3312 yarn lint fix
    
    * ISSUE #3312 yarn lint:fix
    
    * ISSUE #3312 reset the event manager when we close the app
    
    * ISSUE #3312 reset the queue as well when the app is closed
    
    * ISSUE #3312 remove duplicated test
    
    * rename scripts and split review check into 2 scripts
    
    * #3312 fix github action
    
    * fix indentation
    
    Co-authored-by: Carmen Fan <wind5320@hotmail.com>

--- backend/tests/v5/unit/services/chat/socketsManager.test.js
+++ backend/tests/v5/unit/services/chat/socketsManager.test.js
@@ -146,15 +146,20 @@ const testSocketsEvents = () => {
 				const project = generateRandomString();
 				const model = generateRandomString();
 
-				const data = { teamspace, model, project };
+				const data = { teamspace, project };
 				eventFns.leave(data);
-				expect(socket.leave).toHaveBeenCalledWith(`${teamspace}::${project}::${model}`);
+				expect(socket.leave).toHaveBeenCalledWith(`${teamspace}::${project}`);
 				checkMessageCall(socket.emit, ACTIONS.LEAVE, data);
 
-				const data2 = { notifications: true };
+				const data2 = { teamspace, model, project };
 				eventFns.leave(data2);
-				expect(socket.leave).toHaveBeenCalledWith(`notifications::${getUserNameFromSocket(socket)}`);
+				expect(socket.leave).toHaveBeenCalledWith(`${teamspace}::${project}::${model}`);
 				checkMessageCall(socket.emit, ACTIONS.LEAVE, data2);
+
+				const data3 = { notifications: true };
+				eventFns.leave(data3);
+				expect(socket.leave).toHaveBeenCalledWith(`notifications::${getUserNameFromSocket(socket)}`);
+				checkMessageCall(socket.emit, ACTIONS.LEAVE, data3);
 			});
 
 			test('should process the leave function appropriately (v4)', async () => {
@@ -221,6 +226,58 @@ const testSocketsEvents = () => {
 
 		describe('On Join', () => {
 			afterEach(SocketsManager.reset);
+
+			describe('Project room', () => {
+				afterEach(SocketsManager.reset);
+
+				test('should join successfully if the user is authorised to do so', async () => {
+					const { eventFns, socket } = createSocketWithEvents();
+					SocketsManager.addSocket(socket);
+
+					const teamspace = generateRandomString();
+					const project = generateRandomString();
+
+					Permissions.isProjectAdmin.mockResolvedValueOnce(true);
+
+					const data = { teamspace, project };
+					await eventFns.join(data);
+					expect(socket.join).toHaveBeenCalledWith(`${teamspace}::${project}`);
+					checkMessageCall(socket.emit, ACTIONS.JOIN, data);
+				});
+
+				test('should fail to join the room if the user is not project admin', async () => {
+					const { eventFns, socket } = createSocketWithEvents();
+					SocketsManager.addSocket(socket);
+
+					socket.join.mockClear();
+					const teamspace = generateRandomString();
+					const project = generateRandomString();
+
+					Permissions.isProjectAdmin.mockResolvedValueOnce(false);
+
+					const data = { teamspace, project };
+					await eventFns.join(data);
+					expect(socket.join).not.toHaveBeenCalled();
+					checkErrorCall(socket.emit, ERRORS.ROOM_NOT_FOUND, ACTIONS.JOIN, data);
+				});
+
+				test('should fail gracefully if isProjectAdmin() threw an error', async () => {
+					const { eventFns, socket } = createSocketWithEvents();
+					SocketsManager.addSocket(socket);
+
+					socket.join.mockClear();
+					const teamspace = generateRandomString();
+					const project = generateRandomString();
+
+					Permissions.isProjectAdmin.mockRejectedValueOnce(templates.projectNotFound);
+
+					const data = { teamspace, project };
+					await eventFns.join(data);
+					expect(socket.join).not.toHaveBeenCalled();
+					checkErrorCall(socket.emit, ERRORS.ROOM_NOT_FOUND, ACTIONS.JOIN, data);
+				});
+			});
+
 			describe('Model room', () => {
 				afterEach(SocketsManager.reset);
 
