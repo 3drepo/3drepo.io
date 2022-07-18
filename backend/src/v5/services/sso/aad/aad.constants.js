ed654c90e99516d2430b0cc83516e6f9ca02558c
ChristosTsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2022-07-18 12:17:13 +0100

    ISSUE #3395 aad constants get url from config instead of hardcoding

--- backend/src/v5/services/sso/aad/aad.constants.js
+++ backend/src/v5/services/sso/aad/aad.constants.js
@@ -1,7 +1,9 @@
+const config = require('../../../utils/config');
+
 const AadConstants = {};
 
-AadConstants.authenticateRedirectUri = 'http://localhost/api/v5/sso/aad/authenticate-post';
-AadConstants.signupRedirectUri = 'http://localhost/api/v5/sso/aad/signup-post';
+AadConstants.authenticateRedirectUri = `${config.api_server.url}/v5/sso/aad/authenticate-post`;
+AadConstants.signupRedirectUri = `${config.api_server.url}/v5/sso/aad/signup-post`;
 AadConstants.msGraphUserDetailsUri = 'https://graph.microsoft.com/v1.0/me';
 
 module.exports  = AadConstants;
