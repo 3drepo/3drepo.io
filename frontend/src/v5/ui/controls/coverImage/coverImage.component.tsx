0ea9940297cde98119c0c0627db7a4caa3a501dd
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-08-02 15:39:06 +0100

    ISSUE #3435 - fix teamspace image not updating on teamspace change

--- frontend/src/v5/ui/controls/coverImage/coverImage.component.tsx
+++ frontend/src/v5/ui/controls/coverImage/coverImage.component.tsx
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Image } from './coverImage.styles';
 
 type CoverImageProps = {
@@ -26,6 +26,8 @@ type CoverImageProps = {
 export const CoverImage = ({ className, defaultImgSrc, imgSrc }: CoverImageProps) => {
 	const [src, setSrc] = useState(imgSrc);
 	const onError = () => { setSrc(defaultImgSrc); };
+
+	useEffect(() => { setSrc(imgSrc); }, [imgSrc]);
 
 	return (<Image onError={onError} className={className} src={src} />);
 };
