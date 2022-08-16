c3d3bd797148176401953f476d090cb03f3d269a
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-08-16 12:45:00 +0100

    ISSUE #3494 - add store tests

---
+++ frontend/test/projects/projects.store.spec.ts
@@ -0,0 +1,84 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { ProjectsActions } from '@/v5/store/projects/projects.redux';
+import { selectCurrentProject, selectCurrentProjectDetails, selectCurrentProjects } from '@/v5/store/projects/projects.selectors';
+import { TeamspacesActions } from '@/v5/store/teamspaces/teamspaces.redux';
+import { times } from 'lodash';
+import { projectMockFactory } from './projects.fixture';
+import { createTestStore } from '../test.helpers';
+
+const projectListIncludesProject = (projectList, project) => (
+	projectList.map(({ _id }) => _id).includes(project._id)
+);
+
+describe('Projects: store', () => {
+	const teamspace = 'teamspace';
+	let dispatch, getState;
+
+	beforeEach(() => {
+		({ dispatch, getState } = createTestStore());
+		dispatch(TeamspacesActions.setCurrentTeamspace(teamspace));
+	});
+
+	const createAndAddProjectToStore = (projectOverrides = {}) => {
+		const newProject = projectMockFactory(projectOverrides);
+		dispatch(ProjectsActions.fetchSuccess(teamspace, [newProject]));
+		return newProject;
+	};
+
+	it('should set projects', () => {
+		const mockProjects = times(2, () => projectMockFactory());
+		dispatch(ProjectsActions.fetchSuccess(teamspace, mockProjects));
+		const projects = selectCurrentProjects(getState());
+		expect(projects[0]).toEqual(mockProjects[0]);
+		expect(projects[1]).toEqual(mockProjects[1]);
+	});
+
+	it('should set current project', () => {
+		const mockProject = projectMockFactory();
+		dispatch(ProjectsActions.fetchSuccess(teamspace, [mockProject]));
+		dispatch(ProjectsActions.setCurrentProject(mockProject._id));
+
+		const currentProject = selectCurrentProjectDetails(getState());
+		expect(currentProject).toEqual(mockProject);
+
+		const currentProjectId = selectCurrentProject(getState());
+		expect(currentProjectId).toEqual(mockProject._id);
+	});
+
+	describe('Updating projects list:', () => {
+		it('should create new project', () => {
+			createAndAddProjectToStore();
+			const mockProject = projectMockFactory();
+			dispatch(ProjectsActions.createProjectSuccess(teamspace, mockProject));
+
+			const projects = selectCurrentProjects(getState());
+			const projectIsIncluded = projectListIncludesProject(projects, mockProject);
+			expect(projectIsIncluded).toBeTruthy();
+		});
+
+		it('should delete project', () => {
+			const mockProjectId = "123";
+			createAndAddProjectToStore({ _id: mockProjectId });
+			dispatch(ProjectsActions.deleteProjectSuccess(teamspace, mockProjectId));
+			const projects = selectCurrentProjects(getState());
+			const projectIsIncluded = projectListIncludesProject(projects, mockProjectId);
+			expect(projectIsIncluded).toBeFalsy();
+		});
+	});
+});
