73cc970cf2a561b1cc5f17f1c753f7ed5e9b5fe5
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-09-06 15:57:40 +0100

    ISSUE #3583 - Create ticket card

--- frontend/src/v5/ui/routes/viewer/tickets/tickets.component.tsx
+++ frontend/src/v5/ui/routes/viewer/tickets/tickets.component.tsx
@@ -17,54 +17,78 @@
 
 import TicketsIcon from '@mui/icons-material/FormatListBulleted';
 import { FormattedMessage } from 'react-intl';
-import { CardContainer, CardHeader } from '@/v5/ui/components/viewer/cards/card.styles';
-import { CardContent } from '@/v5/ui/components/viewer/cards/cardContent.component';
+import { CardContainer, CardHeader, EmptyCardMessage } from '@/v5/ui/components/viewer/cards/card.styles';
+import { useEffect, useState } from 'react';
+import { ViewerParams } from '@/v5/ui/routes/routes.constants';
+import { useParams } from 'react-router-dom';
+import { FederationsHooksSelectors } from '@/v5/services/selectorsHooks/federationsSelectors.hooks';
+import { FederationsActionsDispatchers } from '@/v5/services/actionsDispatchers/federationsActions.dispatchers';
+import { ContainersActionsDispatchers } from '@/v5/services/actionsDispatchers/containersActions.dispatchers';
+import { ContainersHooksSelectors } from '@/v5/services/selectorsHooks/containersSelectors.hooks';
+import { countBy } from 'lodash';
+import { TicketsList } from './ticketsList/ticketsList.component';
+import { SearchValue, SearchValues, CardContent } from './tickets.styles';
 
-export const Tickets = () => (
-	<CardContainer>
-		<CardHeader>
-			<TicketsIcon fontSize="small" />
-			<FormattedMessage id="viewer.cards.ticketsTitle" defaultMessage="Tickets" />
-		</CardHeader>
-		<CardContent>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi volutpat, ligula a
-			sagittis malesuada, lectus lectus congue tortor, vel molestie enim libero at magna.
-			In hac habitasse platea dictumst. Maecenas ut lectus tristique, laoreet enim at,
-			vulputate diam. Praesent ipsum nisl, vestibulum condimentum risus luctus, egestas
-			feugiat orci. Sed aliquam, ante at lacinia tristique, lorem quam dictum metus, sit
-			amet posuere erat massa sed leo. Quisque ornare euismod tortor, id sollicitudin lorem
-			dignissim et. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-			ridiculus mus. Mauris interdum pretium tincidunt. Nam interdum elit at urna pretium,
-			quis maximus ipsum consequat. Duis in egestas magna. Proin vitae vehicula eros.
-			Maecenas ultrices eu libero a molestie. Nam vitae ultrices felis. Vivamus a magna lectus.
-			Aliquam volutpat ultricies ipsum, nec aliquet eros rhoncus in. Maecenas vitae bibendum
-			arcu. Aenean sit amet ornare lacus, sit amet varius risus. Duis malesuada dui quis
-			ante aliquet consectetur. Ut vel arcu ornare, ornare odio eget, lobortis est. Nam
-			nibh sem, scelerisque sed dui eget, tristique pellentesque mi. Donec molestie at
-			libero at bibendum. Etiam quis mi vitae ligula pellentesque dignissim ut sed justo.
-			Duis consectetur, libero id ultrices pharetra, nunc augue efficitur augue, quis lacinia
-			nunc mi id nibh. Sed est ipsum, iaculis et metus a, accumsan viverra enim.
-			Phasellus ac enim a mauris euismod rutrum. Ut a commodo risus. Pellentesque habitant
-			morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin efficitur
-			faucibus feugiat. Vestibulum tincidunt lacus ligula, non porta nisl congue nec. Curabitur
-			vel enim congue, sollicitudin sem et, tempus lectus. Nunc vehicula feugiat dui, a ullamcorper
-			nisl. Aenean suscipit ultricies lobortis. Mauris sit amet lectus sit amet dui molestie
-			faucibus. Aliquam fringilla turpis sem. Sed tempus mi eu sapien pulvinar viverra. Class
-			aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-			Etiam nec tempor nibh.
-			Nulla tempus turpis id libero malesuada, non sollicitudin nunc laoreet. In in fringilla
-			urna. Ut a felis a lorem bibendum tempus vel nec orci. Vivamus ut dolor ut diam interdum
-			fermentum. Cras gravida placerat ex eget faucibus. Suspendisse potenti. Proin tincidunt
-			aliquam faucibus. Nunc sit amet sapien urna.
-			Fusce blandit eleifend urna. Aliquam blandit, metus sed porta posuere, turpis velit mollis
-			quam, ac sagittis lacus erat et ex. Donec sollicitudin sapien est, sed rutrum nibh venenatis
-			nec. Duis et rutrum ipsum. Quisque eget lorem ornare, blandit ex sit amet, pulvinar velit.
-			Integer ipsum tellus, tincidunt sed leo at, iaculis pretium purus. Etiam a molestie dui.
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis quis mi non efficitur.
-			Donec malesuada maximus sapien ut fermentum. Nulla porttitor aliquet porttitor. Quisque interdum
-			mauris felis, sit amet luctus mi ultrices id. Maecenas rhoncus magna nec massa auctor commodo.
-			Maecenas posuere elit quis nisl iaculis fermentum. Proin aliquet quam et blandit sollicitudin.
-			In sit amet hendrerit ipsum.
-		</CardContent>
-	</CardContainer>
-);
+export const Tickets = () => {
+	const { teamspace, project, containerOrFederation } = useParams<ViewerParams>();
+	const isFederation = FederationsHooksSelectors.selectContainersByFederationId(containerOrFederation)?.length === 1;
+	const tickets = isFederation
+		? FederationsHooksSelectors.selectFederationTickets(containerOrFederation)
+		: ContainersHooksSelectors.selectContainerTickets(containerOrFederation);
+
+	useEffect(() => {
+		if (isFederation) {
+			FederationsActionsDispatchers.fetchFederationTickets(teamspace, project, containerOrFederation);
+		} else {
+			ContainersActionsDispatchers.fetchContainerTickets(teamspace, project, containerOrFederation);
+		}
+	}, [containerOrFederation]);
+
+	const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+
+	const toggleSelectedType = (type: string) => {
+		if (selectedTypes.includes(type)) {
+			setSelectedTypes(selectedTypes.filter((selectedType) => selectedType !== type));
+		} else {
+			setSelectedTypes(selectedTypes.concat(type));
+		}
+	};
+
+	const getFilteredTickets = () => {
+		if (selectedTypes.length === 0) return tickets;
+		return tickets.filter(({ type }) => selectedTypes.includes(type));
+	};
+
+	const getTypesByCount = () => countBy(tickets.map(({ type }) => type));
+
+	return (
+		<CardContainer>
+			<CardHeader>
+				<TicketsIcon fontSize="small" />
+				<FormattedMessage id="viewer.cards.ticketsTitle" defaultMessage="Tickets" />
+			</CardHeader>
+			<CardContent autoHeightMax="100%">
+				{!tickets?.length ? (
+					<EmptyCardMessage>
+						<FormattedMessage id="viewer.tickets.emptyList" defaultMessage="No entries have been created yet" />
+					</EmptyCardMessage>
+				) : (
+					<>
+						<SearchValues>
+							{Object.entries(getTypesByCount()).map(([type, count]) => (
+								<SearchValue
+									key={type}
+									$selected={selectedTypes.includes(type)}
+									onClick={() => toggleSelectedType(type)}
+								>
+									{type} ({count})
+								</SearchValue>
+							))}
+						</SearchValues>
+						<TicketsList tickets={getFilteredTickets()} />
+					</>
+				)}
+			</CardContent>
+		</CardContainer>
+	);
+};
---
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsList/ticketsList.styles.ts
@@ -0,0 +1,55 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import styled from 'styled-components';
+
+export const Id = styled.div`
+	color: ${({ theme }) => theme.palette.base.main};
+	font-weight: 500;
+	font-size: 10px;
+	line-height: 14px;
+`;
+
+export const Title = styled.div`
+	color: ${({ theme }) => theme.palette.secondary.main};
+	font-weight: 500;
+	font-size: 12px;
+	line-height: 16px;
+	padding-top: 5px;
+`;
+
+// TODO - fix after new palette is released
+export const Ticket = styled.div<{ $selected?: boolean }>`
+	cursor: pointer;
+	padding: 12px 14px 16px;
+	background-color: ${({ theme, $selected }) => ($selected ? '#edf0f8' : theme.palette.primary.contrast)};
+
+	&:hover {
+		${({ $selected }) => !$selected && 'background-color: #f9faff;'}
+		${Title} {
+			text-decoration: underline;
+		}
+	}
+`;
+
+export const List = styled.div`
+	border: solid 1px ${({ theme }) => theme.palette.base.lightest};
+	border-radius: 6px;
+	overflow: hidden;
+	${/* sc-selector */ Ticket}:not(:last-child) {
+		border-bottom: solid 1px ${({ theme }) => theme.palette.base.lightest};
+	}
+`;
