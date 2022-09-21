9231b79bd18d23a1d34f9d1dc327c9705f1e2a90
Keif Gwinn <keif.gwinn@3drepo.org>
2022-09-21 14:34:13 +0100

    #3647 use fiename

--- backend/src/scripts/migrations/4.28/moveGridFSToFS.js
+++ backend/src/scripts/migrations/4.28/moveGridFSToFS.js
@@ -58,7 +58,7 @@ const processCollection = async (teamspace, collection) => {
 	const ownerCol = collection.slice(0, -(filesExt.length));
 	const gridFSEntry = await find(teamspace, collection, { }, { filename: 1 });
 	for (const entry of gridFSEntry) {
-		logger.logInfo(`\t\t\t\t${entry}`);
+		logger.logInfo(`\t\t\t\t${entry.filename}`);
 		// eslint-disable-next-line no-await-in-loop
 		await moveFile(teamspace, ownerCol, entry.filename);
 	}
