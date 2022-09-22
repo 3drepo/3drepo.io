82057ebb7069b14c1c1d1ce810de6dc9ba370afb
Keif Gwinn <keif.gwinn@3drepo.org>
2022-09-22 15:52:13 +0100

    #3653 added custom login support for protected folders

--- .azure/Docker/src/init.sh
+++ .azure/Docker/src/init.sh
@@ -15,6 +15,15 @@ if [ "$1" = 'web' ]; then
         chown -R $NODE_UID:$NODE_GID $APP_EFS_ROOT 
     fi
 
+    if [ ! "$APP_WEB_CUSTOMLOGINS" = '' ]; then
+        list=( $(python3 -c 'exec("""\nimport sys, json\no=[]\nc=json.load(open("'$APP_WEB_CUSTOMLOGINS'","r"))\n\nfor k in c:\n  for i in c[k]:\n    if (c[k][i].count("/") ):\n      b=c[k][i].split("/")[0]\n      if b not in o:\n        o.append(b)\n\nprint(" ".join(map(str, o)))\n""")'))
+        for l in "${list[@]}"
+        do
+            mkdir -p /home/node/3drepo.io/public/$l
+            chown node:node /home/node/3drepo.io/public/$l
+        done
+    fi
+
     umask 2
     cd /home/node/3drepo.io
     exec gosu $NODE_USERNAME ./run/run_app $NODE_ENV
