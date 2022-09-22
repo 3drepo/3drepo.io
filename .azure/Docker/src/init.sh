9dcdf3ab926ffa99076730305a2e8ad56e5cf7b9
Keif Gwinn <keif.gwinn@3drepo.org>
2022-09-22 16:56:23 +0100

    #3653 fix shell errors

--- .azure/Docker/src/init.sh
+++ .azure/Docker/src/init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 if [ "$1" = 'web' ]; then
@@ -11,22 +11,22 @@ if [ "$1" = 'web' ]; then
     fi
 
     if [ ! -d "$APP_EFS_ROOT/models" ]; then
-        chmod -R 2774 $APP_EFS_ROOT 
-        chown -R $NODE_UID:$NODE_GID $APP_EFS_ROOT 
+        chmod -R 2774 "$APP_EFS_ROOT" 
+        chown -R "$NODE_UID":"$NODE_GID" "$APP_EFS_ROOT" 
     fi
 
     if [ ! "$APP_WEB_CUSTOMLOGINS" = '' ]; then
-        list=( $(python3 -c 'exec("""\nimport sys, json\no=[]\nc=json.load(open("'$APP_WEB_CUSTOMLOGINS'","r"))\n\nfor k in c:\n  for i in c[k]:\n    if (c[k][i].count("/") ):\n      b=c[k][i].split("/")[0]\n      if b not in o:\n        o.append(b)\n\nprint(" ".join(map(str, o)))\n""")'))
+        list=( "$(python3 -c 'exec("""\nimport sys, json\no=[]\nc=json.load(open("'"$APP_WEB_CUSTOMLOGINS"'","r"))\n\nfor k in c:\n  for i in c[k]:\n    if (c[k][i].count("/") ):\n      b=c[k][i].split("/")[0]\n      if b not in o:\n        o.append(b)\n\nprint(" ".join(map(str, o)))\n""")')" )
         for l in "${list[@]}"
         do
-            mkdir -p /home/node/3drepo.io/public/$l
-            chown node:node /home/node/3drepo.io/public/$l
+            mkdir -p /home/node/3drepo.io/public/"$l"
+            chown node:node /home/node/3drepo.io/public/"$l"
         done
     fi
 
     umask 2
     cd /home/node/3drepo.io
-    exec gosu $NODE_USERNAME ./run/run_app $NODE_ENV
+    exec gosu "$NODE_USERNAME" ./run/run_app "$NODE_ENV"
 fi
 
 exec "$@"
