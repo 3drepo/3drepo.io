789182b95a8eb30955fafaec11adaf85f4b1b1e3
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-09-23 15:58:45 +0100

    ISSUE #3632 - remove unused props type

--- frontend/stories/inputs/FormCheckbox.stories.tsx
+++ frontend/stories/inputs/FormCheckbox.stories.tsx
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { FormCheckbox, FormCheckboxProps } from '@controls/formCheckbox/formCheckbox.component';
+import { FormCheckbox } from '@controls/formCheckbox/formCheckbox.component';
 import { useForm } from 'react-hook-form';
 import { FormContainer } from './FormInput.styles';
 
@@ -33,7 +33,7 @@ export default {
 	parameters: { controls: { exclude: ['control', 'formError', 'ref'] } },
 } as ComponentMeta<typeof FormCheckbox>;
 
-const Controlled: ComponentStory<typeof FormCheckbox> = (args: FormCheckboxProps & { viewerTheme: boolean }) => {
+const Controlled: ComponentStory<typeof FormCheckbox> = (args) => {
 	const { control } = useForm({ mode: 'onChange' });
 
 	return (
