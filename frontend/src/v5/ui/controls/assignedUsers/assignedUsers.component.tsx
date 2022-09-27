ec7684478e30beaedb386a3e4220a7f66d29e1b0
The-Daniel <bibshall@gmail.com>
2022-09-27 13:47:12 +0100

    ISSUE #3584 - Add popovers for assigned user avatars

--- frontend/src/v5/ui/controls/assignedUsers/assignedUsers.component.tsx
+++ frontend/src/v5/ui/controls/assignedUsers/assignedUsers.component.tsx
@@ -15,8 +15,12 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { Popover } from '@/v4/routes/components/messagesList/components/message/components/markdownMessage/ticketReference/ticketReference.styles';
 import { IUser } from '@/v5/store/users/users.redux';
+import { UserPopover } from '@components/shared/userPopover/userPopover.component';
+import { useState } from 'react';
 import { AssignedUsersList, ExtraUsersCircle, UserCircle, WhiteOverlay } from './assignedUsers.styles';
+import { ExtraUsersPopover } from './extraUsersPopover/extraUsersPopover.component';
 
 type AssignedUsersType = {
 	users: IUser[];
@@ -24,6 +28,18 @@ type AssignedUsersType = {
 };
 
 export const AssignedUsers = ({ users, max }: AssignedUsersType) => {
+	const [anchorEl, setAnchorEl] = useState<Element | null>(null);
+	const [selectedUser, setSelectedUser] = useState<IUser | null>(null);
+
+	const handlePopoverOpen = (event: React.MouseEvent<Element, MouseEvent>, user = null) => {
+		setAnchorEl(event.currentTarget);
+		setSelectedUser(user);
+	};
+
+	const handlePopoverClose = () => {
+		setAnchorEl(null);
+	};
+
 	let displayedUsers = [...users];
 	let extraUsers = [];
 	if (max && users.length > max) {
@@ -33,12 +49,42 @@ export const AssignedUsers = ({ users, max }: AssignedUsersType) => {
 	return (
 		<AssignedUsersList>
 			<WhiteOverlay />
-			{displayedUsers.map((user, index) => <UserCircle user={user} index={index} size="small" />)}
+			{displayedUsers.map((user, index) => (
+				<UserCircle
+					key={user.user}
+					user={user}
+					index={index}
+					size="small"
+					onMouseEnter={(e) => handlePopoverOpen(e, user)}
+					onMouseLeave={handlePopoverClose}
+				/>
+			))}
 			{extraUsers.length && (
-				<ExtraUsersCircle>
+				<ExtraUsersCircle
+					onMouseEnter={(e) => handlePopoverOpen(e, null)}
+					onMouseLeave={handlePopoverClose}
+				>
 					+{extraUsers.length}
 				</ExtraUsersCircle>
 			)}
+			<Popover
+				id="mouse-over-popover"
+				open={!!anchorEl}
+				anchorEl={anchorEl}
+				anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'center',
+				}}
+				transformOrigin={{
+					vertical: 'top',
+					horizontal: 'left',
+				}}
+				onClose={handlePopoverClose}
+				disableRestoreFocus
+			>
+				{selectedUser
+					? <UserPopover user={selectedUser} /> : <ExtraUsersPopover users={extraUsers} />}
+			</Popover>
 		</AssignedUsersList>
 	);
 };
