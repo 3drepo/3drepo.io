a35b59b925375f8bb84c907914bedd6ee7c49ed0
Carmen Fan <wind5320@hotmail.com>
2022-09-28 11:25:05 +0100

    ISSUE #3647 run this script in batches as well to prevent timeouts

--- backend/src/scripts/migrations/4.28/removeGridFSBackUps.js
+++ backend/src/scripts/migrations/4.28/removeGridFSBackUps.js
@@ -29,32 +29,65 @@ const GridFS = require(`${v5Path}/handler/gridfs`);
 
 const refExt = '.ref';
 
-const removeGridFSBackup = async (teamspace, col, filename) => {
+const removeGridFSBackup = async (teamspace, col, filename, link) => {
 	const filesCol = `${col}.files`;
 	const legacyFileName = { $regex: `^/\\w+/\\w+.*/${filename}$` };
 	const rec = await find(teamspace, filesCol,
 		{ $or: [{ filename }, { filename: legacyFileName }] }, { filename: 1 });
 	if (rec.length) {
+		// ensure the file exists in fileshare
+		await access(Path.join(fs.path, link), constants.R_OK);
 		await GridFS.removeFiles(teamspace, col, rec.map(({ filename: file }) => file));
 	}
 };
 
 const filesInGridFS = (teamspace, collection) => count(teamspace, `${collection}.files`, { filename: { $not: { $regex: '.*unityAssets.json$' } } });
 
-const processCollection = async (teamspace, collection) => {
+const organiseRefsToProcess = (entries, maxParallelSizeMB, maxParallelFiles) => {
+	const groups = [];
+
+	const maxMem = maxParallelSizeMB * 1024 * 1024;
+
+	let currentGroup = [];
+	let currentGroupSize = 0;
+	for (const entry of entries) {
+		if ((entry.size + currentGroupSize) > maxMem || currentGroup.length >= maxParallelFiles) {
+			groups.push(currentGroup);
+			currentGroupSize = 0;
+			currentGroup = [];
+		}
+
+		currentGroup.push(entry);
+		currentGroupSize += entry.size;
+	}
+
+	if (currentGroup.length) {
+		groups.push(currentGroup);
+	}
+
+	return groups;
+};
+
+const processCollection = async (teamspace, collection, maxParallelSizeMB, maxParallelFiles) => {
 	const ownerCol = collection.slice(0, -(refExt.length));
 	const gridfsCount = await filesInGridFS(teamspace, ownerCol);
 	if (gridfsCount) {
-		const refEntries = await find(teamspace, collection, { type: 'fs' }, { link: 1 });
-		logger.logInfo(`\t\t\t\tChecking ${refEntries.length} references...`);
-		await Promise.all(refEntries.map(async ({ _id, link }) => {
-			try {
-				await access(Path.join(fs.path, link), constants.R_OK);
-				await removeGridFSBackup(teamspace, ownerCol, _id);
-			} catch (err) {
-				logger.logError(`Failed to process file ${_id}: ${err.message}`);
-			}
-		}));
+		const refEntries = await find(teamspace, collection, { type: 'fs' }, { link: 1, size: 1 });
+		const groups = organiseRefsToProcess(refEntries, maxParallelSizeMB, maxParallelFiles);
+		for (let i = 0; i < groups.length; ++i) {
+			const group = groups[i];
+			const totalSize = group.reduce((partialSum, { size }) => partialSum + size, 0) / (1024 * 1024);
+			logger.logInfo(`\t\t\t\t[${i}/${groups.length}] Checking ${group.length} references (${parseFloat(totalSize).toFixed(2)}MiB)...`);
+			const proms = group.map(async ({ _id, link }) => {
+				try {
+					await removeGridFSBackup(teamspace, ownerCol, _id, link);
+				} catch (err) {
+					logger.logError(`Failed to process file ${_id}: ${err.message}`);
+				}
+			});
+			// eslint-disable-next-line no-await-in-loop
+			await Promise.all(proms);
+		}
 	} else {
 		logger.logInfo('\t\t\t\tNo gridfs reference. Skipping...');
 	}
@@ -72,22 +105,22 @@ const processCollection = async (teamspace, collection) => {
 	}
 };
 
-const processTeamspace = async (teamspace) => {
+const processTeamspace = async (teamspace, maxParallelSizeMB, maxParallelFiles) => {
 	const refCols = await getCollectionsEndsWith(teamspace, refExt);
 	for (let i = 0; i < refCols.length; ++i) {
 		const collection = refCols[i].name;
 		logger.logInfo(`\t\t\t${collection}`);
 		// eslint-disable-next-line no-await-in-loop
-		await processCollection(teamspace, collection);
+		await processCollection(teamspace, collection, maxParallelSizeMB, maxParallelFiles);
 	}
 };
 
-const run = async () => {
+const run = async ({ maxParallelSizeMB, maxParallelFiles }) => {
 	const teamspaces = await getTeamspaceList();
 	for (let i = 0; i < teamspaces.length; ++i) {
 		logger.logInfo(`\t\t-${teamspaces[i]}`);
 		// eslint-disable-next-line no-await-in-loop
-		await processTeamspace(teamspaces[i]);
+		await processTeamspace(teamspaces[i], maxParallelSizeMB, maxParallelFiles);
 	}
 };
 
