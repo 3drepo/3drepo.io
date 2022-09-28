44e83ef729376c9160ec6b73f4aef33b08b9c7de
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-09-28 13:07:52 +0100

    ISSUE #3632 - fix import path

--- frontend/stories/inputs/FormMultiSelect.stories.tsx
+++ frontend/stories/inputs/FormMultiSelect.stories.tsx
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { FormMultiSelect } from '@/v5/ui/routes/viewer/formElements/formMultiSelect/formMultiSelect.component';
+import { FormMultiSelect } from '@controls/formMultiSelect/formMultiSelect.component';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { useForm } from 'react-hook-form';
 import { FormContainer } from './FormInput.styles';
