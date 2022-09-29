aaa1878b305fa15ad3bb5c83a3420df3ab68973f
The-Daniel <bibshall@gmail.com>
2022-09-29 16:45:33 +0100

    ISSUE #3584 - add priority level chips

--- frontend/assets/icons/filled/flag-filled.svg.tsx
+++ frontend/assets/icons/filled/flag-filled.svg.tsx
@@ -15,6 +15,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export { Chip } from './chip.styles';
-export { PriorityLevelChip, PriorityLevels } from './variants/priorityLevelChip.component';
-export { TicketStatusChip, TicketStatuses } from './variants/ticketStatusChip.component';
+export default () => (
+	<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M9 2.26172H6V0.9375C6 0.730078 5.83242 0.5625 5.625 0.5625H0.84375V0.09375C0.84375 0.0421875 0.801563 0 0.75 0H0.09375C0.0421875 0 0 0.0421875 0 0.09375V9.28125C0 9.33281 0.0421875 9.375 0.09375 9.375H0.75C0.801563 9.375 0.84375 9.33281 0.84375 9.28125V6.1875H3.75V7.51172C3.75 7.71914 3.91758 7.88672 4.125 7.88672H9C9.20742 7.88672 9.375 7.71914 9.375 7.51172V2.63672C9.375 2.4293 9.20742 2.26172 9 2.26172Z" fill="currentColor" />
+	</svg>
+);
---
+++ frontend/src/v5/ui/controls/chip/variants/priorityLevelChip.component.tsx
@@ -0,0 +1,65 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import FlagOutlineIcon from '@assets/icons/outlined/flag-outlined.svg';
+import FlagFillIcon from '@assets/icons/filled/flag-filled.svg';
+import { formatMessage } from '@/v5/services/intl';
+import { omit } from 'lodash';
+import { COLOR } from '@/v5/ui/themes/theme';
+import { Chip } from '../chip.styles';
+
+export enum PriorityLevels {
+	NONE = 'none',
+	LOW = 'low',
+	MEDIUM = 'medium',
+	HIGH = 'high',
+}
+
+const PRIORITY_LEVELS_MAP = {
+	[PriorityLevels.NONE]: {
+		label: formatMessage({ id: 'chip.priorityLevel.none.label', defaultMessage: 'None' }),
+		title: formatMessage({ id: 'chip.priorityLevel.none.tooltip', defaultMessage: 'No priority' }),
+		colour: COLOR.BASE_LIGHT,
+	},
+	[PriorityLevels.LOW]: {
+		label: formatMessage({ id: 'chip.priorityLevel.low.label', defaultMessage: 'Low' }),
+		title: formatMessage({ id: 'chip.priorityLevel.low.tooltip', defaultMessage: 'Low priority' }),
+		colour: '#0288D1',
+	},
+	[PriorityLevels.MEDIUM]: {
+		label: formatMessage({ id: 'chip.priorityLevel.medium.label', defaultMessage: 'Medium' }),
+		title: formatMessage({ id: 'chip.priorityLevel.medium.tooltip', defaultMessage: 'Medium priority' }),
+		colour: '#ED6C02',
+	},
+	[PriorityLevels.HIGH]: {
+		label: formatMessage({ id: 'chip.priorityLevel.high.label', defaultMessage: 'High' }),
+		title: formatMessage({ id: 'chip.priorityLevel.high.tooltip', defaultMessage: 'High priority' }),
+		colour: COLOR.ERROR_MAIN,
+	},
+};
+
+type IPriorityLevelChip = {
+	noLabel?: boolean,
+	state: PriorityLevels,
+};
+
+export const PriorityLevelChip = ({ state, noLabel = false }: IPriorityLevelChip) => {
+	const props = PRIORITY_LEVELS_MAP[state];
+	const refinedProps = omit(props, noLabel ? 'label' : 'title');
+	const Icon = state === PriorityLevels.NONE ? FlagOutlineIcon : FlagFillIcon;
+	return <Chip variant="noBorder" icon={<Icon />} {...refinedProps} />;
+};
