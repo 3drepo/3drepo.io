58f8dc0b45d1ae958c12323f92a46c7e0ca2d330
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-10-17 17:12:20 +0100

    ISSUE #3622 - fix manyOf including jobs and users

--- frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/manyOfProperty.component.tsx
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/manyOfProperty.component.tsx
@@ -15,32 +15,56 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { TeamspacesHooksSelectors } from '@/v5/services/selectorsHooks/teamspacesSelectors.hooks';
+import { UsersHooksSelectors } from '@/v5/services/selectorsHooks/usersSelectors.hooks';
 import { PropertyDefinition } from '@/v5/store/tickets/tickets.types';
-import { MenuItem, Select } from '@mui/material';
+import { FormMultiSelect } from '@controls/formSearchSelect/formMultiSelect/formMultiSelect.component';
+import { MultiSelectMenuItem } from '@controls/formSearchSelect/formMultiSelect/multiSelectMenuItem/multiSelectMenuItem.component';
+import { store } from '@/v4/modules/store';
+import _ from 'lodash';
 
-export const ManyOfProperty = ({ property, value }: {property: PropertyDefinition, value:any}) => {
-	// eslint-disable-next-line no-case-declarations
-	const selectedValues = (value || []) as string[];
-	if (property.values === 'jobsAndUsers') {
+export const ManyOfProperty = ({
+	property: { name, readOnly, required, values },
+	value: deafultValues,
+}: { property: PropertyDefinition, value: any }) => {
+	const selectedValues = (deafultValues || []) as string[];
+
+	if (values === 'jobsAndUsers') {
+		const teampspace = TeamspacesHooksSelectors.selectCurrentTeamspace();
+		const users = _.sortBy(UsersHooksSelectors.selectUsersByTeamspace(teampspace), 'firstName');
+		// TODO fix after "jobs" is accessible in v5
+		// @ts-ignore
+		const { jobs } = store.getState();
+		const jobItems = jobs.jobs.map(({ _id }) => (
+			<MultiSelectMenuItem key={_id} value={_id}>{_id}</MultiSelectMenuItem>
+		));
+		const userItems = users.map(({ user, firstName, lastName }) => (
+			<MultiSelectMenuItem key={user} value={user}>{`${firstName} ${lastName}`}</MultiSelectMenuItem>
+		));
 		return (
-			<div>
-					&nbsp;
-				{property.name}: <b> {selectedValues.join(',')}</b>
-			</div>
+			<FormMultiSelect
+				label={name}
+				name={name}
+				defaultValue={selectedValues}
+				disabled={readOnly}
+				required={required}
+			>
+				{jobItems.concat(userItems)}
+			</FormMultiSelect>
 		);
 	}
+
 	return (
-		<div>
-				&nbsp;
-			{property.name}
-			<br />
-			<Select value={property.values[0]}>
-				{(property.values as string[]).map((propValue) => (
-					<MenuItem key={propValue} value={propValue}>
-						{selectedValues.includes(propValue) && <b>*</b>}  {propValue}
-					</MenuItem>
-				))}
-			</Select>
-		</div>
+		<FormMultiSelect
+			label={name}
+			name={name}
+			defaultValue={selectedValues}
+			disabled={readOnly}
+			required={required}
+		>
+			{(values as any[]).map((value) => (
+				<MultiSelectMenuItem key={value} value={value}>{value}</MultiSelectMenuItem>
+			))}
+		</FormMultiSelect>
 	);
 };
