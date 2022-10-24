b92f405ca8427f0011dcd5bbfb85ee519bf41eca
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-10-24 17:35:42 +0100

    ISSUE #3682 - Add dynamic property-validation

--- frontend/src/v5/store/tickets/tickets.helpers.ts
+++ frontend/src/v5/store/tickets/tickets.helpers.ts
@@ -15,8 +15,50 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { formatMessage } from '@/v5/services/intl';
 import { FederationsHooksSelectors } from '@/v5/services/selectorsHooks/federationsSelectors.hooks';
+import { trimmedString } from '@/v5/validation/shared/validators';
+import { PropertyDefinition } from './tickets.types';
+import * as Yup from 'yup';
 
 export const modelIsFederation = (modelId: string) => (
 	!!FederationsHooksSelectors.selectContainersByFederationId(modelId).length
 );
+
+export const propertiesValidator = (properties) => {
+	const validators = properties.reduce(
+		(validators, property) => ({
+			...validators,
+			[property.name]: propertyValidator(property),
+		}),
+		{},
+	);
+	return Yup.object().shape(validators);
+};
+
+export const propertyValidator = ({ required, name, type }: PropertyDefinition) => {
+	let validator = trimmedString;
+
+	if (required) {
+		validator = validator.required(
+			formatMessage({
+				id: 'validation.ticket.requiredField',
+				defaultMessage: '{name} is a required field',
+			},
+			{ name }),
+		);
+	}
+
+	if (type === 'text') {
+		const maxLength = 50;
+
+		validator = validator.max(maxLength,
+			formatMessage({
+				id: 'validation.ticket.tooLong',
+				defaultMessage: 'Max length of text is {maxLength}',
+			},
+			{ maxLength }));
+	}
+
+	return validator;
+};
