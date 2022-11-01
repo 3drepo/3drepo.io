4a8b39b5ef3f37aecb841436b4a8d103297a6743
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-11-01 11:21:39 +0000

    ISSUE #3702 - create ticket image and viewpoint stories

---
+++ frontend/stories/inputs/image/TicketViewpoint.stories.tsx
@@ -0,0 +1,40 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import { TicketViewpoint } from '@/v5/ui/routes/viewer/tickets/ticketsForm/properties/basicTicketImage/ticketViewpoint/ticketViewpoint.component';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { MockStoreProvider } from './MockStoreProvider';
+
+export default {
+	title: 'Inputs/Image/TicketViewpoint',
+	component: TicketViewpoint,
+} as ComponentMeta<typeof TicketViewpoint>;
+
+const Template: ComponentStory<typeof TicketViewpoint> = () => (<TicketViewpoint title="Ticket Viewpoint" />);
+
+export const Default = Template.bind({});
+Default.decorators = [(TicketViewpointComponent) => (
+	<MockStoreProvider>
+		<TicketViewpointComponent />
+	</MockStoreProvider>
+)];
+
+export const NotAdmin = Template.bind({});
+NotAdmin.decorators = [(TicketViewpointComponent) => (
+	<MockStoreProvider isAdmin={false}>
+		<TicketViewpointComponent />
+	</MockStoreProvider>
+)];
