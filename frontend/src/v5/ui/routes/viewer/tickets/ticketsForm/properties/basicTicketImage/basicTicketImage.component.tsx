ebd3447a16fc3097870effe191373fb8e4a903f7
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-11-01 10:48:04 +0000

    ISSUE #3702 - create basic ticket image component

---
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/basicTicketImage/basicTicketImage.component.tsx
@@ -0,0 +1,91 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import { FormattedMessage } from 'react-intl';
+import EmptyImageIcon from '@assets/icons/outlined/add_image_thin-outlined.svg';
+import EnlargeImageIcon from '@assets/icons/outlined/enlarge_image-outlined.svg';
+import { useState } from 'react';
+import {
+	ActionsList,
+	ActionsSide,
+	Container,
+	EmptyImageContainer,
+	EnlargeContainer,
+	IconText,
+	Image,
+	ImageSide,
+	OverlappingContainer,
+	PropertyName,
+} from './basicTicketImage.styles';
+import { TicketImageActionContext } from './actions/ticketImageActionContext';
+import { HiddenImageUploader } from '@controls/hiddenImageUploader/hiddenImageUploader.component';
+import { ProjectsHooksSelectors } from '@/v5/services/selectorsHooks/projectsSelectors.hooks';
+
+type BasicTicketImageProps = {
+	imgSrc?: string,
+	viewPoint?: any,
+	onChange?: (e) => void,
+	title: string,
+	className?: string,
+	children: any,
+};
+export const BasicTicketImage = ({ children, imgSrc: inputImgSrc, viewPoint: inputViewPoint, onChange, title, className }: BasicTicketImageProps) => {
+	const [imgSrc, setImgSrc] = useState(inputImgSrc);
+	const [viewPoint, setViewPoint] = useState(inputViewPoint);
+	const { isAdmin } = ProjectsHooksSelectors.selectCurrentProjectDetails();
+
+	const contextValue = {
+		imgSrc,
+		setImgSrc,
+		viewPoint,
+		setViewPoint,
+	};
+
+	return (
+		<Container className={className}>
+			<ActionsSide>
+				<PropertyName>{title}</PropertyName>
+				<TicketImageActionContext.Provider value={contextValue}>
+					<ActionsList>
+						{children}
+					</ActionsList>
+				</TicketImageActionContext.Provider>
+			</ActionsSide>
+			<ImageSide>
+				{!imgSrc ?  (
+					<HiddenImageUploader onUpload={setImgSrc} disabled={!isAdmin}>
+						<EmptyImageContainer disabled={!isAdmin}>
+							<EmptyImageIcon />
+							<IconText>
+								<FormattedMessage id="viewer.cards.ticketImage.addImage" defaultMessage="Add Image" />
+							</IconText>
+						</EmptyImageContainer>
+					</HiddenImageUploader>
+				) : (
+					<OverlappingContainer>
+						<Image src={imgSrc} alt={title} onClick={() => console.log("click")} onChange={onChange} />
+						<EnlargeContainer>
+							<EnlargeImageIcon />
+							<IconText>
+								<FormattedMessage id="viewer.cards.ticketImage.enlarge" defaultMessage="Enlarge" />
+							</IconText>
+						</EnlargeContainer>
+					</OverlappingContainer>
+				)}
+			</ImageSide>
+		</Container>
+	);
+};
---
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/basicTicketImage/basicTicketImage.styles.ts
@@ -0,0 +1,99 @@
+/**
+ *  Copyright (C) 2022 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Typography } from '@mui/material';
+import styled from 'styled-components';
+import { CentredContainer } from '@controls/centredContainer';
+import { hexToOpacity } from '@/v5/ui/themes/theme';
+
+export const Container = styled.div`
+	padding: 13px;
+	border: solid 1px ${({ theme }) => theme.palette.secondary.lightest};
+	background-color: ${({ theme }) => theme.palette.primary.contrast};
+	border-radius: 5px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+`;
+
+export const PropertyName = styled(Typography).attrs({
+	variant: 'h5',
+})`
+	color: ${({ theme }) => theme.palette.secondary.main};
+	margin-bottom: 2px;
+`;
+
+export const ActionsSide = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const ActionsList = styled.ul`
+	list-style-type: none;
+	padding: 0;
+	margin: 0;
+`;
+
+export const ImageSide = styled.div`
+	border-radius: 5px;
+	overflow: hidden;
+	height: 101px;
+	width: 171px;
+	cursor: pointer;
+
+	& > * {
+		height: 100%;
+		width: 100%;
+	}
+`;
+
+export const OverlappingContainer = styled.div`
+	position: relative;
+	color: ${({ theme }) => theme.palette.primary.contrast};
+	& > * {
+		position: absolute;
+		top: 0;
+		left: 0;
+		cursor: pointer;
+	}
+`;
+
+export const Image = styled.img`
+    object-fit: cover;
+	height: 100%;
+	width: 100%;
+`;
+
+export const EnlargeContainer = styled(CentredContainer)`
+	opacity: 0;
+	text-align: center;
+	transition: all .2s;
+	background-color: ${({ theme }) => hexToOpacity(theme.palette.secondary.main, 85)};
+
+	&:hover {
+		opacity: 1;
+	}
+`;
+
+export const EmptyImageContainer = styled(CentredContainer)<{ disabled: boolean }>`
+	background-color: ${({ theme }) => theme.palette.tertiary.lightest};
+	color: ${({ theme }) => theme.palette.base.main};
+	text-align: center;
+	${({ disabled }) => `cursor: ${disabled ? 'unset' : 'pointer'}`};
+`;
+
+export const IconText = styled(Typography).attrs({ variant: 'body1' })``;
