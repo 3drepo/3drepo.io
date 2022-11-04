f47f6371de78a9d67f2991ce55065ffb629c27e8
sanmont3drepo <santiago.montero@3drepo.org>
2022-11-04 21:11:56 +0000

    ISSUE #3657 - Refactor of searchselect

--- frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/oneOfProperty.component.tsx
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/oneOfProperty.component.tsx
@@ -16,9 +16,38 @@
  */
 
 import { TicketsHooksSelectors } from '@/v5/services/selectorsHooks/ticketsSelectors.hooks';
-import { FormSelect } from '@controls/formSelect/formSelect.component';
-import { MenuItem } from '@mui/material';
+// import { FormSelect } from '@controls/formSelect/formSelect.component';
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from '@mui/material';
+import { Controller } from 'react-hook-form';
 import { PropertyProps } from './properties.types';
+
+const FormSelect = ({ label, name, control, children, required, disabled, formError, ...props }) => {
+	console.log('Im here');
+
+	return (
+		<Controller
+			control={control}
+			name={name}
+			render={({ field: { ref, value, ...field } }) => {
+				console.log(JSON.stringify(field, null, '\t'));
+				return (
+					<FormControl required={required} disabled={disabled} error={!!formError}>
+						<InputLabel id={`${name}-label`}>{label}</InputLabel>
+						<Select
+							{...field}
+							{...props}
+							inputRef={ref}
+							value={value || ''}
+						>
+							{children}
+						</Select>
+						<FormHelperText>{formError?.message}</FormHelperText>
+					</FormControl>
+				);
+			}}
+		/>
+	);
+};
 
 export const OneOfProperty = ({ property: { name, readOnly, required, values }, ...props }: PropertyProps) => {
 	const riskCategories: string[] = TicketsHooksSelectors.selectRiskCategories() || [];
