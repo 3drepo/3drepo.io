46597b438d4420389a0bd5c735ff5a38a2abf65a
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-11-10 17:37:17 +0000

    ISSUE #3703 - remove comment

--- frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/basicTicketImage/basicTicketImage.component.tsx
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/basicTicketImage/basicTicketImage.component.tsx
@@ -14,6 +14,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import { ProjectsHooksSelectors } from '@/v5/services/selectorsHooks/projectsSelectors.hooks';
 import {
 	ActionsList,
 	ActionsSide,
@@ -22,7 +23,6 @@ import {
 	Asterisk,
 } from './basicTicketImage.styles';
 import { TicketImageDisplayer } from './ticketImageDisplayer/ticketImageDisplayer.component';
-import { ProjectsHooksSelectors } from '@/v5/services/selectorsHooks/projectsSelectors.hooks';
 
 export type BasicTicketImageProps = {
 	imgSrc: string,
@@ -41,7 +41,7 @@ export const BasicTicketImage = ({
 	className,
 	required,
 	disabled,
-	onEmptyImageClick, // TODO think about renaiming
+	onEmptyImageClick,
 }: BasicTicketImageProps) => {
 	const { isAdmin } = ProjectsHooksSelectors.selectCurrentProjectDetails();
 
