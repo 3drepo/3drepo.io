845bdfbe598ebd7cfd19e82d82e78e36e23c2809
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-11-16 17:52:39 +0000

    ISSUE #3703 - empty trimmedString does not crash validation

--- frontend/src/v5/validation/shared/validators.ts
+++ frontend/src/v5/validation/shared/validators.ts
@@ -18,7 +18,7 @@ import { formatMessage } from '@/v5/services/intl';
 import { isNull } from 'lodash';
 import * as Yup from 'yup';
 
-export const trimmedString = Yup.string().transform((value) => value.trim());
+export const trimmedString = Yup.string().transform((value) => (value ?? '').trim());
 
 export const nullableNumber = Yup.number().transform(
 	(_, val) => ((val || val === 0) ? Number(val) : null),
