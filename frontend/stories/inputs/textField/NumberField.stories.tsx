7f2adb7ed97d0fc098d62444f7e9b5f1fefb124e
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-11-29 10:06:06 +0000

    ISSUE #3777 - fix casing

--- frontend/stories/inputs/textField/NumberField.stories.tsx
+++ frontend/stories/inputs/textField/NumberField.stories.tsx
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { NumberField } from '@controls/inputs/NumberField/NumberField.component';
+import { NumberField } from '@controls/inputs/numberField/numberField.component';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { FormContainer } from '../FormInput.styles';
 
--- frontend/stories/inputs/textField/TextAreaFixedSize.stories.tsx
+++ frontend/stories/inputs/textField/TextAreaFixedSize.stories.tsx
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { TextAreaFixedSize } from '@controls/inputs/textArea/TextAreaFixedSize.component';
+import { TextAreaFixedSize } from '@controls/inputs/textArea/textAreaFixedSize.component';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { FormContainer } from '../FormInput.styles';
 
