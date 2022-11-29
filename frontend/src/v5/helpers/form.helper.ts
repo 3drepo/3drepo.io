46da8110b6f2d226803a6f9b759b3d1b40bb3420
Alessandro Amantini <ale.amantini1997@gmail.com>
2022-11-29 15:33:51 +0000

    ISSUE #3777 - undo toggle fix

--- frontend/src/v5/helpers/form.helper.ts
+++ frontend/src/v5/helpers/form.helper.ts
@@ -98,7 +98,7 @@ export const removeEmptyObjects = (tree) => {
 	return Object.keys(tree).reduce((accum, key) => {
 		const value = tree[key];
 
-		if (_.isEmpty(value) && !_.isNull(value) && !_.isBoolean(value)) {
+		if (_.isEmpty(value) && value !== null) {
 			return accum;
 		}
 
