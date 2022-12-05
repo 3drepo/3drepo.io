15e217404c3342695506dbb17f644aca379f680b
The-Daniel <bibshall@gmail.com>
2022-12-05 11:38:51 +0000

    ISSUE #3727 - sort buttons are now visible

--- frontend/src/v5/ui/v4Adapter/overrides/customTable.overrides.ts
+++ frontend/src/v5/ui/v4Adapter/overrides/customTable.overrides.ts
@@ -57,7 +57,7 @@ export default css`
 			padding-top: 22px;
 		}
 	}
-
+	
 	${UserNameCell} {
 		${({ theme }) => theme.typography.h5};
 		color: ${({ theme }) => theme.palette.secondary.main};
@@ -69,11 +69,11 @@ export default css`
 
 	${SortLabel} {
 		margin: 0;
-		padding-left: 10px;
+		padding: 0;
+    	margin-left: 0;
 		${({ theme }) => theme.typography.kicker};
 		flex-direction: row;
 		svg {
-			fill: transparent;
 			width: 10px;
 			margin-left: 2px;
 		}
