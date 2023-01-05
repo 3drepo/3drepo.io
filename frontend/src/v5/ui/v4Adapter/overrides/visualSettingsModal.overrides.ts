e5e076438958697d6c377ad3f9e4d37d9873d047
The-Daniel <bibshall@gmail.com>
2023-01-05 11:34:26 +0000

    ISSUE #3725 - style vis settings modal action buttons

--- frontend/src/v5/ui/v4Adapter/overrides/visualSettingsModal.overrides.ts
+++ frontend/src/v5/ui/v4Adapter/overrides/visualSettingsModal.overrides.ts
@@ -15,8 +15,52 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import {
+	NegativeActionButton as ResetButton,
+	NeutralActionButton as CancelButton,
+	VisualSettingsButtonsContainer as Actions,
+} from '@/v4/routes/components/topMenu/components/visualSettingsDialog/visualSettingsDialog.styles';
 import styled from 'styled-components';
 
 export const V5VisualSettingsOverrides = styled.div`
-	background-color: ${({ theme }) => theme.palette.primary.contrast};
+	background-color: ${({ theme }) => theme.palette.tertiary.lightest};
+
+	${Actions} {
+		box-shadow: ${({ theme }) => theme.palette.shadows.level_7};
+		height: 65px;
+		bottom: 0;
+		justify-content: left;
+		align-items: center;
+		padding: 0 7px;
+		box-sizing: border-box;
+		.MuiButtonBase-root {
+			margin: 5px;
+		}
+		${ResetButton} {
+			background-color: transparent;
+			color: ${({ theme }) => theme.palette.secondary.main};
+			margin-right: auto;
+			:hover {
+				text-decoration: underline;
+			}
+		}
+		${CancelButton} {
+			border: 1px solid ${({ theme }) => theme.palette.secondary.main};
+			color: ${({ theme }) => theme.palette.secondary.main};
+			:hover {
+				background-color: ${({ theme }) => theme.palette.secondary.main};
+				color: ${({ theme }) => theme.palette.primary.contrast};
+			}
+		}
+		.MuiButtonBase-root:last-of-type {
+			color: ${({ theme }) => theme.palette.primary.contrast};
+			background-color: ${({ theme }) => theme.palette.primary.main};
+			:hover {
+				background-color: ${({ theme }) => theme.palette.primary.dark};
+			}
+			&.Mui-disabled {
+				background-color: ${({ theme }) => theme.palette.base.lightest};
+			}
+		}
+	}
 `;
