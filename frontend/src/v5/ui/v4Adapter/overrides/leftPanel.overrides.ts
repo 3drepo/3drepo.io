4d5d67797d5633845d52d9435bc8b1cf88c73ccc
The-Daniel <bibshall@gmail.com>
2023-01-09 16:02:37 +0000

    ISSUE #3822 - fix hidden panel actions still occupying space

--- frontend/src/v5/ui/v4Adapter/overrides/leftPanel.overrides.ts
+++ frontend/src/v5/ui/v4Adapter/overrides/leftPanel.overrides.ts
@@ -67,12 +67,11 @@ export default css`
 			height: 32px;
 			width: 32px;
 			margin: 0 0 0 5px;
-
 			svg {
 				font-size: 1.2rem;
 			}
 
-			[hidden] {
+			&[hidden] {
 				display: none;
 			}
 		}
