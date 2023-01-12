060c6278d09abeaa5cbdb92209cb67e9a9bfdc99
The-Daniel <bibshall@gmail.com>
2023-01-12 15:43:02 +0000

    ISSUE #3815 - remove duplicate css from git merge

--- frontend/src/v5/ui/routes/dashboard/teamspaces/teamspaceLayout/teamspaceLayout.styles.ts
+++ frontend/src/v5/ui/routes/dashboard/teamspaces/teamspaceLayout/teamspaceLayout.styles.ts
@@ -25,7 +25,6 @@ export const Container = styled.div`
 	height: 100%;
 	display: flex;
 	flex-direction: column;
-	background-color: ${({ theme }) => theme.palette.tertiary.lightest};
 `;
 
 export const Content = styled(DashboardContent)`
