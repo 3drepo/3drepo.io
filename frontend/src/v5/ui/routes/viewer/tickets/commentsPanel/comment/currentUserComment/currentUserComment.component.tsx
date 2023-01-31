c7456f56082e0a58aaa02c63363c9669392970b7
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-01-31 15:33:07 +0000

    ISSUE #3626 - fix edit message wrongly enabled

--- frontend/src/v5/ui/routes/viewer/tickets/commentsPanel/comment/currentUserComment/currentUserComment.component.tsx
+++ frontend/src/v5/ui/routes/viewer/tickets/commentsPanel/comment/currentUserComment/currentUserComment.component.tsx
@@ -72,7 +72,7 @@ export const CurrentUserComment = ({
 
 	if (isEditMode) {
 		const editedMessage = watch('editedMessage');
-		const canUpdate = sanitiseMessage(message) !== editedMessage && editedMessage.length > 0;
+		const canUpdate = message !== sanitiseMessage(editedMessage) && editedMessage.length > 0;
 
 		const updateMessage = () => {
 			let newMessage = sanitiseMessage(editedMessage);
