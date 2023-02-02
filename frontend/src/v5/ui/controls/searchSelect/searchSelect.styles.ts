d5e559d5d3cb9807f71f6615c4d9bc7d586f3b81
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-02-02 14:56:13 +0000

    ISSUE #3907 - fix spacing

--- frontend/src/v5/ui/controls/searchSelect/searchSelect.styles.ts
+++ frontend/src/v5/ui/controls/searchSelect/searchSelect.styles.ts
@@ -25,11 +25,11 @@ export const SearchInputContainer = styled.div`
 
 export const SearchInput = styled(SearchInputBase)`
 	margin: -10px 0 0;
-    padding: 12px;
-    background: ${({ theme }) => theme.palette.primary.contrast};
-    z-index: 2;
-    position: sticky;
-    top: 0;
+	padding: 12px;
+	background: ${({ theme }) => theme.palette.primary.contrast};
+	z-index: 2;
+	position: sticky;
+	top: 0;
 `;
 
 export const NoResults = styled.div`
