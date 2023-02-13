9703deaf5b9c07ffd761e8a9f23678bee18416aa
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-02-13 16:21:41 +0000

    ISSUE #3888 - jest can use paths with @controls

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/internals/testing/setupTests.ts'],
   moduleNameMapper: {
     '@assets/(.*)': '<rootDir>/assets/$1',
+    '@controls/(.*)': '<rootDir>/src/v5/ui/controls/$1',
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   transform: {
