c13761f4f376bcc51b177ccaed39d043cbe860d3
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-02-15 11:21:03 +0000

    ISSUE #3888 - fix className type

--- frontend/assets/icons/outlined/edit_comment-outlined.svg.tsx
+++ frontend/assets/icons/outlined/edit_comment-outlined.svg.tsx
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 type IProps = {
-	className?: any;
+	className?: string;
 };
 
 export default ({ className }: IProps) => (
