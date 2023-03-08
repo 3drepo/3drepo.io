0b5f1eda39af684a35890cd829863b6fcdac24fb
sanmont3drepo <santiago.montero@3drepo.org>
2023-03-08 10:30:01 +0000

    ISSUE #3904 - Fixed linter error

--- frontend/src/v5/ui/components/authTemplate/authTemplate.styles.ts
+++ frontend/src/v5/ui/components/authTemplate/authTemplate.styles.ts
@@ -69,7 +69,6 @@ export const Background = styled.div`
 	height: 100%;
 	width: 100%;
 	display: flex;
-	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
