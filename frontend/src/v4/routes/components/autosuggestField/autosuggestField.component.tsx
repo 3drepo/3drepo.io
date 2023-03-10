234f394cd72f2a87956b4314a67e051221b2e79c
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-03-10 14:13:39 +0000

    ISSUE #3637 - fix import order

--- frontend/src/v4/routes/components/autosuggestField/autosuggestField.component.tsx
+++ frontend/src/v4/routes/components/autosuggestField/autosuggestField.component.tsx
@@ -18,10 +18,9 @@
 import { PureComponent } from 'react';
 import { MenuItem, Paper } from '@mui/material';
 import Autosuggest from 'react-autosuggest';
-import { Highlight } from '../highlight/highlight.component';
-
 import { ConditionalV5OrViewerScrollArea } from '@/v5/ui/v4Adapter/components/conditionalV5OrViewerScrollArea.component';
 import { Container, StyledTextField, SuggestionsList } from './autosuggestField.styles';
+import { Highlight } from '../highlight/highlight.component';
 
 interface IProps {
 	suggestions: any[];
