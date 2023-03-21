4b3de75e60a1ad01413d2ff5328b31eaa944137f
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-03-21 10:45:46 +0000

    ISSUE #4109 - remove disable lint

--- frontend/stories/theme/typography.stories.tsx
+++ frontend/stories/theme/typography.stories.tsx
@@ -14,7 +14,6 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable max-len */
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
