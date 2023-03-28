87ce7a81385452e378cc833c506b66b487df82a2
The-Daniel <bibshall@gmail.com>
2023-03-28 17:44:38 +0100

    ISSUE #4137 - add basic tickets card store tests

---
+++ frontend/test/tickets/card/ticketsCard.store.spec.ts
@@ -0,0 +1,53 @@
+import { TicketsCardActions } from "@/v5/store/tickets/card/ticketsCard.redux";
+import { selectReadOnly, selectSelectedTemplate, selectSelectedTemplateId, selectSelectedTicketId, selectView } from "@/v5/store/tickets/card/ticketsCard.selectors";
+import { TicketsCardViews } from "@/v5/ui/routes/viewer/tickets/tickets.constants";
+import { createTestStore } from "test/test.helpers";
+
+
+describe('Tickets: store', () => {
+	let dispatch, getState;
+	const ticketId = 'ticketId';
+	const templateId = 'templateId';
+
+	beforeEach(() => {
+		({ dispatch, getState } = createTestStore());
+	});
+	describe('card', () => {
+		it('should set the selected ticket ID', () => {
+			dispatch(TicketsCardActions.setSelectedTicket(ticketId));
+			const selectedTicketIdFromState = selectSelectedTicketId(getState());
+		
+			expect(selectedTicketIdFromState).toEqual(ticketId);
+		});
+		it('should set the selected template ID', () => {
+			dispatch(TicketsCardActions.setSelectedTemplate(templateId));
+			const selectedTemplateIdFromState = selectSelectedTemplateId(getState());
+		
+			expect(selectedTemplateIdFromState).toEqual(templateId);
+		});
+		it('should set the selected view', () => {
+			const updatedView = TicketsCardViews.New;
+			dispatch(TicketsCardActions.setCardView(updatedView));
+			const cardViewFromState = selectView(getState());
+		
+			expect(cardViewFromState).toEqual(updatedView);
+		});
+		it('should set the read only status of the card', () => {
+			dispatch(TicketsCardActions.setReadOnly(true));
+			const readOnlyFromState = selectReadOnly(getState());
+		
+			expect(readOnlyFromState).toEqual(true);
+		});
+		it('should reset the state', () => {
+			dispatch(TicketsCardActions.setSelectedTemplate(templateId));
+			dispatch(TicketsCardActions.setSelectedTicket(ticketId));
+			dispatch(TicketsCardActions.resetState());
+
+			const selectedTicketIdFromState = selectSelectedTicketId(getState());
+			const selectedTemplateIdFromState = selectSelectedTemplateId(getState());
+		
+			expect(selectedTicketIdFromState).toEqual(null);
+			expect(selectedTemplateIdFromState).toEqual(null);
+		});
+	});
+});
