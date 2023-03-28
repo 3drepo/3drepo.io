e6d8476ccee7892b5846a334cc6427a6a9e1b937
Charence <charencewong@hotmail.com>
2023-03-28 16:51:08 +0100

    ISSUE #3968 - rm 5.2 files

--- backend/src/scripts/migrations/5.2/index.js
+++ backend/src/scripts/migrations/5.2/index.js
@@ -16,11 +16,9 @@
  */
 
 const moveNotificationsToInternal = require('./moveNotificationsToInternal');
-const moveViews = require('./moveViews');
 
 const scripts = [
 	{ script: moveNotificationsToInternal, desc: 'Move notifications to internal DB' },
-	{ script: moveViews, desc: 'Move model views to views collection' },
 ];
 
 module.exports = scripts;
