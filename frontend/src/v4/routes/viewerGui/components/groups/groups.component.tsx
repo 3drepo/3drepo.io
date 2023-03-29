be6d1c8f01dd7c1865a82b65f25f33642548642e
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-03-29 16:12:28 +0100

    ISSUE #4137 - fix spacing

--- frontend/src/v4/routes/viewerGui/components/groups/groups.component.tsx
+++ frontend/src/v4/routes/viewerGui/components/groups/groups.component.tsx
@@ -305,10 +305,12 @@ export class Groups extends PureComponent<IProps, IState> {
 
 	public renderActionsMenu = () => {
 		const shouldDisble = (name) => {
-			if (!this.canAddOrUpdate && [GROUPS_ACTIONS_ITEMS.DELETE_ALL, GROUPS_ACTIONS_ITEMS.IMPORT].includes(name)) return true;
+			if (!this.canAddOrUpdate && [GROUPS_ACTIONS_ITEMS.DELETE_ALL, GROUPS_ACTIONS_ITEMS.IMPORT].includes(name)) {
+				return true;
+			}
 
 			// disable options that needs at least one group when there are none
-			return !this.props.groups.length && [GROUPS_ACTIONS_ITEMS.DELETE_ALL,GROUPS_ACTIONS_ITEMS.EXPORT].includes(name);
+			return !this.props.groups.length && [GROUPS_ACTIONS_ITEMS.DELETE_ALL, GROUPS_ACTIONS_ITEMS.EXPORT].includes(name);
 		}
 		return (
 			<MenuList>
