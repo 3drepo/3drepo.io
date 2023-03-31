e63a2afc4eee615a8acff4e77087e28b88e936ae
Carmen Fan <wind5320@hotmail.com>
2023-03-31 17:03:29 +0100

    ISSUE #3969 revert debugging changes

--- backend/tests/v5/unit/schema/tickets/index.test.js
+++ backend/tests/v5/unit/schema/tickets/index.test.js
@@ -262,7 +262,7 @@ const testValidateTicket = () => {
 			['One Of', { type: propTypes.ONE_OF, values: ['a', 'b'] }, 'a', generateRandomString()],
 			['Many Of', { type: propTypes.MANY_OF, values: ['a', 'b', 'c'] }, ['a'], ['b', generateRandomString()]],
 			['Image', { type: propTypes.IMAGE }, FS.readFileSync(image, { encoding: 'base64' }), generateRandomString()],
-			['123View (empty)', { type: propTypes.VIEW }, {}, 123],
+			['View (empty)', { type: propTypes.VIEW }, {}, 123],
 			['View (Image only)', { type: propTypes.VIEW }, { screenshot: FS.readFileSync(image, { encoding: 'base64' }) }, { screenshot: 'abc' }],
 			['View', { type: propTypes.VIEW }, { camera: { position: [1, 1, 1], forward: [1, 1, 1], up: [1, 1, 1] }, clippingPlanes: [{ normal: [1, 1, 1], clipDirection: -1, distance: 100 }] }, { camera: {} }],
 			['View (orthographic)', { type: propTypes.VIEW }, { camera: { type: 'orthographic', size: 5, position: [1, 1, 1], forward: [1, 1, 1], up: [1, 1, 1] } }, { camera: { type: 'orthographic' } }],
@@ -291,7 +291,7 @@ const testValidateTicket = () => {
 		];
 
 		testPropertyTypes(propertyTypeSetData, false, true);
-		//	testPropertyTypes(propertyTypeSetData, true, true);
+		testPropertyTypes(propertyTypeSetData, true, true);
 		testPropertyTypes(propertyTypeUnsetData, false, false);
 		testPropertyTypes(propertyTypeUnsetData, true, false);
 		const randomData = generateRandomString();
