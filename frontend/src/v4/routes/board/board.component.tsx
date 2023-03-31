2e3df9eea40c2984d9b752863f645a57fcb51cd5
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-03-31 11:05:59 +0100

    ISSUE #3814 - clean board

--- frontend/src/v4/routes/board/board.component.tsx
+++ frontend/src/v4/routes/board/board.component.tsx
@@ -29,9 +29,9 @@ import { isV5 } from '@/v4/helpers/isV5';
 import { BOARD_ROUTE } from '@/v5/ui/routes/routes.constants';
 import { formatMessage } from '@/v5/services/intl';
 import { ConditionalV5Wrapper } from '@/v5/ui/v4Adapter/conditionalV5Container.component';
-import { ScrollArea as ScrollAreaStyles } from '@controls/scrollArea/scrollArea.styles';
+import { ScrollArea } from '@controls/scrollArea/scrollArea.styles';
 
-import { ContainersHooksSelectors, ProjectsHooksSelectors } from '@/v5/services/selectorsHooks';
+import { ProjectsHooksSelectors } from '@/v5/services/selectorsHooks';
 import { ISSUE_FILTERS, ISSUES_ACTIONS_MENU } from '../../constants/issues';
 import { RISK_FILTERS } from '../../constants/risks';
 import { ROUTES, RouteParams } from '../../constants/routes';
@@ -458,16 +458,13 @@ export function Board(props: IProps) {
 
 	const components = {
 		Card:  isIssuesBoard ? IssueBoardCard : RiskBoardCard,
-		...(isV5() && { ScrollableLane: (args) => <ScrollAreaStyles {...args} /> }),
+		...(isV5() && { ScrollableLane: ScrollArea }),
 	};
 
 	const renderBoard = renderWhenTrue(() => (
 		<BoardContainer>
 			<div ref={boardRef}>
-				<ConditionalV5Wrapper
-					v5Wrapper={ScrollAreaStyles}
-					v5WrapperProps={{ style: { height: '100%' } }}
-				>
+				<ConditionalV5Wrapper v5Wrapper={ScrollArea}>
 					<TrelloBoard
 						data={boardData}
 						hideCardDeleteIcon
