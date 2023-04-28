5cd0c8c485b732edee03cd2b50fd2f873fa38bb0
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-04-28 16:00:07 +0100

    ISSUE #4153 - create color picker input

---
+++ frontend/src/v5/ui/controls/inputs/colorPicker/colorPicker.styles.ts
@@ -0,0 +1,33 @@
+/**
+ *  Copyright (C) 2023 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import styled from 'styled-components';
+import { CustomInput } from '../customInput/customInput.styles';
+
+export const Container = styled(CustomInput)`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	width: fit-content;
+	padding: 7px 9px;
+	cursor: pointer;
+
+	svg {
+		margin-left: 8px;
+		color: ${({ theme }) => theme.palette.base.main};
+	}
+`;
