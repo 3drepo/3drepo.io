795d3caa85876f27cc9abe28b0bef352b95efd84
Carmen Fan <wind5320@hotmail.com>
2023-04-28 11:17:37 +0100

    ISSUE #4201 script completed

---
+++ backend/src/scripts/utility/fileshare/detectMissingFiles.js
@@ -0,0 +1,89 @@
+/**
+ *  Copyright (C) 2021 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This script identifies all references within the database and checks they have an associated file
+ * in the fs
+ */
+const { v5Path } = require('../../../interop');
+
+const { getCollectionsEndsWith } = require('../../utils');
+
+const { logger } = require(`${v5Path}/utils/logger`);
+const { getTeamspaceList } = require('../../utils');
+
+const { fs: fsConfig } = require(`${v5Path}/utils/config`);
+const { path: fsPath } = fsConfig;
+const { find } = require(`${v5Path}/handler/db`);
+const { statSync } = require('fs');
+const Path = require('path');
+
+const joinPath = (a, b) => (a && b ? Path.posix.join(a, b) : a || b);
+
+const getFileSize = (link) => {
+	const filePath = joinPath(fsPath, link);
+	const { size } = statSync(filePath);
+	return size;
+};
+
+const checkFile = (db, col, { _id, link, size }) => {
+	try {
+		const fileSize = getFileSize(link);
+		if (fileSize !== size) {
+			logger.logError(`[${db}.${col}][${_id}][${link}] File size mismatched (expected ${size}, got ${fileSize})`);
+		}
+	} catch (err) {
+		logger.logError(`[${db}.${col}.${_id}][${link}] Failed to verify file: ${err.message ?? err}`);
+	}
+};
+
+const checkFilesInCol = async (database, col) => {
+	const refs = await find(database, col, { type: 'fs' }, { link: 1, size: 1 });
+	await Promise.all(refs.map((ref) => checkFile(database, col, ref)));
+};
+
+const processTeamspace = async (database) => {
+	const cols = await getCollectionsEndsWith(database, '.ref');
+	await Promise.all(cols.map(({ name }) => checkFilesInCol(database, name)));
+};
+
+const run = async () => {
+	logger.logInfo('Checking all FS references are pointing to a file');
+
+	const teamspaces = await getTeamspaceList();
+	for (const ts of teamspaces) {
+		logger.logInfo(`\t-${ts}`);
+		// eslint-disable-next-line no-await-in-loop
+		await processTeamspace(ts);
+	}
+};
+
+const genYargs = /* istanbul ignore next */(yargs) => {
+	const commandName = Path.basename(__filename, Path.extname(__filename));
+	const argsSpec = (subYargs) => subYargs;
+	return yargs.command(
+		commandName,
+		'Identify any missing file(s) in fileshare',
+		argsSpec,
+		run,
+	);
+};
+
+module.exports = {
+	run,
+	genYargs,
+};
