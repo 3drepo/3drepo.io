0478aacd1333fec6c4f79f7e242b6489d58a1ef2
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-05-05 17:14:21 +0100

    ISSUE #4155 - define filter oprations and (fake) fields

---
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/ticketGroups/groups/groupFilters/groupFilters.helpers.ts
@@ -0,0 +1,50 @@
+/**
+ *  Copyright (C) 2023 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export const OPERATIONS_TYPES = {
+	"EXISTS": 'field',
+	"NOT_EXISTS": 'field',
+	"IS": 'text',
+	"IS_NOT": 'text',
+	"CONTAINS": 'text',
+	"NOT_CONTAINS": 'text',
+	"REGEX": 'regex',
+	"EQUALS": 'number',
+	"NOT_EQUALS": 'number',
+	"GT": 'numberComparison',
+	"GTE": 'numberComparison',
+	"LT": 'numberComparison',
+	"LTE": 'numberComparison',
+	"IN_RANGE": 'numberRange',
+	"NOT_IN_RANGE": 'numberRange',
+} as const;
+
+export type Operation = keyof typeof OPERATIONS_TYPES;
+export type OperationType = typeof OPERATIONS_TYPES[Operation];
+
+export type IFilterForm = {
+	field: string,
+	operation: Operation,
+	value: (number | string)[],
+};
+
+export const getFields = () => [
+	'W la pizza',
+	'Che buona la pasta',
+	'Er guanciale e` meglio della pancetta',
+	'Italia forever',
+];
