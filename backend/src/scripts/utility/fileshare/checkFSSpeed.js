2a00191bcf13efb68e26401a9fb14504f2a4472e
Carmen Fan <wind5320@hotmail.com>
2023-05-12 11:50:06 +0100

    ISSUE #4201 add parallel test

--- backend/src/scripts/utility/fileshare/checkFSSpeed.js
+++ backend/src/scripts/utility/fileshare/checkFSSpeed.js
@@ -24,19 +24,15 @@ const { times } = require('lodash');
 const { fs: fsConfig } = require(`${v5Path}/utils/config`);
 const { path: fsPath } = fsConfig;
 const { writeFileSync, readFileSync, rmSync } = require('fs');
+const { writeFile, readFile, rm } = require('fs/promises');
 const { randomBytes } = require('crypto');
 const Path = require('path');
 
 const N_FILES = 1000;
 
-const run = (dir, nFiles, fileSizeMB) => {
-	logger.logInfo('Generating test data...');
-
-	const data = times(10, () => randomBytes(fileSizeMB * 1024 * 1024));
+const serialTest = (data, files) => {
 	const timers = { write: {}, read: {}, delete: {} };
-	const files = times(nFiles, (i) => ({ name: Path.join(dir, `${i}.tmp`), buffer: data[i % 10] }));
 
-	logger.logInfo(`Writing ${nFiles} files with ${fileSizeMB}MB each (dir: ${dir})...`);
 	timers.write.start = Date.now();
 	files.forEach(({ name, buffer }) => {
 		writeFileSync(name, buffer);
@@ -59,7 +55,42 @@ const run = (dir, nFiles, fileSizeMB) => {
 
 	const calculateTimerStr = ({ start, end }) => ((end - start) / 1000).toFixed(2);
 
-	logger.logInfo(`Writes: ${calculateTimerStr(timers.write)}s, Reads: ${calculateTimerStr(timers.read)}s, Deletes: ${calculateTimerStr(timers.delete)}s`);
+	logger.logInfo(`[SERIAL] Writes: ${calculateTimerStr(timers.write)}s, Reads: ${calculateTimerStr(timers.read)}s, Deletes: ${calculateTimerStr(timers.delete)}s`);
+};
+
+const parallelTest = async (data, files) => {
+	const timers = { write: {}, read: {}, delete: {} };
+
+	timers.write.start = Date.now();
+	await Promise.all(files.map(({ name, buffer }) => writeFile(name, buffer)));
+	timers.write.end = Date.now();
+
+	logger.logInfo('Reading files...');
+	timers.read.start = Date.now();
+	await Promise.all(files.map(({ name }) => readFile(name)));
+	timers.read.end = Date.now();
+
+	logger.logInfo('Deleting files...');
+	timers.delete.start = Date.now();
+	await Promise.all(files.map(({ name }) => rm(name)));
+	timers.delete.end = Date.now();
+
+	const calculateTimerStr = ({ start, end }) => ((end - start) / 1000).toFixed(2);
+
+	logger.logInfo(`[PARALLEL] Writes: ${calculateTimerStr(timers.write)}s, Reads: ${calculateTimerStr(timers.read)}s, Deletes: ${calculateTimerStr(timers.delete)}s`);
+};
+
+const run = async (dir, nFiles, fileSizeMB) => {
+	logger.logInfo('Generating test data...');
+
+	const data = times(10, () => randomBytes(fileSizeMB * 1024 * 1024));
+	const files = times(nFiles, (i) => ({ name: Path.join(dir, `${i}.tmp`), buffer: data[i % 10] }));
+
+	logger.logInfo(`Writing ${nFiles} files serially with ${fileSizeMB}MB each (dir: ${dir})...`);
+	serialTest(data, files);
+
+	logger.logInfo(`Writing ${nFiles} files in parallel with ${fileSizeMB}MB each (dir: ${dir})...`);
+	await parallelTest(data, files);
 
 	logger.logInfo('done');
 };
