d89b849d0726b1ab415b119d6385b82be0fd219c
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-06-01 10:31:35 +0100

    ISSUE #4155 - remove unused import

--- frontend/src/v5/ui/controls/actionMenu/actionMenu.component.tsx
+++ frontend/src/v5/ui/controls/actionMenu/actionMenu.component.tsx
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { ReactNode, useState, useEffect } from 'react';
+import { ReactNode, useState } from 'react';
 import { Popover } from '@mui/material';
 import { Menu, Container } from './actionMenu.styles';
 import { ActionMenuContext } from './actionMenuContext';
