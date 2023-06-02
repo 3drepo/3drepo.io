112db75c7732cc0f839fa630515f650d4b45f346
sanmont3drepo <santiago.montero@3drepo.org>
2023-06-02 18:12:39 +0100

    ISSUE #4224 - Now deletion of a group works

--- frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/ticketGroups/groupsAccordion/groupsAccordion.component.tsx
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/ticketGroups/groupsAccordion/groupsAccordion.component.tsx
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GroupsIcon from '@mui/icons-material/GroupWork';
 import { FormattedMessage } from 'react-intl';
 import { EmptyListMessage } from '@controls/dashedContainer/emptyListMessage/emptyListMessage.styles';
@@ -23,14 +23,21 @@ import { ProjectsHooksSelectors } from '@/v5/services/selectorsHooks';
 import { GroupOverride } from '@/v5/store/tickets/tickets.types';
 import { Accordion, NumberContainer, TitleContainer, Checkbox } from './groupsAccordion.styles';
 import { Groups } from '../groups/groups.component';
+import { TicketGroupsContext } from '../ticketGroupsContext';
 import { AddGroupButton } from '../groups/groupActionMenu/addGroupButton/addGroupButton.component';
 
 type GroupsAccordionProps = {
 	title: any;
-	groups: GroupOverride[];
+	overrides: GroupOverride[];
+	colored?: boolean;
+	onChange?: (value) => void;
 };
-export const GroupsAccordion = ({ title, groups = [] }: GroupsAccordionProps) => {
+
+const addIndex = ((overrides: GroupOverride[]) => overrides.map((h, index) => ({ index, ...h })));
+
+export const GroupsAccordion = ({ title, overrides: groups = [], colored, onChange }: GroupsAccordionProps) => {
 	const [checked, setChecked] = useState(false);
+	const [indexedGroups, setIndexedGroups] = useState(addIndex(groups));
 	const isAdmin = ProjectsHooksSelectors.selectIsProjectAdmin();
 
 	const groupsCount = groups.length;
@@ -40,29 +47,44 @@ export const GroupsAccordion = ({ title, groups = [] }: GroupsAccordionProps) =>
 		setChecked(!checked);
 	};
 
+	const onGroupChange = (index, value) => {
+		const newGroupsValue = [...groups];
+
+		if (value) {
+			newGroupsValue[index] = value;
+		} else {
+			newGroupsValue.splice(index, 1);
+		}
+		onChange?.(newGroupsValue);
+	};
+
+	useEffect(() => setIndexedGroups(addIndex(groups)), [groups]);
+
 	return (
-		<Accordion
-			Icon={GroupsIcon}
-			title={(
-				<TitleContainer>
-					{title}
-					<NumberContainer>{groupsCount}</NumberContainer>
-					<Checkbox checked={checked} onClick={toggleCheckbox} />
-				</TitleContainer>
-			)}
-			$groupsCount={groupsCount}
-		>
-			{groupsCount ? (
-				<Groups groups={groups} />
-			) : (
-				<EmptyListMessage>
-					<FormattedMessage
-						id="ticketCard.groupsList.empty"
-						defaultMessage="No Groups"
-					/>
-				</EmptyListMessage>
-			)}
-			{ isAdmin && <AddGroupButton /> }
-		</Accordion>
+		<TicketGroupsContext.Provider value={{ groupType: colored ? 'colored' : 'hidden', onGroupChange }}>
+			<Accordion
+				Icon={GroupsIcon}
+				title={(
+					<TitleContainer>
+						{title}
+						<NumberContainer>{groupsCount}</NumberContainer>
+						<Checkbox checked={checked} onClick={toggleCheckbox} />
+					</TitleContainer>
+				)}
+				$groupsCount={groupsCount}
+			>
+				{groupsCount ? (
+					<Groups groups={indexedGroups} />
+				) : (
+					<EmptyListMessage>
+						<FormattedMessage
+							id="ticketCard.groupsList.empty"
+							defaultMessage="No Groups"
+						/>
+					</EmptyListMessage>
+				)}
+				{ isAdmin && <AddGroupButton /> }
+			</Accordion>
+		</TicketGroupsContext.Provider>
 	);
 };
