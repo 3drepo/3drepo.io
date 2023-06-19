988859450f86c69c5e7255d224c86989c771dd2a
The-Daniel <bibshall@gmail.com>
2023-06-19 15:26:04 +0100

    ISSUE #3585 - set searchbar size and remove unnecessary toLowerCase

--- frontend/src/v5/ui/routes/viewer/tickets/ticketsList/ticketsList.component.tsx
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsList/ticketsList.component.tsx
@@ -108,13 +108,13 @@ export const TicketsList = ({ tickets }: TicketsListProps) => {
 
 	const filterItems = (items, queries: string[]) => items.filter((ticket) => {
 		const templateCode = availableTemplates.find((template) => template._id === ticket.type).code;
-		const ticketCode = `${templateCode.toLowerCase()}:${ticket.number}`;
+		const ticketCode = `${templateCode}:${ticket.number}`;
 		return queries.every((query) => [ticketCode, ticket.title].some((str) => str.toLowerCase().includes(query.toLowerCase())));
 	});
 
 	return (
 		<SearchContextComponent filteringFunction={filterItems} items={filteredTickets}>
-			<SearchInput multiple />
+			<SearchInput multiple size="small" />
 			<Filters>
 				<CompletedFilterChip
 					key="completed"
