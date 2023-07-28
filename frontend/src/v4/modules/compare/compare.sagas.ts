522d7005667b8409b0ae4a99d01e881e54292b7d
The-Daniel <bibshall@gmail.com>
2023-07-28 12:31:00 +0100

    ISSUE #4354 - models get hidden in a fed compare if they have no revision to compare against

--- frontend/src/v4/modules/compare/compare.sagas.ts
+++ frontend/src/v4/modules/compare/compare.sagas.ts
@@ -97,7 +97,7 @@ function* getCompareModelData({ isFederation, revision }) {
 			});
 
 			const selectedModelsMap = compareModels.reduce((map, model) => {
-				map[model._id] = true;
+				map[model._id] = model.revisions?.length > 1;
 				return map;
 			}, {});
 
