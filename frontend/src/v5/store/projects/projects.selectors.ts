6b34c594bc8c3360c8d9a14f7a4af1d152510f48
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-08-08 10:22:07 +0100

    ISSUE #4347 - remove not used yet selector

--- frontend/src/v5/store/projects/projects.selectors.ts
+++ frontend/src/v5/store/projects/projects.selectors.ts
@@ -53,9 +53,3 @@ export const selectCurrentProjectTemplates = createSelector(
 	selectCurrentProject,
 	(state, currentProject) => state.templatesByProject[currentProject] || [],
 );
-
-export const selectCurrentProjectTemplateById = createSelector(
-	selectCurrentProjectTemplates,
-	(templates, templateId) => templateId,
-	(templates, templateId) => templates.find(({ _id }) => _id === templateId),
-);
