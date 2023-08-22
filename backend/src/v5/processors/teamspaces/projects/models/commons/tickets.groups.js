cb9c476dcbc69e0824ab1b74c5f9e057882466d0
ChristosTsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2023-08-22 16:26:00 +0100

    ISSUE #4383 rules are casted before query in v5

--- backend/src/v5/processors/teamspaces/projects/models/commons/tickets.groups.js
+++ backend/src/v5/processors/teamspaces/projects/models/commons/tickets.groups.js
@@ -17,6 +17,7 @@
 
 const { UUIDToString, stringToUUID } = require('../../../../../utils/helper/uuids');
 const { getGroupById, updateGroup } = require('../../../../../models/tickets.groups');
+const { castSchema } = require('../../../../../schemas/rules');
 const { getFile } = require('../../../../../services/filesManager');
 const { getLatestRevision } = require('../../../../../models/revisions');
 const { getMetadataByRules } = require('../../../../../models/metadata');
@@ -87,6 +88,7 @@ TicketGroups.getTicketGroupById = async (teamspace, project, model, revId, ticke
 	if (group.rules) {
 		const modelsToQuery = containers || [model];
 		const rev = containers ? undefined : revId;
+		group.rules = castSchema(group.rules);
 		group.objects = (await Promise.all(
 			modelsToQuery.map(async (con) => {
 				const objs = await getObjectArrayFromRules(teamspace, project, con, rev, group.rules);
