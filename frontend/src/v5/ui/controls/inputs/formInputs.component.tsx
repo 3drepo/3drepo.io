55ce913169224bd5df361c6e9b207a1dcc675b4e
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-09-22 15:32:20 +0100

    ISSUE #4109 - remove unused code

--- frontend/src/v5/ui/controls/inputs/formInputs.component.tsx
+++ frontend/src/v5/ui/controls/inputs/formInputs.component.tsx
@@ -19,12 +19,8 @@ import { ChipSelect } from '@controls/chip/chipSelect/chipSelect.component';
 import { DueDateWithIcon } from '@controls/dueDate/dueDateWithIcon/dueDateWithIcon.component';
 import { forwardRef } from 'react';
 import { AssigneesSelect } from '@controls/assigneesSelect/assigneesSelect.component';
-import { SearchSelect } from '@controls/searchSelect/searchSelect.component';
 import { Checkbox } from './checkbox/checkbox.component';
-import { DatePicker } from './datePicker/datePicker.component';
-import { DateTimePicker } from './datePicker/dateTimePicker.component';
 import { InputController, InputControllerProps } from './inputController.component';
-import { MultiSelect } from './multiSelect/multiSelect.component';
 import { NumberField } from './numberField/numberField.component';
 import { PasswordField } from './passwordField/passwordField.component';
 import { Select } from './select/select.component';
@@ -32,7 +28,6 @@ import { SelectView } from './selectView/selectView.component';
 import { TextArea } from './textArea/textArea.component';
 import { TextAreaFixedSize } from './textArea/textAreaFixedSize.component';
 import { TextField } from './textField/textField.component';
-import { ColorPicker } from './colorPicker/colorPicker.component';
 
 
 // text inputs
@@ -43,18 +38,13 @@ export const FormTextArea = (props: InputControllerProps<typeof TextArea>) => (<
 export const FormTextAreaFixedSize = forwardRef((props: InputControllerProps<typeof TextAreaFixedSize>, ref) => (<InputController Input={TextAreaFixedSize} {...props} ref={ref} />));
 
 // calendar inputs
-export const FormDatePicker = (props: InputControllerProps<typeof DatePicker>) => (<InputController Input={DatePicker} {...props} />);
-export const FormDateTimePicker = (props: InputControllerProps<typeof DateTimePicker>) => (<InputController Input={DateTimePicker} {...props} />);
 export const FormDueDateWithIcon = (props: InputControllerProps<typeof DueDateWithIcon>) => (<InputController Input={DueDateWithIcon} {...props} />);
 
 // select inputs
-export const FormMultiSelect = (props: InputControllerProps<typeof MultiSelect>) => (<InputController Input={MultiSelect} {...props} />);
 export const FormSelectView = (props: InputControllerProps<typeof SelectView>) => (<InputController Input={SelectView} {...props} />);
 export const FormSelect = (props: InputControllerProps<typeof Select>) => (<InputController Input={Select} {...props} />);
 export const FormChipSelect = (props: InputControllerProps<typeof ChipSelect>) => (<InputController Input={ChipSelect} {...props} />);
-export const FormColorPicker = (props: InputControllerProps<typeof ColorPicker>) => (<InputController Input={ColorPicker} {...props} />);
 export const FormAssigneesSelect = (props: InputControllerProps<typeof AssigneesSelect>) => (<InputController Input={AssigneesSelect} {...props} />);
-export const FormSearchSelect = (props: InputControllerProps<typeof SearchSelect>) => (<InputController Input={SearchSelect} {...props} />);
 
 // control inputs
 export const FormCheckbox = (props: InputControllerProps<typeof Checkbox>) => (<InputController Input={Checkbox} {...props} />);
