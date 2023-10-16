e22dbb9d2a6753fe99d26d8f7d5ac9de48a9a5fa
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-10-16 11:55:38 +0100

    ISSUE #3609 - fix: date boundaries are not correctly picked

--- frontend/src/v5/ui/components/viewer/cards/tickets/sequencingProperty/sequencingProperty.component.tsx
+++ frontend/src/v5/ui/components/viewer/cards/tickets/sequencingProperty/sequencingProperty.component.tsx
@@ -15,23 +15,26 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { DateTimePickerProps } from '@controls/inputs/datePicker/dateTimePicker.component';
+import { DateTimePickerProps, DateTimePicker } from '@controls/inputs/datePicker/dateTimePicker.component';
 import SequencingIcon from '@assets/icons/outlined/sequence-outlined.svg';
 import CloseIcon from '@assets/icons/controls/clear_circle.svg';
-import { DateTimePicker } from '@controls/inputs/datePicker/dateTimePicker.component';
-import { IconContainer, Icons } from './sequencingProperty.styles';
 import { useEffect } from 'react';
 import dayjs from 'dayjs';
 import { SequencesActionsDispatchers } from '@/v5/services/actionsDispatchers';
 import { useFormContext } from 'react-hook-form';
 import { SEQUENCING_MODULE_END, SEQUENCING_MODULE_START } from '@/v5/store/tickets/tickets.helpers';
+import { IconContainer, Icons } from './sequencingProperty.styles';
 
 export const SequencingProperty = ({ onChange, onBlur, value, ...props }: DateTimePickerProps) => {
 	const { getValues } = useFormContext();
 
 	const openSequencesCard = () => SequencesActionsDispatchers.showSequenceDate(new Date(value));
 
-	const getDateTimeBoundary = (limit) => dayjs(limit === props.name ? null : getValues(limit));
+	const getDateTimeBoundary = (limit) => {
+		const limitVal = getValues(limit);
+		if (limit === props.name || !limitVal) return undefined;
+		return dayjs(new Date(limitVal));
+	};
 
 	const handleChange = (newValue) => onChange(newValue ? newValue?.toDate()?.getTime() : newValue);
 
@@ -53,7 +56,7 @@ export const SequencingProperty = ({ onChange, onBlur, value, ...props }: DateTi
 			}}
 			value={value}
 			// onChange is a required prop in DatePicker, however it is not needed as onAccept works better
-			// (onChange triggers when changing year, onAccept only when a date is finally chosen)
+			// (onChange triggers at every change: year, minutes, hours, etc., onAccept only when a date is finally chosen)
 			onChange={() => true}
 			onAccept={handleChange}
 			minDateTime={getDateTimeBoundary(SEQUENCING_MODULE_START)}
