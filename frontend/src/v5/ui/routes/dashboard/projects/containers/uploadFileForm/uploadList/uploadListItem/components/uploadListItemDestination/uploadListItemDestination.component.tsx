42ca1130f812404bff2c0184d0353ca5cd29467b
sanmont3drepo <santiago.montero@3drepo.org>
2023-10-24 17:16:45 +0100

    ISSUE #4258 - Fixed reset of the container info when changing from an existing container to a new one

--- frontend/src/v5/ui/routes/dashboard/projects/containers/uploadFileForm/uploadList/uploadListItem/components/uploadListItemDestination/uploadListItemDestination.component.tsx
+++ frontend/src/v5/ui/routes/dashboard/projects/containers/uploadFileForm/uploadList/uploadListItem/components/uploadListItemDestination/uploadListItemDestination.component.tsx
@@ -15,18 +15,17 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { memo, useEffect, useState } from 'react';
+import { memo, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { ErrorTooltip } from '@controls/errorTooltip';
 import { createFilterOptions } from '@mui/material';
-import { IContainer, UploadItemFields } from '@/v5/store/containers/containers.types';
-import { ContainersHooksSelectors, FederationsHooksSelectors, ProjectsHooksSelectors, TeamspacesHooksSelectors } from '@/v5/services/selectorsHooks';
+import { IContainer } from '@/v5/store/containers/containers.types';
+import { ContainersHooksSelectors, FederationsHooksSelectors, ProjectsHooksSelectors } from '@/v5/services/selectorsHooks';
 import { canUploadToBackend, prepareSingleContainerData } from '@/v5/store/containers/containers.helpers';
 import { formatMessage } from '@/v5/services/intl';
 import { Role } from '@/v5/store/currentUser/currentUser.types';
 import { name as containerNameScheme } from '@/v5/validation/containerAndFederationSchemes/validators';
 import { isCollaboratorRole } from '@/v5/store/store.helpers';
-import { ContainersActionsDispatchers } from '@/v5/services/actionsDispatchers';
 import { DestinationAutocomplete, DestinationInput, NewOrExisting } from './uploadListItemDestination.styles';
 import { NewContainer } from './options/newContainer/newContainer.component';
 import { AlreadyUsedName } from './options/alreadyUsedName/alreadyUsedName.component';
@@ -69,8 +68,6 @@ export const UploadListItemDestination = memo(({
 	const { getValues, setValue } = useFormContext();
 
 	const isProjectAdmin = ProjectsHooksSelectors.selectIsProjectAdmin();
-	const teamspace = TeamspacesHooksSelectors.selectCurrentTeamspace();
-	const projectId = ProjectsHooksSelectors.selectCurrentProject();
 	const federationsNames = FederationsHooksSelectors.selectFederationsNames();
 	const containers = ContainersHooksSelectors.selectContainers();
 	const selectedContainer = containers.find((c) => c.name === value);
@@ -151,27 +148,7 @@ export const UploadListItemDestination = memo(({
 		return (<></>);
 	};
 
-	const sanitiseContainer = (baseContainer: IContainer): Partial<UploadItemFields> => ({
-		containerCode: baseContainer?.code || '',
-		containerType: baseContainer?.type || 'Uncategorised',
-		containerUnit: baseContainer?.unit || 'mm',
-	});
-
 	const onDestinationChange = (e, newVal: IContainer | null) => {
-		const sanitisedValue = sanitiseContainer(newVal);
-		if (newVal?._id) {
-
-			ContainersActionsDispatchers.fetchContainerSettings(
-				teamspace,
-				projectId,
-				newVal._id,
-			);
-
-			for (const [key, val] of Object.entries(sanitisedValue)) {
-				setValue(`${revisionPrefix}.${key}`, val);
-			}
-		}
-
 		setValue(`${revisionPrefix}.containerName`, newVal?.name?.trim() || '');
 		setValue(`${revisionPrefix}.containerId`, newVal?._id || '');
 	};
@@ -189,12 +166,6 @@ export const UploadListItemDestination = memo(({
 				.map(({ name }) => name),
 		);
 	};
-
-	useEffect(() => {
-		// This updates the container desc after it is fetched in the fetchContainerSettings
-		setValue(`${revisionPrefix}.containerDesc`, selectedContainer?.desc);
-	}, [selectedContainer]);
-
 
 	return (
 		<DestinationAutocomplete
--- frontend/src/v5/ui/routes/dashboard/projects/containers/uploadFileForm/uploadList/uploadListItem/uploadListItem.component.tsx
+++ frontend/src/v5/ui/routes/dashboard/projects/containers/uploadFileForm/uploadList/uploadListItem/uploadListItem.component.tsx
@@ -17,7 +17,7 @@
 
 import DeleteIcon from '@assets/icons/outlined/delete-outlined.svg';
 import EditIcon from '@assets/icons/outlined/edit-outlined.svg';
-import { RevisionsHooksSelectors } from '@/v5/services/selectorsHooks';
+import { ContainersHooksSelectors, ProjectsHooksSelectors, RevisionsHooksSelectors, TeamspacesHooksSelectors } from '@/v5/services/selectorsHooks';
 import { InputController } from '@controls/inputs/inputController.component';
 import { DashboardListItemRow as UploadListItemRow } from '@components/dashboard/dashboardList/dashboardListItem/components';
 import { UploadListItemFileIcon } from './components/uploadListItemFileIcon/uploadListItemFileIcon.component';
@@ -28,6 +28,8 @@ import { UploadListItemRevisionTag } from './components/uploadListItemRevisionTa
 import { UploadListItemButton } from './uploadListItem.styles';
 import { useFormContext } from 'react-hook-form';
 import { useEffect } from 'react';
+import { IContainer, UploadItemFields } from '@/v5/store/containers/containers.types';
+import { ContainersActionsDispatchers } from '@/v5/services/actionsDispatchers';
 
 type IUploadListItem = {
 	uploadId: string;
@@ -54,14 +56,37 @@ export const UploadListItem = ({
 }: IUploadListItem): JSX.Element => {
 	const revisionPrefix = `uploads.${index}`;
 	const uploadErrorMessage: string = RevisionsHooksSelectors.selectUploadError(uploadId);
-	
-	const { watch, trigger } = useFormContext();
+	const { watch, trigger, setValue } = useFormContext();
+	const teamspace = TeamspacesHooksSelectors.selectCurrentTeamspace();
+	const projectId = ProjectsHooksSelectors.selectCurrentProject();
+	const containerId = watch(`${revisionPrefix}.containerId`);
+	const selectedContainer = ContainersHooksSelectors.selectContainerById(containerId);
 
-	const contName = watch(`${revisionPrefix}.containerName`);
+	const sanitiseContainer = (container: IContainer): Partial<UploadItemFields> => ({
+		containerCode: container?.code || '',
+		containerType: container?.type || 'Uncategorised',
+		containerUnit: container?.unit || 'mm',
+		containerDesc: container?.desc,
+	});
 
 	useEffect(() => {
 		trigger(`${revisionPrefix}.revisionTag`);
-	}, [contName]);
+
+		for (const [key, val] of Object.entries(sanitiseContainer(selectedContainer))) {
+			setValue(`${revisionPrefix}.${key}`, val);
+		}
+	}, [JSON.stringify(selectedContainer)]);
+
+	useEffect(() => {
+		if (!containerId.trim()) return;
+
+		ContainersActionsDispatchers.fetchContainerSettings(
+			teamspace,
+			projectId,
+			containerId,
+		);
+	}, [containerId]);
+
 
 
 	return (
