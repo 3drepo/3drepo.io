39266a5c430f1eae60d494a872939663d681ec63
Carmen Fan <wind5320@hotmail.com>
2023-11-08 17:28:57 +0000

    update to 5.6.1 docs

--- api_project.js
+++ api_project.js
@@ -1,14 +1,14 @@
 define({
   "name": "3drepo.io",
-  "version": "4.16.2",
+  "version": "5.6.1",
   "description": "The backend for 3drepo.io",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-04T12:31:43.457Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.7"
+    "time": "2023-11-08T17:10:50.871Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
   }
 });
