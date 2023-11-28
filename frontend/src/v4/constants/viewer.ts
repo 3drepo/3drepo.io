8249f0eaf73e77270610385a6832e8632ddc67d8
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-11-28 15:49:17 +0000

    ISSUE #4608 - add types for sagas calls

--- frontend/src/v4/constants/viewer.ts
+++ frontend/src/v4/constants/viewer.ts
@@ -20,12 +20,12 @@ import { USE_BETA_VIEWER } from '../modules/viewer/betaViewer.helpers';
 export const VIEWER_NAV_MODES = {
 	HELICOPTER: 'HELICOPTER',
 	TURNTABLE: 'TURNTABLE'
-};
+} as const;
 
 export const VIEWER_CLIP_MODES = {
 	SINGLE: 'SINGLE',
 	BOX: 'BOX'
-};
+} as const;
 
 export const VIEWER_MAP_SOURCES = {
 	OSM: 'OSM',
@@ -144,7 +144,7 @@ export const VIEWER_PANELS = {
 export const VIEWER_PROJECTION_MODES = {
 	ORTHOGRAPHIC: 'orthographic',
 	PERSPECTIVE: 'perspective'
-};
+} as const;
 
 export const VIEWER_TOOLBAR_ITEMS = {
 	HOME: 'Home',
