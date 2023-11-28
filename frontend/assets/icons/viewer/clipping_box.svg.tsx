5cd08ea9da1ea5813d05b5ff1f1a7919bc64dc73
Alessandro Amantini <ale.amantini1997@gmail.com>
2023-11-28 15:47:59 +0000

    ISSUE #4608 - add new toolbar icons

---
+++ frontend/assets/icons/viewer/clipping_box.svg.tsx
@@ -0,0 +1,87 @@
+/**
+ *  Copyright (C) 2023 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+type IProps = {
+	className?: any;
+};
+
+export default ({ className }: IProps) => (
+	<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+		<g className="measure-surface-o">
+			<g className="highlight">
+				<path
+					d="M2.51367 12.1138V12.4717H3.7793V12.1138H2.51367Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M10.4238 12.1138V12.4717H11.6895V12.1138H10.4238Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M14.6865 11.3V11.4199H15.9521V10.3447H15.6465L14.6865 11.3Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M11.6895 10.8482V5.94992L12.966 4.93591V4.6431H11.2998L10.8825 4.97461H4.87873L5.26669 4.6431H3.31848L2.83424 5.05688C2.6428 5.1657 2.51367 5.37148 2.51367 5.60742V10.8482H3.7793V6.24023H10.4238V10.8482H11.6895Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M4.80583 3.37215H6.75404L7.20327 2.98828H13.3831L12.8998 3.37215H13.3831L15.3598 1.72808C15.3325 1.7245 15.3047 1.72266 15.2765 1.72266H6.96973C6.819 1.72266 6.67322 1.77646 6.55863 1.87437L4.80583 3.37215Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M15.9093 2.91718L14.6437 3.96984V9.55715L15.9093 8.29768V2.91718Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M11.6895 13.4473V15.3984H10.4238V13.4473H11.6895Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M2.51367 13.4473V15.3984H3.7793V13.4473H2.51367Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M2.51367 17.3496V16.374H3.7793V16.7168H4.46484V17.9824H3.14648C2.97865 17.9824 2.81769 17.9158 2.69902 17.7971C2.58034 17.6784 2.51367 17.5174 2.51367 17.3496Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M10.4238 16.374H11.2031L11.3473 16.2462L11.4606 16.374H11.6895V16.6322L12.1869 17.1932L11.4789 17.821C11.3669 17.9214 11.2189 17.9824 11.0566 17.9824H9.73828V16.7168H10.4238V16.374Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M8.41992 17.9824H5.7832V16.7168H8.41992V17.9824Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M15.9521 13.5703V12.4951H14.6865V13.2857L14.1891 13.7267L15.0287 14.6737L15.7391 14.0438C15.8746 13.9237 15.9521 13.7514 15.9521 13.5703Z"
+					fill="currentColor"
+				/>
+				<path
+					d="M14.3182 15.3036L12.8973 16.5634L12.0577 15.6163L13.4786 14.3566L14.3182 15.3036Z"
+					fill="currentColor"
+				/>
+			</g>
+			<path
+				className="primary"
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M3.51601 0.326383C3.63472 0.207456 3.79585 0.140625 3.96389 0.140625H17.2641C17.6136 0.140625 17.8969 0.423945 17.8969 0.773438V7.84221C17.8969 8.01058 17.8298 8.17201 17.7105 8.29077L14.0452 11.9383C13.8985 12.0842 13.6924 12.1472 13.4939 12.1138H0.720703C0.37121 12.1138 0.0878906 11.8305 0.0878906 11.481V4.01028C0.0878906 3.7979 0.192519 3.60995 0.353043 3.49517L3.51601 0.326383ZM12.966 10.8482V4.6431H1.35352V10.8482H12.966ZM16.6313 7.57919L14.2383 9.96058V4.30702L16.6313 2.31667V7.57919ZM15.7467 1.40625L13.3831 3.37215H2.26406L4.22635 1.40625H15.7467Z"
+				fill="currentColor"
+			/>
+		</g>
+	</svg>
+);
---
+++ frontend/assets/icons/viewer/focus.svg.tsx
@@ -0,0 +1,51 @@
+/**
+ *  Copyright (C) 2023 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+type IProps = {
+	className?: any;
+};
+
+export default ({ className }: IProps) => (
+	<svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+		<g className="move-cube-o">
+			<path
+				className="highlight"
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M9.26016 3.6775C9.09383 3.60359 8.90399 3.6036 8.73768 3.6775L4.24931 5.67182C4.01694 5.77507 3.86719 6.00549 3.86719 6.25976V11.7433C3.86719 11.9976 4.01695 12.228 4.24931 12.3313L8.73768 14.3256C8.90399 14.3995 9.09383 14.3995 9.26016 14.3256L13.7485 12.3313C13.9809 12.228 14.1307 11.9976 14.1307 11.7433V6.25976C14.1307 6.00549 13.9809 5.77507 13.7485 5.67182L9.26016 3.6775ZM6.05668 6.25369L8.99892 4.94636L11.9412 6.25371L8.99895 7.56074L6.05668 6.25369Z"
+				fill="currentColor"
+			/>
+			<g className="primary">
+				<path
+					fill="currentColor"
+					d="M0.808594 0C0.36202 0 0 0.362019 0 0.808594V5.90625H1.26562V1.26562H5.90625V0H0.808594Z"
+				/>
+				<path
+					fill="currentColor"
+					d="M17.1914 18.0352C17.638 18.0352 18 17.6731 18 17.2266V12.1289H16.7344V16.7695H12.0938V18.0352H17.1914Z"
+				/>
+				<path
+					fill="currentColor"
+					d="M18 0.808594C18 0.362019 17.638 0 17.1914 0H12.0938V1.26562H16.7344V5.90625H18V0.808594Z"
+				/>
+				<path
+					fill="currentColor"
+					d="M0.808594 18.0352C0.36202 18.0352 0 17.6731 0 17.2266V12.1289H1.26562V16.7695H5.90625V18.0352H0.808594Z"
+				/>
+			</g>
+		</g>
+	</svg>
+);
