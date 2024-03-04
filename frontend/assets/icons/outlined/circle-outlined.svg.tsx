3279a215e37785b9841b520c3cf7fec82ac390b4
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-03-04 15:52:19 +0000

    ISSUE #4736 - fix icon too big

--- frontend/assets/icons/outlined/circle-outlined.svg.tsx
+++ frontend/assets/icons/outlined/circle-outlined.svg.tsx
@@ -19,7 +19,7 @@ type IProps = {
 };
 
 export default ({ className }: IProps) => (
-	<svg xmlns="http://www.w3.org/2000/svg" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" className={className}>
-		<circle cx="512" cy="512" r="476" stroke="currentColor" strokeWidth="72" />
+	<svg className={className} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+		<path fillRule="evenodd" clipRule="evenodd" d="M10 1.55078C5.33363 1.55078 1.55078 5.33363 1.55078 10C1.55078 14.6664 5.33363 18.4492 10 18.4492C14.6664 18.4492 18.4492 14.6664 18.4492 10C18.4492 5.33363 14.6664 1.55078 10 1.55078ZM0.0742188 10C0.0742188 4.51814 4.51814 0.0742188 10 0.0742188C15.4819 0.0742188 19.9258 4.51814 19.9258 10C19.9258 15.4819 15.4819 19.9258 10 19.9258C4.51814 19.9258 0.0742188 15.4819 0.0742188 10Z" fill="currentColor" />
 	</svg>
 );
