f3058282064b972168b80aac84407556ef64e0a5
sanmont3drepo <santiago.montero@3drepo.org>
2024-03-28 16:53:12 +0000

    ISSUE #4827 - panzoom replaced with custom implementation

--- frontend/src/v5/ui/components/viewer/drawingViewer/centredPanZoom.ts
+++ frontend/src/v5/ui/components/viewer/drawingViewer/centredPanZoom.ts
@@ -16,8 +16,68 @@
  */
 
 import { aspectRatio } from '@/v4/helpers/aspectRatio';
-import panzoom, { PanZoom, PanZoomOptions } from 'panzoom';
+import { noop } from 'lodash';
+import { PanZoom, PanZoomOptions } from 'panzoom';
 
+
+const panzoom = (target: HTMLElement | SVGElement, options) => {
+	const transform = { scale:1, x: 0, y: 0 };
+	const zoomStep = 0.2;
+	
+	const onWheel = (ev: WheelEvent) => {
+		const newScale = transform.scale + zoomStep * -Math.sign(ev.deltaY);
+		const originalRect = target.getBoundingClientRect();
+		const targetPos = { x: ev.clientX - originalRect.x, y: ev.clientY - originalRect.y } ;
+
+		const scaleChange = newScale / transform.scale;
+		const newPos = { x: transform.x + targetPos.x * (1 - scaleChange), y: transform.y + targetPos.y * ( 1 - scaleChange) };
+
+		transform.scale = newScale;
+		transform.x = newPos.x;
+		transform.y = newPos.y;
+
+		target.style.transform = 'scale(' + transform.scale + ') translateX(' + transform.x / transform.scale + 'px) translateY(' + transform.y / transform.scale  + 'px)';
+	};
+
+	const onMouseMove = (ev: MouseEvent) => {
+		transform.x += ev.movementX;
+		transform.y += ev.movementY;
+		target.style.transform = 'scale(' + transform.scale + ') translateX(' + transform.x / transform.scale + 'px) translateY(' + transform.y / transform.scale  + 'px)';
+	};
+
+	const onMouseDown = () => {
+		target.parentElement.addEventListener('mousemove', onMouseMove);
+	};
+
+	const onMouseUp = () => {
+		target.parentElement.removeEventListener('mousemove', onMouseMove);
+	};
+
+	const subscribeToEvents = () => {
+		target.parentElement.addEventListener('wheel', onWheel);
+		target.parentElement.addEventListener('mousedown', onMouseDown);
+		target.parentElement.addEventListener('mouseup', onMouseUp);
+	};
+
+	const unSubscribeToEvents = () => {
+		// target.parentElement.removeEventListener('wheel', onWheel);
+		// target.parentElement.removeEventListener('mousemove', onMouseMove);
+	};
+
+	target.style.transformOrigin = '0 0';
+	target.setAttribute('draggable', 'false');
+
+	subscribeToEvents();
+
+	const getTransform = () => transform;
+	return { 
+		getTransform, 
+		dispose: unSubscribeToEvents, 
+		on: noop, 
+		smoothZoom:noop,
+		moveTo: noop,
+	};
+};
 
 
 export type PanZoomHandler = PanZoom & { zoomIn : () => void, zoomOut: () => void };
@@ -53,6 +113,7 @@ export const centredPanZoom = (target: HTMLImageElement | SVGSVGElement, padding
 	const options:PanZoomOptions = {
 		maxZoom: 10,
 		minZoom: 1,
+		bounds: parentRect,
 	};
 	
 	const pz = panzoom(target, options);
@@ -86,7 +147,7 @@ export const centredPanZoom = (target: HTMLImageElement | SVGSVGElement, padding
 
 	const zoomIn = () => zoom(1.5);
 
-	const zoomOut = () => zoom(0.8);
+	const zoomOut = () => zoom(1 / 1.5);
 
-	return { pz, zoomIn, zoomOut } ;
+	return { ...pz, zoomIn, zoomOut } ;
 };
