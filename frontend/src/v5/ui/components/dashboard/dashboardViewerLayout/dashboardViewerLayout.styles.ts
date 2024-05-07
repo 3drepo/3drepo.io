13072f037c9b9329014be5a204f01c2735c4a344
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-05-07 17:48:56 +0100

    ISSUE #4832 - fix: drawing viewer size exceeds expected boundaries

--- frontend/src/v5/ui/components/dashboard/dashboardViewerLayout/dashboardViewerLayout.styles.ts
+++ frontend/src/v5/ui/components/dashboard/dashboardViewerLayout/dashboardViewerLayout.styles.ts
@@ -20,6 +20,7 @@ import styled from 'styled-components';
 export const Content = styled.div`
 	background-color: ${({ theme }) => theme.palette.tertiary.lightest};
 	overflow-y: auto;
+	position: relative;
 	flex-grow: 1;
 	height: 100%;
 `;
