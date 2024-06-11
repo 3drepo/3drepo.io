89c5bf1ad8dc12dfc9d2ae2c6ad25198bd69f539
The-Daniel <bibshall@gmail.com>
2024-06-11 10:38:03 +0100

    ISSUE #4926 - color converter functions work for non integer rgba values

--- frontend/src/v4/helpers/colors.ts
+++ frontend/src/v4/helpers/colors.ts
@@ -63,7 +63,7 @@ export const componentToHex = memoize((c) => {
 		return '';
 	}
 
-	const hex = c.toString(16).toUpperCase();
+	const hex = c.toString(16).toUpperCase().slice(0, 2);
 	return hex.length === 1 ? '0' + hex : hex;
 });
 
