7392ac3fbd75033db5863741dede4588a120f541
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-06-12 14:56:08 +0100

    ISSUE #4907 - create info box component

---
+++ frontend/src/v5/ui/controls/infoBox/infoBox.styles.ts
@@ -0,0 +1,72 @@
+/**
+ *  Copyright (C) 2024 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import styled from 'styled-components';
+import InfoIconBase from '@assets/icons/viewer/info.svg';
+
+export const Container = styled.div`
+	position: absolute;
+	z-index: 2;
+	width: 100%;
+	margin-top: 20px;
+	pointer-events: none;
+
+	& > * {
+		pointer-events: all;
+	}
+`;
+
+export const Box = styled.div`
+	min-height: 68px;
+	min-width: 424px;
+	max-width: 600px;
+	margin: auto;
+	box-sizing: border-box;
+	padding: 9px 16px;
+	background-color: ${({ theme }) => theme.palette.primary.contrast};
+	color: ${({ theme }) => theme.palette.secondary.main};
+	border: solid 1px ${({ theme }) => theme.palette.info.light};
+	border-radius: 10px;
+	display: grid;
+	grid-template-columns: 28px 1fr;
+	row-gap: 5px;
+	column-gap: 6px;
+	place-items: center;
+`;
+
+export const InfoIcon = styled(InfoIconBase)`
+	color: ${({ theme }) => theme.palette.info.main};
+`;
+
+export const TitleBar = styled.div`
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	${({ theme }) => theme.typography.h3};
+`;
+
+export const CloseIconContainer = styled.div`
+	cursor: pointer;
+`;
+
+export const Description = styled.div`
+	${({ theme }) => theme.typography.body1};
+	display: flex;
+	align-items: center;
+	gap: 5px;
+	width: 100%;
+`;
