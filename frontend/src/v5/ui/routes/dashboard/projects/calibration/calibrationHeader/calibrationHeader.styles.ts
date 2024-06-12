0bee180159bb888f2c09395fda35d3f1490cf9f3
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-06-12 14:54:15 +0100

    ISSUE #4907 - style steps

--- frontend/src/v5/ui/routes/dashboard/projects/calibration/calibrationHeader/calibrationHeader.styles.ts
+++ frontend/src/v5/ui/routes/dashboard/projects/calibration/calibrationHeader/calibrationHeader.styles.ts
@@ -15,13 +15,70 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Stepper as StepperBase } from '@mui/material';
+import { StepConnector, Stepper as StepperBase } from '@mui/material';
 import { Button as ButtonBase } from '@controls/button';
 import styled from 'styled-components';
 
 export const Stepper = styled(StepperBase)`
 	border-radius: 0;
 	box-shadow: none;
+
+	.MuiStep-root {
+		width: 200px;
+		border-bottom: none;
+
+		.MuiStepLabel-label {
+			&, &.Mui-active, &.Mui-completed, &.Mui-disabled {
+				color: ${({ theme }) => theme.palette.primary.contrast};
+			}
+		}
+
+		.MuiStepLabel-iconContainer {
+			width: 24px;
+			height: 24px;
+			border-radius: 24px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-weight: 500;
+			position: relative;
+			z-index: 1;
+
+			&.Mui-completed {
+				background-color: ${({ theme }) => theme.palette.primary.main};
+				color: ${({ theme }) => theme.palette.primary.contrast};
+			}
+			&.Mui-active {
+				background-color: ${({ theme }) => theme.palette.primary.lightest};
+				color: ${({ theme }) => theme.palette.secondary.main};
+				border: solid 2px ${({ theme }) => theme.palette.primary.main};
+				box-sizing: border-box;
+			}
+			&.Mui-disabled {
+				background-color: ${({ theme }) => theme.palette.primary.contrast};
+				color: ${({ theme }) => theme.palette.secondary.main};
+			}
+		}
+	}
+`;
+
+export const Connector = styled(StepConnector)`
+	&.MuiStepConnector-root {
+		display: unset;
+		top: 20.5px;
+		left: calc(-50% + 11px);
+		right: calc(50% + 11px);
+	}
+
+	.MuiStepConnector-line {
+		height: 3px;
+		border: 0;
+		background-color: ${({ theme }) => theme.palette.primary.main};
+	}
+
+	&.Mui-disabled .MuiStepConnector-line {
+		background-color: ${({ theme }) => theme.palette.primary.contrast};
+	}
 `;
 
 export const Container = styled.div`
