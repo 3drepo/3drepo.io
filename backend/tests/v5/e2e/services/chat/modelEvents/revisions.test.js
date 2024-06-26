f6284a7ac7e576759975cd0b6ebb5c30999cd79e
ChristosTsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2024-06-26 13:08:21 +0100

    ISSUE #4983 missing test added

--- backend/tests/v5/e2e/services/chat/modelEvents/revisions.test.js
+++ backend/tests/v5/e2e/services/chat/modelEvents/revisions.test.js
@@ -18,6 +18,7 @@
 const ServiceHelper = require('../../../../helper/services');
 const { src } = require('../../../../helper/path');
 const SuperTest = require('supertest');
+const { modelTypes } = require('../../../../../../src/v5/models/modelSettings.constants');
 
 const { EVENTS } = require(`${src}/services/chat/chat.constants`);
 const { templates } = require(`${src}/utils/responseCodes`);
@@ -26,7 +27,9 @@ const user = ServiceHelper.generateUserCredentials();
 const teamspace = ServiceHelper.generateRandomString();
 const project = ServiceHelper.generateRandomProject();
 const container = ServiceHelper.generateRandomModel();
+const drawing = ServiceHelper.generateRandomModel({ modelType: modelTypes.DRAWING });
 const containerRevision = ServiceHelper.generateRevisionEntry();
+const drawingRevision = ServiceHelper.generateRevisionEntry(false, true, true);
 
 let agent;
 const setupData = async () => {
@@ -38,11 +41,18 @@ const setupData = async () => {
 			container.name,
 			container.properties,
 		),
+		ServiceHelper.db.createModel(
+			teamspace,
+			drawing._id,
+			drawing.name,
+			drawing.properties,
+		),
 	]);
 	await Promise.all([
 		ServiceHelper.db.createUser(user, [teamspace]),
-		ServiceHelper.db.createProject(teamspace, project.id, project.name, [container._id]),
+		ServiceHelper.db.createProject(teamspace, project.id, project.name, [container._id, drawing._id]),
 		ServiceHelper.db.createRevision(teamspace, container._id, { ...containerRevision, author: user.user }),
+		ServiceHelper.db.createRevision(teamspace, drawing._id, { ...drawingRevision, author: user.user }, true),
 	]);
 };
 
@@ -93,6 +103,32 @@ const revisionUpdateTest = () => {
 				...data,
 				data: {
 					_id: containerRevision._id,
+					void: true,
+				},
+			});
+
+			socket.close();
+		});
+
+		test(`should trigger a ${EVENTS.DRAWING_REVISION_UPDATE} event when a drawing revision has been updated using revision Id`, async () => {
+			const socket = await ServiceHelper.socket.loginAndGetSocket(agent, user.user, user.password);
+
+			const data = { teamspace, project: project.id, model: drawing._id };
+			await ServiceHelper.socket.joinRoom(socket, data);
+
+			const socketPromise = new Promise((resolve, reject) => {
+				socket.on(EVENTS.DRAWING_REVISION_UPDATE, resolve);
+				setTimeout(reject, 1000);
+			});
+
+			await agent.patch(`/v5/teamspaces/${teamspace}/projects/${project.id}/drawings/${drawing._id}/revisions/${drawingRevision._id}?key=${user.apiKey}`)
+				.send({ void: true })
+				.expect(templates.ok.status);
+
+			await expect(socketPromise).resolves.toEqual({
+				...data,
+				data: {
+					_id: drawingRevision._id,
 					void: true,
 				},
 			});
