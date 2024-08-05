f4a07272664920386a4747c2f58bf0c8320fba00
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-08-05 11:22:56 +0100

    ISSUE #4449 - fix linter errors about spacing

--- frontend/src/globals/unity-util.ts
+++ frontend/src/globals/unity-util.ts
@@ -528,8 +528,8 @@ export class UnityUtil {
 	 */
 	public static clipBroadcast(clipInfo: string) {
 		const data = JSON.parse(clipInfo);
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] clipBroadcast", data);
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] clipBroadcast', data);
 		}
 		if (UnityUtil.viewer && UnityUtil.viewer.clipBroadcast) {
 			UnityUtil.viewer.clipBroadcast(JSON.parse(data));
@@ -538,8 +538,8 @@ export class UnityUtil {
 
 	/** @hidden */
 	public static clipUpdated() {
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] clipUpdated");
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] clipUpdated');
 		}
 		if (UnityUtil.viewer && UnityUtil.viewer.clipUpdated) {
 			UnityUtil.viewer.clipUpdated();
@@ -549,8 +549,8 @@ export class UnityUtil {
 	/** @hidden */
 	public static currentPointInfo(pointInfo) {
 		const point = JSON.parse(pointInfo);
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] currentPointInfo", point);
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] currentPointInfo', point);
 		}
 		if (UnityUtil.viewer && UnityUtil.viewer.objectSelected) {
 			UnityUtil.viewer.objectSelected(point);
@@ -589,8 +589,8 @@ export class UnityUtil {
 
 	/** @hidden */
 	public static navMethodChanged(newNavMode) {
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] navMethodChanged", newNavMode);
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] navMethodChanged', newNavMode);
 		}
 		if (UnityUtil.viewer && UnityUtil.viewer.navMethodChanged) {
 			UnityUtil.viewer.navMethodChanged(newNavMode);
@@ -599,8 +599,8 @@ export class UnityUtil {
 
 	/** @hidden */
 	public static objectsSelectedAlert(nodeInfo) {
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] objectsSelectedAlert", JSON.parse(nodeInfo));
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] objectsSelectedAlert', JSON.parse(nodeInfo));
 		}
 		UnityUtil.viewer.objectsSelected(JSON.parse(nodeInfo).nodes);
 	}
@@ -609,8 +609,8 @@ export class UnityUtil {
 	public static objectStatusBroadcast(nodeInfo) {
 		try {
 			const data = JSON.parse(nodeInfo);
-			if(UnityUtil.verbose) {
-				console.debug("[FROM UNITY] objectStatusBroadcast", data);
+			if (UnityUtil.verbose) {
+				console.debug('[FROM UNITY] objectStatusBroadcast', data);
 			}
 			UnityUtil.objectStatusPromises.forEach((promise) => {
 				promise.resolve(data);
@@ -635,8 +635,8 @@ export class UnityUtil {
 	/** @hidden */
 	public static pickPointAlert(pointInfo) {
 		const point = JSON.parse(pointInfo);
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] pickPointAlert", point);
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] pickPointAlert', point);
 		}
 		if (UnityUtil.viewer && UnityUtil.viewer.pickPointEvent) {
 			UnityUtil.viewer.pickPointEvent(point);
@@ -648,8 +648,8 @@ export class UnityUtil {
 		try {
 			const ssJSON = JSON.parse(screenshot);
 
-			if(UnityUtil.verbose) {
-				console.debug("[FROM UNITY] screenshotReady", ssJSON);
+			if (UnityUtil.verbose) {
+				console.debug('[FROM UNITY] screenshotReady', ssJSON);
 			}
 
 			UnityUtil.screenshotPromises.forEach((promise) => {
@@ -668,8 +668,8 @@ export class UnityUtil {
 	public static viewpointReturned(vpInfo) {
 		try {
 			const viewpoint = JSON.parse(vpInfo);
-			if(UnityUtil.verbose) {
-				console.debug("[FROM UNITY] viewpointReturned", viewpoint);
+			if (UnityUtil.verbose) {
+				console.debug('[FROM UNITY] viewpointReturned', viewpoint);
 			}
 
 			UnityUtil.viewpointsPromises.forEach((promise) => {
@@ -689,8 +689,8 @@ export class UnityUtil {
 	public static measurementAlert(strMeasurement) {
 		try {
 			const measurement = JSON.parse(strMeasurement);
-			if(UnityUtil.verbose) {
-				console.debug("[FROM UNITY] measurementAlert", measurement);
+			if (UnityUtil.verbose) {
+				console.debug('[FROM UNITY] measurementAlert', measurement);
 			}
 			if (UnityUtil.viewer && UnityUtil.viewer.measurementAlertEvent) {
 				UnityUtil.viewer.measurementAlertEvent(measurement);
@@ -702,8 +702,8 @@ export class UnityUtil {
 
 	/** @hidden */
 	public static measurementRemoved(measurementId) {
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] measurementRemoved", measurementId);
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] measurementRemoved', measurementId);
 		}
 		if (UnityUtil.viewer && UnityUtil.viewer.measurementRemoved) {
 			UnityUtil.viewer.measurementRemoved(measurementId);
@@ -712,8 +712,8 @@ export class UnityUtil {
 
 	/** @hidden */
 	public static measurementsCleared() {
-		if(UnityUtil.verbose) {
-			console.debug("[FROM UNITY] measurementCleared");
+		if (UnityUtil.verbose) {
+			console.debug('[FROM UNITY] measurementCleared');
 		}
 		if (UnityUtil.viewer && UnityUtil.viewer.measurementsCleared) {
 			UnityUtil.viewer.measurementsCleared();
