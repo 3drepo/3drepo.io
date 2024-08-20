c6ba47212e29f326709c056ecb0db901a8df8fe2
Carmen Fan <wind5320@hotmail.com>
2024-08-20 09:48:39 +0100

    ISSUE #5090 add debug message when putting message to queue

--- backend/src/v5/handler/queue.js
+++ backend/src/v5/handler/queue.js
@@ -143,6 +143,7 @@ Queue.queueMessage = async (queueName, correlationId, msg) => {
 	const dataBuf = Buffer.from(msg);
 	const meta = { correlationId, persistent: true };
 
+	logger.logInfo(`Sending message to ${queueName}: [${correlationId}] ${msg}`);
 	await channel.sendToQueue(queueName, dataBuf, meta);
 	await channel.close();
 };
