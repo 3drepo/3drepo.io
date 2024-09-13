0550d6cae7a05df69f471be1b0a29f8b1cf341a6
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-09-13 12:54:15 +0100

    ISSUE #5044 - add calibration to revision interface

--- frontend/src/v5/store/drawings/revisions/drawingRevisions.types.ts
+++ frontend/src/v5/store/drawings/revisions/drawingRevisions.types.ts
@@ -15,6 +15,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { CalibrationState } from '../drawings.types';
+
 export interface StatusCode {
 	code: string,
 	description: string,
@@ -30,6 +32,7 @@ export interface IDrawingRevision {
 	statusCode?: string;
 	revCode: string;
 	void?: boolean;
+	calibration: CalibrationState;
 }
 
 export interface IDrawingRevisionUploadStatus {
