58851502cb474ba5cb5e07bc874dbf97c9e57e9f
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-09-17 15:13:26 +0100

    ISSUE #5044 - use latest revision selector

--- frontend/src/v5/store/drawings/revisions/drawingRevisions.sagas.ts
+++ frontend/src/v5/store/drawings/revisions/drawingRevisions.sagas.ts
@@ -30,7 +30,7 @@ import { CreateRevisionAction,
 } from './drawingRevisions.redux';
 import { DrawingsActions } from '../drawings.redux';
 import { createDrawingFromRevisionBody, createFormDataFromRevisionBody } from './drawingRevisions.helpers';
-import { selectIsPending, selectRevisions, selectStatusCodes } from './drawingRevisions.selectors';
+import { selectActiveRevisions, selectIsPending, selectStatusCodes } from './drawingRevisions.selectors';
 import { UploadStatus } from '../../containers/containers.types';
 
 export function* fetch({ teamspace, projectId, drawingId, onSuccess }: FetchAction) {
@@ -54,8 +54,7 @@ export function* setVoidStatus({ teamspace, projectId, drawingId, revisionId, is
 	try {
 		yield API.DrawingRevisions.setRevisionVoidStatus(teamspace, projectId, drawingId, revisionId, isVoid);
 		yield put(DrawingRevisionsActions.setVoidStatusSuccess(drawingId, revisionId, isVoid));
-		const revisions = yield select(selectRevisions, drawingId);
-		const activeRevisions = revisions.filter((rev) => !rev.void);
+		const activeRevisions = yield select(selectActiveRevisions, drawingId);
 		const revisionsCount = activeRevisions.length;
 		const latestRevision = revisionsCount ? orderBy(activeRevisions, 'timestamp')[0].name : null;
 		const updates = {
