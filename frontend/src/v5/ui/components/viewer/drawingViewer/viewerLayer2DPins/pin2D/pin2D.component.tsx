14eecab3df50475a8919801de8871dd7a524bc31
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-09-18 13:05:41 +0100

    ISSUE #5040 - fix: clicking on a non-default pin crashes the app

--- frontend/src/v5/ui/components/viewer/drawingViewer/viewerLayer2DPins/pin2D/pin2D.component.tsx
+++ frontend/src/v5/ui/components/viewer/drawingViewer/viewerLayer2DPins/pin2D/pin2D.component.tsx
@@ -19,16 +19,23 @@ import { IPin } from '@/v4/services/viewer/viewer';
 import PinIcon from '@assets/icons/filled/ticket_pin-filled.svg';
 import { PinContainer } from './pin2D.styles';
 import { TicketsCardActionsDispatchers } from '@/v5/services/actionsDispatchers';
+import { TicketsHooksSelectors } from '@/v5/services/selectorsHooks';
+import { useParams } from 'react-router';
+import { ViewerParams } from '@/v5/ui/routes/routes.constants';
 
 type Pin2DProps = IPin & { scale: number };
 export const Pin2D = ({ id, isSelected, position, colour, scale }: Pin2DProps) => {
 	const selectedMagnifierFactor = isSelected ? 1.2 : 1;
 	const height = 23 / scale * selectedMagnifierFactor;
 	const width = 18 / scale * selectedMagnifierFactor;
+	const { containerOrFederation } = useParams<ViewerParams>();
+	const tickets = TicketsHooksSelectors.selectTickets(containerOrFederation);
 
 	const handleClick = (e) => {
 		e.stopPropagation();
 		TicketsCardActionsDispatchers.setSelectedTicketPin(id);
+		if (!tickets.some((t) => t._id === id)) return;
+		
 		TicketsCardActionsDispatchers.openTicket(id);
 	};
 
