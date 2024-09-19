8bbac8b04066d819dcb2255e7bf151e38b6e7491
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-09-19 16:40:09 +0100

    ISSUE #5149 - create update permission modal & co

---
+++ frontend/src/v5/ui/components/shared/updatePermissionModal/updatePermissionModal.component.tsx
@@ -0,0 +1,52 @@
+/**
+ *  Copyright (C) 2024 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { DialogsActionsDispatchers } from '@/v5/services/actionsDispatchers';
+import { IInfoModal, InfoModal } from '../modalsDispatcher/templates/infoModal/infoModal.component';
+import { formatMessage } from '@/v5/services/intl';
+import { isPermissionModalSuppressed } from './updatePermissionModal.helpers';
+import { WarningIcon } from '../modalsDispatcher/modalsDispatcher.styles';
+
+type UpdatePermissionProps = {
+	onConfirm: () => void,
+	permissionType: string,
+	permissionsChange?: number,
+};
+export const UpdatePermissionModal = ({ onConfirm, permissionType, permissionsChange = 1, ...props }: IInfoModal & UpdatePermissionProps) => (
+	<InfoModal
+		Icon={WarningIcon}
+		primaryButtonLabel='Cancel'
+		secondaryButtonLabel='Continue'
+		onClickSecondary={onConfirm}
+		message={formatMessage({
+			id: 'permissionsModal.message',
+			defaultMessage: `
+				You are about to change {permissionsChange} {permissionsChange, plural, one {user} other {users}}
+				permissions to {permissionType}.{br}Are you sure you would like to proceed?
+			`,
+		}, { permissionsChange, permissionType, br: <br /> })}
+		{...props}
+	/>
+);
+
+export const updatePermissionsOrTriggerModal = (props: UpdatePermissionProps) => {
+	if (isPermissionModalSuppressed()) {
+		props.onConfirm();
+		return;
+	}
+	DialogsActionsDispatchers.open(UpdatePermissionModal, props);
+};
