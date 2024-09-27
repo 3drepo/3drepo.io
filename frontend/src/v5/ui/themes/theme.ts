a23b4d04d7b007dcf4ec30b2e1085420383d5eae
The-Daniel <bibshall@gmail.com>
2024-09-27 16:30:03 +0100

    ISSUE #5151 - fix input not having placeholder text

--- frontend/src/v5/ui/themes/theme.ts
+++ frontend/src/v5/ui/themes/theme.ts
@@ -319,13 +319,6 @@ export const theme = createTheme({
 							'-moz-appearance': 'textfield',
 						},
 
-						'::placeholder': {
-							opacity: 1,
-							color: COLOR.BASE_MAIN,
-							'&:hover': {
-								color: COLOR.SECONDARY_MAIN,
-							},
-						},
 					},
 					'.Mui-error': {
 						'.MuiOutlinedInput-notchedOutline': {
@@ -958,9 +951,6 @@ export const theme = createTheme({
 					},
 					'&:hover:not(.Mui-focused)': {
 						backgroundColor: COLOR.TERTIARY_LIGHTER,
-						'.MuiInputBase-input::placeholder': {
-							color: COLOR.SECONDARY_MAIN,
-						},
 					},
 					'&, &:hover, &.Mui-focused': {
 						'&:not(.Mui-disabled):before, &:not(.Mui-disabled):after': {
@@ -970,6 +960,13 @@ export const theme = createTheme({
 				},
 				input: {
 					padding: '0 12px',
+					'::placeholder': {
+						opacity: 1,
+						color: COLOR.BASE_MAIN,
+					},
+					'&:hover:not(.Mui-focused)::placeholder': {
+						color: COLOR.SECONDARY_MAIN,
+					},
 				},
 			},
 		},
@@ -995,6 +992,10 @@ export const theme = createTheme({
 						padding: '0px 15px',
 						height: 35,
 						lineHeight: '35px',
+					},
+					'&&&& ::placeholder': {
+						opacity: '1 !important',
+						color: COLOR.BASE_LIGHTER,
 					},
 				},
 				notchedOutline: {
