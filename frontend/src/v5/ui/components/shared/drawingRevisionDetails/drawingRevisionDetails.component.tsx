07559d0f27d17feb1aadf5c4047bc8969bed5e31
The-Daniel <bibshall@gmail.com>
2024-10-10 14:19:37 +0100

    ISSUE #5151 - fix imports

--- frontend/src/v5/ui/components/shared/drawingRevisionDetails/drawingRevisionDetails.component.tsx
+++ frontend/src/v5/ui/components/shared/drawingRevisionDetails/drawingRevisionDetails.component.tsx
@@ -46,7 +46,7 @@ import { RevisionsListItemTag } from '../revisionDetails/components/revisionsLis
 import { IDrawingRevision } from '@/v5/store/drawings/revisions/drawingRevisions.types';
 import { formatDateTime } from '@/v5/helpers/intl.helper';
 import { UploadStatus } from '@/v5/store/containers/containers.types';
-import { downloadFile } from '@components/authenticatedResource/authenticatedResource.hooks';
+import { downloadFile } from '@/v5/helpers/download.helper';
 
 interface IDrawingRevisionDetails {
 	drawingId: string;
