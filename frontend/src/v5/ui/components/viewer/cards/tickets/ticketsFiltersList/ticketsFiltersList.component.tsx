2fa01cb2e7d9fb8463d321c1dfa7c9d8b32de997
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-11-18 15:20:26 +0000

    ISSUE #5188 - define filters list

---
+++ frontend/src/v5/ui/components/viewer/cards/tickets/ticketsFiltersList/ticketsFiltersList.component.tsx
@@ -0,0 +1,42 @@
+/**
+ *  Copyright (C) 2024 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Fragment } from 'react';
+import { CardFilterOperator } from '../../cardFilters/cardFilters.types';
+import { FiltersAccordion } from '../../cardFilters/filtersList/filtersAccordion/filtersAccordion.component';
+import { FiltersSection } from '../../cardFilters/filtersList/filtersSection/filtersSection.component';
+import { ModuleTitle } from './ticketsFiltersList.styles';
+
+type TicketsFiltersListProps = {
+	onDeleteFilter: (module: string, property: string, operator: CardFilterOperator) => void;
+	onDeleteAllFilters: () => void;
+	filters: Record<string, any>;
+};
+export const TicketsFiltersList = ({ filters, onDeleteFilter, onDeleteAllFilters }: TicketsFiltersListProps) => {
+	const onDeleteModuleFilter = (moduleName) => (property, operator) => onDeleteFilter(moduleName, property, operator);
+
+	return (
+		<FiltersAccordion onClear={onDeleteAllFilters}>
+			{Object.entries(filters).map(([moduleName, moduleFilters]) => (
+				<Fragment key={moduleName}>
+					{moduleName && (<ModuleTitle>{moduleName}</ModuleTitle>)}
+					<FiltersSection filters={moduleFilters} onDeleteFilter={onDeleteModuleFilter(moduleName)} />
+				</Fragment>
+			))}
+		</FiltersAccordion>
+	);
+};
