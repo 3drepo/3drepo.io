f0c39bbb1d4880f44b26798536bcee77af1d055c
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-11-19 17:51:07 +0000

    ISSUE #5257 - add icons for datatype-filters

---
+++ frontend/assets/icons/filters/list.svg.tsx
@@ -0,0 +1,31 @@
+/**
+ *  Copyright (C) 2024 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+type IProps = {
+	className?: any;
+};
+
+export default ({ className }: IProps) => (
+	<svg className={className} width="11" height="10" viewBox="0 0 11 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path fill-rule="evenodd" clip-rule="evenodd" d="M0 0.574219C0 0.36064 0.17314 0.1875 0.386719 0.1875H2.36328C2.57686 0.1875 2.75 0.36064 2.75 0.574219V2.55078C2.75 2.76436 2.57686 2.9375 2.36328 2.9375H0.386719C0.17314 2.9375 0 2.76436 0 2.55078V0.574219ZM0.773438 2.16406V0.960938H1.97656V2.16406H0.773438Z" fill="currentColor"/>
+		<path d="M4.5332 1.17578C4.31962 1.17578 4.14648 1.34892 4.14648 1.5625C4.14648 1.77608 4.31962 1.94922 4.5332 1.94922L10.6133 1.94922C10.8269 1.94922 11 1.77608 11 1.5625C11 1.34892 10.8269 1.17578 10.6133 1.17578L4.5332 1.17578Z" fill="currentColor"/>
+		<path d="M4.5332 4.60254C4.31962 4.60254 4.14648 4.77568 4.14648 4.98926C4.14648 5.20284 4.31962 5.37598 4.5332 5.37598L10.6133 5.37598C10.8269 5.37598 11 5.20284 11 4.98926C11 4.77568 10.8269 4.60254 10.6133 4.60254L4.5332 4.60254Z" fill="currentColor"/>
+		<path d="M4.14648 8.49121C4.14648 8.27763 4.31962 8.10449 4.5332 8.10449L10.6133 8.10449C10.8269 8.10449 11 8.27763 11 8.49121C11 8.70479 10.8269 8.87793 10.6133 8.87793L4.5332 8.87793C4.31962 8.87793 4.14648 8.70479 4.14648 8.49121Z" fill="currentColor"/>
+		<path fillRule="evenodd" clipRule="evenodd" d="M0.386719 3.625C0.17314 3.625 0 3.79814 0 4.01172V5.98828C0 6.20186 0.17314 6.375 0.386719 6.375H2.36328C2.57686 6.375 2.75 6.20186 2.75 5.98828V4.01172C2.75 3.79814 2.57686 3.625 2.36328 3.625H0.386719ZM0.773438 4.39844V5.60156H1.97656V4.39844H0.773438Z" fill="currentColor"/>
+		<path fillRule="evenodd" clipRule="evenodd" d="M0 7.50293C0 7.28935 0.17314 7.11621 0.386719 7.11621H2.36328C2.57686 7.11621 2.75 7.28935 2.75 7.50293V9.47949C2.75 9.69307 2.57686 9.86621 2.36328 9.86621H0.386719C0.17314 9.86621 0 9.69307 0 9.47949V7.50293ZM0.773438 9.09277V7.88965H1.97656V9.09277H0.773438Z" fill="currentColor"/>
+	</svg>
+);
