4f64c66e696d3c8f3bc3f8991e342fc9dbb8b7a0
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-11-27 15:01:57 +0000

    ISSUE #5257 - rename variable

--- frontend/src/v5/ui/controls/circleButton/circleButton.component.tsx
+++ frontend/src/v5/ui/controls/circleButton/circleButton.component.tsx
@@ -22,7 +22,7 @@ interface ICircleButton {
 	disabled?: boolean;
 	onClick?: (e) => void;
 	children: any;
-	$active?: boolean;
+	selected?: boolean;
 }
 
 export const CircleButton = ({ variant = 'primary', ...props }: ICircleButton) => {
