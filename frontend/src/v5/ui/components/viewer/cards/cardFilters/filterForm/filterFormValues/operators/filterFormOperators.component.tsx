61a04e12b4c736fd83e079b81176b2fa4d8788e4
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-12-06 12:00:22 +0000

    ISSUE #5264 - add operator icons

---
+++ frontend/src/v5/ui/components/viewer/cards/cardFilters/filterForm/filterFormValues/operators/filterFormOperators.component.tsx
@@ -0,0 +1,45 @@
+/**
+ *  Copyright (C) 2024 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { FormSelect } from '@controls/inputs/formInputs.component';
+import { FILTER_OPERATOR_ICON, FILTER_OPERATOR_LABEL, getValidOperators } from '../../../cardFilters.helpers';
+import { MenuItem } from '@mui/material';
+import { CardFilterOperator } from '../../../cardFilters.types';
+import { FilterIconContainer } from './filterFormOperators.styles';
+
+const OperatorMenuItem = ({ operator }) => {
+	const Icon = FILTER_OPERATOR_ICON[operator];
+	const label = FILTER_OPERATOR_LABEL[operator];
+
+	return (
+		<MenuItem key={operator} value={operator}>
+			<FilterIconContainer>
+				<Icon />
+			</FilterIconContainer>
+			{label}
+		</MenuItem>
+	);
+};
+
+export const FilterFormOperators = ({ type }) => {
+	const operators = getValidOperators(type);
+	return (
+		<FormSelect name='operator' renderValue={(op: CardFilterOperator) => FILTER_OPERATOR_LABEL[op]}>
+			{operators.map((op) => (<OperatorMenuItem operator={op} />))}
+		</FormSelect>
+	);
+};
