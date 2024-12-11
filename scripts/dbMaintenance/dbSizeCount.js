592696a78779de2b9116f6c2a1f313167ebdde20
Carmen Fan <wind5320@hotmail.com>
2024-12-11 09:43:13 +0000

    remove old scripts

--- scripts/dbMaintenance/dbSizeCount.js
+++
@@ -1,6 +0,0 @@
-
-db.getSiblingDB('admin').adminCommand({listDatabases:1}).databases.forEach(function(database){
-	var myDB = db.getSiblingDB(database.name);
-	var size = myDB.stats(1024*1024*1024).storageSize;
-	if(size > 10) print(database.name + ": " + size );
-});
