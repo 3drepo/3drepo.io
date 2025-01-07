f16fae053c3df21e18a5ea89e5b45e491fab9b05
Bogdan Cornea <bcornea@asite.com>
2025-01-07 11:54:11 +0000

    update the e2e with out of order array check

--- backend/tests/v5/e2e/routes/teamspaces/projects/models/federations/revisions.test.js
+++ backend/tests/v5/e2e/routes/teamspaces/projects/models/federations/revisions.test.js
@@ -19,6 +19,7 @@ const SuperTest = require('supertest');
 const ServiceHelper = require('../../../../../../helper/services');
 const { src } = require('../../../../../../helper/path');
 const CryptoJs = require('crypto-js');
+const {outOfOrderArrayEqual} = require('../../../../../../helper/services')
 
 const { templates } = require(`${src}/utils/responseCodes`);
 const { modelTypes } = require(`${src}/models/modelSettings.constants`);
@@ -261,7 +262,7 @@ const testGetFederationMD5Hash = () => {
 			const res = await agent.get(`${route(parameters)}`).expect(expectedStatus);
 
 			if (success) {
-				expect(JSON.parse(res.text)).toEqual(parameters.response);
+				outOfOrderArrayEqual(JSON.parse(res.text),parameters.response);
 			} else {
 				expect(res.body.code).toEqual(error.code);
 			}
