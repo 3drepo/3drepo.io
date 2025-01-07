68039ce08ba09b6c60f82bec7f561607e6b11b59
The-Daniel <bibshall@gmail.com>
2025-01-07 12:03:02 +0000

    ISSUE #5266 - select filters can show error message

--- frontend/src/v5/ui/components/viewer/cards/cardFilters/filterForm/filterFormValues/filterFormValues.component.tsx
+++ frontend/src/v5/ui/components/viewer/cards/cardFilters/filterForm/filterFormValues/filterFormValues.component.tsx
@@ -108,7 +108,7 @@ export const FilterFormValues = ({ module, property, type }: FilterFolrmValuesTy
 	}
 	if (isSelectType(type)) {
 		return (
-			<FormSelect name={`${name}.0.value`} formError={!!error?.[0]}>
+			<FormSelect name={`${name}.0.value`} formError={!!error?.[0]?.value}>
 				{selectOptions.map((val) => <MenuItem key={val} value={val}>{val}</MenuItem>)}
 			</FormSelect>
 		);
