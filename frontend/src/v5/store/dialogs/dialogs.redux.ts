fb909a84dcac460ac1001933e820324eed523730
sanmont3drepo <santiago.montero@3drepo.org>
2025-01-21 12:11:08 +0000

    ISSUE #5355 - refactored types for modals, switched colours for activity log modal

--- frontend/src/v5/store/dialogs/dialogs.redux.ts
+++ frontend/src/v5/store/dialogs/dialogs.redux.ts
@@ -22,6 +22,13 @@ import { createActions, createReducer } from 'reduxsauce';
 import uuid from 'uuidv4';
 
 import { Constants } from '../../helpers/actions.helper';
+import React from 'react';
+import { AlertModalProps } from '@components/shared/modalsDispatcher/templates/alertModal/alertModal.types';
+import { InfoModalProps } from '@components/shared/modalsDispatcher/templates/infoModal/infoModal.types';
+import { WarningModalProps } from '@components/shared/modalsDispatcher/templates/warningModal/warningModal.types';
+import { DeleteModalProps } from '@components/shared/modalsDispatcher/templates/deleteModal/deleteModal.types';
+import { ShareModalProps } from '@components/shared/modalsDispatcher/templates/shareModal/shareModal.types';
+import { ImagesModalProps } from '@components/shared/modalsDispatcher/templates/imagesModal/imagesModal.types';
 
 export const INITIAL_STATE: IDialogState = {
 	dialogs: [],
@@ -65,16 +72,31 @@ export const dialogsReducer = createReducer(INITIAL_STATE, produceAll({
 type OpenAction = Action<'OPEN'> & { modalType: string | ((any) => JSX.Element), props: any, syncProps: any };
 type CloseAction = Action<'CLOSE'> & { dialogId: string };
 
-export type ModalType = 'alert' | 'warning' | 'delete' | 'info' | 'share' | 'images' | ((any) => JSX.Element);
+type FunctionComponent = ((props) => JSX.Element);
+
+export type ModalType = 'alert' | 'warning' | 'delete' | 'info' | 'share' | 'images' ;
+
+type ModalProps = {
+	['alert']: AlertModalProps,
+	['warning']: WarningModalProps,
+	['delete']: DeleteModalProps,
+	['info']: InfoModalProps,
+	['share']: ShareModalProps,
+	['images']: ImagesModalProps,
+};
+
+
+type ExtractModalProps<T> = T extends ModalType ? ModalProps[T] :  T extends React.FC ? React.ComponentProps<T> : any;
 
 export interface IDialogsActionCreators {
-	open: (type?: ModalType, props?: any | undefined, syncProps?: any | undefined) => OpenAction;
+	open: <T extends ModalType | FunctionComponent>
+	(modalType?: T, props?: ExtractModalProps<T> | undefined, syncProps?: any | undefined) => OpenAction;
 	close: (id: string) => CloseAction;
 }
 
 export interface IDialogConfig {
 	id: string;
-	modalType?: ModalType;
+	modalType?: ModalType | FunctionComponent;
 	props: any;
 	syncProps: any,
 }
