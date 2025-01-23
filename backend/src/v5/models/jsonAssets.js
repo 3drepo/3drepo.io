87c98f80fd576f8f20bde4367ee63a04f8aa84dd
Felix Thiel <fthiel@asite.com>
2025-01-23 16:28:24 +0000

    Issue #4973 Removed coverage requirement of jsonAssets and deactivated unit tests for it to determine its contribution to failing backend tests.

--- backend/src/v5/models/jsonAssets.js
+++ backend/src/v5/models/jsonAssets.js
@@ -15,6 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* istanbul ignore file */
 const DB = require('../handler/db');
 
 const DbConstants = require('../handler/db.constants');
