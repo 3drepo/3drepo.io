29977a94275514e71e67115e49fb211f70636bbf
Alessandro Amantini <ale.amantini1997@gmail.com>
2025-02-25 12:49:10 +0000

    ISSUE #5423 - display fullname instead of username

--- frontend/src/v4/helpers/user.helpers.ts
+++ frontend/src/v4/helpers/user.helpers.ts
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2021 3D Repo Ltd
+ *  Copyright (C) 2025 3D Repo Ltd
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -14,35 +14,14 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { ReactChild } from 'react';
-import { AvatarWrapper, Container, Details, Name, UserData } from './userPopover.styles';
 
-export interface IUser {
-	user: string;
-	firstName: string;
-	lastName: string;
-	company?: string;
-	job?: {
-		_id: string;
-	};
-}
+import { getState } from '@/v5/helpers/redux.helpers';
+import { formatMessage } from '@/v5/services/intl';
+import { selectCurrentTeamspaceUsersByIds } from '@/v5/store/users/users.selectors';
 
-interface IProps {
-	user: IUser;
-	children: ReactChild;
-}
+const USER_NOT_FOUND = formatMessage({ id: 'v4.unknownUser', defaultMessage: 'Unknown user' });
 
-export const UserPopover = ({ user: { firstName, lastName, company, job }, children }: IProps) => {
-	return (
-		<Container>
-			<AvatarWrapper>
-				{children}
-			</AvatarWrapper>
-			<UserData>
-				<Name>{lastName}, {firstName}</Name>
-				<Details>{company}</Details>
-				<Details>{job?._id}</Details>
-			</UserData>
-		</Container>
-	);
+export const getUserFullName = (username) => {
+	const user = selectCurrentTeamspaceUsersByIds(getState())[username];
+	return (!user || user?.isNotTeamspaceMember) ? USER_NOT_FOUND : `${user.firstName} ${user.lastName}`;
 };
