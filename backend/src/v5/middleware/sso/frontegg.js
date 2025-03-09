f482edcd89ff2b68102bfaf0d536c3332961c347
Carmen Fan <wind5320@hotmail.com>
2025-03-09 19:19:59 +0000

    ISSUE #5412 generateAuthLinkToAuthenticator tests done

--- backend/src/v5/middleware/sso/frontegg.js
+++ backend/src/v5/middleware/sso/frontegg.js
@@ -130,7 +130,7 @@ const redirectForAuth = (redirectURL) => async (req, res) => {
 			req.session.reAuth = true;
 		}
 
-		req.authParams = {
+		const authParams = {
 			redirectURL,
 			state: toBase64(JSON.stringify({
 				csrfToken: req.session.csrfToken,
@@ -139,7 +139,7 @@ const redirectForAuth = (redirectURL) => async (req, res) => {
 			codeChallenge: req.session.pkceCodes.challenge,
 		};
 
-		const link = await generateAuthenticationCodeUrl(req.authParams, accountId);
+		const link = await generateAuthenticationCodeUrl(authParams, accountId);
 		respond(req, res, templates.ok, { link });
 	} catch (err) {
 		respond(req, res, err);
--- backend/tests/v5/unit/middleware/sso/frontegg.test.js
+++ backend/tests/v5/unit/middleware/sso/frontegg.test.js
@@ -15,14 +15,31 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+const { cloneDeep } = require('lodash');
 const { src } = require('../../../helper/path');
 const { determineTestGroup, generateRandomString } = require('../../../helper/services');
 
+const { toBase64 } = require(`${src}/utils/helper/strings`);
+
 // This prevents the session service from trigger a mongo service.
 jest.mock('../../../../../src/v5/handler/db');
+
+jest.mock('../../../../../src/v5/middleware/sso/pkce');
+const PKCEMiddleware = require(`${src}/middleware/sso/pkce`);
+
+jest.mock('../../../../../src/v5/middleware/sso');
+const SSOMiddleware = require(`${src}/middleware/sso`);
+
+jest.mock('../../../../../src/v5/models/teamspaceSettings');
+const TeamspaceModel = require(`${src}/models/teamspaceSettings`);
+
 jest.mock('../../../../../src/v5/utils/responder');
 const Responder = require(`${src}/utils/responder`);
-const { templates } = require(`${src}/utils/responseCodes`);
+
+jest.mock('../../../../../src/v5/services/sso/frontegg');
+const FronteggService = require(`${src}/services/sso/frontegg`);
+
+const { templates, createResponseCode } = require(`${src}/utils/responseCodes`);
 
 const Frontegg = require(`${src}/middleware/sso/frontegg`);
 
@@ -53,6 +70,105 @@ const testRedirectToStateURL = () => {
 	});
 };
 
+const testGenerateLinkToAuthenticator = () => {
+	PKCEMiddleware.addPkceProtection.mockImplementation((req, res, next) => next());
+	SSOMiddleware.setSessionInfo.mockImplementation((req, res, next) => next());
+	const authLink = generateRandomString();
+	const ssoPostRedirect = generateRandomString();
+	const redirectUri = generateRandomString();
+	FronteggService.generateAuthenticationCodeUrl.mockResolvedValue(authLink);
+	const reqSample = {
+		query: { redirectUri },
+		params: {},
+		session: {
+			pkceCodes: { challenge: generateRandomString() },
+			csrfToken: generateRandomString(),
+		},
+	};
+	describe('Generate link to authentication', () => {
+		test(`Should return with ${templates.invalidArguments.code} if redirectUri is not provided`, async () => {
+			const req = cloneDeep(reqSample);
+			req.query = {};
+			const res = {};
+			await Frontegg.generateLinkToAuthenticator(ssoPostRedirect)(req, res);
+
+			expect(Responder.respond).toHaveBeenCalledTimes(1);
+			expect(Responder.respond).toHaveBeenCalledWith(req, res,
+				createResponseCode(templates.invalidArguments, 'redirectUri(query string) is required'));
+		});
+
+		test('Should fail with whatever error catch if the catch function is triggered', async () => {
+			const req = cloneDeep(reqSample);
+			const res = {};
+
+			const errMessage = generateRandomString();
+
+			FronteggService.generateAuthenticationCodeUrl.mockRejectedValueOnce(errMessage);
+
+			await Frontegg.generateLinkToAuthenticator(ssoPostRedirect)(req, res);
+
+			expect(Responder.respond).toHaveBeenCalledTimes(1);
+			expect(Responder.respond).toHaveBeenCalledWith(req, res, errMessage);
+		});
+
+		test('Should respond with a link', async () => {
+			const req = cloneDeep(reqSample);
+			const res = {};
+
+			await Frontegg.generateLinkToAuthenticator(ssoPostRedirect)(req, res);
+
+			expect(Responder.respond).toHaveBeenCalledTimes(1);
+			expect(Responder.respond).toHaveBeenCalledWith(req, res, templates.ok, { link: expect.any(String) });
+
+			expect(req.session.reAuth).not.toBeTruthy();
+
+			const authParams = {
+				redirectURL: ssoPostRedirect,
+				state: toBase64(JSON.stringify({
+					csrfToken: req.session.csrfToken,
+					redirectUri: req.query.redirectUri,
+				})),
+				codeChallenge: req.session.pkceCodes.challenge,
+			};
+
+			expect(FronteggService.generateAuthenticationCodeUrl).toHaveBeenCalledTimes(1);
+			expect(FronteggService.generateAuthenticationCodeUrl).toHaveBeenCalledWith(authParams, undefined);
+		});
+
+		test('Should flag reAuth if teamspace is provided', async () => {
+			const teamspace = generateRandomString();
+			const accountId = generateRandomString();
+			const req = { ...cloneDeep(reqSample), params: { teamspace } };
+			const res = {};
+
+			TeamspaceModel.getTeamspaceRefId.mockResolvedValueOnce(accountId);
+
+			await Frontegg.generateLinkToAuthenticator(ssoPostRedirect)(req, res);
+
+			expect(Responder.respond).toHaveBeenCalledTimes(1);
+			expect(Responder.respond).toHaveBeenCalledWith(req, res, templates.ok, { link: expect.any(String) });
+
+			expect(req.session.reAuth).toBeTruthy();
+
+			const authParams = {
+				redirectURL: ssoPostRedirect,
+				state: toBase64(JSON.stringify({
+					csrfToken: req.session.csrfToken,
+					redirectUri: req.query.redirectUri,
+				})),
+				codeChallenge: req.session.pkceCodes.challenge,
+			};
+
+			expect(TeamspaceModel.getTeamspaceRefId).toHaveBeenCalledTimes(1);
+			expect(TeamspaceModel.getTeamspaceRefId).toHaveBeenCalledWith(teamspace);
+
+			expect(FronteggService.generateAuthenticationCodeUrl).toHaveBeenCalledTimes(1);
+			expect(FronteggService.generateAuthenticationCodeUrl).toHaveBeenCalledWith(authParams, accountId);
+		});
+	});
+};
+
 describe(determineTestGroup(__filename), () => {
 	testRedirectToStateURL();
+	testGenerateLinkToAuthenticator();
 });
