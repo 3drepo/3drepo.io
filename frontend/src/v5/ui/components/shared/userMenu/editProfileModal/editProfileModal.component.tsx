23ba6f956bd682ced8532d7d2b2c0bd97e6c96a7
Alessandro Amantini <ale.amantini1997@gmail.com>
2025-03-12 14:34:17 +0000

    ISSUE #5438 - use new password reset flow

--- frontend/src/v5/ui/components/shared/userMenu/editProfileModal/editProfileModal.component.tsx
+++ frontend/src/v5/ui/components/shared/userMenu/editProfileModal/editProfileModal.component.tsx
@@ -21,37 +21,35 @@ import { TabContext } from '@mui/lab';
 import { FormProvider, useForm } from 'react-hook-form';
 import { useErrorInterceptor } from '@controls/errorMessage/useErrorInterceptor';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { EditProfileUpdatePasswordSchema, EditProfileUpdatePersonalSchema, EditProfileUpdateSSOPasswordSchema } from '@/v5/validation/userSchemes/editProfileSchemes';
+import { EditProfileUpdatePersonalSchema } from '@/v5/validation/userSchemes/editProfileSchemes';
 import { CurrentUserHooksSelectors } from '@/v5/services/selectorsHooks';
 import { FormModal, TabList, Tab, TabPanel, TruncatableName } from './editProfileModal.styles';
 import { EditProfilePersonalTab, IUpdatePersonalInputs } from './editProfilePersonalTab/editProfilePersonalTab.component';
-import { EditProfileAuthenticationTab, EMPTY_PASSWORDS, IUpdatePasswordInputs } from './editProfileAuthenticationTab/editProfileAuthenticationTab.component';
+import { EditProfilePasswordTab } from './editProfilePasswordTab/editProfilePasswordTab.component';
 import { EditProfileIntegrationsTab } from './editProfileIntegrationsTab/editProfileIntegrationsTab.component';
 
 const PERSONAL_TAB = 'personal';
-const AUTHENTICATION_TAB = 'authentication';
+const PASSWORD_TAB = 'password';
 const INTEGRATIONS_TAB = 'integrations';
 
 const TAB_LABELS = {
 	personal: formatMessage({ defaultMessage: 'Personal', id: 'editProfile.tab.title.personal' }),
-	authentication: formatMessage({ defaultMessage: 'Authentication', id: 'editProfile.tab.title.authentication' }),
+	password: formatMessage({ defaultMessage: 'Password', id: 'editProfile.tab.title.password' }),
 	integrations: formatMessage({ defaultMessage: 'Integrations', id: 'editProfile.tab.title.integrations' }),
 };
 
 type EditProfileUnexpectedErrors = {
 	[PERSONAL_TAB]?: any;
-	[AUTHENTICATION_TAB]?: any;
 	[INTEGRATIONS_TAB]?: any;
 };
 
 type EditProfileModalProps = {
 	open: boolean;
 	onClickClose: () => void;
-	initialTab?: 'authentication' | 'integrations';
+	initialTab?: 'password' | 'integrations';
 };
 export const EditProfileModal = ({ open, onClickClose, initialTab }: EditProfileModalProps) => {
 	const [activeTab, setActiveTab] = useState(initialTab || PERSONAL_TAB);
-	const [incorrectPassword, setIncorrectPassword] = useState(false);
 	const [alreadyExistingEmails, setAlreadyExistingEmails] = useState([]);
 	const [unexpectedErrors, setUnexpectedErrors] = useState<EditProfileUnexpectedErrors>({});
 	const interceptedError = useErrorInterceptor();
@@ -71,17 +69,7 @@ export const EditProfileModal = ({ open, onClickClose, initialTab }: EditProfile
 		defaultValues: defaultPersonalValues,
 	});
 
-	const authenticationFormData = useForm<IUpdatePasswordInputs>({
-		mode: 'all',
-		resolver: yupResolver(
-			user.sso
-				? EditProfileUpdateSSOPasswordSchema
-				: EditProfileUpdatePasswordSchema(incorrectPassword),
-		),
-		defaultValues: EMPTY_PASSWORDS,
-	});
-
-	const isSubmitting = personalFormData.formState.isSubmitting || authenticationFormData.formState.isSubmitting;
+	const isSubmitting = personalFormData.formState.isSubmitting;
 
 	useEffect(() => {
 		setUnexpectedErrors({ ...unexpectedErrors, [activeTab]: interceptedError });
@@ -101,7 +89,7 @@ export const EditProfileModal = ({ open, onClickClose, initialTab }: EditProfile
 			<TabContext value={activeTab}>
 				<TabList onChange={onTabChange} textColor="primary" indicatorColor="primary">
 					<Tab value={PERSONAL_TAB} label={TAB_LABELS.personal} disabled={isSubmitting} />
-					<Tab value={AUTHENTICATION_TAB} label={TAB_LABELS.authentication} disabled={isSubmitting} />
+					<Tab value={PASSWORD_TAB} label={TAB_LABELS.password} disabled={isSubmitting} />
 					<Tab value={INTEGRATIONS_TAB} label={TAB_LABELS.integrations} disabled={isSubmitting} />
 				</TabList>
 				<FormProvider {...personalFormData}>
@@ -114,16 +102,9 @@ export const EditProfileModal = ({ open, onClickClose, initialTab }: EditProfile
 						/>
 					</TabPanel>
 				</FormProvider>
-				<FormProvider {...authenticationFormData}>
-					<TabPanel value={AUTHENTICATION_TAB}>
-						<EditProfileAuthenticationTab
-							incorrectPassword={incorrectPassword}
-							setIncorrectPassword={setIncorrectPassword}
-							unexpectedError={unexpectedErrors[AUTHENTICATION_TAB]}
-							onClickClose={onClickClose}
-						/>
-					</TabPanel>
-				</FormProvider>
+				<TabPanel value={PASSWORD_TAB}>
+					<EditProfilePasswordTab onClickClose={onClickClose} />
+				</TabPanel>
 				<TabPanel value={INTEGRATIONS_TAB}>
 					<EditProfileIntegrationsTab
 						unexpectedError={unexpectedErrors[INTEGRATIONS_TAB]}
