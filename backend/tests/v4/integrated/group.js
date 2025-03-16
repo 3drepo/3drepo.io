de7de52b7cb5d61240d5fb4ac340d7f20ac0aebd
Carmen Fan <wind5320@hotmail.com>
2025-03-16 18:59:36 +0000

    ISSUE #5412 fix group tests

--- backend/tests/v4/integrated/group.js
+++ backend/tests/v4/integrated/group.js
@@ -134,7 +134,7 @@ describe("Groups", function () {
 		it("with some other teamspace should fail", function(done){
 			agent.get(`/${noAccessUser}/${model}/revision/f640aa3dec2/groups/${groupID}`)
 				.expect(404 , function(err, res) {
-					expect(res.body.value).to.equal(responseCodes.MODEL_NOT_FOUND.code);
+					expect(res.body.value).to.equal(responseCodesV5.teamspaceNotFound.code);
 					done(err);
 				});
 		});
