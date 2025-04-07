4046765b95d70c498658def19d39cf0cdd7b5cf9
sanmont3drepo <santiago.montero@3drepo.org>
2025-04-07 14:50:30 +0100

    ISSUE #5490 - Fixed mail urls filters

--- frontend/src/v5/ui/routes/viewer/ticketFiltersSetter/ticketFiltersSetter.component.tsx
+++ frontend/src/v5/ui/routes/viewer/ticketFiltersSetter/ticketFiltersSetter.component.tsx
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { TicketsCardActionsDispatchers } from '@/v5/services/actionsDispatchers';
+import { TicketsCardActionsDispatchers, ViewerGuiActionsDispatchers } from '@/v5/services/actionsDispatchers';
 import { TicketsCardHooksSelectors } from '@/v5/services/selectorsHooks';
 import { uniq } from 'lodash';
 import { useParams } from 'react-router-dom';
@@ -28,9 +28,10 @@ import { TicketStatusDefaultValues, TicketStatusTypes, TreatmentStatuses } from 
 import { selectStatusConfigByTemplateId } from '@/v5/store/tickets/tickets.selectors';
 import { getState } from '@/v5/helpers/redux.helpers';
 import { modelIsFederation } from '@/v5/store/tickets/tickets.helpers';
+import { VIEWER_PANELS } from '@/v4/constants/viewerGui';
 
 const TICKET_CODE_REGEX = /^[a-zA-Z]{3}:\d+$/;
-export const DefaultTicketFiltersSetter = () => {
+export const TicketFiltersSetter = () => {
 	const [ticketSearchParam, setTicketSearchParam] = useSearchParam('ticketSearch', Transformers.STRING_ARRAY);
 	const templates = TicketsCardHooksSelectors.selectCurrentTemplates();
 	const { teamspace, project, containerOrFederation } = useParams<ViewerParams>();
@@ -43,12 +44,12 @@ export const DefaultTicketFiltersSetter = () => {
 		TicketsCardActionsDispatchers.fetchFilteredTickets(teamspace, project, containerOrFederation, isFed);
 	}, [tickets, cardFilters]);
 
-	const getTicketFiltersFromURL = (values): CardFilter[] => [{
+	const getTicketFiltersFromCodes = (values): CardFilter[] => [{
 		module: '',
 		property: 'Ticket ID',
 		type: 'ticketCode',
 		filter: {
-			operator: 'eq',
+			operator: 'is',
 			values,
 		},
 	}];
@@ -103,8 +104,13 @@ export const DefaultTicketFiltersSetter = () => {
 	useEffect(() => {
 		if (templates.length) {
 			const ticketCodes = ticketSearchParam.filter((query) => TICKET_CODE_REGEX.test(query)).map((q) => q.toUpperCase());
-			const filters: CardFilter[] = ticketCodes.length ? getTicketFiltersFromURL(ticketCodes) : getNonCompletedTicketFilters();
+			const filters: CardFilter[] = ticketCodes.length ? getTicketFiltersFromCodes(ticketCodes) : getNonCompletedTicketFilters();
 			filters.forEach(TicketsCardActionsDispatchers.upsertFilter);
+
+			if (ticketCodes.length) {
+				ViewerGuiActionsDispatchers.setPanelVisibility(VIEWER_PANELS.TICKETS, true);
+			}
+
 			setTicketSearchParam();
 		}
 	}, [templates.length]); 
