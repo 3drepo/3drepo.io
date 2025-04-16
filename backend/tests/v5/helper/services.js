bdc5f6c1d9663fb92b7817199ec0a4bf26c6485d
Carmen Fan <wind5320@hotmail.com>
2025-04-16 17:25:11 +0100

    ISSUE #5493 more debugging

--- backend/tests/v5/helper/services.js
+++ backend/tests/v5/helper/services.js
@@ -59,8 +59,10 @@ const queue = {};
 const ServiceHelper = { db, queue, socket: {} };
 
 queue.purgeQueues = async () => {
+	console.log('purging queues...');
 	const { host, worker_queue, model_queue, callback_queue } = config.cn_queue;
 	const conn = await amqp.connect(host);
+	console.log('connected');
 
 	const purgeQueue = async (queueName) => {
 		try {
@@ -76,10 +78,11 @@ queue.purgeQueues = async () => {
 	};
 
 	await Promise.all([
-		purgeQueue(worker_queue),
-		purgeQueue(model_queue),
-		purgeQueue(callback_queue),
+		purgeQueue(worker_queue).then(() => console.log('worker queue done')),
+		purgeQueue(model_queue).then(() => console.log('model queue done')),
+		purgeQueue(callback_queue).then(() => console.log('callback queue done')),
 	]);
+	console.log('queues purged');
 
 	await conn.close();
 };
