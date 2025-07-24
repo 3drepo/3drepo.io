45cbfe6df621515295ba8e345e7a3fc3e5196421
Sebastian Friston <sebastian.friston@gmail.com>
2025-07-24 09:46:53 +0100

    ISSUE #781 removed conditional around groups

--- backend/src/v4/models/group.js
+++ backend/src/v4/models/group.js
@@ -370,17 +370,15 @@ Group.getList = async function (account, model, branch, revId, ids, queryParams,
 
 	]);
 
-	if(modelRev) {
-		await getSubModels(account, model, branch, revId, async (ts, subModel) => {
-			const revNode = await findLatest(ts, subModel, {_id: 1});
+	await getSubModels(account, model, branch, revId, async (ts, subModel) => {
+		const revNode = await findLatest(ts, subModel, {_id: 1});
 
-			if(revNode) {
-				submodels.add({model: subModel, revId: revNode._id});
-				await prepareCache(ts, subModel, revNode._id);
-			}
+		if(revNode) {
+			submodels.add({model: subModel, revId: revNode._id});
+			await prepareCache(ts, subModel, revNode._id);
+		}
 
-		});
-	}
+	});
 
 	let models = [];
 	if(submodels.size) {
