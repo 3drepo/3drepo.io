61a85f1f431a54af4da42b345f5ccc4e830c580a
sanmont3drepo <smontero@asite.com>
2025-09-05 15:59:47 +0100

    ISSUE #5545 - Fixed virtualist not going back to the original size when toggling the selected items  in the list

--- frontend/src/v5/ui/controls/virtualList/virtualList.component.tsx
+++ frontend/src/v5/ui/controls/virtualList/virtualList.component.tsx
@@ -14,8 +14,6 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-import { height } from '@mui/system';
 import { useRef, useState, useEffect } from 'react';
 
 interface Props {
@@ -24,7 +22,7 @@ interface Props {
 	itemContent:  (value: any, index: number, array: any[]) => JSX.Element;
 }
 
-const emptyRect = { x:0, y:0, width:0, top:0, bottom: 0 , height:0 } as DOMRect
+const emptyRect = { x:0, y:0, width:0, top:0, bottom: 0, height:0 } as DOMRect;
 const HeightTolerance = 2; // this is how much the height of the html item can differ from itemHeight to be considered equals
 
 const equalsHeight = (a, b) =>  Math.abs(a - b) < HeightTolerance;
@@ -36,7 +34,7 @@ export const VirtualList = ({ items, itemHeight, itemContent }:Props) => {
 	const itemsContainer = useRef<Element>();
 	const prevRect = useRef<DOMRect>();
 	const prevInnerHeight = useRef(0);
-	const expandedItem = useRef({ index:0, height: itemHeight});
+	const expandedItem = useRef({ index:0, height: itemHeight });
 	const [containerRect, setContainerRect] = useState<DOMRect>(emptyRect);
 	const firstElementIndex = useRef(0);
 
@@ -45,14 +43,14 @@ export const VirtualList = ({ items, itemHeight, itemContent }:Props) => {
 	const bottom = Math.min(Math.max(0, containerRect.bottom), innerHeight);
 	
 	const getFirstItemIndex = (y) => {
-		let firstItemindex =  Math.ceil(y/itemHeight);
+		let firstItemindex =  Math.ceil(y / itemHeight);
 		let spacerStart = firstItemindex * itemHeight;
 		
-		const y2 = -containerRect.y
-		const topExp = (expandedItem.current.index*itemHeight) - y2 ;
+		const y2 = -containerRect.y;
+		const topExp = (expandedItem.current.index * itemHeight) - y2 ;
 		const bottomExp = topExp + expandedItem.current.height ;
 
-		if (lineInRange(0,topExp, bottomExp) || 
+		if (lineInRange(0, topExp, bottomExp) || 
 			lineInRange(bottom, topExp, bottomExp) || 
 			lineInRange(topExp, 0, bottom) || 
 			lineInRange(bottomExp, 0, bottom) ) {
@@ -69,8 +67,8 @@ export const VirtualList = ({ items, itemHeight, itemContent }:Props) => {
 			// If its not visible and the first item is passed the expanded item
 			// then the first item might be further down the list because expanded item
 			// might take up space for many items
-			if(firstItemindex > expandedItem.current.index) {
-				firstItemindex = Math.ceil(-bottomExp/itemHeight) + expandedItem.current.index;
+			if (firstItemindex > expandedItem.current.index) {
+				firstItemindex = Math.ceil(-bottomExp / itemHeight) + expandedItem.current.index;
 				spacerStart = (firstItemindex - 1) * itemHeight + expandedItem.current.height;
 			}
 		}
@@ -78,7 +76,7 @@ export const VirtualList = ({ items, itemHeight, itemContent }:Props) => {
 		return { firstItemindex, spacerStart };
 	};
 
-	const contentCount = Math.ceil(((bottom-top)) / itemHeight) + ((bottom-top) > 0 ? 1: 0); 
+	const contentCount = Math.ceil(((bottom - top)) / itemHeight) + ((bottom - top) > 0 ? 1 : 0); 
 	const { firstItemindex, spacerStart } =  getFirstItemIndex(Math.max(0, -containerRect.y));
 	firstElementIndex.current = firstItemindex;
 	const lastIndex =  Math.min(contentCount + firstItemindex, items.length);
@@ -89,7 +87,7 @@ export const VirtualList = ({ items, itemHeight, itemContent }:Props) => {
 
 	useEffect(()=> {
 		prevInnerHeight.current = 0;
-	}, [items])
+	}, [items]);
 
 	let onScroll;
 	onScroll = () => {
@@ -98,18 +96,19 @@ export const VirtualList = ({ items, itemHeight, itemContent }:Props) => {
 		let shouldUpdate = rect.y !== prevRect.current?.y || prevInnerHeight.current !== window.innerHeight;
 
 		const children = itemsContainer.current.children;
+		const { height: expHeight, index: expIndex } =  expandedItem.current;
 
-		for (let i=0; i < children.length ; i++ ){
-			const elementHeight = children[i].clientHeight;	
-			if (!equalsHeight(elementHeight, itemHeight)) {
-				const {height: expHeight, index: expIndex} =  expandedItem.current;
-				const itemIndex = i + firstElementIndex.current;
-
+		for (let i = 0; i < children.length ; i++ ) {
+			const elementHeight = children[i].clientHeight;
+			const itemIndex = i + firstElementIndex.current;
+			if (!equalsHeight(elementHeight, itemHeight) || (itemIndex === expIndex &&  !equalsHeight(elementHeight, expHeight))) {
 				if (expIndex !== itemIndex || expHeight !== elementHeight) {
 					expandedItem.current = {
 						index: itemIndex,
-						height: elementHeight
+						height: elementHeight,
 					};
+
+					shouldUpdate = true;
 				}
 
 				break;
@@ -130,10 +129,10 @@ export const VirtualList = ({ items, itemHeight, itemContent }:Props) => {
 	}, []);
 
 	return (
-		<div style={{ height: containerHeight,  display:'block'}} ref={containerRef as any} >
+		<div style={{ height: containerHeight,  display:'block' }} ref={containerRef as any} >
 			<div style={{ height: spacerStart } } id='startSpacer'/>
 			<div  ref={itemsContainer as any}   >
-			{itemsSlice.map(itemContent)}
+				{itemsSlice.map(itemContent)}
 			</div>
 		</div>
 	);
