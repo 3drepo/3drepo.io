8fcd8d71e65bf39f2b49647dde9a61228e7655f1
sanmont3drepo <smontero@asite.com>
2025-10-13 11:41:39 +0100

    ISSUE #5678 - Added test for sequencing module

--- frontend/test/tickets/ticketFilters.spec.ts
+++ frontend/test/tickets/ticketFilters.spec.ts
@@ -24,232 +24,265 @@ import { pick, times } from 'lodash';
 import { mockRiskCategories } from "./tickets.fixture";
 
 describe('Tickets: filters', () => {
-    const template: ITemplate = {
-        _id: 'test-template',
-        name: 'Test template',
-        code: 'TTT',
-        properties: [
-            {
-                name:'Colours',
-                type:'manyOf',
-                values: [
-                    'black',
-                    'silver',
-                    'gray',
-                    'white',
-                    'maroon',
-                    'red',
-                    'purple',
-                    'fuchsia',
-                    'green',
-                    'lime',
-                    'olive',
-                    'yellow',
-                    'navy',
-                    'blue',
-                    'teal',
-                    'aqua'
-                ]
-            },
-            {
-                name:'Assignees',
-                type:'manyOf',
-                values: 'jobsAndUsers'
-            },
-            {
-                name: 'Risky risks',
-                type: 'oneOf',
-                values: 'riskCategories'
-            },
-            {
-                name: 'Birthday',
-                type: 'pastDate',
-            },
-            {
-                name: 'Expiration',
-                type: 'date',
-            }
-        ],
-        modules: [
-            {
-                name:'Users module',
-                properties: [
-                    {
-                        name:'Cool users',
-                        type:'oneOf',
-                        values: 'jobsAndUsers',
-                    }
-                ]
-            },
-        ]
-    }
+	const template: ITemplate = {
+		_id: 'test-template',
+		name: 'Test template',
+		code: 'TTT',
+		properties: [
+			{
+				name:'Colours',
+				type:'manyOf',
+				values: [
+					'black',
+					'silver',
+					'gray',
+					'white',
+					'maroon',
+					'red',
+					'purple',
+					'fuchsia',
+					'green',
+					'lime',
+					'olive',
+					'yellow',
+					'navy',
+					'blue',
+					'teal',
+					'aqua'
+				]
+			},
+			{
+				name:'Assignees',
+				type:'manyOf',
+				values: 'jobsAndUsers'
+			},
+			{
+				name: 'Risky risks',
+				type: 'oneOf',
+				values: 'riskCategories'
+			},
+			{
+				name: 'Birthday',
+				type: 'pastDate',
+			},
+			{
+				name: 'Expiration',
+				type: 'date',
+			}
+		],
+		modules: [
+			{
+				name:'Users module',
+				properties: [
+					{
+						name:'Cool users',
+						type:'oneOf',
+						values: 'jobsAndUsers',
+					}
+				]
+			},
+		]
+	}
 
-    const users = times(5, () => userWithoutAvatarMockFactory());
-    const risks = mockRiskCategories();
+	const users = times(5, () => userWithoutAvatarMockFactory());
+	const risks = mockRiskCategories();
 
-    describe('helpers', () => {
-        it('split by non escaped characters should work', () => {
-            let str = 'b\\,lack,sil\\ver,gray';
-            let arr = splitByNonEscaped(str, ',');
-            expect(arr).toEqual(['b\\,lack','sil\\ver','gray']);
+	describe('helpers', () => {
+		it('split by non escaped characters should work', () => {
+			let str = 'b\\,lack,sil\\ver,gray';
+			let arr = splitByNonEscaped(str, ',');
+			expect(arr).toEqual(['b\\,lack','sil\\ver','gray']);
 
-            str = 'aqua';
-            arr = splitByNonEscaped(str, ',');
-            expect(arr).toEqual(['aqua']);
+			str = 'aqua';
+			arr = splitByNonEscaped(str, ',');
+			expect(arr).toEqual(['aqua']);
 
-            str = 'violet,';
-            arr = splitByNonEscaped(str, ',');
-            expect(arr).toEqual(['violet','']);
+			str = 'violet,';
+			arr = splitByNonEscaped(str, ',');
+			expect(arr).toEqual(['violet','']);
 
-            str = ',green';
-            arr = splitByNonEscaped(str, ',');
-            expect(arr).toEqual(['','green']);
+			str = ',green';
+			arr = splitByNonEscaped(str, ',');
+			expect(arr).toEqual(['','green']);
 
-            str = '\\,li\\,me\\,';
-            arr = splitByNonEscaped(str, ',');
-            expect(arr).toEqual([str]);
+			str = '\\,li\\,me\\,';
+			arr = splitByNonEscaped(str, ',');
+			expect(arr).toEqual([str]);
 
-        });
-    });
+		});
+	});
 
-    describe('serialization', () => {
-        // Containers
-        it('should work for property of type manyOf', () => {
-            const filter:TicketFilter = {
-                type:'manyOf',
-                property: 'Colours',
-                filter: {
-                    operator: 'is',
-                    values: ['black', 'green', 'red'],
-                }
-            }
+	describe('serialization', () => {
+		// Containers
+		it('should work for property of type manyOf', () => {
+			const filter:TicketFilter = {
+				type:'manyOf',
+				property: 'Colours',
+				filter: {
+					operator: 'is',
+					values: ['black', 'green', 'red'],
+				}
+			}
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work for general jobsAndUsers', () => {
-            const filter:TicketFilter = {
-                type:'manyOf',
-                property: 'Assignees',
-                filter: {
-                    operator: 'eq',
-                    values: [users[2].user, 'Techinician', users[1].user],
-                    displayValues: [getFullnameFromUser(users[2]), 'Techinician', getFullnameFromUser(users[1])].join(',')
-                }
-            }
+		it('should work for general jobsAndUsers', () => {
+			const filter: TicketFilter = {
+				type:'manyOf',
+				property: 'Assignees',
+				filter: {
+					operator: 'eq',
+					values: [users[2].user, 'Techinician', users[1].user],
+					displayValues: [getFullnameFromUser(users[2]), 'Techinician', getFullnameFromUser(users[1])].join(',')
+				}
+			}
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work for jobsAndUsers in a module', () => {
-            const filter:TicketFilter = {
-                type:'oneOf',
-                property: 'Cool users',
-                module: 'Users module',
-                filter: {
-                    operator: 'eq',
-                    values: ['Electrician', users[4].user, users[2].user],
-                    displayValues: ['Electrician', getFullnameFromUser(users[4]), getFullnameFromUser(users[2])].join(',')
-                }
-            }
+		it('should work for jobsAndUsers in a module', () => {
+			const filter: TicketFilter = {
+				type:'oneOf',
+				property: 'Cool users',
+				module: 'Users module',
+				filter: {
+					operator: 'eq',
+					values: ['Electrician', users[4].user, users[2].user],
+					displayValues: ['Electrician', getFullnameFromUser(users[4]), getFullnameFromUser(users[2])].join(',')
+				}
+			}
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work for type owner', () => {
-            const filter:TicketFilter = {
-                type:'owner',
-                property: 'Owner',
-                filter: {
-                    operator: 'eq',
-                    values: [users[0].user, 'Architect', users[3].user],
-                    displayValues: [getFullnameFromUser(users[0]), 'Architect', getFullnameFromUser(users[3])].join(',')
-                }
-            }
+		it('should work for type owner', () => {
+			const filter: TicketFilter = {
+				type:'owner',
+				property: 'Owner',
+				filter: {
+					operator: 'eq',
+					values: [users[0].user, 'Architect', users[3].user],
+					displayValues: [getFullnameFromUser(users[0]), 'Architect', getFullnameFromUser(users[3])].join(',')
+				}
+			}
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work for type risks', () => {
-            const filter:TicketFilter = {
-                type: 'oneOf',
-                property: 'Risky risks',
-                filter: {
-                    operator: 'eq',
-                    values: [risks[1], risks[3], risks[2]],
-                }
-            }
+		it('should work for type risks', () => {
+			const filter: TicketFilter = {
+				type: 'oneOf',
+				property: 'Risky risks',
+				filter: {
+					operator: 'eq',
+					values: [risks[1], risks[3], risks[2]],
+				}
+			}
 
-            const serialized = serializeFilter(template, risks, filter);            
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);            
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work with general date', () => {
-            const filter:TicketFilter = {
-                property: 'Expiration',
-                type: 'date',
-                filter: {
-                    operator: 'gte',
-                    values: [2004782460000],
-                    displayValues: '12/07/2033'
-                }
-            };
+		it('should work with general date', () => {
+			const filter: TicketFilter = {
+				property: 'Expiration',
+				type: 'date',
+				filter: {
+					operator: 'gte',
+					values: [2004782460000],
+					displayValues: '12/07/2033'
+				}
+			};
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work with general past date', () => {
-            const filter:TicketFilter = {
-                property: 'Birthday',
-                type: 'pastDate',
-                filter: {
-                    operator: 'lte',
-                    values: [1735689600000],
-                    displayValues: '01/01/2025'
-                }
-            };
+		it('should work with general past date', () => {
+			const filter: TicketFilter = {
+				property: 'Birthday',
+				type: 'pastDate',
+				filter: {
+					operator: 'lte',
+					values: [1735689600000],
+					displayValues: '01/01/2025'
+				}
+			};
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work with create at type', () => {
-            const filter:TicketFilter = {
-                property: 'Created at',
-                type: 'createdAt',
-                filter: {
-                    operator: 'lte',
-                    values: [1759273259999],
-                    displayValues: '01/10/2025'
-                }
-            };
+		it('should work with create at type', () => {
+			const filter: TicketFilter = {
+				property: 'Created at',
+				type: 'createdAt',
+				filter: {
+					operator: 'lte',
+					values: [1759273259999],
+					displayValues: '01/10/2025'
+				}
+			};
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-        it('should work with range in type', () => {
-            const filter:TicketFilter ={
-                property:'Expiration',
-                type:'date',
-                filter:{
-                    'operator':'rng',
-                    values:[
-                        [1601553593000,1761782459999],
-                        [1767225600000,1774911719999]],
-                    displayValues:'01/10/2020 to 30/10/2025, 01/01/2026 to 31/03/2026'
-                }
-            };
+		it('should work with range in type', () => {
+			const filter: TicketFilter ={
+				property:'Expiration',
+				type:'date',
+				filter:{
+					'operator':'rng',
+					values:[
+						[1601553593000,1761782459999],
+						[1767225600000,1774911719999]],
+					displayValues:'01/10/2020 to 30/10/2025, 01/01/2026 to 31/03/2026'
+				}
+			};
 
-            const serialized = serializeFilter(template, risks, filter);
-            expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
-        });
+			const serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+		});
 
-    })
+		it('should work with sequencing module', () => {
+			let filter: TicketFilter = {
+				module: 'sequencing',
+				property: 'Start Time',
+				type: 'date',
+				filter: {
+					operator:'rng',
+					values:[[1759273200000,1760742059999]],
+					displayValues:'01/10/2025 to 18/10/2025'
+				}
+			};
+			
+			let serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
 
+			filter = {
+				module: 'sequencing',
+				property: 'End Time',
+				type: 'date',
+				filter: {
+					operator:'gte',
+					values: [1759532400000],
+					displayValues: '04/10/2025'
+				}
+			};
+			
+			serialized = serializeFilter(template, risks, filter);
+			expect(filter).toEqual(deserializeFilter(template, users, risks, serialized));
+
+		});
+
+
+
+
+	})
 });
