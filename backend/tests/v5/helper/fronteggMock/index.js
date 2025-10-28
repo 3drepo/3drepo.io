e7c94a0dd26f8b0aa6aa68515a5cdd38dabf858f
carmenfan <wind5320@hotmail.com>
2025-10-28 14:38:38 +0000

    ISSUE #5705 v4 tests: setup frontegg mock with users/teamspace base on db records

--- backend/tests/v5/helper/fronteggMock/index.js
+++ backend/tests/v5/helper/fronteggMock/index.js
@@ -15,6 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+const { listDatabases, updateOne, find } = require('../../../../src/v5/handler/db');
 const Accounts = require('./components/accounts');
 const Auth = require('./components/auth');
 const Users = require('./components/users');
@@ -22,5 +23,45 @@ const Users = require('./components/users');
 const Frontegg = { ...Auth, ...Users, ...Accounts };
 
 Frontegg.init = () => Promise.resolve();
+
+// NOTE: this file cannot import any source code from v4/v5 otherwise the original frontegg service will be used.
+const v4ImportUsers = async (teamspaceToAccountId) => {
+	const users = await find('admin', 'system.users', { 'customData.email': { $exists: true } },
+		{ user: 1, 'customData.email': 1, 'customData.firstName': 1, 'customData.lastName': 1, roles: 1 });
+	await Promise.all(users.map(async ({ user, customData: { email, firstName, lastName }, roles }) => {
+		const fullName = `${firstName || ''} ${lastName || ''}`.trim();
+		let firstTime = true;
+		await Promise.all(roles.map(async ({ db, role }) => {
+			const accountId = teamspaceToAccountId[db];
+			if (role !== 'team_member' || accountId === undefined) {
+				return;
+			}
+
+			const userId = await Accounts.addUserToAccount(accountId, email, fullName);
+			if (firstTime) {
+				firstTime = false;
+				updateOne('admin', 'system.users', { user }, { $set: { 'customData.userId': userId } });
+			}
+		}));
+	}));
+};
+
+const v4ImportTeamspaces = async () => {
+	const dbs = await listDatabases();
+	const teamspaceToAccountId = {};
+	await Promise.all(dbs.map(async ({ name: dbName }) => {
+		const refId = await Accounts.createAccount(dbName);
+		teamspaceToAccountId[dbName] = refId;
+		await updateOne(dbName, 'teamspace', { _id: dbName }, { $set: { refId } });
+	}));
+	return teamspaceToAccountId;
+};
+
+Frontegg.v4Setup = async () => {
+	// eslint-disable-next-line no-console
+	console.log('setting up data based on existing data from database...');
+	const teamspaceToAccountId = await v4ImportTeamspaces();
+	await v4ImportUsers(teamspaceToAccountId);
+};
 
 module.exports = Frontegg;
