7c72698971a8cb7f2d184f03f093565ea68e8183
ChristosTsiotsias <71635617+ChristosTsiotsias@users.noreply.github.com>
2026-01-06 09:54:41 +0000

    ISSUE #5752 small typo

--- backend/tests/v5/unit/processors/teamspaces/projects/models/commons/metadata.test.js
+++ backend/tests/v5/unit/processors/teamspaces/projects/models/commons/metadata.test.js
@@ -19,7 +19,7 @@ const { times } = require('lodash');
 const { src } = require('../../../../../../helper/path');
 const { generateRandomString, generateUUID } = require('../../../../../../helper/services');
 
-const { UUIDToString } = require(`${src}utils/helper/uuids`);
+const { UUIDToString } = require(`${src}/utils/helper/uuids`);
 const Metadata = require(`${src}/processors/teamspaces/projects/models/commons/metadata`);
 jest.mock('../../../../../../../../src/v5/models/metadata');
 const MetadataModel = require(`${src}/models/metadata`);
