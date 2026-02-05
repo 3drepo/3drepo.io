be2a80be846e5cbb9cbd7a670c0e1c9160da061b
Bogdan Cornea <bcornea@asite.com>
2026-02-05 14:00:54 +0000

    Issue #5288 - address feedback

--- backend/src/v5/services/eventsListener/components/modelEvents.js
+++ backend/src/v5/services/eventsListener/components/modelEvents.js
@@ -131,13 +131,13 @@ const modelProcessingCompleted = async ({ teamspace, project, model, revId, user
 		try {
 			const { zipPath, logPreview } = (await getLogArchive(UUIDToString(revId))) || {};
 			const { errorCode, message } = errorReason;
-			const code = codeExists(errorCode) ? templates[errorCode].message : errorCode;
+			const { message: messageText } = getInfoFromCode(errorCode, message);
 
 			await sendSystemEmail(emailTemplates.MODEL_IMPORT_ERROR.name,
 				{
 					errInfo: {
-						code,
-						message,
+						code: errorCode,
+						message: messageText,
 					},
 					teamspace,
 					model,
