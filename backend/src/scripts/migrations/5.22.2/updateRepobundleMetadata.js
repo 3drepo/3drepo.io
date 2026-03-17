ca5e3218decf39152a222fed1c0bcecaffc4b4f5
Sebastian Friston <sfriston@asite.com>
2026-03-17 10:35:44 +0000

    ISSUE #5872 update script with bug fixes, error handling and check for 5.22.1 (date range tbc)

---
+++ backend/src/scripts/migrations/5.22.2/updateRepobundleMetadata.js
@@ -0,0 +1,197 @@
+/**
+ *  Copyright (C) 2026 3D Repo Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const { v5Path } = require('../../../interop');
+const FilesManager = require('../../../v5/services/filesManager');
+const { UUIDToString } = require('../../../v5/utils/helper/uuids');
+const { getTeamspaceList, getCollectionsEndsWith } = require('../../utils');
+
+const { find, replaceOne, findOne } = require(`${v5Path}/handler/db`);
+const { logger } = require(`${v5Path}/utils/logger`);
+const { getFile } = require(`${v5Path}/services/filesManager`);
+
+const zlib = require('zlib');
+
+const HEADER_LENGTH_START = 16;
+const HEADER_DATA_START = 20;
+
+function getNumComponents(mapping) {
+	let maxIndex = -1;
+	for (const entry of mapping) {
+		for (const usage of entry.usage) {
+			const parts = usage.split('_');
+			const idx = parseInt(parts[1], 10);
+			if (idx > maxIndex) {
+				maxIndex = idx;
+			}
+		}
+	}
+	return maxIndex + 1;
+}
+
+async function getRepoBundleStream(teamspace, collection, bundle) {
+	const fileInfo = await FilesManager.getFileAsStream(teamspace, collection, bundle);
+	if (fileInfo.encoding === 'gzip') {
+		return fileInfo.readStream.pipe(zlib.createGunzip());
+	}
+	return fileInfo.readStream;
+}
+
+async function getRepoBundleHeader(teamspace, collection, bundle) {
+	const stream = await getRepoBundleStream(teamspace, collection, bundle);
+	return new Promise((resolve, reject) => {
+		stream.once('error', reject);
+		const chunks = [];
+		let bytesRead = 0;
+		let bytesToRead = Number.MAX_SAFE_INTEGER;
+		let headerLength = 0;
+		stream.on('data', (chunk) => {
+			chunks.push(chunk);
+			bytesRead += chunk.length;
+			if (bytesRead >= 20 && headerLength === 0) {
+				const preamble = Buffer.concat(chunks);
+				const view = new DataView(preamble.buffer);
+				headerLength = view.getInt32(HEADER_LENGTH_START, true);
+				bytesToRead = headerLength + HEADER_DATA_START;
+			}
+			if (bytesRead >= bytesToRead) {
+				const preamble = Buffer.concat(chunks);
+				const header = preamble.subarray(HEADER_DATA_START, HEADER_DATA_START + headerLength);
+				const json = header.toString();
+				stream.destroy();
+				resolve(JSON.parse(json));
+			}
+		});
+		stream.on('end', () => reject(new Error(`Got to end of bundle before processing header ${teamspace} ${collection} ${bundle}`)));
+	});
+}
+
+async function getMetadataEntryFromBundle(teamspace, collection, bundle) {
+	const header = await getRepoBundleHeader(teamspace, collection, bundle);
+	const metadata = {
+		numVertices: 0,
+		numFaces: 0,
+		numUVChannels: 0,
+		primitive: 3,
+		min: {
+			x: Number.MAX_VALUE,
+			y: Number.MAX_VALUE,
+			z: Number.MAX_VALUE,
+		},
+		max: {
+			x: -Number.MAX_VALUE,
+			y: -Number.MAX_VALUE,
+			z: -Number.MAX_VALUE,
+		},
+	};
+	for (const m of header.meshes) {
+		metadata.min.x = Math.min(metadata.min.x, m.bounds.min.x);
+		metadata.min.y = Math.min(metadata.min.y, m.bounds.min.y);
+		metadata.min.z = Math.min(metadata.min.z, m.bounds.min.z);
+		metadata.max.x = Math.max(metadata.max.x, m.bounds.max.x);
+		metadata.max.y = Math.max(metadata.max.y, m.bounds.max.y);
+		metadata.max.z = Math.max(metadata.max.z, m.bounds.max.z);
+		metadata.numVertices += m.vertexCount;
+		metadata.numFaces += m.indexCount / 3;
+		metadata.primitive = m.type; // The current generation of RepoBundles does not mix primitive types
+		if (m.vertexLayout.includes(4)) {
+			metadata.numUVChannels = 1;
+		}
+	}
+	return metadata;
+}
+
+const processRevision = async (teamspace, collection, revision) => {
+	try {
+		let requiresUpdate = false;
+		const history = await findOne(teamspace, collection.replace('stash.repobundles', 'history'), { _id: revision._id }, { timestamp: 1 });
+		// For the Date constructor, the month is a zero-based index.
+		if (history.timestamp > new Date(2026, 2, 13) && history.timestamp < new Date(2026, 2, 31)) {
+			// Check if this is a 5.22.1 import and rebuild if so to make the primitive
+			// types consistent with older imports. This check relies on the date
+			// range we know 5.22.1 was active.
+
+			// eslint-disable-next-line no-param-reassign
+			delete revision.metadata;
+		}
+		if (!revision.metadata) {
+			// This is a v1 import so is missing the metadata entirely. We can
+			// get this information from the bundle header.
+
+			// eslint-disable-next-line no-param-reassign
+			revision.metadata = await Promise.all(
+				revision.assets.map((asset) => getMetadataEntryFromBundle(teamspace, collection, asset)));
+
+			requiresUpdate = true;
+		}
+		for (let i = 0; i < revision.metadata.length; i++) {
+			if (!revision.metadata[i].numSubmeshes) {
+				// We can get both the number of submeshes and components/repomeshes
+				// from the mpc document.
+				const fileName = revision.jsonFiles[i];
+
+				// eslint-disable-next-line no-await-in-loop
+				const fileContents = await getFile(teamspace, collection.replace('repobundles', 'json_mpc'), fileName);
+				const { mapping } = JSON.parse(fileContents.toString());
+
+				// Unfortunately we cannot rely on the numberOfIds or maxGeoCount as these
+				// were broken in bouncer for a short while, so we just get them directly from
+				// the array length and usage counts.
+
+				// It is OK to assign to the function parameter here because this
+				// document will be written back to the db inside this call.
+
+				// eslint-disable-next-line no-param-reassign
+				revision.metadata[i].numSubmeshes = mapping.length;
+				// eslint-disable-next-line no-param-reassign
+				revision.metadata[i].numComponents = getNumComponents(mapping);
+
+				requiresUpdate = true;
+			}
+		}
+		if (requiresUpdate) {
+			logger.logInfo(`Updating Repobundles metadata for: ${teamspace}/${collection}/${UUIDToString(revision._id)}...`);
+			await replaceOne(
+				teamspace,
+				collection,
+				{
+					_id: revision._id,
+				},
+				revision,
+			);
+		}
+	} catch (error) {
+		logger.logInfo(`Exception processing ${teamspace}/${collection}/${UUIDToString(revision._id)}: ${error}`);
+	}
+};
+
+const processCollection = async (teamspace, collection) => {
+	const revisionAssets = await find(teamspace, collection, {});
+	await Promise.all(revisionAssets.map((revision) => processRevision(teamspace, collection, revision)));
+};
+
+const processTeamspace = async (teamspace) => {
+	const collections = await getCollectionsEndsWith(teamspace, '.stash.repobundles');
+	await Promise.all(collections.map((collection) => processCollection(teamspace, collection.name)));
+};
+
+const run = async () => {
+	const teamspaces = await getTeamspaceList();
+	await Promise.all(teamspaces.map((ts) => processTeamspace(ts)));
+};
+
+module.exports = run;
