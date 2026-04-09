404caa29188789d5e55f969fdbb86b6d0748b7fc
the-Daniel <bibshall@gmail.com>
2026-04-09 10:55:49 +0100

    ISSUE #5935 - fix failing test

--- frontend/src/v5/store/tickets/tickets.selectors.ts
+++ frontend/src/v5/store/tickets/tickets.selectors.ts
@@ -135,7 +135,7 @@ export const selectTickets = createSelector(
 			];
 			return orderBy(tickets, ticketCodeSorting, [order, order]);
 		}
-		return orderBy(tickets, (ticket) => get(ticket, property).toString().toLowerCase(), order);
+		return orderBy(tickets, (ticket) => get(ticket, property)?.toString().toLowerCase(), order);
 	},
 );
 
