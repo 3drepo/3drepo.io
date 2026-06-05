adfbddd3ec9f411853b4a4af14c37d11c92139b6
carmenfan <wind5320@hotmail.com>
2026-06-05 14:17:02 +0100

    ISSUE #6047 add `hideOtherObjects` to plan.ticket config

--- backend/src/v5/routes/teamspaces/projects/clashes.js
+++ backend/src/v5/routes/teamspaces/projects/clashes.js
@@ -208,6 +208,10 @@ const establishRoutes = () => {
 	 *                         type: string
 	 *                         description: Set the ticket status for reopened clash tickets
 	 *                         example: Open
+	 *                   hideOtherObjects:
+	 *                     type: boolean
+	 *                     description: Hide all objects outside the clash objects in generated ticket views; false is omitted
+	 *                     example: true
 	 *     responses:
 	 *       401:
 	 *         $ref: "#/components/responses/notLoggedIn"
@@ -369,6 +373,11 @@ const establishRoutes = () => {
 	 *                         type: string
 	 *                         description: Default status for reopened clash tickets
 	 *                         example: Open
+	 *                   hideOtherObjects:
+	 *                     type: boolean
+	 *                     nullable: true
+	 *                     description: Hide all objects outside the clash objects in generated ticket views; false is omitted, null removes the flag
+	 *                     example: true
 	 *     responses:
 	 *       401:
 	 *         $ref: "#/components/responses/notLoggedIn"
