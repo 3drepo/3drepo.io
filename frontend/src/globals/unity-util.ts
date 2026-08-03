472164f2ab459bcef2d4f417de72d23837a24af2
Sebastian Friston <sfriston@asite.com>
2026-08-03 16:46:55 +0100

    ISSUE #6181 change documented behaviour for request bounds

--- frontend/src/globals/unity-util.ts
+++ frontend/src/globals/unity-util.ts
@@ -1905,11 +1905,10 @@ export class UnityUtil {
 	 * @param container name of the container - this must be the exact
 	 * container holding the unique id, it cannot be a federation.
 	 * @param uniqueIds a list of uniqueids - the bounds will encompass all of
-	 * them.
-	 * @returns a promise containing the bounds in project coordinates. If no ids
-	 * are provided, the bounds for the whole container will be returned. If the
-	 * teamspace/project/container/id combination are not valid, the arrays will
-	 * be empty, the promise will be rejected.
+	 * them. The array may not be empty.
+	 * @returns a promise containing the bounds in project coordinates. If the
+	 * teamspace/project/container/id combination are not valid, the promise
+	 * will be rejected.
 	 * @example ```
 	 * UnityUtil.viewer.objectSelected = (ev) => {
 		UnityUtil.requestBounds(ev.database, "", ev.model, [ev.id]).then((bounds) => {
