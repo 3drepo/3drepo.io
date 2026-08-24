96e8d6d0b66b2991988085c8385b293c67476828
Santiago Montero <smontero@asite.com>
2026-08-24 14:55:42 +0100

    ISSUE #6118 - Fixed insonsistent pin icons sizes

--- frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/coordsProperty/coordsProperty.styles.ts
+++ frontend/src/v5/ui/routes/viewer/tickets/ticketsForm/properties/coordsProperty/coordsProperty.styles.ts
@@ -108,10 +108,10 @@ export const SelectPinButton = styled.div<{ color: string, isSelected: boolean; 
 		}
 	`};
 
-	> svg {
+	> svg, > span svg {
 		color: ${({ color, theme }) => color || theme.palette.primary.main};
-		max-height: 20px;
-		max-width: 20px;
+		height: 20px;
+		width: 20px;
 		overflow: visible;
 	}
 `;
