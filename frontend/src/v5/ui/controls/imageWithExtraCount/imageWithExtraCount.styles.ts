70698af5e62da3ea40d1a320049e834e30e9a8e7
the-Daniel <bibshall@gmail.com>
2026-09-16 14:15:26 +0100

    ISSUE #6282 - fix ticket comment images thumbnails being really tall

--- frontend/src/v5/ui/controls/imageWithExtraCount/imageWithExtraCount.styles.ts
+++ frontend/src/v5/ui/controls/imageWithExtraCount/imageWithExtraCount.styles.ts
@@ -28,6 +28,7 @@ export const OverlappingContainer = styled(OverlappingContainerBase)`
 export const Image = styled(ImageWithSkeleton)`
 	object-fit: cover;
 	aspect-ratio: 1 / 1;
+	width: 100%;
 	cursor: pointer;
 	border-radius: 5px;
 	overflow: hidden;
